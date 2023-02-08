464bd6c6f76a5790ac67265ae9bb915db8823929
anovazzi1 <otavio2204@gmail.com>
2023-02-08 20:15:01 -0300

    react flow started

--- space_flow/src/App.tsx
+++ space_flow/src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Flow from "./flow";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen">
+      <Flow></Flow>
     </div>
   );
 }
