45087c54ec533ec8987bb88e5b71ef84b643b74b
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-10 16:03:16 -0300

    Project structure changed

--- space_flow/src/index.tsx
+++ space_flow/src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import ContextWrapper from './contexts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ContextWrapper>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextWrapper>
 );
 
 // If you want to start measuring performance in your app, pass a function
