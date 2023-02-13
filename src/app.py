d7798912901cfce56d3db8e1419edeb3832b7140
Ibis Prevedello <ibiscp@gmail.com>
2023-02-12 21:04:32 -0300

    feat: add tools and improve code

--- src/app.py
+++ src/app.py
@@ -13,8 +13,9 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
     import uvicorn
 
-    app = create_app()
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
