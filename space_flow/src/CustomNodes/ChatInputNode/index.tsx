2833c3fb66008d2ad56a8ea0b814f120c0f5b743
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-16 00:22:02 -0300

    Tooltips added to match the types

--- space_flow/src/CustomNodes/ChatInputNode/index.tsx
+++ space_flow/src/CustomNodes/ChatInputNode/index.tsx
@@ -2,10 +2,12 @@ import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/outline";
 import Input from "../../components/inputComponent";
 import { snakeToNormalCase } from "../../utils";
 import { Handle, Position } from "reactflow";
+import Tooltip from "../../components/TooltipComponent";
 
 export default function ChatInputNode({ data }) {
   return (
     <div className="prompt-node relative rounded-lg solid border flex justify-center align-center py-3 px-6 bg-blue-600">
+        <Tooltip title={"Message: str"}>
       <Handle
         type="target"
         position={Position.Right}
@@ -13,6 +15,7 @@ export default function ChatInputNode({ data }) {
         isValidConnection={({sourceHandle, targetHandle}) => (targetHandle === sourceHandle || data.types[targetHandle] === sourceHandle || sourceHandle === 'str')}
         className="-mr-1 bg-transparent border-solid border-l-8 border-l-blue-600 border-y-transparent border-y-8 border-r-0 rounded-none"
       ></Handle>
+      </Tooltip>
       <div className="flex gap-3 text-lg font-medium text-white items-center">
         <ChatBubbleBottomCenterTextIcon className="h-8 w-8 mt-1" />
         Input
