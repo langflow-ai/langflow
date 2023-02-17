c752f63af551c8582c6b2759d9ba9c4c158e3f14
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-17 00:18:32 -0300

    fitView disabled

--- space_flow/src/pages/FlowPage/index.tsx
+++ space_flow/src/pages/FlowPage/index.tsx
@@ -100,7 +100,6 @@ export default function FlowPage() {
         connectionLineComponent={connection}
         onDragOver={onDragOver}
         onDrop={onDrop}
-        fitView
       >
         <Background />
         <Controls></Controls>
