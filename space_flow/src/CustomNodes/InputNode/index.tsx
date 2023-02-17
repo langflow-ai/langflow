d34e7890c3b4f8d7dd20f97ccf4737902de6fa04
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-16 22:49:51 -0300

    fixed things during meeting

--- space_flow/src/CustomNodes/InputNode/index.tsx
+++ space_flow/src/CustomNodes/InputNode/index.tsx
@@ -1,12 +1,31 @@
 import { Bars3CenterLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Input from "../../components/inputComponent";
-import { isValidConnection, nodeColors, nodeIcons, snakeToNormalCase } from "../../utils";
+import {
+  isValidConnection,
+  nodeColors,
+  nodeIcons,
+  snakeToNormalCase,
+} from "../../utils";
 import { Handle, Position } from "reactflow";
 import { useEffect } from "react";
+import Tooltip from "../../components/TooltipComponent";
 
 export default function InputNode({ data }) {
   return (
     <div className="prompt-node relative bg-white w-96 rounded-lg solid border flex flex-col justify-center">
+      <Tooltip title="Prefix: str">
+        <Handle
+          type="source"
+          position={Position.Left}
+          id={"str|Prefix|" + data.id}
+          isValidConnection={(connection) =>
+            isValidConnection(data, connection)
+          }
+          className="ml-1 bg-transparent border-solid border-l-8 border-y-transparent border-y-8 border-r-0 rounded-none"
+          style={{ borderLeftColor: nodeColors[data.type] }}
+        ></Handle>
+      </Tooltip>
+
       <div className="w-full flex items-center justify-between p-4 gap-8 bg-gray-50 border-b ">
         <div className="flex items-center gap-4 text-lg">
           <Bars3CenterLeftIcon
@@ -34,9 +53,9 @@ export default function InputNode({ data }) {
         type="target"
         position={Position.Right}
         id={data.name}
-        isValidConnection={(connection) => isValidConnection(data,connection)}
+        isValidConnection={(connection) => isValidConnection(data, connection)}
         className="-mr-1 bg-transparent border-solid border-l-8 border-y-transparent border-y-8 border-r-0 rounded-none"
-        style={{borderLeftColor: nodeColors[data.type]}}
+        style={{ borderLeftColor: nodeColors[data.type] }}
       ></Handle>
     </div>
   );
