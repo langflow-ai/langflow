94019692c1fbd6c3ec791c366d8a2f860c20fd5a
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-17 00:16:52 -0300

    Inputs and outputs lines implemented at the generic node

---
+++ space_flow/src/CustomNodes/GenericNode/components/parameterComponent/index.tsx
@@ -0,0 +1,54 @@
+import { Handle, Position, useUpdateNodeInternals } from "reactflow";
+import Tooltip from "../../../../components/TooltipComponent";
+import {
+  isValidConnection,
+  nodeColors,
+  snakeToNormalCase,
+} from "../../../../utils";
+import { useEffect, useRef, useState } from "react";
+
+export default function ParameterComponent({
+  left,
+  id,
+  data,
+  tooltipTitle,
+  title,
+  color,
+}) {
+  const ref = useRef(null);
+  const updateNodeInternals = useUpdateNodeInternals();
+  const [position, setPosition] = useState(0);
+  useEffect(() => {
+    if (ref.current && ref.current.offsetTop && ref.current.clientHeight) {
+      setPosition(ref.current.offsetTop + (ref.current.clientHeight / 2));
+      updateNodeInternals(data.id);
+    }
+  }, [data.id, ref, updateNodeInternals]);
+
+  useEffect(() => {
+    updateNodeInternals(data.id);
+  }, [data.id, position, updateNodeInternals])
+
+  return (
+    <div ref={ref} className="w-full bg-gray-50 mt-1 px-5 py-2">
+        <>
+          <div className="text-sm truncate">{title}</div>
+          <Tooltip title={tooltipTitle}>
+            <Handle
+              type={left ? "source" : "target"}
+              position={left ? Position.Left : Position.Right}
+              id={id}
+              isValidConnection={(connection) =>
+                isValidConnection(data, connection)
+              }
+              className={(left ? "-ml-0.5 " : "-mr-0.5 ") +"w-3 h-3 rounded-full border-2 bg-white"}
+              style={{
+                borderColor: color,
+                top: position,
+              }}
+            ></Handle>
+          </Tooltip>
+        </>
+    </div>
+  );
+}
