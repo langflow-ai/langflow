055dfaed82c9dbfc44b0c7c4f3bfa991ca311790
anovazzi1 <otavio2204@gmail.com>
2023-02-19 22:36:02 -0300

    commented and removed some dead code and comments

--- space_flow/src/index.tsx
+++ space_flow/src/index.tsx
@@ -15,8 +15,4 @@ root.render(
       </BrowserRouter>
     </ContextWrapper>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
