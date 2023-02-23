cc48424f7a963e513c8af2c12aae2b60d5a2aec1
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-23 18:16:57 -0300

    fix messages v6

--- space_flow/src/components/chatComponent/index.tsx
+++ space_flow/src/components/chatComponent/index.tsx
@@ -109,6 +109,7 @@ export default function Chat({ reactFlowInstance }) {
                             setChatValue("");
                             addChatHistory(message, true);
                             sendAll({...reactFlowInstance.toObject(),message}).then((r) => {console.log(r.data.result); addChatHistory(r.data.result, false);});
+                            addChatHistory('teste', false);
                           } else {
                             setErrorData({title: 'Error sending message', list:['Chat nodes are missing.']})
                           }
