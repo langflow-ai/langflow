78569d61235c128c5183b83e866ea1741c255a2a
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-02-22 23:05:53 -0300

    Correction for merge

--- space_flow/src/pages/FlowPage/flowManager/index.tsx
+++ space_flow/src/pages/FlowPage/flowManager/index.tsx
@@ -4,39 +4,47 @@ import { ReactFlowProvider } from "reactflow";
 import FlowPage from "..";
 import { TabsContext } from "../../../contexts/tabsContext";
 import TabComponent from "./tabComponent";
-import { PlusIcon } from '@heroicons/react/24/outline';
+import { PlusIcon } from "@heroicons/react/24/outline";
 var _ = require("lodash");
 
 export function TabsManager() {
-	const { flows, addFlow, tabIndex, setTabIndex } = useContext(TabsContext);
-  const [inputMode,setInputMode] = useState(false)
-	useEffect(() => {
-		if (flows.length === 0) {
-      const id = _.uniqueId()
-			addFlow({ name: "flow "+id, data: null, id });
-		}
-	}, []);
+  const { flows, addFlow, tabIndex, setTabIndex } = useContext(TabsContext);
+  useEffect(() => {
+    if (flows.length === 0) {
+      const id = _.uniqueId();
+      addFlow({ name: "flow " + id, data: null, id });
+    }
+  }, []);
 
-	return (
-		<div className="h-full w-full flex flex-col">
-			<div className="w-full flex pr-2 flex-row text-center items-center">
-				{flows.map((flow, index) => {
-          console.log(tabIndex)
-					return (
-						<TabComponent onClick={() => setTabIndex(index)} selected={index === tabIndex} key={index} id={flow.id}>
-							<div onClick={()=>setInputMode(true)}>{flow.name}</div>
-						</TabComponent>
-					);
-				})}
-        <div onClick={()=>{
-          const id = _.uniqueId()
-          addFlow({ name: "flow"+id, data: null, id})}} className="cursor-pointer"><PlusIcon color="black" width={24}></PlusIcon></div>
-			</div>
-			<div className="w-full h-full">
-				<ReactFlowProvider>
-					<FlowPage flow={flows[tabIndex]}></FlowPage>
-				</ReactFlowProvider>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-full w-full flex flex-col">
+      <div className="w-full flex pr-2 flex-row text-center items-center bg-gray-100">
+        {flows.map((flow, index) => {
+          console.log(tabIndex);
+          return (
+            <TabComponent
+              onClick={() => setTabIndex(index)}
+              selected={index === tabIndex}
+              key={index}
+              flow={flow}
+            />
+          );
+        })}
+        <TabComponent
+          onClick={() => {
+            const id = _.uniqueId();
+            addFlow({ name: "flow" + id, data: null, id });
+          }}
+          selected={false}
+          key={0}
+          flow={null}
+        />
+      </div>
+      <div className="w-full h-full">
+        <ReactFlowProvider>
+          <FlowPage flow={flows[tabIndex]}></FlowPage>
+        </ReactFlowProvider>
+      </div>
+    </div>
+  );
 }
