9656dc1f08c37acbd267437b00842115bcbd5365
anovazzi1 <otavio2204@gmail.com>
2023-02-24 17:28:14 -0300

    chat display updated

--- space_flow/src/components/chatComponent/index.tsx
+++ space_flow/src/components/chatComponent/index.tsx
@@ -102,14 +102,14 @@ export default function Chat({flow, reactFlowInstance }) {
               {chatHistory.map((c, i) => (
                 <div key={i}>
                   {c.isSend ? (
-                    <div className="w-full text-start">
-                      <div className="text-start inline-block bg-gray-200 rounded-xl p-3 overflow-hidden w-fit max-w-[280px] px-5 text-sm font-normal rounded-tl-none">
+                    <div className="w-full text-end">
+                      <div className="text-start inline-block bg-gray-200 rounded-xl p-3 overflow-hidden w-fit max-w-[280px] px-5 text-sm font-normal rounded-tr-none">
                         {c.message}
                       </div>
                     </div>
                   ) : (
-                    <div className="w-full text-end">
-                      <div style={{backgroundColor: nodeColors['chat']}} className="text-start inline-block rounded-xl p-3 overflow-hidden w-fit max-w-[280px] px-5 text-sm text-white font-normal rounded-tr-none">
+                    <div className="w-full text-start">
+                      <div style={{backgroundColor: nodeColors['chat']}} className="text-start inline-block rounded-xl p-3 overflow-hidden w-fit max-w-[280px] px-5 text-sm text-white font-normal rounded-tl-none">
                         {c.message}
                       </div>
                     </div>
