64595516f6fbcfd226a73f12cbe810ccd8153ad2
anovazzi1 <otavio2204@gmail.com>
2023-02-27 14:25:39 -0300

    refactor typesContext

--- space_flow/src/contexts/typesContext.tsx
+++ space_flow/src/contexts/typesContext.tsx
@@ -1,37 +1,44 @@
-import { ReactEventHandler, createContext, useState } from "react";
+import { createContext, useState } from "react";
 import { ReactFlowInstance } from "reactflow";
 
-type typesContextType=
-{
-    reactFlowInstance: ReactFlowInstance;
-    setReactFlowInstance: any;
-    deleteNode:(idx:number)=>void;
-    types: {};
-    setTypes:(newState:{})=>void;
+//context to share types adn functions from nodes to flow
+type typesContextType = {
+	reactFlowInstance: ReactFlowInstance;
+	setReactFlowInstance: any;
+	deleteNode: (idx: number) => void;
+	types: {};
+	setTypes: (newState: {}) => void;
+};
 
-}
-
-const initialValue= {
-    reactFlowInstance: null,
-    setReactFlowInstance: ()=>{},
-    deleteNode: ()=>{},
-    types: {},
-    setTypes:()=>{},
-}
+const initialValue = {
+	reactFlowInstance: null,
+	setReactFlowInstance: () => {},
+	deleteNode: () => {},
+	types: {},
+	setTypes: () => {},
+};
 
 export const typesContext = createContext<typesContextType>(initialValue);
 
-export function TypesProvider({children}){
-    const [types, setTypes] = useState({});
-    const [reactFlowInstance, setReactFlowInstance] = useState(null);
-    function deleteNode(idx){
-        reactFlowInstance.setNodes(
-            reactFlowInstance.getNodes().filter((n) => n.id !== idx)
-          );
-    }
-    return (
-        <typesContext.Provider value={{ types, setTypes, reactFlowInstance, setReactFlowInstance, deleteNode}}>
-            {children}
-        </typesContext.Provider>
-    )
+export function TypesProvider({ children }) {
+	const [types, setTypes] = useState({});
+	const [reactFlowInstance, setReactFlowInstance] = useState(null);
+	function deleteNode(idx) {
+		reactFlowInstance.setNodes(
+			reactFlowInstance.getNodes().filter((n) => n.id !== idx)
+		);
+	}
+	return (
+		<typesContext.Provider
+			value={{
+				types,
+				setTypes,
+				reactFlowInstance,
+				setReactFlowInstance,
+				deleteNode,
+			}}
+		>
+			{children}
+		</typesContext.Provider>
+	);
 }
