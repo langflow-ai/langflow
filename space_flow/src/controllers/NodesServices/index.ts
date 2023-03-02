0b3b62360da17c4e6c8d20dcd69b6f6c134f3112
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-03-02 09:30:58 -0300

    Changed port to previous

--- space_flow/src/controllers/NodesServices/index.ts
+++ space_flow/src/controllers/NodesServices/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export async function getAll() {
-    return await axios.get("http://localhost:8000/all");
+    return await axios.get("http://localhost:5003/");
 }
 
 export async function sendAll(data) {
     console.log(data);
-    return await axios.post("http://localhost:8000/predict", data);
+    return await axios.post("http://localhost:5003/predict", data);
 }
