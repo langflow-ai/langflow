6ad55a78162f50f4ddde16c484e6d33c655633c5
Gabriel Luiz Freitas Almeida <gabrielf.almeida90@gmail.com>
2023-03-05 21:55:44 +0000

    feat: add port option to cli

--- langflow/cli.py
+++ langflow/cli.py
@@ -1,24 +1,23 @@
+from langflow.backend.app import create_app
+
 import typer
+import uvicorn
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-from langflow.backend.app import create_app
-
-# get the directory of the current file
-path = Path(__file__).parent
-static_files_dir = path / "frontend/build"
 app = create_app()
-app.mount(
-    "/",
-    StaticFiles(directory=static_files_dir, html=True),
-    name="static",
-)
 
 
 def serve(port: int = 5003):
-    import uvicorn
-
-    uvicorn.run(app, host="localhost", port=port)
+    # get the directory of the current file
+    path = Path(__file__).parent
+    static_files_dir = path / "frontend/build"
+    app.mount(
+        "/",
+        StaticFiles(directory=static_files_dir, html=True),
+        name="static",
+    )
+    uvicorn.run(app, port=port)
 
 
 def main():
