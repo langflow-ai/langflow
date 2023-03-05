ca7c57f90559671cc9b1493beeb1e0e093ee8376
anovazzi1 <otavio2204@gmail.com>
2023-03-05 14:45:33 -0300

    changed api link

--- langflow/frontend/src/controllers/NodesServices/index.ts
+++ langflow/frontend/src/controllers/NodesServices/index.ts
@@ -4,7 +4,7 @@ import axios, { AxiosResponse } from "axios";
 const backendUrl = process.env.BACKEND || "http://localhost:5003";
 
 export async function getAll():Promise<AxiosResponse<APIObjectType>> {
-    return await axios.get(`${backendUrl}/`);
+    return await axios.get(`${backendUrl}/all`);
 }
 
 export async function sendAll(data:sendAllProps) {
