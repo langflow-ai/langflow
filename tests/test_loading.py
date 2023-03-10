b4aac65e5127326924ac580d2447abeba8309142
ogabrielluiz <gabrielf.almeida90@gmail.com>
2023-03-10 07:20:40 -0300

    feat: adding test and make commands for testing

---
+++ tests/test_loading.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from langflow import load_flow_from_json
+
+
+def test_load_flow_from_json():
+    """Test loading a flow from a json file"""
+    path = Path(__file__).parent.absolute()
+    path = f"{path}/data/example_flow.json"
+    loaded = load_flow_from_json(path)
+    assert loaded is not None
