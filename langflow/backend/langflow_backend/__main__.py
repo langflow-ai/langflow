0117d7aa21c6d3e30087e80c7975adc7be100714
Gabriel Almeida <gabrielf.almeida90@gmail.com>
2023-03-14 10:27:47 -0300

    fix: set workers to 0 if env variable is not set

--- langflow/backend/langflow_backend/__main__.py
+++ langflow/backend/langflow_backend/__main__.py
@@ -1,4 +1,7 @@
 import multiprocessing
+import os
+import platform
+
 from langflow_backend.main import create_app
 
 import typer
@@ -6,6 +9,18 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 from langflow_backend.server import LangflowApplication
+
+
+def get_number_of_workers(workers=None):
+    if workers is None:
+        workers = (
+            int(os.environ.get("OBJC_DISABLE_INITIALIZE_FORK_SAFETY", "0") == "YES")
+            if platform.system() == "Darwin"
+            else 1
+        )
+    elif workers == -1:
+        workers = (multiprocessing.cpu_count() * 2) + 1
+    return workers
 
 
 def serve(
@@ -21,17 +36,13 @@ def serve(
         StaticFiles(directory=static_files_dir, html=True),
         name="static",
     )
-    if not workers:
-        workers = 1
-    elif workers == -1:
-        workers = (multiprocessing.cpu_count() * 2) + 1
 
     if not timeout:
         timeout = 60
 
     options = {
         "bind": "127.0.0.1:5003",
-        "workers": workers,
+        "workers": get_number_of_workers(workers),
         "worker_class": "uvicorn.workers.UvicornWorker",
         "timeout": timeout,
     }
