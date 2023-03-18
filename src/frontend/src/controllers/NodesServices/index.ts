0a4003fb742626bfe3a8693b18e45411be6ab6a7
anovazzi1 <otavio2204@gmail.com>
2023-03-18 19:47:27 -0300

    trying to fix to hugging face spaces

--- src/frontend/src/controllers/NodesServices/index.ts
+++ src/frontend/src/controllers/NodesServices/index.ts
@@ -4,7 +4,7 @@ import axios, { AxiosResponse } from "axios";
 const backendUrl = window.sessionStorage.getItem('port') || "http://localhost:7860";
 
 export async function getAll():Promise<AxiosResponse<APIObjectType>> {
-    return await axios.get(`${backendUrl}/all`);
+    return await axios.get(`/all`);
 }
 
 export async function sendAll(data:sendAllProps) {
