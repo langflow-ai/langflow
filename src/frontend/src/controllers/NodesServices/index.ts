108cd9a43a13533680b928e333e019de6cfd0523
anovazzi1 <otavio2204@gmail.com>
2023-03-20 11:56:40 -0300

    change place of the import/export buttons

--- src/frontend/src/controllers/NodesServices/index.ts
+++ src/frontend/src/controllers/NodesServices/index.ts
@@ -4,10 +4,10 @@ import axios, { AxiosResponse } from "axios";
 const backendUrl = process.env.BACKEND || "http://localhost:5003";
 
 export async function getAll():Promise<AxiosResponse<APIObjectType>> {
-    return await axios.get(`${backendUrl}/all`);
+    return await axios.get(`/all`);
 }
 
 export async function sendAll(data:sendAllProps) {
     console.log(data);
-    return await axios.post(`${backendUrl}/predict`, data);
+    return await axios.post(`/predict`, data);
 }
