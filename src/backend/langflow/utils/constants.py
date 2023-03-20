78037d0ddb9412e496b60dcafd438c7fb15a7844
Gabriel Almeida <gabriel@logspace.ai>
2023-03-20 20:54:05 -0300

    feat: added constants file to support model types and others

---
+++ src/backend/langflow/utils/constants.py
@@ -0,0 +1,8 @@
+OPENAI_MODELS = [
+    "text-davinci-003",
+    "text-davinci-002",
+    "text-curie-001",
+    "text-babbage-001",
+    "text-ada-001",
+]
+CHAT_OPENAI_MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k"]
