88065a13407ed4d654781e139e53de5911bec7b7
Gabriel Almeida <gabriel@logspace.ai>
2023-03-20 09:49:54 -0300

    fix: removing unused variable

--- src/frontend/src/controllers/NodesServices/index.ts
+++ src/frontend/src/controllers/NodesServices/index.ts
@@ -1,8 +1,6 @@
 import { APIObjectType, sendAllProps } from '../../types/api/index';
 import axios, { AxiosResponse } from "axios";
 
-const backendUrl = window.sessionStorage.getItem('port') || "http://localhost:7860";
-
 export async function getAll():Promise<AxiosResponse<APIObjectType>> {
     return await axios.get(`/all`);
 }
