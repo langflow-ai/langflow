0f58e12ed7ac3205e177f87ab56de7855836cda4
seshubonam <seshu.bonam@gmail.com>
2023-03-26 02:29:23 +0530

    Update set_proxy.sh

--- src/frontend/set_proxy.sh
+++ src/frontend/set_proxy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Edit package.json to set proxy
-backend_url=$BACKEND_URL
+backend_url=http://localhost:8000
 echo "Setting proxy to $backend_url"
 # Load package.json file and edit proxy
 packagejson=$(cat package.json)
