0cabdbd2590948dc8870b6181e2dfee8239a0996
Gabriel Almeida <gabriel@logspace.ai>
2023-03-27 21:46:44 -0300

    feat: agents as tools working

--- src/backend/langflow/api/endpoints.py
+++ src/backend/langflow/api/endpoints.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 from fastapi import APIRouter, HTTPException
 
-from langflow.interface.run import process_data_graph
+from langflow.interface.run import process_graph
 from langflow.interface.types import build_langchain_types_dict
 
 # build router
@@ -17,6 +17,6 @@ def get_all():
 @router.post("/predict")
 def get_load(data: Dict[str, Any]):
     try:
-        return process_data_graph(data)
+        return process_graph(data)
     except Exception as e:
         return HTTPException(status_code=500, detail=str(e))
