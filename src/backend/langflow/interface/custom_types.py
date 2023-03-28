6e27468daf7ddad1dafd10997d4fb0b717dee393
Gabriel Almeida <gabriel@logspace.ai>
2023-03-28 17:16:22 -0300

    feat: added validate endpoint

--- src/backend/langflow/interface/custom_types.py
+++ src/backend/langflow/interface/custom_types.py
@@ -1,16 +1,24 @@
+from typing import Callable
 from langflow.utils import util
 from pydantic import BaseModel, validator
-from langchain.agents import tool
+
 
 
 class Function(BaseModel):
     code: str
 
+    # Eval code and store the function
+    def __init__(self, **data):
+        super().__init__(**data)
+
     # Validate the function
     @validator("code")
     def validate_func(cls, v):
         # Validate with LangChain's tool decorator
-        tool(v)
+        func = util.eval_function(v)
+        if not isinstance(func, Callable):
+            raise ValueError("Function must be a callable")
+
         return v
 
     def get_function(self):
