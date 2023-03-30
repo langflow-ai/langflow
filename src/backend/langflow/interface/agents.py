f791d9c9389a50c227c771bee5b9703bb64cf177
Gabriel Almeida <gabriel@logspace.ai>
2023-03-30 10:56:32 -0300

    refac: Factory implementation of LangChainTypes

---
+++ src/backend/langflow/interface/agents.py
@@ -0,0 +1,28 @@
+from langchain.agents import loading
+from langflow.interface.base import LangChainTypeCreator
+from langflow.utils.util import build_template_from_class
+from langflow.settings import settings
+from typing import Dict, List
+
+
+class AgentCreator(LangChainTypeCreator):
+    type_name: str = "agents"
+
+    @property
+    def type_to_loader_dict(self) -> Dict:
+        return loading.AGENT_TO_CLASS
+
+    def get_signature(self, name: str) -> Dict | None:
+        try:
+            return build_template_from_class(
+                name, self.type_to_loader_dict, add_function=True
+            )
+        except ValueError as exc:
+            raise ValueError("Agent not found") from exc
+
+    def to_list(self) -> List[str]:
+        return [
+            agent.__name__
+            for agent in self.type_to_loader_dict.values()
+            if agent.__name__ in settings.agents or settings.dev
+        ]
---
+++ src/backend/langflow/interface/chains.py
@@ -0,0 +1,35 @@
+from typing import Dict, List
+from langflow.interface.base import LangChainTypeCreator
+from langflow.interface.signature import get_chain_signature
+from langflow.template.template import Field, FrontendNode, Template
+from langflow.utils.util import build_template_from_function
+from langflow.settings import settings
+from langchain.chains import loading as chains_loading
+
+# Assuming necessary imports for Field, Template, and FrontendNode classes
+
+
+class ChainCreator(LangChainTypeCreator):
+    type_name: str = "chains"
+
+    @property
+    def type_to_loader_dict(self) -> Dict:
+        return chains_loading.type_to_loader_dict
+
+    def get_signature(self, name: str) -> Dict | None:
+        try:
+            return build_template_from_function(
+                name, self.type_to_loader_dict, add_function=True
+            )
+        except ValueError as exc:
+            raise ValueError("Chain not found") from exc
+
+    def to_list(self) -> List[str]:
+        return [
+            chain.__annotations__["return"].__name__
+            for chain in self.type_to_loader_dict.values()
+            if (
+                chain.__annotations__["return"].__name__ in settings.chains
+                or settings.dev
+            )
+        ]
