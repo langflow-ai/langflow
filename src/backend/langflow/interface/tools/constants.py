5b277913cf0f1affc62c7ef06b2b0d6e46fea92a
Gabriel Almeida <gabriel@logspace.ai>
2023-03-31 14:00:47 -0300

    fix: improvement to tools dict name

--- src/backend/langflow/interface/tools/constants.py
+++ src/backend/langflow/interface/tools/constants.py
@@ -4,8 +4,8 @@ from langflow.interface.custom.types import PythonFunction
 from langchain.tools.json.tool import JsonSpec
 
 
-OTHER_TOOLS = {"JsonSpec": JsonSpec}
+FILE_TOOLS = {"JsonSpec": JsonSpec}
 CUSTOM_TOOLS = {"Tool": Tool, "PythonFunction": PythonFunction}
 ALL_TOOLS_NAMES = set(
-    get_all_tool_names() + list(CUSTOM_TOOLS.keys()) + list(OTHER_TOOLS.keys())
+    get_all_tool_names() + list(CUSTOM_TOOLS.keys()) + list(FILE_TOOLS.keys())
 )
