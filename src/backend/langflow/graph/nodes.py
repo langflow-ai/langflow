ef4fe40ce4ab7f2a31d692d386aa5f35e20f69b6
Gabriel Almeida <gabriel@logspace.ai>
2023-03-31 14:07:23 -0300

    feat: toolkit node implementation

--- src/backend/langflow/graph/nodes.py
+++ src/backend/langflow/graph/nodes.py
@@ -1,12 +1,14 @@
+import json
 from copy import deepcopy
 from typing import Any, Dict, List, Optional, Union
 
 from langflow.graph.base import Node
+from langflow.interface.toolkits.base import toolkits_creator
 
 
 class AgentNode(Node):
     def __init__(self, data: Dict):
-        super().__init__(data)
+        super().__init__(data, base_type="agents")
         self.tools: List[ToolNode] = []
         self.chains: List[ChainNode] = []
 
@@ -35,7 +37,7 @@ class AgentNode(Node):
 
 class ToolNode(Node):
     def __init__(self, data: Dict):
-        super().__init__(data)
+        super().__init__(data, base_type="tools")
 
     def build(self, force: bool = False) -> Any:
         if not self._built or force:
@@ -45,7 +47,7 @@ class ToolNode(Node):
 
 class PromptNode(Node):
     def __init__(self, data: Dict):
-        super().__init__(data)
+        super().__init__(data, base_type="prompts")
 
     def build(
         self,
@@ -68,7 +70,7 @@ class PromptNode(Node):
 
 class ChainNode(Node):
     def __init__(self, data: Dict):
-        super().__init__(data)
+        super().__init__(data, base_type="chains")
 
     def build(
         self,
@@ -88,9 +90,67 @@ class ChainNode(Node):
 
 class ToolkitNode(Node):
     def __init__(self, data: Dict):
+        super().__init__(data, base_type="toolkits")
+
+    def build(self, force: bool = False) -> Any:
+        if not self._built or force:
+            if toolkits_creator.has_create_function(self.node_type):
+                self.find_llm()
+            self._build()
+            # Now that the toolkit is built, we need to find the llm
+            # and add it to the self.params
+
+            # go through the edges and find the llm
+
+        return deepcopy(self._built_object)
+
+    def find_llm(self, node=None, edges_visited=[]) -> None:
+        if node is None:
+            node = self
+        # Move recursively through the edges
+        # the targets of this node edges are this node
+        # If we find an LLMNode, we add it to the params
+        if len(node.edges) == 1:
+            return
+        for edge in node.edges:
+            source = edge.source
+            if source in edges_visited:
+                continue
+            edges_visited.append(source)
+            if isinstance(source, LLMNode):
+                self.params["llm"] = source.build()
+                break
+            else:
+                self.find_llm(source, edges_visited)
+
+
+class LLMNode(Node):
+    def __init__(self, data: Dict):
+        super().__init__(data, base_type="llms")
+
+    def build(self, force: bool = False) -> Any:
+        if not self._built or force:
+            self._build()
+        return deepcopy(self._built_object)
+
+
+class FileToolNode(ToolNode):
+    def __init__(self, data: Dict):
         super().__init__(data)
 
     def build(self, force: bool = False) -> Any:
         if not self._built or force:
+            self._build()
+        return deepcopy(self._built_object)
+
+
+class WrapperNode(Node):
+    def __init__(self, data: Dict):
+        super().__init__(data, base_type="wrappers")
+
+    def build(self, force: bool = False) -> Any:
+        if not self._built or force:
+            if "headers" in self.params:
+                self.params["headers"] = eval(self.params["headers"])
             self._build()
         return deepcopy(self._built_object)
