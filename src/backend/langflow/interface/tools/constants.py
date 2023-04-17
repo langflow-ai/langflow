571f407ef3eadfaa695f896c1cdc812a3f49ae47
Ibis Prevedello <ibiscp@gmail.com>
2023-04-17 15:59:15 -0300

    refac: refactor tools and add QuerySQLDataBaseTool

--- src/backend/langflow/interface/tools/constants.py
+++ src/backend/langflow/interface/tools/constants.py
@@ -6,11 +6,13 @@ from langchain.agents.load_tools import (
     _LLM_TOOLS,
 )
 from langchain.tools.json.tool import JsonSpec
+from langchain.tools.sql_database.tool import QuerySQLDataBaseTool
 
 from langflow.interface.tools.custom import PythonFunction
 
 FILE_TOOLS = {"JsonSpec": JsonSpec}
 CUSTOM_TOOLS = {"Tool": Tool, "PythonFunction": PythonFunction}
+OTHER_TOOLS = {"QuerySQLDataBaseTool": QuerySQLDataBaseTool}
 ALL_TOOLS_NAMES = {
     **_BASE_TOOLS,
     **_LLM_TOOLS,  # type: ignore
@@ -18,4 +20,5 @@ ALL_TOOLS_NAMES = {
     **{k: v[0] for k, v in _EXTRA_OPTIONAL_TOOLS.items()},
     **CUSTOM_TOOLS,
     **FILE_TOOLS,  # type: ignore
+    **OTHER_TOOLS,
 }
