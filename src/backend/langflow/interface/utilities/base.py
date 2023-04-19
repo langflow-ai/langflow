183d866d906244887c31b8e2e75c993fa1403300
Ibis Prevedello <ibiscp@gmail.com>
2023-04-19 10:31:13 -0300

    feat: add SQLDatabaseChain
    Close #173

--- src/backend/langflow/interface/utilities/base.py
+++ src/backend/langflow/interface/utilities/base.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Optional
 
+from langflow.custom.customs import get_custom_nodes
 from langflow.interface.base import LangChainTypeCreator
 from langflow.interface.custom_lists import utility_type_to_cls_dict
 from langflow.settings import settings
@@ -17,6 +18,8 @@ class UtilityCreator(LangChainTypeCreator):
     def get_signature(self, name: str) -> Optional[Dict]:
         """Get the signature of a utility."""
         try:
+            if name in get_custom_nodes(self.type_name).keys():
+                return get_custom_nodes(self.type_name)[name]
             return build_template_from_class(name, utility_type_to_cls_dict)
         except ValueError as exc:
             raise ValueError(f"Utility {name} not found") from exc
