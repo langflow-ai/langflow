ce7e9d02543ed5434bb7a4de58cd83c88225a283
anovazzi1 <otavio2204@gmail.com>
2023-04-24 23:31:42 -0300

    shortcut on ctrl shift k

--- src/frontend/src/components/chatComponent/index.tsx
+++ src/frontend/src/components/chatComponent/index.tsx
@@ -1,31 +1,31 @@
-import { Transition } from "@headlessui/react";
 import {
-	Bars3CenterLeftIcon,
-	LockClosedIcon,
-	PaperAirplaneIcon,
-	XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-	MouseEventHandler,
-	useContext,
 	useEffect,
 	useRef,
 	useState,
 } from "react";
-import { sendAll } from "../../controllers/API";
-import { alertContext } from "../../contexts/alertContext";
-import { classNames, nodeColors, snakeToNormalCase } from "../../utils";
-import { TabsContext } from "../../contexts/tabsContext";
+
 import { ChatType } from "../../types/chat";
-import ChatMessage from "./chatMessage";
-import { NodeType } from "../../types/flow";
 import ChatTrigger from "./chatTrigger";
 import ChatModal from "../../modals/chatModal";
 
 const _ = require("lodash");
 
 export default function Chat({ flow }: ChatType) {
+
 	const [open, setOpen] = useState(false);
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			console.log(event)
+		  if (event.key === "K" && event.shiftKey && (event.metaKey||event.ctrlKey)) {
+			console.log("dfdsfds")
+			setOpen(oldState=>!oldState);
+		  }
+		};
+		document.addEventListener("keydown", handleKeyDown);
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		  };
+		}, []);
 	return (
 		<>
 			<ChatModal flow={flow} open={open} setOpen={setOpen} />
