e2fc12223e08b3c1984910aa3a40f7576be5a451
anovazzi1 <otavio2204@gmail.com>
2023-04-25 17:34:14 -0300

    removed chat history from flow save

--- src/frontend/src/components/chatComponent/index.tsx
+++ src/frontend/src/components/chatComponent/index.tsx
@@ -4,14 +4,13 @@ import {
 	useState,
 } from "react";
 
-import { ChatType } from "../../types/chat";
+import { ChatMessageType, ChatType } from "../../types/chat";
 import ChatTrigger from "./chatTrigger";
 import ChatModal from "../../modals/chatModal";
 
 const _ = require("lodash");
 
 export default function Chat({ flow }: ChatType) {
-
 	const [open, setOpen] = useState(false);
 	useEffect(() => {
 		const handleKeyDown = (event: KeyboardEvent) => {
