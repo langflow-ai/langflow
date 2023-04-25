2d6854165024a6bdffa98e05e78b792a6a88db17
Gabriel Almeida <gabriel@logspace.ai>
2023-04-25 17:30:53 -0300

    refactor(api): remove sender field from ChatMessage and ChatResponse schemas fix(api): fix ChatManager.get_history method to exclude start and stream messages feat(api): add is_bot field to ChatMessage, ChatResponse, and FileResponse schemas

--- src/backend/langflow/api/chat.py
+++ src/backend/langflow/api/chat.py
@@ -8,6 +8,5 @@ chat_manager = ChatManager()
 
 @router.websocket("/chat/{client_id}")
 async def websocket_endpoint(client_id: str, websocket: WebSocket):
+    """Websocket endpoint for chat."""
     await chat_manager.handle_websocket(client_id, websocket)
-
-
