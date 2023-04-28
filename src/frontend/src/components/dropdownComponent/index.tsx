b489513cb56906f7763f8fd21e5aa45de4bca230
ngmisl <98217124+ngmisl@users.noreply.github.com>
2023-04-28 16:38:22 +0200

    Update index.tsx
    should fix the non-existing darkmode css for the dropdown. 
    
    Note: I'm unable to test the code locally, someone please test

--- src/frontend/src/components/dropdownComponent/index.tsx
+++ src/frontend/src/components/dropdownComponent/index.tsx
@@ -15,7 +15,7 @@ export default function Dropdown({value, options, onSelect}:DropDownComponentTyp
         {({ open }) => (
           <>
             <div className="relative mt-1">
-              <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+              <Listbox.Button className="relative w-full cursor-default dark:bg-gray-900 dark:border-gray-600 rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
                 <span className="block w-max truncate">{internalValue}</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                   <ChevronUpDownIcon
