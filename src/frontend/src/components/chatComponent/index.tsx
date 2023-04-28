e42ab20967884a860f4200c3fc0407e814f57d53
Gabriel Almeida <gabriel@logspace.ai>
2023-04-28 14:46:05 -0300

    prettier

--- src/frontend/src/components/chatComponent/index.tsx
+++ src/frontend/src/components/chatComponent/index.tsx
@@ -1,8 +1,4 @@
-import {
-	useEffect,
-	useRef,
-	useState,
-} from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { ChatMessageType, ChatType } from "../../types/chat";
 import ChatTrigger from "./chatTrigger";
@@ -11,23 +7,26 @@ import ChatModal from "../../modals/chatModal";
 const _ = require("lodash");
 
 export default function Chat({ flow }: ChatType) {
-	const [open, setOpen] = useState(false);
-	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			event.preventDefault()
-		  if ((event.key === "K"||event.key==="k") && (event.metaKey||event.ctrlKey)) {
-			setOpen(oldState=>!oldState);
-		  }
-		};
-		document.addEventListener("keydown", handleKeyDown);
-		return () => {
-			document.removeEventListener("keydown", handleKeyDown);
-		  };
-		}, []);
-	return (
-		<>
-			<ChatModal key={flow.id} flow={flow} open={open} setOpen={setOpen} />
-			<ChatTrigger open={open} setOpen={setOpen} flow={flow} />
-		</>
-	);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // event.preventDefault()
+      if (
+        (event.key === "K" || event.key === "k") &&
+        (event.metaKey || event.ctrlKey)
+      ) {
+        setOpen((oldState) => !oldState);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  return (
+    <>
+      <ChatModal key={flow.id} flow={flow} open={open} setOpen={setOpen} />
+      <ChatTrigger open={open} setOpen={setOpen} flow={flow} />
+    </>
+  );
 }
--- src/frontend/src/types/tabs/index.ts
+++ src/frontend/src/types/tabs/index.ts
@@ -1,15 +1,15 @@
 import { FlowType } from "../flow";
 
 export type TabsContextType = {
-	save:()=>void;
-	tabIndex: number;
-	setTabIndex: (index: number) => void;
-	flows: Array<FlowType>;
-	removeFlow: (id: string) => void;
-	addFlow: (flowData?: FlowType) => void;
-	updateFlow: (newFlow: FlowType) => void;
-	incrementNodeId: () => number;
-	downloadFlow: (flow:FlowType) => void;
-	uploadFlow: () => void;
-	hardReset:()=>void;
+  save: () => void;
+  tabIndex: number;
+  setTabIndex: (index: number) => void;
+  flows: Array<FlowType>;
+  removeFlow: (id: string) => void;
+  addFlow: (flowData?: FlowType) => void;
+  updateFlow: (newFlow: FlowType) => void;
+  incrementNodeId: () => number;
+  downloadFlow: (flow: FlowType) => void;
+  uploadFlow: () => void;
+  hardReset: () => void;
 };
