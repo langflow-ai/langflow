2bc1fdd32efe2d9dbbfa5114555186d7d79905e6
Gabriel Almeida <gabriel@logspace.ai>
2023-04-28 14:46:05 -0300

    prettier

--- src/frontend/src/svg.d.ts
+++ src/frontend/src/svg.d.ts
@@ -1,5 +1,4 @@
-declare module '*.svg' {
-    const content: any;
-    export default content;
-  }
-  
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
--- src/frontend/src/types/typesContext/index.ts
+++ src/frontend/src/types/typesContext/index.ts
@@ -1,11 +1,11 @@
 import { ReactFlowInstance } from "reactflow";
 
-const types:{[char: string]: string}={}
+const types: { [char: string]: string } = {};
 
 export type typesContextType = {
-	reactFlowInstance: ReactFlowInstance|null;
-	setReactFlowInstance: any;
-	deleteNode: (idx: string) => void;
-	types: typeof types;
-	setTypes: (newState: {}) => void;
+  reactFlowInstance: ReactFlowInstance | null;
+  setReactFlowInstance: any;
+  deleteNode: (idx: string) => void;
+  types: typeof types;
+  setTypes: (newState: {}) => void;
 };
