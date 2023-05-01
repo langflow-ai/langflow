836568ab5850c2344a1a427431e304c6345d55b6
Gabriel Almeida <gabriel@logspace.ai>
2023-05-01 18:40:24 -0300

    refactor(agents): import BaseLanguageModel from base_language module instead of schema module

--- src/backend/langflow/interface/agents/custom.py
+++ src/backend/langflow/interface/agents/custom.py
@@ -28,7 +28,7 @@ from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS
 from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS as SQL_FORMAT_INSTRUCTIONS
 from langchain.llms.base import BaseLLM
 from langchain.memory.chat_memory import BaseChatMemory
-from langchain.schema import BaseLanguageModel
+from langchain.base_language import BaseLanguageModel
 from langchain.sql_database import SQLDatabase
 from langchain.tools.python.tool import PythonAstREPLTool
 from langchain.tools.sql_database.prompt import QUERY_CHECKER
