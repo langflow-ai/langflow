4ca064ebdcc97908f2b3714467531791cad9765d
anovazzi1 <otavio2204@gmail.com>
2023-05-09 18:02:43 -0300

    fixed duplicated message

---
+++ src/frontend/src/modals/chatModal/chatMessage/codeBlock/index.tsx
@@ -0,0 +1,8 @@
+export default function codeBlock() {
+	// a html custom element for code blocks in markdown texts, it will allow to download the code as a file or copy it to the clipboard
+    return (
+        <div>
+            <h1>Code Block</h1>
+        </div>
+    )
+}
