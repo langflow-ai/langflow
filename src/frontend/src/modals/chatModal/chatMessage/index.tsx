5c2c9b943b61064146ebba9d346fef1e417aa42b
anovazzi1 <otavio2204@gmail.com>
2023-05-09 19:33:44 -0300

    fancy code block implemmented

--- src/frontend/src/modals/chatModal/chatMessage/index.tsx
+++ src/frontend/src/modals/chatModal/chatMessage/index.tsx
@@ -9,6 +9,7 @@ import ReactMarkdown from "react-markdown";
 import rehypeMathjax from "rehype-mathjax";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+import { CodeBlock } from "./codeBlock";
 var Convert = require("ansi-to-html");
 var convert = new Convert({ newline: true });
 
@@ -65,14 +66,32 @@ export default function ChatMessage({ chat }: { chat: ChatMessageType }) {
 										remarkPlugins={[remarkGfm, remarkMath]}
 										rehypePlugins={[rehypeMathjax]}
 										className="markdown prose"
-										// components={{
-										// 	code: (props) => (
-										// 		<code
-										// 			className="bg-pink-600 dark:bg-gray-700 rounded-md p-1"
-										// 			{...props}
-										// 		/>
-										// 	),
-										// }}
+										components={{
+											code({ node, inline, className, children, ...props }) {
+												if (children.length) {
+												  if (children[0] == '▍') {
+													return <span className="animate-pulse cursor-default mt-1">▍</span>
+												  }
+							
+												  children[0] = (children[0] as string).replace("`▍`", "▍")
+												}
+							
+												const match = /language-(\w+)/.exec(className || '');
+							
+												return !inline ? (
+												  <CodeBlock
+													key={Math.random()}
+													language={(match && match[1]) || ''}
+													value={String(children).replace(/\n$/, '')}
+													{...props}
+												  />
+												) : (
+												  <code className={className} {...props}>
+													{children}
+												  </code>
+												);
+											  }
+										}}
 									>
 										{message}
 									</ReactMarkdown>
