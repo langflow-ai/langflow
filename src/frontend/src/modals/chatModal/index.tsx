d01db97cd33ee6450533c23f4f75dd08b4cef539
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-05-11 00:00:17 -0300

    Changed Chat modal to work with dark mode

--- src/frontend/src/modals/chatModal/index.tsx
+++ src/frontend/src/modals/chatModal/index.tsx
@@ -181,7 +181,7 @@ export default function ChatModal({
       }
     } catch (error) {
       setErrorData({
-        title: "There was an erro sending the message",
+        title: "There was an error sending the message",
         list: [error.message],
       });
       setChatValue(data.message);
@@ -325,7 +325,7 @@ export default function ChatModal({
                 <div className="relative w-full">
                   <button
                     onClick={() => clearChat()}
-                    className="absolute top-2 right-2 hover:text-red-500"
+                    className="absolute top-2 right-2 hover:text-red-500 dark:text-gray-300 dark:hover:text-red-500"
                   >
                     <FaEraser className="w-4 h-4" />
                   </button>
@@ -337,12 +337,12 @@ export default function ChatModal({
                     <div className="flex flex-col h-full text-center justify-center w-full items-center align-middle ">
                       <span>
                         👋{" "}
-                        <span className="text-gray-600 text-lg">
+                        <span className="text-gray-600 dark:text-gray-300 text-lg">
                           LangFlow Chat
                         </span>
                       </span>
                       <br />
-                      <div className="bg-gray-100 rounded-md w-2/4 px-6 py-8 border border-gray-200">
+                      <div className="bg-gray-100 dark:bg-gray-900 rounded-md w-2/4 px-6 py-8 border border-gray-200 dark:border-gray-700">
                         <span className="text-base text-gray-500">
                           Start a conversation and click the agent’s thoughts{" "}
                           <span>
