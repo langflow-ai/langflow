683e97fb3458279e7535ccd9b7525f2884ecc6cc
Gabriel Almeida <gabriel@logspace.ai>
2023-05-10 22:48:49 -0300

    refactor(agents/custom.py): create CustomAgentExecutor abstract class and make all agents inherit from it fix(agents/custom.py): fix import error for NotEnoughElementsException refactor(run.py): remove unused import statement for loading module

--- src/backend/langflow/interface/run.py
+++ src/backend/langflow/interface/run.py
@@ -2,12 +2,11 @@ import contextlib
 import io
 from typing import Any, Dict
 
-from chromadb.errors import NotEnoughElementsException
+from chromadb.errors import NotEnoughElementsException  # type: ignore
 
 from langflow.api.callback import AsyncStreamingLLMCallbackHandler, StreamingLLMCallbackHandler  # type: ignore
 from langflow.cache.base import compute_dict_hash, load_cache, memoize_dict
 from langflow.graph.graph import Graph
-from langflow.interface import loading
 from langflow.utils.logger import logger
 
 
