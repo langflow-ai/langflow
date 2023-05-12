64010cd2f428fdae7064d90ac498fc14072e6dc6
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-05-11 23:20:27 -0300

    changed dark mode at node modal

--- src/frontend/src/components/inputComponent/index.tsx
+++ src/frontend/src/components/inputComponent/index.tsx
@@ -27,7 +27,7 @@ export default function InputComponent({
       <input
         value={myValue}
         className={classNames(
-          "block w-full pr-12 form-input dark:bg-gray-900 dark:border-gray-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+          "block w-full pr-12 form-input dark:text-gray-300 dark:bg-gray-900 dark:border-gray-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
           disabled ? " bg-gray-200 dark:bg-gray-700" : "",
           password && !pwdVisible && myValue!=="" ? "password" : ""
         )}
