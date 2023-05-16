a2b31272802fc9aa45cdf8e87163acc766d653c7
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-05-16 20:05:03 -0300

    Changed exibition of the modal, using the selection menu provided by the ReactFlow, with some tweaks.

--- src/frontend/src/pages/FlowPage/components/SelectionMenuComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/SelectionMenuComponent/index.tsx
@@ -1,24 +1,37 @@
-import { Dialog } from "@headlessui/react";
-import { useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { NodeToolbar } from "reactflow";
 
-export default function SelectionMenu({onClick, position, isVisible, setIsVisible}){
-    const dialogRef = useRef(null);
+export default function SelectionMenu({onClick, nodes, isVisible}){
+    const [isOpen, setIsOpen] = useState(false);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+    const [lastNodes, setLastNodes] = useState(nodes);
+
+    // nodes get saved to not be gone after the toolbar closes
+    useEffect(() => {
+        setLastNodes(nodes);
+    }, [isOpen]);
+
+    // transition starts after and ends before the toolbar closes
+    useEffect(() => {
+        if(isVisible){
+            setIsOpen(true);
+            setTimeout(() => {setIsTransitioning(true);}, 50);
+            
+        } else {
+            setIsTransitioning(false)
+            setTimeout(() => {setIsOpen(false);}, 500);
+        }
+    }, [isVisible]);
+
     return (
-        <Dialog
-				as="div"
-				className="absolute z-50" style={{left: position.x, top: position.y}}
-                open={isVisible}
-				onClose={setIsVisible}
-                initialFocus={dialogRef}
-			>
-                <Dialog.Panel as="div" ref={dialogRef} className="bg-red-500 text-white w-24 h-8 text-center">
-                <button type="button" onClick={onClick}>
-                    <div className="">teste</div>
-                </button>
-                </Dialog.Panel>
-
-
-            </Dialog>
+        <NodeToolbar isVisible={isOpen} offset={5} nodeId={lastNodes && lastNodes.length > 0 ? lastNodes.map((n) => n.id): [] }>
+            <div className="overflow-hidden w-24 h-8">
+            <div className={"transition-all ease-in-out duration-500 w-24 h-8 bg-white dark:bg-gray-800 rounded-md shadow-md border text-gray-700 dark:text-gray-300" + (isTransitioning ? " translate-y-0" : " translate-y-8")}>
+                    Testeeee
+                    </div>
+            </div>
+            </NodeToolbar>
     )
 }
     
