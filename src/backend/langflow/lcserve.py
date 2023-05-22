aeba16fa8a6071919dc0aef493da475d13cf83dd
Gabriel Almeida <gabriel@logspace.ai>
2023-05-22 07:46:40 -0300

    🔥 chore(lcserve.py): remove unused import statements The import statements for pathlib.Path and fastapi.staticfiles.StaticFiles were not used in the code and were therefore removed to improve code readability and maintainability.

--- src/backend/langflow/lcserve.py
+++ src/backend/langflow/lcserve.py
@@ -1,7 +1,9 @@
 # This file is used by lc-serve to load the mounted app and serve it.
 
 from pathlib import Path
+
 from fastapi.staticfiles import StaticFiles
+
 from langflow.main import create_app
 
 app = create_app()
