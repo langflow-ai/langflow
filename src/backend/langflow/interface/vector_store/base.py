e597a6e20a59f7a6ff8690e634ca202df7d89271
Gabriel Almeida <gabriel@logspace.ai>
2023-05-23 16:51:29 -0300

    🔀 chore(config): add DocArrayHnswSearch and DocArrayInMemorySearch to vectorstores 🐛 fix(base.py): correctly handle nested lists in Node.build() method ✨ feat(vector_store): add VectorStoreFrontendNode to handle vector store templates 🐛 fix(util.py): add build_template_from_method to correctly build templates from class methods The configuration file now includes two new vector stores, DocArrayHnswSearch and DocArrayInMemorySearch. The Node.build() method now correctly handles nested lists. A new VectorStoreFrontendNode has been added to handle vector store templates. The build_template_from_method function has been added to correctly build templates from class methods.
    Issue #335

--- src/backend/langflow/interface/vector_store/base.py
+++ src/backend/langflow/interface/vector_store/base.py
@@ -1,14 +1,19 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Type
 
 from langflow.interface.base import LangChainTypeCreator
 from langflow.interface.custom_lists import vectorstores_type_to_cls_dict
 from langflow.settings import settings
+from langflow.template.nodes import VectorStoreFrontendNode
 from langflow.utils.logger import logger
-from langflow.utils.util import build_template_from_class
+from langflow.utils.util import build_template_from_class, build_template_from_method
 
 
 class VectorstoreCreator(LangChainTypeCreator):
     type_name: str = "vectorstores"
+
+    @property
+    def frontend_node_class(self) -> Type[VectorStoreFrontendNode]:
+        return VectorStoreFrontendNode
 
     @property
     def type_to_loader_dict(self) -> Dict:
@@ -17,25 +22,29 @@ class VectorstoreCreator(LangChainTypeCreator):
     def get_signature(self, name: str) -> Optional[Dict]:
         """Get the signature of an embedding."""
         try:
-            signature = build_template_from_class(name, vectorstores_type_to_cls_dict)
+            signature = build_template_from_method(
+                name,
+                type_to_cls_dict=vectorstores_type_to_cls_dict,
+                method_name="from_texts",
+            )
 
             # TODO: Use FrontendendNode class to build the signature
-            signature["template"] = {
-                "documents": {
-                    "type": "TextSplitter",
-                    "required": True,
-                    "show": True,
-                    "name": "documents",
-                    "display_name": "Text Splitter",
-                },
-                "embedding": {
-                    "type": "Embeddings",
-                    "required": True,
-                    "show": True,
-                    "name": "embedding",
-                    "display_name": "Embedding",
-                },
-            }
+            # signature["template"] = {
+            #     "documents": {
+            #         "type": "TextSplitter",
+            #         "required": True,
+            #         "show": True,
+            #         "name": "documents",
+            #         "display_name": "Text Splitter",
+            #     },
+            #     "embedding": {
+            #         "type": "Embeddings",
+            #         "required": True,
+            #         "show": True,
+            #         "name": "embedding",
+            #         "display_name": "Embedding",
+            #     },
+            # }
             return signature
 
         except ValueError as exc:
