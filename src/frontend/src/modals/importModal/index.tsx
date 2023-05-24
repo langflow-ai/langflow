b7980e8ad5d6a41095f9a757ab867d089e18392d
anovazzi1 <otavio2204@gmail.com>
2023-05-24 16:27:21 -0300

    fix size of import on dev

--- src/frontend/src/modals/importModal/index.tsx
+++ src/frontend/src/modals/importModal/index.tsx
@@ -180,7 +180,7 @@ export default function ImportModal() {
 													<div id="index">
 														{" "}
 														<ButtonBox
-															size="big"
+															size="small"
 															bgColor="bg-emerald-500 dark:bg-emerald-500/75"
 															description={
 																example.description ?? "Prebuilt Examples"
