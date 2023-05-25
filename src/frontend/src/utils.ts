35d93e70ae14c7e5c0f50d434ea7bace8fc19041
anovazzi1 <otavio2204@gmail.com>
2023-05-25 16:38:37 -0300

    create function that change display parameters of input components of a group node template

--- src/frontend/src/utils.ts
+++ src/frontend/src/utils.ts
@@ -804,6 +804,25 @@ function isHandleConnected(flow:FlowType, handleId:string){
 
 }
 
+export function updateGroupNodeTemplate(template:APITemplateType){
+	/*this function receives a template, iterates for it's items 
+	updating the visibility of all basic types setting it to advanced true*/
+	Object.keys(template).forEach((key)=>{
+		let type = template[key].type;
+		if(type === "str" ||
+        type === "bool" ||
+        type === "float" ||
+        type === "code" ||
+        type === "prompt" ||
+        type === "file" ||
+        type === "int"){
+			template[key].advanced = true;
+		}
+		
+	})
+	return template;
+}
+
 export function generateNodeTemplate(Flow:FlowType){
 	/*
 		this function receives a flow and generate a template for the group node
