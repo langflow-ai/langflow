d3e76fa9e4d18954896002988e91704e14556a4c
Gabriel Almeida <gabriel@logspace.ai>
2023-05-30 21:24:45 -0300

    🎉 feat(database): add database module with base and models modules The database module has been added to the project, which includes the base and models modules. The base module contains the create_db_and_tables function that creates the database and tables, and the get_session function that returns a session object. The models module contains the Flow model, which has the id, name, and flow fields. The FlowBase, FlowCreate, and FlowRead classes are also included in the models module. These classes are used to define the schema for the Flow model.

---
+++ src/backend/langflow/database/base.py
@@ -0,0 +1,17 @@
+from langflow.settings import settings
+from sqlmodel import SQLModel, Session, create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
