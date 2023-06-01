5b5eea98955c6fb7ba0614b783fd1109a6eed7ce
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-01 15:07:56 -0300

    🚀 feat(langflow): reorganize graph package to improve modularity and maintainability The changes include: - Moved the `Edge` class to a new `edge` package - Moved the `Graph` class to a new `graph` package - Moved the `Node` class to a new `node` package - Moved the `VectorStoreNode` class to the `node/types.py` module - Moved the `Edge`, `Graph`, and `Node` classes to their respective `base.py` modules - Added an `__init__.py` file to each package to allow for importing of classes - Added a `constants.py` module to the `graph` package to store constants used in the `Graph` class - Refactored the `Graph` class to use the new `Node` and `Edge` classes - Refactored the `Graph` class to use a dictionary to map node types to their respective classes - Refactored the `Graph` class to remove invalid nodes from the graph - Refactored the `Graph` class to handle the LLM node within the graph - Refactored the `Graph` class to build the nodes before building the edges - Refactored the `Graph` class to use the `get_node` method to find nodes by id - Refactored the `Graph` class to use the `get_node_neighbors` method to find the neighbors of a node - Refactored the `Graph` class to use the `get_children_by_node_type` method to find the children of a node based on the node type
    These changes improve the modularity and maintainability of the `langflow` package by separating the classes into their respective packages and modules. The changes also make it easier to add new node types to the `Graph` class by using a dictionary to map node types to their respective classes.
    
    🚀 feat(node): add Node class to represent a node in the graph
    🚀 feat(constants.py): add DIRECT_TYPES constant to represent direct types in a node's template
    The Node class represents a node in the graph and is responsible for parsing the data and building the module. The DIRECT_TYPES constant is a list of direct types in a node's template.
    
    🚧 chore(types.py): add import statements for typing and Node classes
    This commit adds import statements for the typing module and the Node class to the types.py file. This is necessary for the code to run properly as it uses these classes and modules.
    
    🚧 chore(loading.py): remove unnecessary import statement
    This commit removes an unnecessary import statement from the loading.py file. The import statement was causing a circular import error and was not needed for the code to run properly.
    
    🚧 chore(run.py): update import statement for Graph class
    This commit updates the import statement for the Graph class in the run.py file. The import statement was outdated and was causing an import error.
    
    🚧 chore(conftest.py): update import statement for Graph class
    This commit updates the import statement for the Graph class in the conftest.py file. The import statement was outdated and was causing an import error.
    
    🚧 chore(test_graph.py): update import statements for Node and Edge classes
    This commit updates the import statements for the Node and Edge classes in the test_graph.py file. The import statements were outdated and were causing import errors.

--- src/backend/langflow/graph/node/base.py
+++ src/backend/langflow/graph/node/base.py
@@ -1,27 +1,27 @@
-# Description: Graph class for building a graph of nodes and edges
-# Insights:
-#   - Defer prompts building to the last moment or when they have all the tools
-#   - Build each inner agent first, then build the outer agent
-
-import contextlib
-import inspect
-import types
-import warnings
-from typing import Any, Dict, List, Optional
-
 from langflow.cache import base as cache_utils
-from langflow.graph.constants import DIRECT_TYPES
+from langflow.graph.node.constants import DIRECT_TYPES
 from langflow.interface import loading
 from langflow.interface.listing import ALL_TYPES_DICT
 from langflow.utils.logger import logger
 from langflow.utils.util import sync_to_async
 
 
+import contextlib
+import inspect
+import types
+import warnings
+from typing import Any, Dict, List, Optional
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from langflow.graph.edge.base import Edge
+
+
 class Node:
     def __init__(self, data: Dict, base_type: Optional[str] = None) -> None:
         self.id: str = data["id"]
         self._data = data
-        self.edges: List[Edge] = []
+        self.edges: List["Edge"] = []
         self.base_type: Optional[str] = base_type
         self._parse_data()
         self._built_object = None
@@ -227,50 +227,3 @@ class Node:
 
     def _built_object_repr(self):
         return repr(self._built_object)
-
-
-class Edge:
-    def __init__(self, source: "Node", target: "Node"):
-        self.source: "Node" = source
-        self.target: "Node" = target
-        self.validate_edge()
-
-    def validate_edge(self) -> None:
-        # Validate that the outputs of the source node are valid inputs
-        # for the target node
-        self.source_types = self.source.output
-        self.target_reqs = self.target.required_inputs + self.target.optional_inputs
-        # Both lists contain strings and sometimes a string contains the value we are
-        # looking for e.g. comgin_out=["Chain"] and target_reqs=["LLMChain"]
-        # so we need to check if any of the strings in source_types is in target_reqs
-        self.valid = any(
-            output in target_req
-            for output in self.source_types
-            for target_req in self.target_reqs
-        )
-        # Get what type of input the target node is expecting
-
-        self.matched_type = next(
-            (
-                output
-                for output in self.source_types
-                for target_req in self.target_reqs
-                if output in target_req
-            ),
-            None,
-        )
-        no_matched_type = self.matched_type is None
-        if no_matched_type:
-            logger.debug(self.source_types)
-            logger.debug(self.target_reqs)
-        if no_matched_type:
-            raise ValueError(
-                f"Edge between {self.source.node_type} and {self.target.node_type} "
-                f"has no matched type"
-            )
-
-    def __repr__(self) -> str:
-        return (
-            f"Edge(source={self.source.id}, target={self.target.id}, valid={self.valid}"
-            f", matched_type={self.matched_type})"
-        )
