a662c5a6ccda92ff08a80467a3c39826372361b6
Alexandre Henrique <alexandrehptavares@gmail.com>
2023-06-01 16:21:11 -0300

    reformatted with black

--- src/backend/langflow/template/frontend_node/vectorstores.py
+++ src/backend/langflow/template/frontend_node/vectorstores.py
@@ -3,6 +3,7 @@ from typing import Optional
 from langflow.template.field.base import TemplateField
 from langflow.template.frontend_node.base import FrontendNode
 
+
 class VectorStoreFrontendNode(FrontendNode):
     def add_extra_fields(self) -> None:
         if self.template.type_name == "Weaviate":
