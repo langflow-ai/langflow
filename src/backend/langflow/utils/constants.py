a3190768ce506e1a03bea36f54bcd0bf794d22bb
Jim White <jim@ifcx.org>
2023-06-02 12:18:38 -0700

    Trim too long comments about Anthropic models.

--- src/backend/langflow/utils/constants.py
+++ src/backend/langflow/utils/constants.py
@@ -11,14 +11,14 @@ ANTHROPIC_MODELS = [
     "claude-v1",  # largest model, ideal for a wide range of more complex tasks.
     "claude-v1-100k",  # An enhanced version of claude-v1 with a 100,000 token (roughly 75,000 word) context window.
     "claude-instant-v1",  # A smaller model with far lower latency, sampling at roughly 40 words/sec!
-    "claude-instant-v1-100k",  # An enhanced version of claude-instant-v1 with a 100,000 token context window that retains its performance.
+    "claude-instant-v1-100k",  # Like claude-instant-v1 with a 100,000 token context window but retains its performance.
     # Specific sub-versions of the above models:
-    "claude-v1.3",  # Compared to claude-v1.2, it's more robust against red-team inputs, better at precise instruction-following, better at code, and better and non-English dialogue and writing.
+    "claude-v1.3",  # Vs claude-v1.2: better instruction-following, code, and non-English dialogue and writing.
     "claude-v1.3-100k",  # An enhanced version of claude-v1.3 with a 100,000 token (roughly 75,000 word) context window.
-    "claude-v1.2",  # An improved version of claude-v1. It is slightly improved at general helpfulness, instruction following, coding, and other tasks. It is also considerably better with non-English languages. This model also has the ability to role play (in harmless ways) more consistently, and it defaults to writing somewhat longer and more thorough responses.
+    "claude-v1.2",  # Vs claude-v1.1: small adv in general helpfulness, instruction following, coding, and other tasks.
     "claude-v1.0",  # An earlier version of claude-v1.
-    "claude-instant-v1.1",  # latest version of claude-instant-v1. It is better than claude-instant-v1.0 at a wide variety of tasks including writing, coding, and instruction following.
-    "claude-instant-v1.1-100k",  # An enhanced version of claude-instant-v1.1 with a 100,000 token context window that retains its lightning fast 40 word/sec performance.
+    "claude-instant-v1.1",  # Latest version of claude-instant-v1. Better than claude-instant-v1.0 at most tasks.
+    "claude-instant-v1.1-100k",  # Version of claude-instant-v1.1 with a 100K token context window.
     "claude-instant-v1.0",  # An earlier version of claude-instant-v1.
 ]
 
