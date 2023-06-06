056da6f0e01f6bc347030bbfdab83583dde81706
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-06 10:06:51 -0300

    update endpoint

--- tests/test_vectorstore_template.py
+++ tests/test_vectorstore_template.py
@@ -5,7 +5,7 @@ from langflow.settings import settings
 # check that all agents are in settings.agents
 # are in json_response["agents"]
 def test_vectorstores_settings(client: TestClient):
-    response = client.get("/all")
+    response = client.get("api/v1/all")
     assert response.status_code == 200
     json_response = response.json()
     vectorstores = json_response["vectorstores"]
