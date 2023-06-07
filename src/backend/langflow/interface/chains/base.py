f0975ddf63fa54e8d9f46ecdc8d4b640afda0e75
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-07 19:48:51 -0300

    🚀 feat(langflow): add new chains to config.yaml and custom chains to interface/chains/custom.py ✨ feat(langflow): add new chains to config.yaml and custom chains to interface/chains/custom.py The following chains were added to the config.yaml file: RetrievalQA, RetrievalQAWithSourcesChain, QAWithSourcesChain, ConversationalRetrievalChain, and CombineDocsChain. These chains were added to improve the functionality of the application and provide more options for users.
    In addition, custom chains were added to the interface/chains/custom.py file. The CombineDocsChain was added to allow users to combine multiple documents into a single document for use in the question answering chains. The QA_CHAIN_TYPES constant was also added to the frontend_node/constants.py file to provide a list of available question answering chain types.

--- src/backend/langflow/interface/chains/base.py
+++ src/backend/langflow/interface/chains/base.py
@@ -1,12 +1,13 @@
-from typing import Dict, List, Optional, Type
+from typing import Any, Dict, List, Optional, Type
 
 from langflow.custom.customs import get_custom_nodes
 from langflow.interface.base import LangChainTypeCreator
-from langflow.interface.custom_lists import chain_type_to_cls_dict
+from langflow.interface.importing.utils import import_class
 from langflow.settings import settings
 from langflow.template.frontend_node.chains import ChainFrontendNode
 from langflow.utils.logger import logger
-from langflow.utils.util import build_template_from_class
+from langflow.utils.util import build_template_from_class, build_template_from_method
+from langchain import chains
 
 # Assuming necessary imports for Field, Template, and FrontendNode classes
 
@@ -18,10 +19,16 @@ class ChainCreator(LangChainTypeCreator):
     def frontend_node_class(self) -> Type[ChainFrontendNode]:
         return ChainFrontendNode
 
+    #! We need to find a better solution for this
+    from_method_nodes = {"ConversationalRetrievalChain": "from_llm"}
+
     @property
     def type_to_loader_dict(self) -> Dict:
         if self.type_dict is None:
-            self.type_dict = chain_type_to_cls_dict
+            self.type_dict: dict[str, Any] = {
+                chain_name: import_class(f"langchain.chains.{chain_name}")
+                for chain_name in chains.__all__
+            }
             from langflow.interface.chains.custom import CUSTOM_CHAINS
 
             self.type_dict.update(CUSTOM_CHAINS)
@@ -37,20 +44,38 @@ class ChainCreator(LangChainTypeCreator):
         try:
             if name in get_custom_nodes(self.type_name).keys():
                 return get_custom_nodes(self.type_name)[name]
+            elif name in self.from_method_nodes.keys():
+                return build_template_from_method(
+                    name,
+                    type_to_cls_dict=self.type_to_loader_dict,
+                    method_name=self.from_method_nodes[name],
+                    add_function=True,
+                )
             return build_template_from_class(
                 name, self.type_to_loader_dict, add_function=True
             )
         except ValueError as exc:
-            raise ValueError("Chain not found") from exc
+            raise ValueError(f"Chain {name} not found: {exc}") from exc
         except AttributeError as exc:
             logger.error(f"Chain {name} not loaded: {exc}")
             return None
 
-    def to_list(self) -> List[str]:
-        custom_chains = list(get_custom_nodes("chains").keys())
-        default_chains = list(self.type_to_loader_dict.keys())
+    # def to_list(self) -> List[str]:
+    #     custom_chains = list(get_custom_nodes("chains").keys())
+    #     default_chains = list(self.type_to_loader_dict.keys())
 
-        return default_chains + custom_chains
+    #     return default_chains + custom_chains
+
+    def to_list(self) -> List[str]:
+        names = []
+        for _, chain in self.type_to_loader_dict.items():
+            chain_name = (
+                chain.function_name()
+                if hasattr(chain, "function_name")
+                else chain.__name__
+            )
+            names.append(chain_name)
+        return names
 
 
 chain_creator = ChainCreator()
