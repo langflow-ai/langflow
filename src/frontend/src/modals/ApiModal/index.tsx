a1118450ae2d4d01ca6cdada2a9975c64c41d7cd
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-06-07 18:59:11 -0300

    changing code, export, python modal to shadcn ui

--- src/frontend/src/modals/ApiModal/index.tsx
+++ src/frontend/src/modals/ApiModal/index.tsx
@@ -1,4 +1,3 @@
-import { Dialog, Transition } from "@headlessui/react";
 import { IconCheck, IconClipboard, IconDownload } from "@tabler/icons-react";
 import {
   XMarkIcon,
@@ -15,13 +14,22 @@ import "ace-builds/src-noconflict/ext-language_tools";
 import { darkContext } from "../../contexts/darkContext";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../components/ui/dialog";
+import { Button } from "../../components/ui/button";
 
 export default function ApiModal({ flowName }) {
   const [open, setOpen] = useState(true);
   const { dark } = useContext(darkContext);
   const { closePopUp } = useContext(PopUpContext);
   const [activeTab, setActiveTab] = useState(0);
-  const ref = useRef();
   const [isCopied, setIsCopied] = useState<Boolean>(false);
 
   const copyToClipboard = () => {
@@ -40,9 +48,7 @@ export default function ApiModal({ flowName }) {
   function setModalOpen(x: boolean) {
     setOpen(x);
     if (x === false) {
-      setTimeout(() => {
         closePopUp();
-      }, 300);
     }
   }
 
@@ -82,71 +88,34 @@ flow("Hey, have you heard of LangFlow?")`;
     },
   ];
   return (
-    <Transition.Root show={open} appear={true} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        onClose={setModalOpen}
-        initialFocus={ref}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 dark:bg-gray-600 dark:bg-opacity-75 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative flex flex-col justify-between transform h-[600px] overflow-hidden rounded-lg bg-white dark:bg-gray-800 text-left shadow-xl transition-all sm:my-8 w-[700px]">
-                <div className=" z-50 absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
-                  <button
-                    type="button"
-                    className="rounded-md text-gray-400 hover:text-gray-500"
-                    onClick={() => {
-                      setModalOpen(false);
-                    }}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="h-full w-full flex flex-col justify-center items-center">
-                  <div className="flex w-full pb-4 z-10 justify-center shadow-sm">
-                    <div className="mx-auto mt-4 flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-gray-900 sm:mx-0 sm:h-10 sm:w-10">
-                      <CodeBracketSquareIcon
-                        className="h-6 w-6 text-blue-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-4 text-center sm:ml-4 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium dark:text-white leading-10 text-gray-900"
-                      >
-                        Code
-                      </Dialog.Title>
-                    </div>
-                  </div>
-                  <div className="h-full w-full bg-gray-200 overflow-auto dark:bg-gray-900 p-4 gap-4 flex flex-row justify-center items-center">
+
+    <Dialog open={true} onOpenChange={setModalOpen}>
+    <DialogTrigger>
+    </DialogTrigger>
+    <DialogContent className="lg:max-w-[800px] sm:max-w-[600px] h-[600px]">
+      <DialogHeader>
+        <DialogTitle className="flex items-center">
+          <span className="pr-2">
+          Code
+          </span>
+            <CodeBracketSquareIcon
+            className="h-6 w-6 text-gray-800 pl-1 dark:text-white"
+            aria-hidden="true"
+            />
+          
+          </DialogTitle>
+        <DialogDescription>
+        Export your flow to use it with this code.
+
+        </DialogDescription>
+      </DialogHeader>
+
+
                     <div className="flex flex-col h-full w-full ">
                       <div className="flex px-5 z-10">
                         {tabs.map((tab, index) => (
-                          <button
+                          <button key={index}
                             onClick={() => {
                               setActiveTab(index);
                             }}
@@ -162,11 +131,9 @@ flow("Hey, have you heard of LangFlow?")`;
                           </button>
                         ))}
                       </div>
-                      <div className="overflow-hidden px-4 py-5 sm:p-6 w-full h-full rounded-lg shadow bg-white dark:bg-gray-800">
-                        <div className="w-full flex items-center justify-between mb-2">
-                          <span className="text-sm text-gray-500 dark:text-gray-300">
-                            Export your flow to use it with this code.
-                          </span>
+                      <div className="overflow-hidden px-4 py-2 sm:p-4 sm:pb-0 sm:pt-2 w-full h-full rounded-lg shadow bg-white dark:bg-gray-800">
+                        <div className="items-center mb-2">
+                          <div className="float-right">
                           <button
                             className="flex gap-1.5 items-center rounded bg-none p-1 text-xs text-gray-500 dark:text-gray-300"
                             onClick={copyToClipboard}
@@ -178,9 +145,11 @@ flow("Hey, have you heard of LangFlow?")`;
                             )}
                             {isCopied ? "Copied!" : "Copy code"}
                           </button>
+                            </div>
+
                         </div>
                         <SyntaxHighlighter
-                          className="h-[370px]"
+                          className="h-[370px] w-full"
                           language={tabs[activeTab].mode}
                           style={oneDark}
                           customStyle={{ margin: 0 }}
@@ -189,13 +158,8 @@ flow("Hey, have you heard of LangFlow?")`;
                         </SyntaxHighlighter>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    </DialogContent>
+  </Dialog>
+
   );
 }
