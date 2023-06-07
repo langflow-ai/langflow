b8aae5f702cf496d36a4f6c61e9cf3e2f130c85f
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-06-06 22:55:15 -0300

    starting migration modals to shadcn

--- src/frontend/src/components/dropdownComponent/index.tsx
+++ src/frontend/src/components/dropdownComponent/index.tsx
@@ -8,10 +8,12 @@ export default function Dropdown({
   value,
   options,
   onSelect,
+  editNode = false
 }: DropDownComponentType) {
   let [internalValue, setInternalValue] = useState(
     value === "" || !value ? "Choose an option" : value
   );
+  
   return (
     <>
       <Listbox
@@ -23,10 +25,20 @@ export default function Dropdown({
       >
         {({ open }) => (
           <>
-            <div className="relative mt-1 w-full">
-              <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white dark:bg-gray-900 py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+            <div className={
+              editNode ? "relative mt-0 w-full" :
+              "relative mt-1 w-full"
+              }>
+              <Listbox.Button 
+              className=
+              {editNode ? 
+                "pr-9 arrow-hide placeholder:text-center border-0 block w-full pt-0.5 pb-0.5 form-input dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 rounded-md border-gray-300 shadow-sm sm:text-sm focus:outline-none focus:ring-1 focus:ring-inset focus:ring-gray-200" : 
+                "relative w-full cursor-default rounded-md border border-gray-300 bg-white dark:bg-gray-900 py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"}
+              >
                 <span className="block truncate w-full">{internalValue}</span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <span className={
+                  editNode ? "hidden" :
+                  "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"}>
                   <ChevronUpDownIcon
                     className="h-5 w-5 text-gray-400"
                     aria-hidden="true"
@@ -41,16 +53,27 @@ export default function Dropdown({
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full dark:bg-gray-800 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options 
+                className={
+                  editNode ? "arrow-hide" :
+                  "absolute z-50 mt-1 max-h-60 w-full dark:bg-gray-800 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                }>
+                  
                   {options.map((option, id) => (
                     <Listbox.Option
                       key={id}
                       className={({ active }) =>
                         classNames(
-                          active
+                          active && !editNode
                             ? "text-white bg-indigo-600 dark:bg-indigo-500"
                             : "text-gray-900",
-                          "relative cursor-default select-none py-2 pl-3 pr-9 dark:text-gray-300 dark:bg-gray-800"
+                            active && editNode
+                            ? "text-white bg-gray-400 dark:bg-gray-500"
+                            : "",
+                            editNode ?  
+                            "relative cursor-default select-none py-0.5 pl-3 pr-12 dark:text-gray-300 dark:bg-gray-800"
+                            : "relative cursor-default select-none py-2 pl-3 pr-9 dark:text-gray-300 dark:bg-gray-800",
+                          
                         )
                       }
                       value={option}
@@ -69,7 +92,8 @@ export default function Dropdown({
                           {selected ? (
                             <span
                               className={classNames(
-                                active ? "text-white" : "text-indigo-600",
+                                editNode ? "text-gray-600" : "text-indigo-600",
+                                active ? "text-white" : "",
                                 "absolute inset-y-0 right-0 flex items-center pr-4"
                               )}
                             >
--- src/frontend/src/contexts/tabsContext.tsx
+++ src/frontend/src/contexts/tabsContext.tsx
@@ -159,7 +159,7 @@ export function TabsProvider({ children }: { children: ReactNode }) {
     // simulate a click on the link element to trigger the download
     link.click();
     setNoticeData({
-      title: "Warning: Critical data,JSON file may including API keys.",
+      title: "Warning: Critical data, JSON file may include API keys.",
     });
   }
 
