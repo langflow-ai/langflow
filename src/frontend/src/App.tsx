61207db4da7069829254f935a4c5b4c1209e6620
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-09 17:17:55 -0300

    🐛 fix(App.tsx): change fetch URL to relative path to fix incorrect API call The fetch URL was incorrect and was calling the API with an absolute path instead of a relative path. This caused the API call to fail. The URL has been changed to a relative path to fix the issue.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
   // Initialize state variable for the version
   const [version, setVersion] = useState("");
   useEffect(() => {
-    fetch("api/v1/version")
+    fetch("/version")
       .then((res) => res.json())
       .then((data) => {
         setVersion(data.version);
