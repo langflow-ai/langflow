3fcf67d88add6408e9f7d6e4a70163b41d19c09c
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-12 18:31:10 -0300

    🐛 fix(loading.tsx): fix broken link to the source of the Loading component The React import is not used in the file, so it has been removed to improve code readability. The link to the source of the Loading component was broken, so it has been fixed to ensure that the component can be used properly. 🔥 chore(loading.tsx): remove unused React import

--- src/frontend/src/components/ui/loading.tsx
+++ src/frontend/src/components/ui/loading.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { SVGProps } from "react";
 
+// https://github.com/feathericons/feather/issues/695#issuecomment-1503699643
 export const Loading = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
