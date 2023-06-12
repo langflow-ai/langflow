50c145b5177c76e121911422febea3f94b72d61b
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-12 09:51:32 -0300

    🔥 chore(test_websocket.py): remove outdated test_chat_history test The test_chat_history test is outdated and no longer works with the current implementation of the application. It has been removed to avoid confusion and to keep the test suite clean.

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -10,38 +10,40 @@ def test_websocket_connection(client: TestClient):
         assert websocket.scope["path"] == "/api/v1/chat/test_client"
 
 
-def test_chat_history(client: TestClient):
-    # Mock the process_graph function to return a specific value
-    with patch("langflow.chat.manager.process_graph") as mock_process_graph:
-        mock_process_graph.return_value = ("Hello, I'm a mock response!", "")
+# This does not work anymore because now we require
+# the flow to be built before sending messages
+# def test_chat_history(client: TestClient):
+#     # Mock the process_graph function to return a specific value
+#     with patch("langflow.chat.manager.process_graph") as mock_process_graph:
+#         mock_process_graph.return_value = ("Hello, I'm a mock response!", "")
 
-        with client.websocket_connect("api/v1/chat/test_client") as websocket:
-            # First message should be the history
-            history = websocket.receive_json()
-            assert history == []  # Empty history
-            # Send a message
-            payload = {"message": "Hello"}
-            websocket.send_json(json.dumps(payload))
+#         with client.websocket_connect("api/v1/chat/test_client") as websocket:
+#             # First message should be the history
+#             history = websocket.receive_json()
+#             assert history == []  # Empty history
+#             # Send a message
+#             payload = {"message": "Hello"}
+#             websocket.send_json(json.dumps(payload))
 
-            # Receive the response from the server
-            response = websocket.receive_json()
-            assert response == {
-                "is_bot": True,
-                "message": None,
-                "type": "start",
-                "intermediate_steps": "",
-                "files": [],
-            }
-            # Send another message
-            payload = {"message": "How are you?"}
-            websocket.send_json(json.dumps(payload))
+#             # Receive the response from the server
+#             response = websocket.receive_json()
+#             assert response == {
+#                 "is_bot": True,
+#                 "message": None,
+#                 "type": "start",
+#                 "intermediate_steps": "",
+#                 "files": [],
+#             }
+#             # Send another message
+#             payload = {"message": "How are you?"}
+#             websocket.send_json(json.dumps(payload))
 
-            # Receive the response from the server
-            response = websocket.receive_json()
-            assert response == {
-                "is_bot": True,
-                "message": "Hello, I'm a mock response!",
-                "type": "end",
-                "intermediate_steps": "",
-                "files": [],
-            }
+#             # Receive the response from the server
+#             response = websocket.receive_json()
+#             assert response == {
+#                 "is_bot": True,
+#                 "message": "Hello, I'm a mock response!",
+#                 "type": "end",
+#                 "intermediate_steps": "",
+#                 "files": [],
+#             }
