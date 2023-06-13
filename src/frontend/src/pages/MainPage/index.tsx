1a92ceca7ee3396159bae0d51e865f822aa32b14
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-06-13 19:09:24 -0300

    Added community page, fixed styling of buttons, added promise at AddFlow

--- src/frontend/src/pages/MainPage/index.tsx
+++ src/frontend/src/pages/MainPage/index.tsx
@@ -8,7 +8,7 @@ import ExtraSidebar from "../../components/ExtraSidebarComponent";
 import { ReactFlowProvider } from "reactflow";
 import FlowPage from "../FlowPage";
 import { useContext, useEffect, useState } from "react";
-import { SunIcon, MoonIcon, BellIcon, GithubIcon, Download, Upload, Plus } from "lucide-react";
+import { SunIcon, MoonIcon, BellIcon, GithubIcon, Download, Upload, Plus, Home } from "lucide-react";
 import { TabsContext } from "../../contexts/tabsContext";
 import AlertDropdown from "../../alerts/alertDropDown";
 import { alertContext } from "../../contexts/alertContext";
@@ -21,8 +21,8 @@ import { FaGithub } from "react-icons/fa";
 import _ from "lodash";
 
 import { updateFlowInDatabase, uploadFlowsToDatabase } from "../../controllers/API";
-import { CardComponent } from "./components/cardComponent";
 import { MenuBar } from "../../components/headerComponent/components/menuBar";
+import { CardComponent } from "./components/cardComponent";
 export default function HomePage() {
   const {
     flows,
@@ -36,26 +36,26 @@ export default function HomePage() {
   }, [])
   return (
       <div
-        className="w-full h-full flex overflow-auto flex-col bg-muted"
+        className="w-full h-full flex overflow-auto flex-col bg-muted px-16"
       >
-          <div className="w-full flex justify-between py-12 px-8">
-            <span className="text-xl font-semibold">
-              Flows
+          <div className="w-full flex justify-between py-12 pb-8 px-6">
+            <span className="text-2xl flex items-center justify-center gap-2 font-semibold">
+              <Home className="w-6" />My Projects
             </span>
             <div className="flex gap-2">
-              <Button variant="outline" onClick={() => {
+              <Button variant="primary" onClick={() => {
                 downloadFlows();
               }}>
               <Download className="w-4 mr-2" />
               Download Database
               </Button>
-              <Button variant="outline" onClick={() => {uploadFlows();}}>
+              <Button variant="primary" onClick={() => {uploadFlows();}}>
               <Upload className="w-4 mr-2" />
               Upload Database
               </Button>
-              <Button variant="outline" onClick={() => {addFlow();}}>
+              <Button variant="primary" onClick={() => {addFlow();}}>
               <Plus className="w-4 mr-2" />
-              Add Flow
+              New Project
               </Button>
             </div>
           </div>
