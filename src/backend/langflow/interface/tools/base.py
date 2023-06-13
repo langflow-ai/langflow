8e9bbb6018358c837128e0bc57af1ffd215bf8a9
gustavoschaedler <gustavopoa@gmail.com>
2023-06-13 21:05:26 +0100

    WIP: Small adjustments to reduce the complexity of the base tools class

--- src/backend/langflow/interface/tools/base.py
+++ src/backend/langflow/interface/tools/base.py
@@ -66,7 +66,7 @@ TOOL_INPUTS = {
 }
 
 
-class ToolCreator_new(LangChainTypeCreator):
+class ToolCreator(LangChainTypeCreator):
     type_name: str = "tools"
 
     @property
@@ -82,7 +82,7 @@ class ToolCreator_new(LangChainTypeCreator):
             }
         return self.type_dict
 
-    def get_signature_new(self, name: str) -> Optional[Dict]:
+    def get_signature(self, name: str) -> Optional[Dict]:
         """Get the signature of an embedding."""
         try:
             return build_template_from_method(
@@ -96,82 +96,6 @@ class ToolCreator_new(LangChainTypeCreator):
             logger.error(f"Tool {name} not loaded: {exc}")
             return None
 
-    def get_signature(self, name: str) -> Optional[Dict]:
-        """Get the signature of a tool."""
-
-        base_classes = ["Tool"]
-        fields = []
-        params = []
-        tool_params = {}
-
-        # Raise error if name is not in tools
-        if name not in self.type_to_loader_dict.keys():
-            raise ValueError("Tool not found")
-
-        tool_type: str = self.type_to_loader_dict[name]["type"]  # type: ignore
-
-        # if tool_type in _BASE_TOOLS.keys():
-        #     params = []
-        if tool_type in _LLM_TOOLS.keys():
-            params = ["llm"]
-        elif tool_type in _EXTRA_LLM_TOOLS.keys():
-            extra_keys = _EXTRA_LLM_TOOLS[tool_type][1]
-            params = ["llm"] + extra_keys
-        elif tool_type in _EXTRA_OPTIONAL_TOOLS.keys():
-            extra_keys = _EXTRA_OPTIONAL_TOOLS[tool_type][1]
-            params = extra_keys
-        # elif tool_type == "Tool":
-        #     params = ["name", "description", "func"]
-        elif tool_type in CUSTOM_TOOLS:
-            # Get custom tool params
-            params = self.type_to_loader_dict[name]["params"]  # type: ignore
-            base_classes = ["function"]
-            if node := customs.get_custom_nodes("tools").get(tool_type):
-                return node
-        elif tool_type in FILE_TOOLS:
-            params = self.type_to_loader_dict[name]["params"]  # type: ignore
-            base_classes += [name]
-        elif tool_type in OTHER_TOOLS:
-            tool_dict = build_template_from_class(tool_type, OTHER_TOOLS)
-            fields = tool_dict["template"]
-
-            # Pop unnecessary fields and add name
-            fields.pop("_type")  # type: ignore
-            fields.pop("return_direct")  # type: ignore
-            fields.pop("verbose")  # type: ignore
-
-            tool_params = {
-                "name": fields.pop("name")["value"],  # type: ignore
-                # type: ignore
-                "description": fields.pop("description")["value"],
-            }
-
-            fields = [
-                TemplateField(name=name, field_type=field["type"], **field)
-                for name, field in fields.items()  # type: ignore
-            ]
-            base_classes += tool_dict["base_classes"]
-
-        # Copy the field and add the name
-        for param in params:
-            field = TOOL_INPUTS.get(param, TOOL_INPUTS["str"]).copy()
-            field.name = param
-            field.advanced = False
-            if param == "aiosession":
-                field.show = False
-                field.required = False
-
-            fields.append(field)
-
-        template = Template(fields=fields, type_name=tool_type)
-
-        tool_params = {**tool_params, **self.type_to_loader_dict[name]["params"]}
-        return {
-            "template": util.format_dict(template.to_dict()),
-            **tool_params,
-            "base_classes": base_classes,
-        }
-
     def to_list(self) -> List[str]:
         return [
             tool
@@ -180,7 +104,7 @@ class ToolCreator_new(LangChainTypeCreator):
         ]
 
 
-class ToolCreator(LangChainTypeCreator):
+class ToolCreator_OLD(LangChainTypeCreator):
     type_name: str = "tools"
     tools_dict: Optional[Dict] = None
 
@@ -196,7 +120,8 @@ class ToolCreator(LangChainTypeCreator):
 
                 if tool_name in settings.tools or settings.dev:
                     if tool_name == "JsonSpec":
-                        tool_params["path"] = tool_params.pop("dict_")  # type: ignore
+                        tool_params["path"] = tool_params.pop(
+                            "dict_")  # type: ignore
                     all_tools[tool_name] = {
                         "type": tool,
                         "params": tool_params,
@@ -276,7 +201,8 @@ class ToolCreator(LangChainTypeCreator):
 
         template = Template(fields=fields, type_name=tool_type)
 
-        tool_params = {**tool_params, **self.type_to_loader_dict[name]["params"]}
+        tool_params = {**tool_params, **
+                       self.type_to_loader_dict[name]["params"]}
         return {
             "template": util.format_dict(template.to_dict()),
             **tool_params,
@@ -293,3 +219,115 @@ tool_creator = ToolCreator()
 
 # TODO: Delete, just for test
 xx = 1
+
+'''
+class ToolCreator(LangChainTypeCreator):
+    type_name: str = "tools"
+    tools_dict: Optional[Dict] = None
+
+    @property
+    def type_to_loader_dict(self) -> Dict:
+        if self.tools_dict is None:
+            all_tools = {}
+
+            for tool, tool_fcn in ALL_TOOLS_NAMES.items():
+                tool_params = get_tool_params(tool_fcn)
+
+                tool_name = tool_params.get("name") or tool
+
+                if tool_name in settings.tools or settings.dev:
+                    if tool_name == "JsonSpec":
+                        tool_params["path"] = tool_params.pop(
+                            "dict_")  # type: ignore
+                    all_tools[tool_name] = {
+                        "type": tool,
+                        "params": tool_params,
+                        "fcn": tool_fcn,
+                    }
+
+            self.tools_dict = all_tools
+
+        return self.tools_dict
+
+    def get_signature(self, name: str) -> Optional[Dict]:
+        """Get the signature of a tool."""
+
+        base_classes = ["Tool"]
+        fields = []
+        params = []
+        tool_params = {}
+
+        # Raise error if name is not in tools
+        if name not in self.type_to_loader_dict.keys():
+            raise ValueError("Tool not found")
+
+        tool_type: str = self.type_to_loader_dict[name]["type"]  # type: ignore
+
+        # if tool_type in _BASE_TOOLS.keys():
+        #     params = []
+        if tool_type in _LLM_TOOLS.keys():
+            params = ["llm"]
+        elif tool_type in _EXTRA_LLM_TOOLS.keys():
+            extra_keys = _EXTRA_LLM_TOOLS[tool_type][1]
+            params = ["llm"] + extra_keys
+        elif tool_type in _EXTRA_OPTIONAL_TOOLS.keys():
+            extra_keys = _EXTRA_OPTIONAL_TOOLS[tool_type][1]
+            params = extra_keys
+        # elif tool_type == "Tool":
+        #     params = ["name", "description", "func"]
+        elif tool_type in CUSTOM_TOOLS:
+            # Get custom tool params
+            params = self.type_to_loader_dict[name]["params"]  # type: ignore
+            base_classes = ["function"]
+            if node := customs.get_custom_nodes("tools").get(tool_type):
+                return node
+        elif tool_type in FILE_TOOLS:
+            params = self.type_to_loader_dict[name]["params"]  # type: ignore
+            base_classes += [name]
+        elif tool_type in OTHER_TOOLS:
+            tool_dict = build_template_from_class(tool_type, OTHER_TOOLS)
+            fields = tool_dict["template"]
+
+            # Pop unnecessary fields and add name
+            fields.pop("_type")  # type: ignore
+            fields.pop("return_direct")  # type: ignore
+            fields.pop("verbose")  # type: ignore
+
+            tool_params = {
+                "name": fields.pop("name")["value"],  # type: ignore
+                # type: ignore
+                "description": fields.pop("description")["value"],
+            }
+
+            fields = [
+                TemplateField(name=name, field_type=field["type"], **field)
+                for name, field in fields.items()  # type: ignore
+            ]
+            base_classes += tool_dict["base_classes"]
+
+        # Copy the field and add the name
+        for param in params:
+            field = TOOL_INPUTS.get(param, TOOL_INPUTS["str"]).copy()
+            field.name = param
+            field.advanced = False
+            if param == "aiosession":
+                field.show = False
+                field.required = False
+
+            fields.append(field)
+
+        template = Template(fields=fields, type_name=tool_type)
+
+        tool_params = {**tool_params, **
+                       self.type_to_loader_dict[name]["params"]}
+        return {
+            "template": util.format_dict(template.to_dict()),
+            **tool_params,
+            "base_classes": base_classes,
+        }
+
+    def to_list(self) -> List[str]:
+        """List all load tools"""
+
+        return list(self.type_to_loader_dict.keys())
+'''
