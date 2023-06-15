6022600d4c10c025b6800c4f2e272d1a37cab028
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-06-15 13:27:59 -0300

    Adding nowheel to scroll inside a node

--- src/frontend/src/components/dropdownComponent/index.tsx
+++ src/frontend/src/components/dropdownComponent/index.tsx
@@ -61,7 +61,7 @@ export default function Dropdown({
                   className={
                     editNode
                       ? "absolute z-10 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm w-[215px]"
-                      : "absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm "
+                      : "nowheel absolute z-10 mt-1 max-h-60 w-full overflow-auto overflow-y rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm "
                   }
                 >
                   {options.map((option, id) => (
