d264f9d8aafa9f1eaeedfb1ae497012269e7f5c7
anovazzi1 <otavio2204@gmail.com>
2023-06-16 17:16:36 -0300

    fix(chatComponent): add isBuilt dependency to useEffect hook to avoid stale closure and improve performance

--- src/frontend/src/components/chatComponent/index.tsx
+++ src/frontend/src/components/chatComponent/index.tsx
@@ -16,7 +16,7 @@ export default function Chat({ flow }: ChatType) {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (
         (event.key === "K" || event.key === "k") &&
-        (event.metaKey || event.ctrlKey)
+        (event.metaKey || event.ctrlKey) && isBuilt
       ) {
         event.preventDefault();
         setOpen((oldState) => !oldState);
@@ -26,7 +26,7 @@ export default function Chat({ flow }: ChatType) {
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [isBuilt]);
 
   useEffect(() => {
     // Define an async function within the useEffect hook
