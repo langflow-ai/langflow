b0773f07e92d630c80a21c3cc8b260df7bea35a5
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-20 15:13:44 -0300

    🔧 chore(base.py): change build method to async to allow for async operations 🔧 chore(types.py): change build method to async to allow for async operations The build method in both base.py and types.py files has been changed to async to allow for async operations to be performed during the build process. This change will allow for more flexibility in the build process and will enable the use of async/await syntax in the build process.

--- src/backend/langflow/graph/vertex/types.py
+++ src/backend/langflow/graph/vertex/types.py
@@ -49,12 +49,12 @@ class AgentVertex(LangChainVertex):
             elif isinstance(source_node, ChainVertex):
                 self.chains.append(source_node)
 
-    def build(self, force: bool = False) -> Any:
+    async def build(self, force: bool = False) -> Any:
         if not self._built or force:
             self._set_tools_and_chains()
             # First, build the tools
             for tool_node in self.tools:
-                tool_node.build()
+                await tool_node.build()
 
             # Next, build the chains and the rest
             for chain_node in self.chains:
@@ -77,72 +77,6 @@ class ToolVertex(LangChainVertex):
         super().__init__(data, base_type="tools")
 
 
-class PromptVertex(LangChainVertex):
-    def __init__(self, data: Dict):
-        super().__init__(data, base_type="prompts")
-
-    def build(
-        self,
-        force: bool = False,
-        tools: Optional[Union[List[Vertex], List[ToolVertex]]] = None,
-    ) -> Any:
-        if not self._built or force:
-            if (
-                "input_variables" not in self.params
-                or self.params["input_variables"] is None
-            ):
-                self.params["input_variables"] = []
-            # Check if it is a ZeroShotPrompt and needs a tool
-            if "ShotPrompt" in self.vertex_type:
-                tools = (
-                    [tool_node.build() for tool_node in tools]
-                    if tools is not None
-                    else []
-                )
-                self.params["tools"] = tools
-                prompt_params = [
-                    key
-                    for key, value in self.params.items()
-                    if isinstance(value, str) and key != "format_instructions"
-                ]
-            else:
-                prompt_params = ["template"]
-            for param in prompt_params:
-                prompt_text = self.params[param]
-                variables = extract_input_variables_from_prompt(prompt_text)
-                self.params["input_variables"].extend(variables)
-            self.params["input_variables"] = list(set(self.params["input_variables"]))
-
-            self._build()
-        return deepcopy(self._built_object)
-
-
-class ChainVertex(LangChainVertex):
-    can_be_root: bool = True
-
-    def __init__(self, data: Dict):
-        super().__init__(data, base_type="chains")
-
-    def build(
-        self,
-        force: bool = False,
-        tools: Optional[Union[List[Vertex], List[ToolVertex]]] = None,
-    ) -> Any:
-        if not self._built or force:
-            # Check if the chain requires a PromptVertex
-            for key, value in self.params.items():
-                if isinstance(value, PromptVertex):
-                    # Build the PromptVertex, passing the tools if available
-                    self.params[key] = value.build(tools=tools, force=force)
-
-            self._build()
-
-        #! Cannot deepcopy SQLDatabaseChain
-        if self.vertex_type in ["SQLDatabaseChain"]:
-            return self._built_object
-        return deepcopy(self._built_object)
-
-
 class LLMVertex(LangChainVertex):
     def __init__(self, data: Dict):
         super().__init__(data, base_type="llms")
@@ -162,7 +96,7 @@ class WrapperVertex(LangChainVertex):
     def __init__(self, data: Dict):
         super().__init__(data, base_type="wrappers")
 
-    def build(self, force: bool = False) -> Any:
+    async def build(self, force: bool = False) -> Any:
         if not self._built or force:
             if "headers" in self.params:
                 self.params["headers"] = eval(self.params["headers"])
@@ -218,7 +152,7 @@ class ChainVertex(Vertex):
     def __init__(self, data: Dict):
         super().__init__(data, base_type="chains")
 
-    def build(
+    async def build(
         self,
         force: bool = False,
         tools: Optional[List[Union[ToolkitVertex, ToolVertex]]] = None,
@@ -239,7 +173,7 @@ class PromptVertex(Vertex):
     def __init__(self, data: Dict):
         super().__init__(data, base_type="prompts")
 
-    def build(
+    async def build(
         self,
         force: bool = False,
         tools: Optional[List[Union[ToolkitVertex, ToolVertex]]] = None,
@@ -253,7 +187,7 @@ class PromptVertex(Vertex):
             # Check if it is a ZeroShotPrompt and needs a tool
             if "ShotPrompt" in self.vertex_type:
                 tools = (
-                    [tool_node.build() for tool_node in tools]
+                    [await tool_node.build() for tool_node in tools]
                     if tools is not None
                     else []
                 )
