07abd63afb80363fa8fee2cccf1e50cdd3a6762f
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-06-22 20:08:03 -0300

    🎨 style(inputFileComponent): add loading spinner to input file component to indicate file upload progress

---
+++ src/frontend/src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,9 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { RadialProgressType } from "../../types/components";
+
+export default function LoadingSpinner({}) {
+
+  return (
+    <></>
+  );
+}
