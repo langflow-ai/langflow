c6b823da49f91e5419ff7674f961641e9bae59db
Rodrigo Nader <rodrigosilvanader@gmail.com>
2023-06-22 02:15:07 -0300

    Change installation

--- docs/.docusaurus/routes.js
+++ docs/.docusaurus/routes.js
@@ -4,47 +4,47 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
   {
     path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '437'),
+    component: ComponentCreator('/__docusaurus/debug', 'd49'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', 'f3c'),
+    component: ComponentCreator('/__docusaurus/debug/config', 'c5a'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', '5ed'),
+    component: ComponentCreator('/__docusaurus/debug/content', 'c19'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', '6e2'),
+    component: ComponentCreator('/__docusaurus/debug/globalData', '338'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '00b'),
+    component: ComponentCreator('/__docusaurus/debug/metadata', '39f'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '46d'),
+    component: ComponentCreator('/__docusaurus/debug/registry', 'e70'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', 'ce2'),
+    component: ComponentCreator('/__docusaurus/debug/routes', 'b1f'),
     exact: true
   },
   {
     path: '/markdown-page',
-    component: ComponentCreator('/markdown-page', 'd68'),
+    component: ComponentCreator('/markdown-page', 'b57'),
     exact: true
   },
   {
     path: '/',
-    component: ComponentCreator('/', '6c4'),
+    component: ComponentCreator('/', '5b5'),
     routes: [
       {
         path: '/',
