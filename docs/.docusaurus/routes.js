c3265e79c1da24f2b7e86ce4cf2f4349b6672221
carlosrcoelho <carlosrodrigo.coelho@gmail.com>
2023-06-26 19:17:12 -0300

    organized folders, added components tab, adjusted mendable style.

--- docs/.docusaurus/routes.js
+++ docs/.docusaurus/routes.js
@@ -3,13 +3,48 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '437'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', 'f3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '5ed'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', '6e2'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '00b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '46d'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', 'ce2'),
+    exact: true
+  },
+  {
     path: '/markdown-page',
     component: ComponentCreator('/markdown-page', 'd68'),
     exact: true
   },
   {
     path: '/',
-    component: ComponentCreator('/', '6c4'),
+    component: ComponentCreator('/', '73a'),
     routes: [
       {
         path: '/',
@@ -18,116 +53,194 @@ export default [
         sidebar: "docs"
       },
       {
-        path: '/buffer-memory',
-        component: ComponentCreator('/buffer-memory', '859'),
+        path: '/components/agents',
+        component: ComponentCreator('/components/agents', '843'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/collection',
-        component: ComponentCreator('/collection', '373'),
+        path: '/components/chains',
+        component: ComponentCreator('/components/chains', '7ac'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/community',
-        component: ComponentCreator('/community', '80d'),
+        path: '/components/embeddings',
+        component: ComponentCreator('/components/embeddings', '9c7'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/components',
-        component: ComponentCreator('/components', '997'),
+        path: '/components/llms',
+        component: ComponentCreator('/components/llms', 'ae5'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/conversation-chain',
-        component: ComponentCreator('/conversation-chain', 'cb6'),
+        path: '/components/loaders',
+        component: ComponentCreator('/components/loaders', '80c'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/creating-flows',
-        component: ComponentCreator('/creating-flows', '058'),
+        path: '/components/memories',
+        component: ComponentCreator('/components/memories', '723'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/csv-loader',
-        component: ComponentCreator('/csv-loader', 'bb7'),
+        path: '/components/prompts',
+        component: ComponentCreator('/components/prompts', '268'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/features',
-        component: ComponentCreator('/features', 'ef0'),
+        path: '/components/text-splitters',
+        component: ComponentCreator('/components/text-splitters', '7f3'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/gcp-deployment',
-        component: ComponentCreator('/gcp-deployment', '1f3'),
+        path: '/components/toolkits',
+        component: ComponentCreator('/components/toolkits', 'b6c'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/github-issues',
-        component: ComponentCreator('/github-issues', '385'),
+        path: '/components/tools',
+        component: ComponentCreator('/components/tools', '960'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/how-contribute',
-        component: ComponentCreator('/how-contribute', '5ea'),
+        path: '/components/utilities',
+        component: ComponentCreator('/components/utilities', '24a'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/how-upload-examples',
-        component: ComponentCreator('/how-upload-examples', '143'),
+        path: '/components/vector-stores',
+        component: ComponentCreator('/components/vector-stores', 'bca'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/hugging-face-spaces',
-        component: ComponentCreator('/hugging-face-spaces', 'dd3'),
+        path: '/components/wrappers',
+        component: ComponentCreator('/components/wrappers', 'f16'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/installation',
-        component: ComponentCreator('/installation', 'd10'),
+        path: '/contributing/community',
+        component: ComponentCreator('/contributing/community', 'a0d'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/jina-deployment',
-        component: ComponentCreator('/jina-deployment', '370'),
+        path: '/contributing/github-issues',
+        component: ComponentCreator('/contributing/github-issues', 'bba'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/midjourney-prompt-chain',
-        component: ComponentCreator('/midjourney-prompt-chain', 'ef0'),
+        path: '/contributing/how-contribute',
+        component: ComponentCreator('/contributing/how-contribute', '3f1'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/multiple-vectorstores',
-        component: ComponentCreator('/multiple-vectorstores', '293'),
+        path: '/deployment/gcp-deployment',
+        component: ComponentCreator('/deployment/gcp-deployment', '4f2'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/python-function',
-        component: ComponentCreator('/python-function', 'c91'),
+        path: '/deployment/jina-deployment',
+        component: ComponentCreator('/deployment/jina-deployment', 'def'),
         exact: true,
         sidebar: "docs"
       },
       {
-        path: '/serp-api-tool',
-        component: ComponentCreator('/serp-api-tool', '558'),
+        path: '/examples/buffer-memory',
+        component: ComponentCreator('/examples/buffer-memory', 'b72'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/conversation-chain',
+        component: ComponentCreator('/examples/conversation-chain', '0ce'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/csv-loader',
+        component: ComponentCreator('/examples/csv-loader', '6f7'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/how-upload-examples',
+        component: ComponentCreator('/examples/how-upload-examples', '274'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/midjourney-prompt-chain',
+        component: ComponentCreator('/examples/midjourney-prompt-chain', 'a73'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/multiple-vectorstores',
+        component: ComponentCreator('/examples/multiple-vectorstores', '7b0'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/python-function',
+        component: ComponentCreator('/examples/python-function', '1d9'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/examples/serp-api-tool',
+        component: ComponentCreator('/examples/serp-api-tool', '9d4'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/getting-started/creating-flows',
+        component: ComponentCreator('/getting-started/creating-flows', '362'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/getting-started/hugging-face-spaces',
+        component: ComponentCreator('/getting-started/hugging-face-spaces', 'a7a'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/getting-started/installation',
+        component: ComponentCreator('/getting-started/installation', 'd04'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/guidelines/collection',
+        component: ComponentCreator('/guidelines/collection', '0cc'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/guidelines/components',
+        component: ComponentCreator('/guidelines/components', 'df5'),
+        exact: true,
+        sidebar: "docs"
+      },
+      {
+        path: '/guidelines/features',
+        component: ComponentCreator('/guidelines/features', '648'),
         exact: true,
         sidebar: "docs"
       }
--- docs/src/theme/Footer.js
+++ docs/src/theme/Footer.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { MendableFloatingButton } from '@mendable/search';
 
 export default function FooterWrapper(props) {
-  const {
-    siteConfig: { customFields },
-  } = useDocusaurusContext();
-
   const iconSpan1 = React.createElement('img', {
-    src: 'img/chain.png',
+    src: '/img/chain.png',
     style: { width: '40px' }
   }, null);
 
   const icon = React.createElement('div', {
     style: {
-      color: '#ffffff',
+      color: '#000000',
       fontSize: '22px',
       width: '48px',
       height: '48px',
@@ -31,12 +26,8 @@ export default function FooterWrapper(props) {
   const mendableFloatingButton = React.createElement(
     MendableFloatingButton,
     {
-      style: { darkMode: true, accentColor: '#4051b5' },
-      floatingButtonStyle: { color: '#ffffff', backgroundColor: '#010810' },
-      anon_key: customFields.mendableAnonKey, // Mendable Search Public ANON key, ok to be public
-      messageSettings: {
-        openSourcesInNewTab: false,
-      },
+      floatingButtonStyle: { color: '#000000', backgroundColor: '#f6f6f6' },
+      anon_key: 'b7f52734-297c-41dc-8737-edbd13196394', // Mendable Search Public ANON key, ok to be public
       showSimpleSearch: true,
       icon: icon,
     }
