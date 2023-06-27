f3abe695913898806de7e3c8171d0c58944678d2
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-06-27 17:55:32 -0300

    🎉 feat(frontend): add Accordion component to improve UI/UX 🔧 chore(frontend): add keyframes and animations for Accordion component to tailwind.config.js

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -15,6 +15,20 @@ module.exports = {
       },
     },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
