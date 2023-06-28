15e1efc0a126629e0dc9766f11f0757beae08dc0
Rodrigo Nader <rodrigosilvanader@gmail.com>
2023-06-28 11:35:26 -0300

    feat: Add new stylesheet for Switch component
    Imported a new stylesheet for the Switch component to enhance its visual appearance.

--- src/frontend/src/components/ui/switch.tsx
+++ src/frontend/src/components/ui/switch.tsx
@@ -4,8 +4,6 @@ import * as React from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
 import { cn } from "../../utils";
 
-import "./SwitchStyles.css"; // Import the new stylesheet
-
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
