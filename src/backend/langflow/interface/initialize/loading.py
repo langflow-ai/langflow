270d5fb1066fc643bea6d8808352eca89c47c0cb
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-06-30 11:17:32 -0300

    🐛 fix(loading.py): add missing condition to check if variable is an instance of BaseOutputParser and has "get_format_instructions" attribute The code was missing a condition to check if the variable is an instance of BaseOutputParser and has the "get_format_instructions" attribute. This condition is necessary to properly handle the variable and avoid potential errors.

--- src/backend/langflow/interface/initialize/loading.py
+++ src/backend/langflow/interface/initialize/loading.py
@@ -149,6 +149,14 @@ def instantiate_prompt(node_type, class_object, params):
                 )
                 # handle_keys will be a list but it does not exist yet
                 # so we need to create it
+
+            if (
+                isinstance(variable, List)
+                and all(isinstance(item, Document) for item in variable)
+            ) or (
+                isinstance(variable, BaseOutputParser)
+                and hasattr(variable, "get_format_instructions")
+            ):
                 if "handle_keys" not in params:
                     format_kwargs["handle_keys"] = []
 
