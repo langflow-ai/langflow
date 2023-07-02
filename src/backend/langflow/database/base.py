9f2b6443cf97e3a3ddfab22477d3d7d1cc2f650f
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-07-02 16:11:51 -0300

    debugging improvements

--- src/backend/langflow/database/base.py
+++ src/backend/langflow/database/base.py
@@ -6,6 +6,7 @@ if settings.database_url.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 else:
     connect_args = {}
+
 engine = create_engine(settings.database_url, connect_args=connect_args)
 
 
@@ -13,6 +14,15 @@ def create_db_and_tables():
     logger.debug("Creating database and tables")
     SQLModel.metadata.create_all(engine)
     logger.debug("Database and tables created")
+    # Now check if the table Flow exists, if not, something went wrong
+    # and we need to create the tables again.
+    from sqlalchemy import inspect
+
+    inspector = inspect(engine)
+    if "flow" not in inspector.get_table_names():
+        logger.error("Something went wrong creating the database and tables.")
+        logger.error("Please check your database settings.")
+        raise RuntimeError("Something went wrong creating the database and tables.")
 
 
 def get_session():
