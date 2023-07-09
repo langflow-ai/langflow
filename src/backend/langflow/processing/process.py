008a1c40791310105c653e2e479afc9f1da6f412
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-07-09 18:02:12 -0300

    🐛 fix(types.py): replace eval() with ast.literal_eval() to safely evaluate headers parameter as dictionary-like string 🐛 fix(process.py): rename input parameter to flow for clarity and to avoid shadowing built-in function

--- src/backend/langflow/processing/process.py
+++ src/backend/langflow/processing/process.py
@@ -123,23 +123,23 @@ def process_graph_cached(data_graph: Dict[str, Any], inputs: Optional[dict] = No
 
 
 def load_flow_from_json(
-    input: Union[Path, str, dict], tweaks: Optional[dict] = None, build=True
+    flow: Union[Path, str, dict], tweaks: Optional[dict] = None, build=True
 ):
     """
     Load flow from a JSON file or a JSON object.
 
-    :param input: JSON file path or JSON object
+    :param flow: JSON file path or JSON object
     :param tweaks: Optional tweaks to be processed
     :param build: If True, build the graph, otherwise return the graph object
     :return: Langchain object or Graph object depending on the build parameter
     """
     # If input is a file path, load JSON from the file
-    if isinstance(input, (str, Path)):
-        with open(input, "r", encoding="utf-8") as f:
+    if isinstance(flow, (str, Path)):
+        with open(flow, "r", encoding="utf-8") as f:
             flow_graph = json.load(f)
     # If input is a dictionary, assume it's a JSON object
-    elif isinstance(input, dict):
-        flow_graph = input
+    elif isinstance(flow, dict):
+        flow_graph = flow
     else:
         raise TypeError(
             "Input must be either a file path (str) or a JSON object (dict)"
