750ddd4f5493c62def73229be6d8696d950966b0
gustavoschaedler <gustavopoa@gmail.com>
2023-07-12 15:10:37 +0100

    🐛 fix(components.py): change variable name from `component` to `component_model` in create_component and update_component functions for better readability ✨ feat(components.py): add support for `create_at` and `update_at` fields in Component model to track creation and update timestamps 🔥 refactor(components.py): remove unused imports and variables in components.py 🔥 refactor(component.py): remove unused imports and variables in component.py 🔥 refactor(component.py): remove commented out code in component.py

--- src/backend/langflow/api/v1/components.py
+++ src/backend/langflow/api/v1/components.py
@@ -1,30 +1,35 @@
 from typing import List
 from uuid import UUID
-from langflow.database.models.component import Component
+from langflow.database.models.component import Component, ComponentModel
 from langflow.database.base import get_session
 from sqlmodel import Session, select
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
+from datetime import datetime
 
 
 COMPONENT_NOT_FOUND = "Component not found"
+COMPONENT_ALREADY_EXISTS = "A component with the same id already exists."
+COMPONENT_DELETED = "Component deleted"
+
 
 router = APIRouter(prefix="/components", tags=["Components"])
 
 
 @router.post("/", response_model=Component)
-def create_component(component: Component, db: Session = Depends(get_session)):
+def create_component(component: ComponentModel, db: Session = Depends(get_session)):
+    db_component = Component(**component.dict())
     try:
-        db.add(component)
+        db.add(db_component)
         db.commit()
-        db.refresh(component)
+        db.refresh(db_component)
     except IntegrityError as e:
         db.rollback()
         raise HTTPException(
             status_code=400,
-            detail="A component with the same id already exists.",
+            detail=COMPONENT_ALREADY_EXISTS,
         ) from e
-    return component
+    return db_component
 
 
 @router.get("/{component_id}", response_model=Component)
@@ -37,19 +42,25 @@ def read_component(component_id: UUID, db: Session = Depends(get_session)):
 
 @router.get("/", response_model=List[Component])
 def read_components(skip: int = 0, limit: int = 50, db: Session = Depends(get_session)):
-    return db.execute(select(Component).offset(skip).limit(limit)).fetchall()
+    query = select(Component)
+    query = query.offset(skip).limit(limit)
+
+    return db.execute(query).fetchall()
 
 
 @router.patch("/{component_id}", response_model=Component)
 def update_component(
-    component_id: UUID, component: Component, db: Session = Depends(get_session)
+    component_id: UUID, component: ComponentModel, db: Session = Depends(get_session)
 ):
     db_component = db.get(Component, component_id)
     if not db_component:
         raise HTTPException(status_code=404, detail=COMPONENT_NOT_FOUND)
     component_data = component.dict(exclude_unset=True)
+
     for key, value in component_data.items():
         setattr(db_component, key, value)
+
+    db_component.update_at = datetime.utcnow()
     db.commit()
     db.refresh(db_component)
     return db_component
@@ -62,69 +73,4 @@ def delete_component(component_id: UUID, db: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail=COMPONENT_NOT_FOUND)
     db.delete(component)
     db.commit()
-    return {"detail": "Component deleted"}
-
-
-# @router.post("/", response_model=ComponentRead, status_code=201)
-# def create(*, session: Session = Depends(get_session), component: ComponentCreate):
-#     db = Component.from_orm(component)
-#     session.add(db)
-#     session.commit()
-#     session.refresh(db)
-
-#     return db
-
-
-# @router.get("/", response_model=list[ComponentRead], status_code=200)
-# def read_all(*, session: Session = Depends(get_session)):
-#     try:
-#         sql = select(Component)
-#         components = session.exec(sql).all()
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e)) from e
-
-#     return [jsonable_encoder(component) for component in components]
-
-
-# @router.get("/{id}", response_model=ComponentRead, status_code=200)
-# def read(*, session: Session = Depends(get_session), id: UUID):
-#     if component := session.get(Component, id):
-#         return component
-#     else:
-#         raise HTTPException(status_code=404, detail=COMPONENT_NOT_FOUND)
-
-
-# @router.patch("/{id}", response_model=ComponentRead, status_code=200)
-# def update(
-#     *, session: Session = Depends(get_session), id: UUID, component: ComponentUpdate
-# ):
-#     db = session.get(Component, id)
-#     if not db:
-#         raise HTTPException(status_code=404, detail=COMPONENT_NOT_FOUND)
-
-#     data = component.dict(exclude_unset=True)
-
-#     if settings.remove_api_keys:
-#         data = remove_api_keys(data)
-
-#     for key, value in data.items():
-#         setattr(db, key, value)
-
-#     session.add(db)
-#     session.commit()
-#     session.refresh(db)
-
-#     return db
-
-
-# @router.delete("/{id}", status_code=200)
-# def delete(*, session: Session = Depends(get_session), id: UUID):
-#     component = session.get(Component, id)
-
-#     if not component:
-#         raise HTTPException(status_code=404, detail=COMPONENT_NOT_FOUND)
-
-#     session.delete(component)
-#     session.commit()
-
-#     return {"message": "Component deleted successfully"}
+    return {"detail": COMPONENT_DELETED}
