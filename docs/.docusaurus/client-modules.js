5a778b94f3293d51eac1aefb29ba534e2783110f
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-07-12 18:20:12 -0300

    removing build files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/home/carlos/repo/logspace/langflow/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/carlos/repo/logspace/langflow/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/carlos/repo/logspace/langflow/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/carlos/repo/logspace/langflow/docs/src/css/custom.css'),
-  require('/home/carlos/repo/logspace/langflow/docs/node_modules/docusaurus-plugin-image-zoom/lib/zoom'),
-];
