996b97c74d055b2f52c55cfced936eb8cb03d8e1
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-07-12 17:18:49 -0300

    Revert "Merge branch 'dev' into gh-pages"
    This reverts commit 30e868751a163f1a098d6891470adf4182181e4f, reversing
    changes made to 3b41e189d3e603dcc68ae14d9431cbfaa945e9bb.

--- assets/js/3a824148.aa724eac.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[721],{5745:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
