207e76a09d33c84dc202b700aca08cc296afdf4f
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-07-16 16:22:01 -0300

    Fixed dropdown component on editNode

--- src/frontend/src/components/dropdownComponent/index.tsx
+++ src/frontend/src/components/dropdownComponent/index.tsx
@@ -1,6 +1,6 @@
 import { Listbox, Transition } from "@headlessui/react";
 import { Check, ChevronsUpDown } from "lucide-react";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { DropDownComponentType } from "../../types/components";
 import { classNames } from "../../utils";
 
@@ -15,6 +15,10 @@ export default function Dropdown({
   let [internalValue, setInternalValue] = useState(
     value === "" || !value ? "Choose an option" : value
   );
+
+  useEffect(() => {
+    setInternalValue(value === "" || !value ? "Choose an option" : value);
+  }, [value]);
 
   return (
     <>
