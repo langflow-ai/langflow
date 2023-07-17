56eb70674a6d07cc89fd9bbdf4c78ee58a82ea42
Igor Carvalho <igorsilvabhz6@gmail.com>
2023-07-17 19:47:15 -0300

    refactor[genericIconComponent]: Remove unnecessary svg method function

--- src/frontend/src/components/genericIconComponent/index.tsx
+++ src/frontend/src/components/genericIconComponent/index.tsx
@@ -1,14 +1,5 @@
-import { IconComponentProps, IconProps } from "../../types/components";
+import { IconComponentProps } from "../../types/components";
 import { nodeIconsLucide } from "../../utils";
-
-export function IconFromLucide({
-  name,
-  style,
-  iconColor,
-}: IconProps): JSX.Element {
-  const TargetIcon = nodeIconsLucide[name] ?? nodeIconsLucide["unknown"];
-  return <TargetIcon className={style} style={{ color: iconColor }} />;
-}
 
 export default function IconComponent({
   name,
