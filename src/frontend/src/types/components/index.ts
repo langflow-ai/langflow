c8e7ab925785db1cb4b77ae4cf49a51a184211c6
Igor Carvalho <igorsilvabhz6@gmail.com>
2023-07-20 16:02:09 -0300

    refactor[utils.ts]: Add type to functions that didnt have it

--- src/frontend/src/types/components/index.ts
+++ src/frontend/src/types/components/index.ts
@@ -203,3 +203,215 @@ export type HeaderProps = { children: ReactNode; description: string };
 export interface languageMap {
   [key: string]: string | undefined;
 }
+
+export type groupedObjType = {
+  component: string;
+  family: string;
+  type: string;
+};
+
+export type dataObjType = {
+  base_classes: string[];
+  custom_fields: object;
+  description: string;
+  display_name: string;
+  documentation: string;
+  output_types: Array<void | string>;
+  template: object;
+};
+
+export type documentloadersType = {
+  AZLyricsLoader: dataObjType;
+  AirbyteJSONLoader: dataObjType;
+  BSHTMLLoader: dataObjType;
+  CSVLoader: dataObjType;
+  CoNLLULoader: dataObjType;
+  CollegeConfidentialLoader: dataObjType; 
+  DirectoryLoader: dataObjType;
+  EverNoteLoader: dataObjType;
+  FacebookChatLoader: dataObjType;
+  GitLoader: dataObjType;
+  GitbookLoader: dataObjType;
+  GutenbergLoader: dataObjType;
+  HNLoader: dataObjType;
+  IFixitLoader: dataObjType;
+  IMSDbLoader: dataObjType;
+  NotionDirectoryLoader: dataObjType;
+  PyPDFDirectoryLoader: dataObjType;
+  PyPDFLoader: dataObjType;
+  ReadTheDocsLoader: dataObjType;
+  SRTLoader: dataObjType;
+  SlackDirectoryLoader: dataObjType;
+  TextLoader: dataObjType;
+  UnstructuredEmailLoader: dataObjType;
+  UnstructuredHTMLLoader: dataObjType;
+  UnstructuredMarkdownLoader: dataObjType;
+  UnstructuredPowerPointLoader: dataObjType;
+  UnstructuredWordDocumentLoader: dataObjType;
+  WebBaseLoader: dataObjType;
+};
+
+export type agentsType = {
+  AgentInitializer: dataObjType;
+  CSVAgent: dataObjType;
+  JsonAgent: dataObjType;
+  SQLAgent: dataObjType;
+  VectorStoreAgent: dataObjType;
+  VectorStoreRouterAgent: dataObjType;
+  ZeroShotAgent: dataObjType;
+};
+
+export type chainsType = {
+  CombineDocsChain: dataObjType;
+  ConversationChain: dataObjType;
+  ConversationalRetrievalChain: dataObjType;
+  LLMChain: dataObjType;
+  LLMCheckerChain: dataObjType;
+  LLMMathChain: dataObjType;
+  MidJourneyPromptChain: dataObjType;
+  RetrievalQA: dataObjType
+  RetrievalQAWithSourcesChain: dataObjType;
+  SQLDatabaseChain: dataObjType;
+  SeriesCharacterChain: dataObjType;
+  TimeTravelGuideChain: dataObjType;
+}
+
+export type embeddingsType = {
+  CohereEmbeddings: dataObjType;
+  HuggingFaceEmbeddings: dataObjType;
+  OpenAIEmbeddings: dataObjType;
+};
+
+export type llmsTypes = {
+  Anthropic: dataObjType;
+  CTransformers: dataObjType;
+  ChatAnthropic: dataObjType;
+  ChatOpenAI: dataObjType;
+  Cohere: dataObjType;
+  HuggingFaceHub: dataObjType;
+  LlamaCpp: dataObjType;
+  OpenAI: dataObjType;
+  VertexAI: dataObjType;
+}
+
+export type memoriesType = {
+  ConversationBufferMemory: dataObjType;
+  ConversationBufferWindowMemory: dataObjType;
+  ConversationEntityMemory: dataObjType;
+  ConversationKGMemory: dataObjType;
+  ConversationSummaryMemory: dataObjType;
+  MongoDBChatMessageHistory: dataObjType;
+  PostgresChatMessageHistory: dataObjType;
+  VectorStoreRetrieverMemory: dataObjType;
+};
+
+export type outputParsersType = {
+  ResponseSchema: dataObjType;
+  StructuredOutputParser: dataObjType;
+};
+
+export type promptsType = {
+  ChatMessagePromptTemplate: dataObjType;
+  ChatPromptTemplate: dataObjType;
+  HumanMessagePromptTemplate: dataObjType;
+  PromptTemplate: dataObjType;
+  SystemMessagePromptTemplate: dataObjType;
+}
+
+export type retrieversType = {
+  MultiQueryRetriever: dataObjType;
+};
+
+export type textSplittersType = {
+  CharacterTextSplitter: dataObjType;
+  RecursiveCharacterTextSplitter: dataObjType;
+};
+
+export type toolkitsType = {
+  JsonToolkit: dataObjType;
+  OpenAPIToolkit: dataObjType;
+  VectorStoreInfo: dataObjType;
+  VectorStoreRouterToolkit: dataObjType;
+  VectorStoreToolkit: dataObjType;
+};
+
+export type toolsType = {
+  BingSearchRun: dataObjType;
+  Calculator: dataObjType;
+  GoogleSearchResults: dataObjType;
+  GoogleSearchRun: dataObjType;
+  GoogleSerperRun: dataObjType;
+  InfoSQLDatabaseTool: dataObjType;
+  JsonGetValueTool: dataObjType;
+  JsonListKeysTool: dataObjType;
+  JsonSpec: dataObjType;
+  ListSQLDatabaseTool: dataObjType;
+  "News API": dataObjType;
+  'PAL-MATH': dataObjType;
+  "Podcast API": dataObjType;
+  PythonAstREPLTool: dataObjType;
+  PythonFunction: dataObjType;
+  PythonFunctionTool: dataObjType;
+  PythonREPLTool: dataObjType;
+  QuerySQLDataBaseTool: dataObjType;
+  RequestsDeleteTool: dataObjType;
+  RequestsGetTool: dataObjType;
+  RequestsPatchTool: dataObjType;
+  RequestsPostTool: dataObjType;
+  RequestsPutTool: dataObjType;
+  Search: dataObjType;
+  'TMDB API': dataObjType;
+  Tool: dataObjType;
+  WikipediaQueryRun: dataObjType;
+  WolframAlphaQueryRun: dataObjType;
+};
+
+export type utilitiesType = {
+  BingSearchAPIWrapper: dataObjType;
+  GoogleSearchAPIWrapper: dataObjType;
+  GoogleSerperAPIWrapper: dataObjType;
+  SearxSearchWrapper: dataObjType;
+  SerpAPIWrapper: dataObjType;
+  WikipediaAPIWrapper: dataObjType;
+  WolframAlphaAPIWrapper: dataObjType;
+};
+
+export type vectorStoresType = {
+  Chroma: dataObjType;
+  FAISS: dataObjType;
+  MongoDBAtlasVectorSearch: dataObjType;
+  Pinecone: dataObjType;
+  Qdrant: dataObjType;
+  SupabaseVectorStore: dataObjType;
+  Weaviate: dataObjType;
+};
+
+export type wrappersType = {
+  SQLDatabase: dataObjType;
+  TextRequestsWrapper: dataObjType; 
+};
+
+export type dataType = {
+  agents?: agentsType;
+  chains?: chainsType;
+  documentloaders?: documentloadersType;
+  embeddings?: embeddingsType;
+  llms?: llmsTypes;
+  memories?: memoriesType;
+  output_parsers?: outputParsersType;
+  prompts?: promptsType;
+  retrievers?: retrieversType;
+  textsplitters?: textSplittersType;
+  toolkits?: toolkitsType;
+  tools?: toolsType
+  utilities?: utilitiesType;
+  vectorstores?: vectorStoresType;
+  wrappers?: wrappersType;
+};
+
+export type tweakType = {
+  "ChatOpenAI-TxuiN": object;
+  "LLMChain-zPC3w": object;
+  "PromptTemplate-iNj5W": object;
+  "ConversationBufferMemory-JnodM": object;
+}
