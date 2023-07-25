1ed210c915ad9cf78a4039823a3c452499ed341e
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-07-25 09:53:05 -0300

    Formatting fixes

--- src/frontend/src/components/ui/card.tsx
+++ src/frontend/src/components/ui/card.tsx
@@ -77,9 +77,9 @@ CardFooter.displayName = "CardFooter";
 
 export {
   Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
   CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 };
