7c5d1b3232938962b9bfa2355d484ef5adb4ece4
anovazzi1 <otavio2204@gmail.com>
2023-07-27 09:46:29 -0300

    fix(codeAreaModal): clear error state when setting value to code and closing modal to prevent displaying previous errors

--- src/frontend/src/modals/codeAreaModal/index.tsx
+++ src/frontend/src/modals/codeAreaModal/index.tsx
@@ -96,6 +96,7 @@ export default function CodeAreaModal({
         if (data) {
           setNodeClass(data);
           setValue(code);
+          setError({ detail: { error: undefined, traceback: undefined } });
           setOpen(false);
         }
       })
