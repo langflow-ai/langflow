f268f29ca658cdbe8f58a25ee1ab4d6d16d2628e
anovazzi1 <otavio2204@gmail.com>
2023-07-28 14:58:05 -0300

    fix(extraSidebarComponent): add useEffect hook to show components with errors on load fix(extraSidebarComponent): update key prop in ShadTooltip component to use unique value fix(extraSidebarComponent): update draggable attribute in component div to be disabled if there is an error feat(api): add error property to APIClassType to indicate if there is an error in the component

--- src/frontend/src/types/api/index.ts
+++ src/frontend/src/types/api/index.ts
@@ -16,6 +16,7 @@ export type APIClassType = {
   output_types?: Array<string>;
   beta?: boolean;
   documentation: string;
+  error?: string;
   [key: string]: Array<string> | string | APITemplateType | boolean;
 };
 
