11854ccdb36ecb0e0aa6eaa53fd68f39b875c0d5
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-01 17:44:02 -0300

    🔧 chore(constants.py): update default custom component code to improve readability and remove unnecessary imports ✨ feat(constants.py): update display name and description of the custom component to improve clarity and user experience 🔧 chore(custom_components.py): update display name of the custom component to improve clarity and user experience

--- src/backend/langflow/template/frontend_node/custom_components.py
+++ src/backend/langflow/template/frontend_node/custom_components.py
@@ -6,7 +6,7 @@ from langflow.interface.custom.constants import DEFAULT_CUSTOM_COMPONENT_CODE
 
 class CustomComponentFrontendNode(FrontendNode):
     name: str = "CustomComponent"
-    display_name: str = "Your Component"
+    display_name: str = "Custom Component"
     beta: bool = True
     template: Template = Template(
         type_name="CustomComponent",
