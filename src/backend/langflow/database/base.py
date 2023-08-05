427f0dfd30e75e4cd092c92b5696dd0b30a91526
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-05 18:33:17 -0300

    📦 chore(alembic): add migration script to create Flow table and FlowStyle table 🔧 refactor(base.py): refactor DatabaseManager class to handle database operations and migrations 🔧 refactor(base.py): refactor session_getter function to use DatabaseManager instance 🔧 refactor(base.py): refactor get_session function to use DatabaseManager instance 🔧 refactor(models/__init__.py): add Flow model to __all__ list

--- src/backend/langflow/database/base.py
+++ src/backend/langflow/database/base.py
@@ -1,8 +1,11 @@
 from contextlib import contextmanager
 import os
-
+from pathlib import Path
+from langflow.database import models  # noqa
 from sqlmodel import SQLModel, Session, create_engine
 from langflow.utils.logger import logger
+from alembic.config import Config
+from alembic import command
 
 
 class Engine:
@@ -60,10 +63,67 @@ def create_db_and_tables():
         logger.debug("Database and tables created successfully")
 
 
+class DatabaseManager:
+    def __init__(self, database_url: str):
+        self.database_url = database_url
+        # This file is in langflow.database.base.py
+        # the ini is in langflow
+        self.script_location = Path(__file__).parent.parent / "alembic"
+        self.alembic_cfg_path = Path(__file__).parent.parent / "alembic.ini"
+        self.engine = create_engine(database_url)
+
+    def __enter__(self):
+        self._session = Session(self.engine)
+        return self._session
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:  # If an exception has been raised
+            logger.error(
+                f"Session rollback because of exception: {exc_type.__name__} {exc_value}"
+            )
+            self._session.rollback()
+        else:
+            self._session.commit()
+        self._session.close()
+
+    def get_session(self):
+        with Session(self.engine) as session:
+            yield session
+
+    def run_migrations(self):
+        logger.info(
+            f"Running DB migrations in {self.script_location} on {self.database_url}"
+        )
+        alembic_cfg = Config()
+        alembic_cfg.set_main_option("script_location", str(self.script_location))
+        alembic_cfg.set_main_option("sqlalchemy.url", self.database_url)
+        command.upgrade(alembic_cfg, "head")
+
+    def create_db_and_tables(self):
+        logger.debug("Creating database and tables")
+        try:
+            SQLModel.metadata.create_all(self.engine)
+        except Exception as exc:
+            logger.error(f"Error creating database and tables: {exc}")
+            raise RuntimeError("Error creating database and tables") from exc
+
+        # Now check if the table "flow" exists, if not, something went wrong
+        # and we need to create the tables again.
+        from sqlalchemy import inspect
+
+        inspector = inspect(self.engine)
+        if "flow" not in inspector.get_table_names():
+            logger.error("Something went wrong creating the database and tables.")
+            logger.error("Please check your database settings.")
+            raise RuntimeError("Something went wrong creating the database and tables.")
+        else:
+            logger.debug("Database and tables created successfully")
+
+
 @contextmanager
-def session_getter():
+def session_getter(db_manager: DatabaseManager):
     try:
-        session = Session(Engine.get())
+        session = Session(DatabaseManager.engine)
         yield session
     except Exception as e:
         print("Session rollback because of exception:", e)
@@ -71,8 +131,3 @@ def session_getter():
         raise
     finally:
         session.close()
-
-
-def get_session():
-    with session_getter() as session:
-        yield session
