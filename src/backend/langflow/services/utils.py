056304ce9126e29344e7a2bc814f848e30cd8960
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-05 23:18:31 -0300

    🎉 feat(utils.py): add new utility function `get_settings_manager()` to retrieve the settings manager from the service manager 🎉 feat(utils.py): add new utility function `get_session()` to retrieve a session from the database manager

---
+++ src/backend/langflow/services/utils.py
@@ -0,0 +1,14 @@
+from langflow.services import ServiceType, service_manager
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from langflow.services.settings.manager import SettingsManager
+
+
+def get_settings_manager() -> "SettingsManager":
+    return service_manager.get(ServiceType.SETTINGS_MANAGER)
+
+
+def get_session():
+    db_manager = service_manager.get(ServiceType.DATABASE_MANAGER)
+    yield from db_manager.get_session()
