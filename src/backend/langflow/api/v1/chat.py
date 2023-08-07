239811dbff9c31616922b860a9d1ff222f1c51d0
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-07 18:46:13 -0300

    🐛 fix(chat.py): change "input_keys" value from an empty dictionary to None to improve readability and reduce redundancy

--- src/backend/langflow/api/v1/chat.py
+++ src/backend/langflow/api/v1/chat.py
@@ -155,7 +155,7 @@ async def stream_build(flow_id: str):
                 )
             else:
                 input_keys_response = {
-                    "input_keys": {},
+                    "input_keys": None,
                     "memory_keys": [],
                     "handle_keys": [],
                 }
