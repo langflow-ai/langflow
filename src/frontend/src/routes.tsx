c1d1684c73c2ec14bdb15136c19b09f54fc64986
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-08-07 11:40:42 -0300

    ✨ feat(routes.tsx): add new route for LoginPage component at /login path to enable user login functionality

--- src/frontend/src/routes.tsx
+++ src/frontend/src/routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import CommunityPage from "./pages/CommunityPage";
 import FlowPage from "./pages/FlowPage";
 import HomePage from "./pages/MainPage";
+import LoginPage from "./pages/loginPage";
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="" element={<FlowPage />} />
       </Route>
       <Route path="*" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
