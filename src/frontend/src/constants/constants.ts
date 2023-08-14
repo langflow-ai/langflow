9acf9e39aac4c11dc34726a27682073957b89425
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2023-08-14 11:15:26 -0300

    🔧 fix(constants.ts): add specialCharsRegex constant to match special characters in strings 🔧 fix(reactflowUtils.ts): update handleKeyDown function to prevent deletion of block when a special character is the last character in the input value

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -29,6 +29,7 @@ export const INVALID_CHARACTERS = [
  */
 
 export const regexHighlight = /\{([^}]+)\}/g;
+export const specialCharsRegex = /[!@#$%^&*()\-_=+[\]{}|;:'",.<>/?\\]/;
 
 export const programmingLanguages: languageMap = {
   javascript: ".js",
