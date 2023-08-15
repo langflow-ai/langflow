3836d3ba4b43604ff2b2cf474e7c7b49cd874f49
gustavoschaedler <gustavopoa@gmail.com>
2023-08-15 20:00:02 +0100

    🔧 fix(auth.py): move SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, and REFRESH_TOKEN_EXPIRE_MINUTES to environment variables for better security and configurability ✨ feat(auth.py): add support for loading settings from environment variables in create_token and create_user_tokens functions 🔧 fix(auth.py): use settings_manager to access SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, and REFRESH_TOKEN_EXPIRE_MINUTES in create_token and create_user_tokens functions 🔧 fix(auth.py): use settings_manager to access SECRET_KEY and ALGORITHM in get_current_user function ✨ feat(auth.py): add create_user_longterm_token function to create long-term access token for auto login ✨ feat(login.py): add auto_login endpoint to automatically log in as a super user if AUTO_LOGIN is enabled in the settings 🔧 fix(settings/base.py): add SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_MINUTES, and AUTO_LOGIN settings to the base settings class ✨ feat(settings/base.py): add AUTO_LOGIN setting to control whether auto login is enabled or not 🔧 fix(settings/base.py): use secrets.token_hex(32) to generate a random SECRET_KEY instead of hardcoding it 🔧 fix(settings/base.py): update comments and formatting in the base settings class ✨ feat(settings/base.py): add AUTO_LOGIN setting to control whether auto login is enabled or not 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings class 🔧 fix(settings/base.py): update comments and formatting in the base settings
    🔥 refactor(settings.py): remove unused imports and code, clean up formatting and comments
    🔀 chore(settings.py): merge duplicated code into reusable functions
    📝 docs(settings.py): add missing docstrings and comments for better code documentation
    🔧 chore(settings.py): update settings file to improve readability and maintainability

--- src/backend/langflow/routers/login.py
+++ src/backend/langflow/routers/login.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
@@ -8,7 +9,10 @@ from langflow.auth.auth import (
     authenticate_user,
     create_user_tokens,
     create_refresh_token,
+    create_user_longterm_token,
 )
+
+from langflow.services.utils import get_settings_manager
 
 router = APIRouter()
 
@@ -27,6 +31,23 @@ async def login_to_get_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+@router.get("/auto_login")
+async def auto_login(db: Session = Depends(get_session)):
+    settings_manager = get_settings_manager()
+
+    if settings_manager.settings.AUTO_LOGIN:
+        user_id = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+        return create_user_longterm_token(user_id, db)
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={
+            "message": "Auto login is disabled. Please enable it in the settings",
+            "auto_login": False,
+        },
+    )
 
 
 @router.post("/refresh")
