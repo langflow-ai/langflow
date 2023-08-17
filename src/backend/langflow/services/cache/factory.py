c49e660e80a73263e0e53db782d8cf16d59c8ef8
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-17 17:23:31 -0300

    🐛 fix(cache/factory.py): change variable name from settings_service to settings_manager for consistency and clarity 🐛 fix(cache/factory.py): change variable name from settings_service to settings_manager for consistency and clarity 🐛 fix(database/factory.py): change variable name from settings_service to settings_manager for consistency and clarity

--- src/backend/langflow/services/cache/factory.py
+++ src/backend/langflow/services/cache/factory.py
@@ -7,17 +7,17 @@ class CacheManagerFactory(ServiceFactory):
     def __init__(self):
         super().__init__(BaseCacheManager)
 
-    def create(self, settings_service):
+    def create(self, settings_manager):
         # Here you would have logic to create and configure a CacheManager
         # based on the settings_service
 
-        if settings_service.settings.CACHE_TYPE == "redis":
+        if settings_manager.settings.CACHE_TYPE == "redis":
             logger.debug("Creating Redis cache")
             redis_cache = RedisCache(
-                host=settings_service.settings.REDIS_HOST,
-                port=settings_service.settings.REDIS_PORT,
-                db=settings_service.settings.REDIS_DB,
-                expiration_time=settings_service.settings.REDIS_CACHE_EXPIRE,
+                host=settings_manager.settings.REDIS_HOST,
+                port=settings_manager.settings.REDIS_PORT,
+                db=settings_manager.settings.REDIS_DB,
+                expiration_time=settings_manager.settings.REDIS_CACHE_EXPIRE,
             )
             if redis_cache.is_connected():
                 logger.debug("Redis cache is connected")
@@ -27,5 +27,5 @@ class CacheManagerFactory(ServiceFactory):
             )
             return InMemoryCache()
 
-        elif settings_service.settings.CACHE_TYPE == "memory":
+        elif settings_manager.settings.CACHE_TYPE == "memory":
             return InMemoryCache()
