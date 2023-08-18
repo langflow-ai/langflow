5fd3da010e244bad1ccf59b647ce8e8efc833a03
DiogenesBR <diogenesdirkx@gmail.com>
2023-08-18 21:10:03 +0000

    Atualização do ChromaComponent adicionando campos que havia na versão antiga

--- src/backend/langflow/components/vectorstores/Chroma.py
+++ src/backend/langflow/components/vectorstores/Chroma.py
@@ -6,6 +6,7 @@ from langchain.schema import Document
 from langchain.vectorstores.base import VectorStore
 from langchain.schema import BaseRetriever
 from langchain.embeddings.base import Embeddings
+import chromadb
 
 
 class ChromaComponent(CustomComponent):
@@ -32,15 +33,34 @@ class ChromaComponent(CustomComponent):
             "code": {"show": False, "display_name": "Code"},
             "documents": {"display_name": "Documents", "is_list": True},
             "embedding": {"display_name": "Embedding"},
+            "chroma_server_cors_allow_origins": {
+                "display_name": "Server CORS Allow Origins",
+                "advanced": True,
+            },
+            "chroma_server_host": {"display_name": "Server Host", "advanced": True},
+            "chroma_server_port": {"display_name": "Server Port", "advanced": True},
+            "chroma_server_grpc_port": {
+                "display_name": "Server gRPC Port",
+                "advanced": True,
+            },
+            "chroma_server_ssl_enabled": {
+                "display_name": "Server SSL Enabled",
+                "advanced": True,
+            },
         }
 
     def build(
         self,
         collection_name: str,
         persist: bool,
+        chroma_server_ssl_enabled: bool,
         persist_directory: Optional[str] = None,
         embedding: Optional[Embeddings] = None,
         documents: Optional[Document] = None,
+        chroma_server_cors_allow_origins: Optional[str] = None,
+        chroma_server_host: Optional[str] = None,
+        chroma_server_port: Optional[int] = None,
+        chroma_server_grpc_port: Optional[int] = None,
     ) -> Union[VectorStore, BaseRetriever]:
         """
         Builds the Vector Store or BaseRetriever object.
@@ -48,13 +68,32 @@ class ChromaComponent(CustomComponent):
         Args:
         - collection_name (str): The name of the collection.
         - persist_directory (Optional[str]): The directory to persist the Vector Store to.
+        - chroma_server_ssl_enabled (bool): Whether to enable SSL for the Chroma server.
         - persist (bool): Whether to persist the Vector Store or not.
         - embedding (Optional[Embeddings]): The embeddings to use for the Vector Store.
         - documents (Optional[Document]): The documents to use for the Vector Store.
+        - chroma_server_cors_allow_origins (Optional[str]): The CORS allow origins for the Chroma server.
+        - chroma_server_host (Optional[str]): The host for the Chroma server.
+        - chroma_server_port (Optional[int]): The port for the Chroma server.
+        - chroma_server_grpc_port (Optional[int]): The gRPC port for the Chroma server.
 
         Returns:
         - Union[VectorStore, BaseRetriever]: The Vector Store or BaseRetriever object.
         """
+
+        # Chroma settings
+        chroma_settings = None
+
+        if chroma_server_host is not None:
+            chroma_settings = chromadb.config.Settings(
+                chroma_server_cors_allow_origins=chroma_server_cors_allow_origins
+                or None,
+                chroma_server_host=chroma_server_host,
+                chroma_server_port=chroma_server_port or None,
+                chroma_server_grpc_port=chroma_server_grpc_port or None,
+                chroma_server_ssl_enabled=chroma_server_ssl_enabled,
+            )
+
         # If documents, then we need to create a Chroma instance using .from_documents
         if documents is not None and embedding is not None:
             return Chroma.from_documents(
@@ -62,6 +101,7 @@ class ChromaComponent(CustomComponent):
                 persist_directory=persist_directory if persist else None,
                 collection_name=collection_name,
                 embedding=embedding,
+                client_settings=chroma_settings,
             )
 
         return Chroma(
