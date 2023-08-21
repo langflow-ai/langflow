13981e0d16d78c8846a273d73859323bb5a8b935
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-21 16:58:09 -0300

    📦 chore(factory.py): add AuthManagerFactory class to handle creation of AuthManager service 🐛 fix(service.py): add type hints and import for SettingsManager in AuthManager class

--- src/backend/langflow/services/auth/service.py
+++ src/backend/langflow/services/auth/service.py
@@ -1,10 +1,14 @@
 from langflow.services.base import Service
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from langflow.services.settings.manager import SettingsManager
 
 
 class AuthManager(Service):
     name = "auth_manager"
 
-    def __init__(self, settings_manager):
+    def __init__(self, settings_manager: "SettingsManager"):
         self.settings_manager = settings_manager
 
     # We need to define a function that can be passed to the Depends() function.
