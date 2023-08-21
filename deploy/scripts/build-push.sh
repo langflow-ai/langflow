74a7ec526a1301eaa4ca3b5b284c17b4a6a75072
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-21 13:47:02 -0300

    🚀 feat(build-push.sh): add script to build and push Docker images
    🚀 feat(build.sh): add script to build Docker images

---
+++ deploy/scripts/build-push.sh
@@ -0,0 +1,11 @@
+
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+TAG=${TAG?Variable not set} \
+FRONTEND_ENV=${FRONTEND_ENV-production} \
+sh ./scripts/build.sh
+
+docker-compose -f docker-compose.yml push
