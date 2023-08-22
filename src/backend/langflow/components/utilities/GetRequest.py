62b9bb523c7ee20a846bf47bb63ee51c0bc6c5bd
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-22 14:56:34 -0300

    🔧 fix(GetRequest.py): improve code readability and add support for multiple URLs and headers in GetRequest component 🔧 fix(PostRequest.py): improve code readability and add support for multiple documents, URLs, and headers in PostRequest component

--- src/backend/langflow/components/utilities/GetRequest.py
+++ src/backend/langflow/components/utilities/GetRequest.py
@@ -7,14 +7,56 @@ from typing import Optional
 
 class GetRequest(CustomComponent):
     display_name: str = "Get Request"
-    description: str = "Make a GET request to the given URL"
+    description: str = "Make a GET request to the given URL."
     output_types: list[str] = ["Document"]
     beta = True
     field_config = {
-        "url": {"display_name": "URL"},
-        "headers": {"display_name": "Headers", "field_type": "code"},
+        "url": {
+            "display_name": "URL",
+            "info": "The URL to make the request to",
+            "is_list": True,
+        },
+        "headers": {
+            "display_name": "Headers",
+            "field_type": "code",
+            "info": "The headers to send with the request.",
+        },
         "code": {"show": False},
     }
+
+    def get_document(
+        self,
+        url: str,
+        headers: Optional[dict] = None,
+    ) -> Document:
+        try:
+            if headers is None:
+                headers = {}
+            with requests.Session() as session:
+                response = session.get(url, headers=headers)
+                try:
+                    response_json = response.json()
+                    result = orjson_dumps(response_json, indent_2=False)
+                except Exception:
+                    result = response.text
+                self.repr_value = result
+                return Document(
+                    page_content=result,
+                    metadata={
+                        "source": url,
+                        "headers": headers,
+                        "status_code": response.status_code,
+                    },
+                )
+        except Exception as exc:
+            return Document(
+                page_content=str(exc),
+                metadata={
+                    "source": url,
+                    "headers": headers,
+                    "status_code": 500,
+                },
+            )
 
     def build(
         self,
@@ -23,7 +65,6 @@ class GetRequest(CustomComponent):
     ) -> Document:
         if headers is None:
             headers = {}
-        with requests.get(url, headers=headers) as result:
-            result = result.json()
-            self.repr_value = result
-            return Document(page_content=orjson_dumps(result))
+        if not isinstance(url, list):
+            url = [url]
+        return [self.get_document(u, headers) for u in url]
