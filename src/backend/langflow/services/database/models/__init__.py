c6b41043162435a1b518bc213c0defc63fafa8e2
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-25 15:19:13 -0300

    🔥 refactor(models): remove Token model and references to it to simplify the codebase and remove unused code 🔥 refactor(test_custom_component.py): remove unused fixture and imports to clean up the test file 🔧 chore(test_custom_component.py): update component fixture to include user_id parameter for testing purposes

--- src/backend/langflow/services/database/models/__init__.py
+++ src/backend/langflow/services/database/models/__init__.py
@@ -1,6 +1,5 @@
 from .flow import Flow
 from .user import User
-from .token import Token
 from .api_key import ApiKey
 
-__all__ = ["Flow", "User", "Token", "ApiKey"]
+__all__ = ["Flow", "User", "ApiKey"]
