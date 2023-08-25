4e8b1562914621d26daadd60ef7f664830969ce5
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-25 10:10:19 -0300

    🔀 chore(router.py): add login_router to the APIRouter to include login functionality 🔀 chore(__init__.py): import and include login_router in the APIRouter to enable login functionality 🔀 chore(login.py): add tags to the login router to categorize it as "Login" in the API documentation

--- src/backend/langflow/api/v1/__init__.py
+++ src/backend/langflow/api/v1/__init__.py
@@ -5,6 +5,7 @@ from langflow.api.v1.flows import router as flows_router
 from langflow.api.v1.components import router as component_router
 from langflow.api.v1.users import router as users_router
 from langflow.api.v1.api_key import router as api_key_router
+from langflow.api.v1.login import router as login_router
 
 __all__ = [
     "chat_router",
@@ -14,4 +15,5 @@ __all__ = [
     "flows_router",
     "users_router",
     "api_key_router",
+    "login_router",
 ]
