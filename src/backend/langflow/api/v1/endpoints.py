fb01cdd8547e3302695fbca0f469afd4ced499f5
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-25 20:02:53 -0300

    🐛 fix(endpoints.py): import validate_api_key function from auth.utils to fix missing dependency ✨ feat(endpoints.py): add validation of API key in process_flow endpoint to ensure only valid requests are processed 🔧 chore(utils.py): add validate_api_key function to validate API key against database 🔧 chore(test_endpoints.py): add test case for process_flow endpoint to test API key validation and processing

--- src/backend/langflow/api/v1/endpoints.py
+++ src/backend/langflow/api/v1/endpoints.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from typing import Annotated, Optional, Union
-from langflow.services.auth.utils import get_current_active_user
+from langflow.services.auth.utils import get_current_active_user, validate_api_key
 
 from langflow.services.cache.utils import save_uploaded_file
 from langflow.services.database.models.flow import Flow
@@ -86,10 +86,13 @@ async def process_flow(
     clear_cache: Annotated[bool, Body(embed=True)] = False,  # noqa: F821
     session_id: Annotated[Union[None, str], Body(embed=True)] = None,  # noqa: F821
     session: Session = Depends(get_session),
+    valid: bool = Depends(validate_api_key),
 ):
     """
     Endpoint to process an input with a given flow_id.
     """
+    if not valid:
+        raise HTTPException(status_code=401, detail="Invalid API key")
 
     try:
         flow = session.get(Flow, flow_id)
