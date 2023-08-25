811376637427320098cc6071f47ddde8509cd4b6
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-25 15:21:52 -0300

    ✨ feat(schemas.py): add Token schema to represent access and refresh tokens in API responses

--- src/backend/langflow/api/v1/schemas.py
+++ src/backend/langflow/api/v1/schemas.py
@@ -163,3 +163,9 @@ class ApiKeysResponse(BaseModel):
 
 class CreateApiKeyRequest(BaseModel):
     name: str
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
