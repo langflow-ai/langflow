c90acaa209b8943c8e17f0a19ee053fc264b46f5
igorrCarvalho <igorsilvabhz6@gmail.com>
2023-08-29 18:34:55 -0300

    Feat[DropdownButtonComponent]: Create Dropdown Button component

---
+++ src/frontend/src/components/DropdownButtonComponent/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import IconComponent from "../genericIconComponent";
+import { Button } from "../ui/button";
+
+export default function DropdownButton({
+  firstButtonName,
+  onFirstBtnClick,
+  options,
+}): JSX.Element {
+  const [showOptions, setShowOptions] = useState(false);
+
+  return (
+    <div className="align-center relative flex">
+      <div>
+        <Button
+          variant="primary"
+          className="mr-4 w-full"
+          onClick={onFirstBtnClick}
+        >
+          {firstButtonName}
+        </Button>
+      </div>
+      <div>
+        <button
+          className="absolute inset-y-0 right-0 items-center text-muted-foreground"
+          onClick={(event) => {
+            event.stopPropagation();
+            setShowOptions(!showOptions);
+          }}
+        >
+          {!showOptions ? (
+            <IconComponent name="ChevronDown" className="" aria-hidden="true" />
+          ) : (
+            <IconComponent name="ChevronUp" />
+          )}
+        </button>
+      </div>
+      {showOptions && (
+        <div className="absolute top-10 w-full">
+          {options.map((optionName) => (
+            <Button className="w-full" variant="primary">
+              {optionName}
+            </Button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
