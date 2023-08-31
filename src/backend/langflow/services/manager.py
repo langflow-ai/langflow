d9e326b982895fd36f8c6556886e7672b713479a
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-31 11:49:51 -0300

    🔀 chore(manager.py): ignore type error for session_manager_factory import to prevent linting errors

--- src/backend/langflow/services/manager.py
+++ src/backend/langflow/services/manager.py
@@ -135,7 +135,7 @@ def initialize_session_manager():
     """
     Initialize the session manager.
     """
-    from langflow.services.session import factory as session_manager_factory
+    from langflow.services.session import factory as session_manager_factory  # type: ignore
     from langflow.services.cache import factory as cache_factory
 
     initialize_settings_manager()
