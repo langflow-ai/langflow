8fe222185d966bd3a1e580704351f18cdf123976
anovazzi1 <otavio2204@gmail.com>
2023-08-31 19:07:30 -0300

    addded initial test files with .har working

---
+++ src/frontend/tests/SuccesLogin.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Login", () => {
+  test("har_file", async ({ page }) => {
+    await page.routeFromHAR("harFiles/login.har", {
+      url: "**/api/v1/**",
+      update: true,
+    });
+    await page.goto("http://localhost:3000/");
+    await page.waitForURL("http://localhost:3000/login");
+    expect(page.url()).toBe("http://localhost:3000/login");
+  });
+});
--- src/frontend/tests/loginPage.spec.ts
+++ src/frontend/tests/loginPage.spec.ts
@@ -1,7 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.goto('http://localhost:3000/login');
-  await expect(page.getByText('Sign in to Langflow')).toBeVisible();
+test("har_file", async ({ page }) => {
+  await page.routeFromHAR("harFiles/langflow.har", {
+    url: "**/api/v1/**",
+    update: false,
+  });
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("button", { name: "Community Examples" }).click();
+  await page.waitForSelector(".community-pages-flows-panel");
+  expect(
+    await page
+      .locator(".community-pages-flows-panel")
+      .evaluate((el) => el.children)
+  ).toBeTruthy();
 });
