6418d5e56c40bb5056eaf8383fdc4d4af85d43cf
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-09-01 17:22:01 -0300

    🔀 chore(utils.py): add type hints to imported modules for better code readability and maintainability ✨ feat(utils.py): add get_task_manager() function to retrieve the TaskManager instance from the service manager

--- src/backend/langflow/services/utils.py
+++ src/backend/langflow/services/utils.py
@@ -5,6 +5,10 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from langflow.services.database.manager import DatabaseManager
     from langflow.services.settings.manager import SettingsManager
+    from langflow.services.cache.manager import BaseCacheManager
+    from langflow.services.session.manager import SessionManager
+    from langflow.services.task.manager import TaskManager
+    from sqlmodel import Session
 
 
 def get_settings_manager() -> "SettingsManager":
@@ -15,14 +19,18 @@ def get_db_manager() -> "DatabaseManager":
     return service_manager.get(ServiceType.DATABASE_MANAGER)
 
 
-def get_session():
+def get_session() -> "Session":
     db_manager = service_manager.get(ServiceType.DATABASE_MANAGER)
     yield from db_manager.get_session()
 
 
-def get_cache_manager():
+def get_cache_manager() -> "BaseCacheManager":
     return service_manager.get(ServiceType.CACHE_MANAGER)
 
 
-def get_session_manager():
+def get_session_manager() -> "SessionManager":
     return service_manager.get(ServiceType.SESSION_MANAGER)
+
+
+def get_task_manager() -> "TaskManager":
+    return service_manager.get(ServiceType.TASK_MANAGER)
