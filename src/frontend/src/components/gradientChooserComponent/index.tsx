880f87997710274e9585107f9517c6e243f1bd15
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-09-11 16:07:40 -0300

    Formatting Fixes

--- src/frontend/src/components/gradientChooserComponent/index.tsx
+++ src/frontend/src/components/gradientChooserComponent/index.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
 import { gradients } from "../../utils/styleUtils";
 
-
-export default function GradientChooserComponent({value, onChange}){
-    return (
-        <div className="flex flex-wrap gap-4 justify-center items-center">
-            {gradients.map((gradient, idx) => 
-            <div onClick={() => {onChange(gradient)}} className={"cursor-pointer w-12 h-12 rounded-full transition-all duration-400 " + gradient + (value === gradient ?  " shadow-lg ring-2 ring-primary" : "")} key={idx}></div>
-            )}
-            </div>
-    )
+export default function GradientChooserComponent({ value, onChange }) {
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-4">
+      {gradients.map((gradient, idx) => (
+        <div
+          onClick={() => {
+            onChange(gradient);
+          }}
+          className={
+            "duration-400 h-12 w-12 cursor-pointer rounded-full transition-all " +
+            gradient +
+            (value === gradient ? " shadow-lg ring-2 ring-primary" : "")
+          }
+          key={idx}
+        ></div>
+      ))}
+    </div>
+  );
 }
