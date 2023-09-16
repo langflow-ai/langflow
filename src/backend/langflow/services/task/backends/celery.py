be8e1b9b95a647d911029fee5f27c4bd8d7863ce
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-09-16 11:13:08 -0300

    🐛 fix(anyio.py): add error handling when launching a task to prevent crashes and log the error 🐛 fix(celery.py): add type hinting to the launch_task method and return the AsyncResult object for better task tracking

--- src/backend/langflow/services/task/backends/celery.py
+++ src/backend/langflow/services/task/backends/celery.py
@@ -1,5 +1,5 @@
 from typing import Any, Callable
-from celery.result import AsyncResult  # type: ignore
+from celery.result import AsyncResult
 from langflow.services.task.backends.base import TaskBackend
 from langflow.worker import celery_app
 
@@ -11,10 +11,13 @@ class CeleryBackend(TaskBackend):
     def launch_task(
         self, task_func: Callable[..., Any], *args: Any, **kwargs: Any
     ) -> str:
+        # I need to type the delay method to make it easier
+        from celery import Task
+
         if not hasattr(task_func, "delay"):
             raise ValueError(f"Task function {task_func} does not have a delay method")
-        task = task_func.delay(*args, **kwargs)
-        return task.id
+        task: Task = task_func.delay(*args, **kwargs)
+        return task.id, AsyncResult(task.id, app=self.celery_app)
 
     def get_task(self, task_id: str) -> Any:
         return AsyncResult(task_id, app=self.celery_app)
