befe79775b1d1c63b8764e56745e868a5d91adb5
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-09-23 23:28:37 -0300

    🐛 fix(celery_app.py): add return type annotation to make_celery function to improve code readability and maintainability 🔧 chore(celeryconfig.py): update broker_url default value to use RabbitMQ instead of Redis for better performance and scalability

--- src/backend/langflow/core/celery_app.py
+++ src/backend/langflow/core/celery_app.py
@@ -1,7 +1,7 @@
 from celery import Celery  # type: ignore
 
 
-def make_celery(app_name: str, config: str):
+def make_celery(app_name: str, config: str) -> Celery:
     celery_app = Celery(app_name)
     celery_app.config_from_object(config)
     celery_app.conf.task_routes = {"langflow.worker.tasks.*": {"queue": "langflow"}}
