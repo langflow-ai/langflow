318ce886db0b6f889f056befc5c0d0a505098cb6
anovazzi1 <otavio2204@gmail.com>
2023-10-03 17:19:20 -0300

    refactor(sideBarDraggableComponent): remove unused useRef and useState hooks to simplify code feat(sideBarDraggableComponent): add right-click functionality to open the select menu for additional options

--- src/frontend/src/pages/FlowPage/components/extraSidebarComponent/sideBarDraggableComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/extraSidebarComponent/sideBarDraggableComponent/index.tsx
@@ -1,4 +1,4 @@
-import { DragEventHandler, useRef, useState } from "react";
+import { DragEventHandler, useRef } from "react";
 import IconComponent from "../../../../../components/genericIconComponent";
 import {
   Select,
@@ -20,101 +20,79 @@ export default function SidebarDraggableComponent({
   color: string;
   onDragStart: DragEventHandler<HTMLDivElement>;
 }) {
-  const isOpen = useRef(false);
-  const [editMode, setEditMode] = useState(false);
-  const inside = useRef(false);
+  const open = useRef(false);
 
   return (
-    <div key={itemName} data-tooltip-id={itemName}>
+    <Select
+      onOpenChange={(change) => (open.current = change)}
+      open={open.current}
+    >
       <div
-        draggable={!error}
-        className={
-          "side-bar-components-border bg-background" +
-          (error ? " cursor-not-allowed select-none" : "")
-        }
-        style={{
-          borderLeftColor: color,
+        onContextMenuCapture={(e) => {
+          e.preventDefault();
+          open.current = true;
         }}
-        onDragStart={onDragStart}
-        onDragEnd={() => {
-          document.body.removeChild(
-            document.getElementsByClassName("cursor-grabbing")[0]
-          );
-        }}
+        key={itemName}
+        data-tooltip-id={itemName}
       >
-        <div className="side-bar-components-div-form">
-          <span className="side-bar-components-text">{display_name}</span>
-          <div
-            onMouseLeave={() => {
-              if (!isOpen.current) {
-                inside.current = false;
-                setEditMode(false);
-              }
-            }}
-            onMouseOver={() => {
-              inside.current = true;
-              setTimeout(() => {
-                if (inside.current) setEditMode(true);
-              }, 800);
-            }}
-          >
-            {editMode ? (
-              <Select
-                onOpenChange={(open) => {
-                  if (!open) {
-                    isOpen.current = false;
-                    inside.current = false;
-                    setEditMode(false);
-                  } else {
-                    isOpen.current = true;
-                  }
-                }}
-              >
-                <SelectTrigger>
-                  <IconComponent
-                    name="MoreHorizontal"
-                    className="side-bar-components-icon "
-                  />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value={"share"}>
-                    <div className="flex">
-                      <IconComponent
-                        name="Share2"
-                        className="relative top-0.5 mr-2 h-4 w-4"
-                      />{" "}
-                      Share{" "}
-                    </div>{" "}
-                  </SelectItem>
-                  <SelectItem value={"download"}>
-                    <div className="flex">
-                      <IconComponent
-                        name="Download"
-                        className="relative top-0.5 mr-2 h-4 w-4"
-                      />{" "}
-                      Download{" "}
-                    </div>{" "}
-                  </SelectItem>
-                  <SelectItem value={"delete"}>
-                    <div className="flex">
-                      <IconComponent
-                        name="Trash2"
-                        className="relative top-0.5 mr-2 h-4 w-4"
-                      />{" "}
-                      Delete{" "}
-                    </div>{" "}
-                  </SelectItem>
-                </SelectContent>
-              </Select>
-            ) : (
-              <IconComponent
-                name="Menu"
-                className="side-bar-components-icon "
-              />
-            )}
+        <div
+          draggable={!error}
+          className={
+            "side-bar-components-border bg-background" +
+            (error ? " cursor-not-allowed select-none" : "")
+          }
+          style={{
+            borderLeftColor: color,
+          }}
+          onDragStart={onDragStart}
+          onDragEnd={() => {
+            document.body.removeChild(
+              document.getElementsByClassName("cursor-grabbing")[0]
+            );
+          }}
+        >
+          <div className="side-bar-components-div-form">
+            <span className="side-bar-components-text">{display_name}</span>
+            <div>
+              <SelectTrigger>
+                <IconComponent
+                  name="Menu"
+                  className="side-bar-components-icon "
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value={"share"}>
+                  <div className="flex">
+                    <IconComponent
+                      name="Share2"
+                      className="relative top-0.5 mr-2 h-4 w-4"
+                    />{" "}
+                    Share{" "}
+                  </div>{" "}
+                </SelectItem>
+                <SelectItem value={"download"}>
+                  <div className="flex">
+                    <IconComponent
+                      name="Download"
+                      className="relative top-0.5 mr-2 h-4 w-4"
+                    />{" "}
+                    Download{" "}
+                  </div>{" "}
+                </SelectItem>
+                <SelectItem value={"delete"}>
+                  <div className="flex">
+                    <IconComponent
+                      name="Trash2"
+                      className="relative top-0.5 mr-2 h-4 w-4"
+                    />{" "}
+                    Delete{" "}
+                  </div>{" "}
+                </SelectItem>
+              </SelectContent>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Select>
   );
 }
