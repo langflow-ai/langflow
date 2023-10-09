f7534516ef4a11251d220b0bc99b331645c40dd6
anovazzi1 <otavio2204@gmail.com>
2023-10-09 17:04:07 -0300

    create shell scripts to run frontend test

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["html", { open: "never", outputFolder: "playwright-report/test-results" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
