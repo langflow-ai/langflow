cbf7845eca08802725573400fb1d206d080f8ae7
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-10-09 19:06:01 -0300

    🔧 fix(buildTrigger/index.tsx): remove unused imports and variables to improve code readability and maintainability ✨ feat(buildTrigger/index.tsx): add support for fetching and building vertices in a specific order to improve build process efficiency

--- src/frontend/src/components/chatComponent/buildTrigger/index.tsx
+++ src/frontend/src/components/chatComponent/buildTrigger/index.tsx
@@ -4,12 +4,9 @@ import Loading from "../../../components/ui/loading";
 import { useSSE } from "../../../contexts/SSEContext";
 import { alertContext } from "../../../contexts/alertContext";
 import { typesContext } from "../../../contexts/typesContext";
-import { postBuildInit } from "../../../controllers/API";
 import { FlowType } from "../../../types/flow";
 
 import { TabsContext } from "../../../contexts/tabsContext";
-import { parsedDataType } from "../../../types/components";
-import { TabsState } from "../../../types/tabs";
 import { validateNodes } from "../../../utils/reactflowUtils";
 import RadialProgressComponent from "../../RadialProgress";
 import IconComponent from "../../genericIconComponent";
@@ -31,6 +28,9 @@ export default function BuildTrigger({
   const [isIconTouched, setIsIconTouched] = useState(false);
   const eventClick = isBuilding ? "pointer-events-none" : "";
   const [progress, setProgress] = useState(0);
+  const [verticesOrder, setVerticesOrder] = useState([]);
+  const [buildResults, setBuildResults] = useState({});
+  const [error, setError] = useState(null);
 
   async function handleBuild(flow: FlowType): Promise<void> {
     try {
@@ -49,7 +49,32 @@ export default function BuildTrigger({
       const startTime = Date.now();
       setIsBuilding(true);
 
-      const allNodesValid = await streamNodeData(flow);
+      // Step 1: Get vertices order
+      await getVerticesOrder(flow.id);
+
+      // Step 2: Build each vertex in the order received
+      console.log("verticesOrder", verticesOrder);
+      const buildResults = [];
+      for (let vertexId of verticesOrder) {
+        const buildResponse = await fetch(
+          `/api/v1/build/${flow.id}/vertices/${vertexId}`,
+          {
+            method: "POST",
+          }
+        );
+        const buildData = await buildResponse.json();
+        if (buildData.valid) {
+          setProgress(
+            (prevProgress) => prevProgress + 1 / verticesOrder.length
+          );
+        }
+
+        buildResults.push(buildData.valid);
+      }
+
+      // Determine if all nodes are valid
+      const allNodesValid = buildResults.every((result) => result);
+
       await enforceMinimumLoadingTime(startTime, minimumLoadingTime);
       setIsBuilt(allNodesValid);
       if (!allNodesValid) {
@@ -71,78 +96,33 @@ export default function BuildTrigger({
       setIsBuilding(false);
     }
   }
-  async function streamNodeData(flow: FlowType) {
-    // Step 1: Make a POST request to send the flow data and receive a unique session ID
-    const response = await postBuildInit(flow);
-    const { flowId } = response.data;
-    // Step 2: Use the session ID to establish an SSE connection using EventSource
-    let validationResults: boolean[] = [];
-    let finished = false;
-    const apiUrl = `/api/v1/build/stream/${flowId}`;
-    const eventSource = new EventSource(apiUrl);
 
-    eventSource.onmessage = (event) => {
-      // If the event is parseable, return
-      if (!event.data) {
-        return;
-      }
-      const parsedData = JSON.parse(event.data);
-      // if the event is the end of the stream, close the connection
-      if (parsedData.end_of_stream) {
-        // Close the connection and finish
-        finished = true;
-        eventSource.close();
-
-        return;
-      } else if (parsedData.log) {
-        // If the event is a log, log it
-        setSuccessData({ title: parsedData.log });
-      } else if (parsedData.input_keys !== undefined) {
-        //@ts-ignore
-        setTabsState((old: TabsState) => {
-          return {
-            ...old,
-            [flowId]: {
-              ...old[flowId],
-              formKeysData: parsedData,
-            },
-          };
-        });
-      } else {
-        // Otherwise, process the data
-        const isValid = processStreamResult(parsedData);
-        setProgress(parsedData.progress);
-        validationResults.push(isValid);
-      }
-    };
-
-    eventSource.onerror = (error: any) => {
-      console.error("EventSource failed:", error);
-      eventSource.close();
-      if (error.data) {
-        const parsedData = JSON.parse(error.data);
-        setErrorData({ title: parsedData.error });
-        setIsBuilding(false);
-      }
-    };
-    // Step 3: Wait for the stream to finish
-    while (!finished) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      finished = validationResults.length === flow.data!.nodes.length;
+  // Function to get vertices order
+  async function getVerticesOrder(flowId) {
+    try {
+      const response = await fetch(`/api/v1/build/${flowId}/vertices`);
+      const data = await response.json();
+      setVerticesOrder(data.ids);
+    } catch (err) {
+      setError(err.message);
     }
-    // Step 4: Return true if all nodes are valid, false otherwise
-    return validationResults.every((result) => result);
   }
 
-  function processStreamResult(parsedData: parsedDataType) {
-    // Process each chunk of data here
-    // Parse the chunk and update the context
+  // Function to build a vertex and update the results
+  async function buildVertex(flowId, vertexId) {
     try {
-      updateSSEData({ [parsedData.id]: parsedData });
+      const response = await fetch(
+        `/api/v1/build/${flowId}/vertices/${vertexId}`,
+        { method: "POST" }
+      );
+      const data = await response.json();
+      setBuildResults((prevResults) => ({
+        ...prevResults,
+        [vertexId]: data,
+      }));
     } catch (err) {
-      console.log("Error parsing stream data: ", err);
+      setError(err.message);
     }
-    return parsedData.valid;
   }
 
   async function enforceMinimumLoadingTime(
