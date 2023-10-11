41f83e4bf84180c913a9ccacc0df319d91c4879d
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-10-11 18:31:05 -0300

    🔀 chore(field_typing): add support for additional types in field_typing/__init__.py
    🔀 chore(field_typing): add support for Data type in field_typing/base.py
    
    🔀 chore(field_typing): add constants.py file to define supported types in field_typing
    
    🔀 chore(interface/custom): remove duplicate imports and use field_typing types
    
    🔀 chore(utils): fix syntax error in get_base_classes function

--- src/backend/langflow/field_typing/__init__.py
+++ src/backend/langflow/field_typing/__init__.py
@@ -1,3 +1,8 @@
-from .base import NestedDict
+from .base import NestedDict, Data
+from .constants import *
 
-__all__ = ["NestedDict"]
+__all__ = (
+    ["NestedDict", "Data"]
+    + list(LANGCHAIN_BASE_TYPES.keys())
+    + list(CUSTOM_COMPONENT_SUPPORTED_TYPES.keys())
+)
