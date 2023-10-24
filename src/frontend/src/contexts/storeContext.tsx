5222775c56b31b2a838fa0bd221624f125cc8769
anovazzi1 <otavio2204@gmail.com>
2023-10-24 20:07:42 -0300

    feat(storeContext): add storeContext to manage and share user components and their updates feat(storeContext): create storeProvider component to provide the store context to child components feat(storeContext): define storeContextType to specify the shape of the store context data

---
+++ src/frontend/src/contexts/storeContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { storeContextType } from "../types/contexts/store";
+import { FlowType } from "../types/flow";
+
+//store context to share user components and update them
+const initialValue = {
+  savedFlows: {},
+  setSavedFlows: () => {},
+};
+
+export const StoreContext = createContext<storeContextType>(initialValue);
+
+export function storeProvider({ children }) {
+  const [savedFlows, setSavedFlows] = useState<{ [key: string]: FlowType }>({});
+
+  return (
+    <StoreContext.Provider value={{ savedFlows, setSavedFlows }}>
+      {children}
+    </StoreContext.Provider>
+  );
+}
