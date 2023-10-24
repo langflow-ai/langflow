d468f40fb655a7af4dd103cf43da5f63da358f6e
anovazzi1 <otavio2204@gmail.com>
2023-10-24 20:47:56 -0300

    fix(contexts/index.tsx): move import statement for StoreProvider to the correct position for better organization fix(contexts/storeContext.tsx): change the type of savedFlows state from an object to a Set to improve data structure fix(controllers/API/index.ts): add a new function getStoreSavedComponents to fetch saved components from the store fix(pages/StorePage/components/market-card.tsx): use the savedFlows state from StoreContext to determine if a component is added to the store fix(pages/StorePage/index.tsx): add a new function getSavedComponents to fetch saved components from the store and update the savedFlows state in StoreContext fix(types/contexts/store.ts): change the type of savedFlows state in storeContextType from an object to a Set for better data structure

--- src/frontend/src/types/contexts/store.ts
+++ src/frontend/src/types/contexts/store.ts
@@ -1,6 +1,4 @@
-import { FlowType } from "../flow";
-
 export type storeContextType = {
-  savedFlows: { [key: string]: FlowType };
-  setSavedFlows: (newState: { [key: string]: FlowType }) => void;
+  savedFlows: Set<string>;
+  setSavedFlows: (newState: Set<string>) => void;
 };
