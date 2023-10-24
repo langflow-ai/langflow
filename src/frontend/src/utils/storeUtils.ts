55e28bdebaedc50128c8bb24163f480baa23f142
anovazzi1 <otavio2204@gmail.com>
2023-10-23 23:37:32 -0300

    fix(StorePage/index.tsx): add is_component parameter to handleFork function to properly clone flow with parent fix(StorePage/index.tsx): pass is_component parameter to cloneFLowWithParent function when cloning flow fix(StorePage/index.tsx): log newFlow after cloning with parent fix(StorePage/index.tsx): log parsed response after saving cloned flow to flow store fix(StorePage/index.tsx): pass item.is_component parameter to handleFork function when adding MarketCardComponent fix(StorePage/index.tsx): pass item.id and item.is_component parameters to handleFork function when adding MarketCardComponent fix(StorePage/index.tsx): pass data[0].id and data[0].is_component parameters to handleFork function when clicking test button fix(storeUtils.ts): add is_component parameter to cloneFLowWithParent function to properly set is_component property of cloned flow

--- src/frontend/src/utils/storeUtils.ts
+++ src/frontend/src/utils/storeUtils.ts
@@ -1,10 +1,11 @@
 import { cloneDeep } from "lodash";
 import { FlowType } from "../types/flow";
 
-export default function cloneFLowWithParent(flow: FlowType) {
+export default function cloneFLowWithParent(flow: FlowType, is_component) {
   const parent = flow.id;
   let childFLow = cloneDeep(flow);
   childFLow.parent = parent;
   childFLow.id = "";
+  childFLow.is_component = is_component;
   return childFLow;
 }
