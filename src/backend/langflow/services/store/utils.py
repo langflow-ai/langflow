75079731c9a57a1b5746b149947903b144dab757
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-01 09:45:42 -0300

    🐛 fix(store.py): fix indentation issue in list_components function to improve code readability 🐛 fix(service.py): remove unused user_data parameter in get_liked_by_user_components function 🐛 fix(utils.py): rename in_users_collection attribute to in_user_collection for consistency

--- src/backend/langflow/services/store/utils.py
+++ src/backend/langflow/services/store/utils.py
@@ -1,3 +1,11 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from langflow.services.store.schema import ListComponentResponse
+    from langflow.services.store.service import StoreService
+
+
 def process_tags_for_post(component_dict):
     tags = component_dict.pop("tags", None)
     if tags and all(isinstance(tag, str) for tag in tags):
@@ -5,7 +13,11 @@ def process_tags_for_post(component_dict):
     return component_dict
 
 
-def update_components_with_user_data(components, store_service, store_api_Key):
+def update_components_with_user_data(
+    components: "ListComponentResponse",
+    store_service: "StoreService",
+    store_api_Key: str,
+):
     """
     Updates the components with the user data (liked_by_user and in_users_collection)
     """
@@ -20,5 +32,5 @@ def update_components_with_user_data(components, store_service, store_api_Key):
     # Now we need to set the liked_by_user attribute
     for component in components:
         component.liked_by_user = str(component.id) in liked_by_user_ids
-        component.in_users_collection = str(component.id) in in_users_collection_ids
+        component.in_user_collection = str(component.id) in in_users_collection_ids
     return components
