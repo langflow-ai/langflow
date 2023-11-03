1dd7b60b9e0ea43e8a73bf784d69c7c1cc2b5709
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-03 11:50:03 -0300

    🐛 fix(PromptRunner.py): change beta variable type from string to boolean to match expected type 🐛 fix(Metaphor.py): change beta variable type from string to boolean to match expected type 🐛 fix(GetRequest.py): change beta variable type from string to boolean to match expected type 🐛 fix(JSONDocumentBuilder.py): change beta variable type from string to boolean to match expected type 🐛 fix(PostRequest.py): change beta variable type from string to boolean to match expected type 🐛 fix(UpdateRequest.py): change beta variable type from string to boolean to match expected type 🐛 fix(Chroma.py): change beta variable type from string to boolean to match expected type 🐛 fix(Vectara.py): change beta variable type from string to boolean to match expected type 🐛 fix(component.py): change ERROR_CODE_NULL variable type from dict to string to match expected type 🐛 fix(custom_component.py): change code_class_base_inheritance variable type from dict to string to match expected type 🐛 fix(custom_component.py): change function_entrypoint_name variable type from dict to string to match expected type 🐛 fix(initialize/vector_store.py): change chromadb variable name to chromadb_instance for clarity 🐛 fix(database/manager.py): change expected_columns to use model_fields instead of __fields__ to match new SQLModel version 🐛 fix(database/models/base.py): remove unused model_config variable 🐛 fix(database/models/flow/flow.py): change validator decorator to field_validator to match new pydantic version 🐛 fix(settings.py): change root_validator decorator to model_validator to match new pydantic version

--- src/backend/langflow/services/database/models/base.py
+++ src/backend/langflow/services/database/models/base.py
@@ -1,6 +1,5 @@
 from sqlmodel import SQLModel
 import orjson
-from pydantic import ConfigDict
 
 
 def orjson_dumps(v, *, default=None, sort_keys=False, indent_2=True):
@@ -22,8 +21,4 @@ def orjson_dumps(v, *, default=None, sort_keys=False, indent_2=True):
 class SQLModelSerializable(SQLModel):
     # TODO[pydantic]: The following keys were removed: `json_loads`, `json_dumps`.
     # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-config for more information.
-    model_config = ConfigDict(
-        from_attributes=True,
-        # json_loads=orjson.loads,
-        # json_dumps=orjson_dumps
-    )
+    pass
