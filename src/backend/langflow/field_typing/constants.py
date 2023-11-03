e7440f7d578f0d5dd4964cd7f95cd58df56b7fb0
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-03 16:47:44 -0300

    🔧 chore(constants.py): import BasePromptTemplate to improve code organization and maintainability

--- src/backend/langflow/field_typing/constants.py
+++ src/backend/langflow/field_typing/constants.py
@@ -3,7 +3,7 @@ from langchain.chains.base import Chain
 from langchain.document_loaders.base import BaseLoader
 from langchain.llms.base import BaseLLM, BaseLanguageModel
 from langchain.memory.chat_memory import BaseChatMemory
-from langchain.prompts import PromptTemplate, ChatPromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, BasePromptTemplate
 from langchain.schema import BaseOutputParser, BaseRetriever, Document
 from langchain.schema.embeddings import Embeddings
 from langchain.schema.memory import BaseMemory
@@ -28,6 +28,7 @@ LANGCHAIN_BASE_TYPES = {
     "BaseLanguageModel": BaseLanguageModel,
     "PromptTemplate": PromptTemplate,
     "ChatPromptTemplate": ChatPromptTemplate,
+    "BasePromptTemplate": BasePromptTemplate,
     "BaseLoader": BaseLoader,
     "Document": Document,
     "TextSplitter": TextSplitter,
