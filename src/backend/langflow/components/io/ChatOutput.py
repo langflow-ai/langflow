5e9d6891d46014d7bc06a18549a083f293524e70
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-06 16:28:26 -0300

    🔧 refactor(ChatOutput.py): remove unused imports and unused code block in build method 🔧 refactor(ChatOutput.py): change build method signature to accept is_ai as the first argument and message as the second argument to improve readability and consistency

--- src/backend/langflow/components/io/ChatOutput.py
+++ src/backend/langflow/components/io/ChatOutput.py
@@ -1,9 +1,6 @@
-from typing import Optional, Text
-from langflow.api.v1.schemas import ChatMessage
-from langflow.services.getters import get_chat_service
+from typing import Optional
 from langflow import CustomComponent
-from anyio.from_thread import start_blocking_portal
-from loguru import logger
+
 from langflow.field_typing import Text
 
 
@@ -20,21 +17,6 @@ class ChatOutput(CustomComponent):
     def build_config(self):
         return {"message": {"input_types": ["Text"]}}
 
-    def build(self, message: Optional[Text], is_ai: bool = False) -> Text:
-        if not message:
-            return ""
-        try:
-            chat_manager = get_chat_service()
-            chat_message = ChatMessage(message=message, is_bot=is_ai)
-            # send_message is a coroutine
-            # run in a thread safe manner
-            with start_blocking_portal() as portal:
-                portal.call(chat_manager.send_message, chat_message)
-            chat_manager.chat_history.add_message(
-                chat_manager.cache_manager.current_client_id, chat_message
-            )
-        except Exception as exc:
-            logger.exception(exc)
-            logger.debug(f"Error sending message to chat: {exc}")
+    def build(self, is_ai: bool = True, message: Optional[Text] = "") -> Text:
         self.repr_value = message
-        return message
+        return {"message": message, "is_ai": is_ai}
