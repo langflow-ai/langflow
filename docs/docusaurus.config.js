5311b5ee8f2e50a8447eb37c38a042915c9ce326
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-06 18:53:30 +0200

    Use github-dark theme for code examples

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -31,7 +31,7 @@ module.exports = {
             [
               remarkCodeHike,
               {
-                theme: "github-light",
+                theme: "github-dark",
                 showCopyButton: true,
                 lineNumbers: true,
               },
