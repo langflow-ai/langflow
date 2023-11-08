45b2e27c5f2e0846e1b7593b682e0d34843a798c
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-08 15:42:10 -0300

    Update OpenAI conversational agent and Redis component

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -76,14 +76,11 @@ PROMPT_REQUEST = {
                             "text-ada-001",
                         ],
                         "ChatOpenAI": [
-                            "gpt-3.5-turbo-0613",
-                            "gpt-3.5-turbo",
-                            "gpt-3.5-turbo-16k-0613",
-                            "gpt-3.5-turbo-16k",
-                            "gpt-4-0613",
-                            "gpt-4-32k-0613",
+                            "gpt-4-1106-preview",
                             "gpt-4",
                             "gpt-4-32k",
+                            "gpt-3.5-turbo",
+                            "gpt-3.5-turbo-16k",
                         ],
                         "Anthropic": [
                             "claude-v1",
