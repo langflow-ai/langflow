a07000b5fe5e14085194f722ba2ffa3af1495f46
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-08 16:23:23 -0300

    Update langchain-experimental version and dependencies

--- src/backend/langflow/interface/agents/custom.py
+++ src/backend/langflow/interface/agents/custom.py
@@ -16,8 +16,10 @@ from langchain.agents.agent_toolkits import (
 )
 from langchain.agents.agent_toolkits.json.prompt import JSON_PREFIX, JSON_SUFFIX
 from langchain.agents.agent_toolkits.json.toolkit import JsonToolkit
-from langchain.agents.agent_toolkits.pandas.prompt import PREFIX as PANDAS_PREFIX
-from langchain.agents.agent_toolkits.pandas.prompt import (
+from langchain_experimental.agents.agent_toolkits.pandas.prompt import (
+    PREFIX as PANDAS_PREFIX,
+)
+from langchain_experimental.agents.agent_toolkits.pandas.prompt import (
     SUFFIX_WITH_DF as PANDAS_SUFFIX,
 )
 from langchain.agents.agent_toolkits.sql.prompt import SQL_PREFIX, SQL_SUFFIX
