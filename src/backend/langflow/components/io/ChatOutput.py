86a7d54bcfe389ea9cd86c17b4bfc2433e7fbb9a
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-11-09 17:42:53 -0300

    Remove chat history functionality from ChatOutput component.

--- src/backend/langflow/components/io/ChatOutput.py
+++ src/backend/langflow/components/io/ChatOutput.py
@@ -2,9 +2,6 @@ from typing import Optional, Union
 from langflow import CustomComponent
 
 from langflow.field_typing import Text, Data
-import pandas as pd
-from platformdirs import user_cache_dir
-from datetime import datetime
 
 
 class ChatOutput(CustomComponent):
@@ -29,21 +26,6 @@ class ChatOutput(CustomComponent):
         sender: Optional[str] = "Machine",
         sender_name: Optional[str] = "AI",
         message: Optional[Text] = "",
-        file_path: str = "chat_history.json",
     ) -> Union[Text, Data]:
         self.repr_value = message
-        # Load the chat history df
-        chat_history_df = pd.read_json(user_cache_dir("langflow") + "/" + file_path)
-        # Add the message to the df
-        chat_history_df = chat_history_df.append(
-            {
-                "sender": sender,
-                "sender_name": sender_name,
-                "message": message,
-                "timestamp": datetime.utcnow().isoformat(),
-            },
-            ignore_index=True,
-        )
-        # Save the df
-        chat_history_df.to_json(user_cache_dir("langflow") + "/" + file_path)
         return message
