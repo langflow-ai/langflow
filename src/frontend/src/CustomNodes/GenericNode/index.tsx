324aadbed638c244dc13b3afd7d37940a8f255ad
igorrCarvalho <igorsilvabhz6@gmail.com>
2023-11-20 21:03:00 -0300

    Fix: Add tailwind class to avoid text breaking component box

--- src/frontend/src/CustomNodes/GenericNode/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/index.tsx
@@ -408,7 +408,7 @@ export default function GenericNode({
               />
             ) : (
               <div
-                className="generic-node-desc-text"
+                className="generic-node-desc-text break-all"
                 onDoubleClick={() => setInputDescription(true)}
               >
                 {data.node?.description}
