020c9f86d1e77960e9f7b32a6782c4f269d99575
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-12-10 20:27:12 -0300

    Refactor import statements in __init__.py

--- src/backend/langflow/field_typing/__init__.py
+++ src/backend/langflow/field_typing/__init__.py
@@ -1,30 +1,31 @@
-from langflow.template.field.base import TemplateField
+from typing import Any
 
-from .constants import (
-    AgentExecutor,
-    BaseChatMemory,
-    BaseLanguageModel,
-    BaseLLM,
-    BaseLoader,
-    BaseMemory,
-    BaseOutputParser,
-    BasePromptTemplate,
-    BaseRetriever,
-    Callable,
-    Chain,
-    ChatPromptTemplate,
-    Data,
-    Document,
-    Embeddings,
-    NestedDict,
-    Object,
-    Prompt,
-    PromptTemplate,
-    TextSplitter,
-    Tool,
-    VectorStore,
-)
+from .constants import (AgentExecutor, BaseChatMemory, BaseLanguageModel,
+                        BaseLLM, BaseLoader, BaseMemory, BaseOutputParser,
+                        BasePromptTemplate, BaseRetriever, Callable, Chain,
+                        ChatPromptTemplate, Data, Document, Embeddings,
+                        NestedDict, Object, Prompt, PromptTemplate,
+                        TextSplitter, Tool, VectorStore)
 from .range_spec import RangeSpec
+
+
+def import_template_field():
+    from langflow.template.field.base import TemplateField
+    return TemplateField
+
+
+def __getattr__(name: str) -> Any:
+    # This is to avoid circular imports
+    if name == "TemplateField":
+        return import_template_field()
+    elif name == "RangeSpec":
+        return RangeSpec
+    # The other names should work as if they were imported from constants
+    return getattr(constants, name)
+
+
+
+
 
 __all__ = [
     "NestedDict",
