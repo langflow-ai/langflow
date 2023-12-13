12bf89dbac267c6e4375b04559599cfda565d2ef
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-12-13 19:08:35 -0300

    Fix build failure and optimize imports

--- src/backend/langflow/services/cache/factory.py
+++ src/backend/langflow/services/cache/factory.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
-from langflow.services.cache.service import (BaseCacheService, InMemoryCache,
-                                             RedisCache)
+from langflow.services.cache.service import BaseCacheService, InMemoryCache, RedisCache
 from langflow.services.factory import ServiceFactory
 from langflow.utils.logger import logger
 
