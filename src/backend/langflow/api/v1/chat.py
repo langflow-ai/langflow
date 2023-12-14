086a91dadc84c7df062f7406db10ee3270b5859a
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-12-14 17:44:25 -0300

    Pull start_time out of try block

--- src/backend/langflow/api/v1/chat.py
+++ src/backend/langflow/api/v1/chat.py
@@ -1,18 +1,21 @@
 import time
 
-from fastapi import APIRouter, Depends, HTTPException, Query, WebSocket, WebSocketException, status
+from fastapi import (APIRouter, Depends, HTTPException, Query, WebSocket,
+                     WebSocketException, status)
 from fastapi.responses import StreamingResponse
-from loguru import logger
-from sqlmodel import Session
-
 from langflow.api.utils import build_input_keys_response, format_elapsed_time
-from langflow.api.v1.schemas import BuildStatus, BuiltResponse, InitResponse, StreamData
+from langflow.api.v1.schemas import (BuildStatus, BuiltResponse, InitResponse,
+                                     StreamData)
 from langflow.graph.graph.base import Graph
-from langflow.services.auth.utils import get_current_active_user, get_current_user_by_jwt
+from langflow.services.auth.utils import (get_current_active_user,
+                                          get_current_user_by_jwt)
 from langflow.services.cache.service import BaseCacheService
 from langflow.services.cache.utils import update_build_status
 from langflow.services.chat.service import ChatService
-from langflow.services.deps import get_cache_service, get_chat_service, get_session
+from langflow.services.deps import (get_cache_service, get_chat_service,
+                                    get_session)
+from loguru import logger
+from sqlmodel import Session
 
 router = APIRouter(tags=["Chat"])
 
@@ -149,13 +152,12 @@ async def stream_build(
                 logger.debug("No user_id found in cache_service")
                 user_id = None
             for i, vertex in enumerate(graph.generator_build(), 1):
+                start_time = time.perf_counter()
                 try:
                     log_dict = {
                         "log": f"Building node {vertex.vertex_type}",
                     }
                     yield str(StreamData(event="log", data=log_dict))
-                    # time this
-                    start_time = time.perf_counter()
                     if vertex.is_task:
                         vertex = await try_running_celery_task(vertex, user_id)
                     else:
