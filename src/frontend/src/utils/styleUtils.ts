99e048862574829cdd3bfa0d2a0abb405745a619
Yassine Selmi <yassine.selmi@emt.inrs.ca>
2023-12-20 20:01:30 +0000

    Fixed wrong style attribute

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -251,7 +251,7 @@ export const nodeIconsLucide: iconsType = {
   MongoDBAtlasVectorSearch: MongoDBIcon,
   NotionDirectoryLoader: NotionIcon,
   ChatOpenAI: OpenAiIcon,
-  AzureChatOpenAIComponent: OpenAiIcon,
+  AzureChatOpenAI: OpenAiIcon,
   OpenAI: OpenAiIcon,
   OpenAIEmbeddings: OpenAiIcon,
   Pinecone: PineconeIcon,
