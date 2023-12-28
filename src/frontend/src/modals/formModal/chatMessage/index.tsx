1fb513bd3244cf11b18948ec7e42a421ead2e124
cristhianzl <cristhian.lousa@gmail.com>
2023-12-28 10:57:02 -0300

    fix(dropdownComponent): add condition to check if options object is empty before rendering dropdown component fix(chatInput): add condition to check if chatValue is an empty object before displaying message fix(chatMessage): remove useMemo hook and fix condition to check if chat message is empty before displaying message fix(formModal): add conditions to check if thought and files exist before updating chat object fix(genericModal): add condition to check if inputVariables exist before displaying notice or success message

--- src/frontend/src/modals/formModal/chatMessage/index.tsx
+++ src/frontend/src/modals/formModal/chatMessage/index.tsx
@@ -1,5 +1,5 @@
 import Convert from "ansi-to-html";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeMathjax from "rehype-mathjax";
 import remarkGfm from "remark-gfm";
@@ -74,77 +74,71 @@ export default function ChatMessage({
             <div className="w-full">
               <div className="w-full dark:text-white">
                 <div className="w-full">
-                  {useMemo(
-                    () =>
-                      chat.message.toString() === "" && lockChat ? (
-                        <IconComponent
-                          name="MoreHorizontal"
-                          className="h-8 w-8 animate-pulse"
-                        />
-                      ) : (
-                        <ReactMarkdown
-                          remarkPlugins={[remarkGfm, remarkMath]}
-                          rehypePlugins={[rehypeMathjax]}
-                          className="markdown prose min-w-full text-primary word-break-break-word
+                  {chat.message.toString() === "" && lockChat ? (
+                    <IconComponent
+                      name="MoreHorizontal"
+                      className="h-8 w-8 animate-pulse"
+                    />
+                  ) : (
+                    <ReactMarkdown
+                      remarkPlugins={[remarkGfm, remarkMath]}
+                      rehypePlugins={[rehypeMathjax]}
+                      className="markdown prose min-w-full text-primary word-break-break-word
                       dark:prose-invert"
-                          components={{
-                            pre({ node, ...props }) {
-                              return <>{props.children}</>;
-                            },
-                            code: ({
-                              node,
-                              inline,
-                              className,
-                              children,
-                              ...props
-                            }) => {
-                              if (children.length) {
-                                if (children[0] === "▍") {
-                                  return (
-                                    <span className="form-modal-markdown-span">
-                                      ▍
-                                    </span>
-                                  );
-                                }
-
-                                children[0] = (children[0] as string).replace(
-                                  "`▍`",
-                                  "▍"
-                                );
-                              }
-
-                              const match = /language-(\w+)/.exec(
-                                className || ""
+                      components={{
+                        pre({ node, ...props }) {
+                          return <>{props.children}</>;
+                        },
+                        code: ({
+                          node,
+                          inline,
+                          className,
+                          children,
+                          ...props
+                        }) => {
+                          if (children.length) {
+                            if (children[0] === "▍") {
+                              return (
+                                <span className="form-modal-markdown-span">
+                                  ▍
+                                </span>
                               );
+                            }
 
-                              return !inline ? (
-                                <CodeTabsComponent
-                                  isMessage
-                                  tabs={[
-                                    {
-                                      name: (match && match[1]) || "",
-                                      mode: (match && match[1]) || "",
-                                      image:
-                                        "https://curl.se/logo/curl-symbol-transparent.png",
-                                      language: (match && match[1]) || "",
-                                      code: String(children).replace(/\n$/, ""),
-                                    },
-                                  ]}
-                                  activeTab={"0"}
-                                  setActiveTab={() => {}}
-                                />
-                              ) : (
-                                <code className={className} {...props}>
-                                  {children}
-                                </code>
-                              );
-                            },
-                          }}
-                        >
-                          {chat.message.toString()}
-                        </ReactMarkdown>
-                      ),
-                    [chat.message, chat.message.toString()]
+                            children[0] = (children[0] as string).replace(
+                              "`▍`",
+                              "▍"
+                            );
+                          }
+
+                          const match = /language-(\w+)/.exec(className || "");
+
+                          return !inline ? (
+                            <CodeTabsComponent
+                              isMessage
+                              tabs={[
+                                {
+                                  name: (match && match[1]) || "",
+                                  mode: (match && match[1]) || "",
+                                  image:
+                                    "https://curl.se/logo/curl-symbol-transparent.png",
+                                  language: (match && match[1]) || "",
+                                  code: String(children).replace(/\n$/, ""),
+                                },
+                              ]}
+                              activeTab={"0"}
+                              setActiveTab={() => {}}
+                            />
+                          ) : (
+                            <code className={className} {...props}>
+                              {children}
+                            </code>
+                          );
+                        },
+                      }}
+                    >
+                      {chat.message.toString()}
+                    </ReactMarkdown>
                   )}
                 </div>
                 {chat.files && (
