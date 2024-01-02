adf7174107ae21c8070bb711cfb0946e23c13d3e
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-01-02 08:11:10 -0300

    Add input_keys and prompt attributes to inner function

--- src/backend/langflow/components/llamaindex/query_chain.py
+++ src/backend/langflow/components/llamaindex/query_chain.py
@@ -1,22 +1,15 @@
 """Query Chain."""
 
-from typing import Optional, List, cast, Callable, Union
+from typing import Callable, Union
+
 from langflow import CustomComponent
-from langflow.utils.util import build_loader_repr_from_documents
-from llama_index.schema import Document, TextNode
-from llama_index.node_parser import SentenceSplitter
-from llama_index import VectorStoreIndex, ServiceContext
-from langflow.field_typing import Object, BaseLanguageModel, Chain
-from llama_index.retrievers import VectorIndexRetriever
-from llama_index.query_engine import RetrieverQueryEngine
-from llama_index.llms import LangChainLLM
-from langflow.field_typing import BasePromptTemplate
+from langflow.field_typing import BasePromptTemplate, Chain, Object
 
 
 class QueryChainComponent(CustomComponent):
-    display_name: str = "Query Chain" 
+    display_name: str = "Query Chain"
     description: str = "Synthesizes an answer from a query engine."
-    
+
     def build_config(self):
         return {
             "query_engine": {
@@ -28,16 +21,19 @@ class QueryChainComponent(CustomComponent):
                 "info": "The prompt to use",
             },
         }
-    
+
     def build(
         self,
         query_engine: Object,
         prompt: BasePromptTemplate,
     ) -> Union[Chain, Callable]:
         """Build."""
+
         def query_chain_fn(*args, **kwargs) -> str:
             fmt_prompt = prompt.format(**kwargs)
             return str(query_engine.query(fmt_prompt))
 
+        query_chain_fn.input_keys = prompt.input_variables
+        query_chain_fn.prompt = prompt
+
         return query_chain_fn
-        
