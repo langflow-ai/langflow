fbd64033dc955940b091ddd97bad30325798363e
anovazzi1 <otavio2204@gmail.com>
2024-01-15 20:25:43 -0300

    Update ZeroShotAgent and OpenAIComponent

--- src/backend/langflow/components/agents/ZeroShotAgent.py
+++ src/backend/langflow/components/agents/ZeroShotAgent.py
@@ -3,9 +3,7 @@ from langflow import CustomComponent
 from langchain.agents import ZeroShotAgent
 from langchain_core.tools import BaseTool
 from typing import List, Optional
-from langflow.field_typing import (
-    BaseLanguageModel,
-)
+from langflow.components.chains.LLMChain import LLMChain
 
 class ZeroShotAgentComponent(CustomComponent):
     display_name = "ZeroShotAgent"
@@ -21,9 +19,9 @@ class ZeroShotAgentComponent(CustomComponent):
 
     def build(
         self,
-        llm: BaseLanguageModel,
+        llm: LLMChain,
         tools: List[BaseTool],
         prefix: Optional[str] = "Answer the following questions as best you can. You have access to the following tools:",
         suffix: Optional[str] = "Begin!\n\nQuestion: {input}\nThought:{agent_scratchpad}",
     ) -> ZeroShotAgent:
-        return ZeroShotAgent(llm=llm, tools=tools, prefix=prefix, suffix=suffix)
+        return ZeroShotAgent(llm_chain=llm, tools=tools, prefix=prefix, suffix=suffix)
