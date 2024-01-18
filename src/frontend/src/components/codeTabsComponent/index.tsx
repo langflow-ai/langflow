7a0bc1e3ba66f56480c863f270deabff773b632e
anovazzi1 <otavio2204@gmail.com>
2024-01-18 14:47:43 -0300

    Add unselectAll function to CodeTabsComponent and FlowStore

--- src/frontend/src/components/codeTabsComponent/index.tsx
+++ src/frontend/src/components/codeTabsComponent/index.tsx
@@ -35,7 +35,6 @@ import {
   convertObjToArray,
   convertValuesToNumbers,
   hasDuplicateKeys,
-  unselectAllNodes,
 } from "../../utils/reactflowUtils";
 import { classNames } from "../../utils/utils";
 import DictComponent from "../dictComponent";
@@ -54,6 +53,7 @@ export default function CodeTabsComponent({
   const [data, setData] = useState(flow ? flow["data"]!["nodes"] : null);
   const [openAccordion, setOpenAccordion] = useState<string[]>([]);
   const dark = useDarkStore((state) => state.dark);
+  const unselectAll = useFlowStore((state) => state.unselectAll);
 
   const setNodes = useFlowStore((state) => state.setNodes);
 
@@ -67,12 +67,7 @@ export default function CodeTabsComponent({
 
   useEffect(() => {
     if (tweaks && data) {
-      unselectAllNodes({
-        data,
-        updateNodes: (nodes) => {
-          setNodes(nodes);
-        },
-      });
+      unselectAll();
     }
   }, []);
 
