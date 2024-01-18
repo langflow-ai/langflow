5e5a64872c0fc9ea27094c83d6439c25c3727fde
anovazzi1 <otavio2204@gmail.com>
2024-01-18 16:38:46 -0300

    chore(flowsIOStore.ts): remove unused variables and functions for better code readability and maintainability refactor(flowsIOStore.ts): remove unused variables and functions to improve code quality and reduce complexity

--- src/frontend/src/types/zustand/flowIOStore/index.ts
+++ src/frontend/src/types/zustand/flowIOStore/index.ts
@@ -1,6 +1,4 @@
-import { ReactFlowInstance } from "reactflow";
-import { tweakType } from "../../components";
-import { FlowType, NodeDataType, NodeType } from "../../flow";
+import { FlowType, NodeType } from "../../flow";
 
 export type chatInputType = {
   result: string;
@@ -25,37 +23,22 @@ export type FlowPoolType = {
 };
 
 export type flowIOStoreType = {
-  actualFlow: null | FlowType;
   flowPool: FlowPoolType;
-  getFilterEdge: any[];
-  isBuilt: boolean;
   inputTypes: string[];
   outputTypes: string[];
   inputIds: string[];
   outputIds: string[];
-  isBuilding: boolean;
   setFlowPool: (flowPool: FlowPoolType) => void;
-  setIsBuilding: (state: boolean) => void;
-  setIsBuilt: (state: boolean) => void;
   setOutputTypes: (outputTypes: string[]) => void;
   setInputTypes: (inputTypes: string[]) => void;
   setInputIds: (inputIds: string[]) => void;
   setOutputIds: (outputIds: string[]) => void;
-  setFilterEdge: (newState) => void;
   updateFlowPoolNodes: (nodes: NodeType[]) => void;
   addDataToFlowPool: (data: any, nodeId: string) => void;
   CleanFlowPool: () => void;
-  updateNodeFlowData: (nodeId: string, newData: NodeDataType) => void;
   checkInputandOutput: (flow?: FlowType) => boolean;
   getInputTypes: (flow?: FlowType) => string[];
   getOutputTypes: (flow?: FlowType) => string[];
   getInputIds: (flow?: FlowType) => string[];
   getOutputIds: (flow?: FlowType) => string[];
-  pasteFileOnFLow: (file?: File) => Promise<void>;
-  downloadFlow: (
-    flow: FlowType,
-    flowName: string,
-    flowDescription?: string
-  ) => void;
-  buildFlow: (nodeId?:string) => Promise<void>;
 };
