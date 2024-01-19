e7629226d379295f2c5282ef704a3f30a34abdcb
anovazzi1 <otavio2204@gmail.com>
2024-01-19 17:59:00 -0300

    Add process_tweaks_on_graph function to chat.py

--- src/backend/langflow/api/v1/chat.py
+++ src/backend/langflow/api/v1/chat.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Query, WebSocket, WebSock
 from fastapi.responses import StreamingResponse
 from langflow.api.utils import build_input_keys_response, format_elapsed_time
 from langflow.api.v1.schemas import BuildStatus, BuiltResponse, InitResponse, ResultDict, StreamData, VerticesOrderResponse
+from langflow.processing.process import process_tweaks_on_graph
 from langflow.services.database.models.flow.flow import Flow
 from langflow.graph.graph.base import Graph
 from langflow.services.auth.utils import get_current_active_user, get_current_user_by_jwt
