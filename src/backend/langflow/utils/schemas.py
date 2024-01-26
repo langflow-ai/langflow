7eac47f4c87e95780766dfea7a74b3ab84664e01
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-01-26 17:08:10 -0300

    Add support for message conversion in ChatOutputResponse class

--- src/backend/langflow/utils/schemas.py
+++ src/backend/langflow/utils/schemas.py
@@ -1,10 +1,18 @@
+from typing import Dict, List, Optional, Union
+
+from langchain_core.messages import BaseMessage
 from pydantic import BaseModel
-from typing import Optional
 
 
 class ChatOutputResponse(BaseModel):
     """Chat output response schema."""
 
-    message: str
+    message: Union[str, List[Union[str, Dict]]]
     sender: Optional[str] = "Machine"
     sender_name: Optional[str] = "AI"
+
+    @classmethod
+    def from_message(cls, message: BaseMessage, sender: Optional[str] = "Machine", sender_name: Optional[str] = "AI"):
+        """Build chat output response from message."""
+        content = message.content
+        return cls(message=content, sender=sender, sender_name=sender_name)
