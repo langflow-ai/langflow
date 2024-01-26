ec48d12f1c808d30b84f2a8878de8c5f52b7e2d2
anovazzi1 <otavio2204@gmail.com>
2024-01-26 15:31:21 -0300

    Add IOFileInput component to IOInputField

---
+++ src/frontend/src/components/IOInputs/FileInput/index.tsx
@@ -0,0 +1,7 @@
+import { IOFileInputProps } from "../../../types/components";
+
+export default function IOFileInput({ field, updateValue }: IOFileInputProps) {
+  //component to handle file upload from chatIO
+  function handleCLick() {}
+  return <div></div>;
+}
