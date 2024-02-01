8f6cab7ed36e94c1c84b8e2e4932a5c635ac255c
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-02-01 12:13:51 -0300

    Add support for external input and output in Vertex class

--- src/backend/langflow/graph/vertex/utils.py
+++ src/backend/langflow/graph/vertex/utils.py
@@ -1,5 +1,39 @@
+from typing import Any, Optional
+
+from langchain_core.messages import BaseMessage
+from langchain_core.runnables import Runnable
+from langflow.services.deps import Union, get_socket_service
 from langflow.utils.constants import PYTHON_BASIC_TYPES
 
 
 def is_basic_type(obj):
     return type(obj) in PYTHON_BASIC_TYPES
+
+
+async def invoke_lc_runnable(
+    built_object: Runnable, inputs: dict, has_external_output: bool, session_id: Optional[str] = None
+) -> Union[str, BaseMessage]:
+    if has_external_output:
+        socketio_service = get_socket_service()
+        result = ""
+        stream = built_object.astream(inputs)
+        async for chunk in stream:
+            await socketio_service.emit_token(session_id, chunk)
+            result += chunk
+    return built_object.invoke(inputs)
+
+
+async def generate_result(built_object: Any, inputs: dict, has_external_output: bool, session_id: Optional[str] = None):
+    # If the built_object is instance of Runnable
+    # we can call `invoke` or `stream` on it
+    # if it has_external_outputl, we need to call `stream` if it has it
+    # if not, we call `invoke` if it has it
+    if isinstance(built_object, Runnable):
+        result = await invoke_lc_runnable(
+            built_object=built_object, inputs=inputs, has_external_output=has_external_output, session_id=session_id
+        )
+    else:
+        result = built_object
+    return result
+        result = built_object
+    return result
