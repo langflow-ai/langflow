c3a71f408dcb20ec5810ff2968ee9667c715e6c0
anovazzi1 <otavio2204@gmail.com>
2024-02-08 14:16:03 -0300

    Refactor globalVariablesStore to use optional provider

--- src/frontend/src/types/zustand/globalVariables/index.ts
+++ src/frontend/src/types/zustand/globalVariables/index.ts
@@ -1,9 +1,9 @@
 export type GlobalVariablesStore = {
   globalVariablesEntries: Array<string>;
-  globalVariables: { [key: string]: { id: string; provider: string } };
+  globalVariables: { [key: string]: { id: string; provider?: string } };
   setGlobalVariables: (variables: {
-    [key: string]: { id: string; provider: string };
+    [key: string]: { id: string; provider?: string };
   }) => void;
-  addGlobalVariable: (key: string, value: string, provider?: string) => void;
+  addGlobalVariable: (key: string, id: string, provider?: string) => void;
   removeGlobalVariable: (key: string) => void;
 };
