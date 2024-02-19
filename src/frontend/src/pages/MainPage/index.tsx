f8e4fe1d7c19b5cecef34349bb1cb6e325a95b1e
cristhianzl <cristhian.lousa@gmail.com>
2024-02-19 11:01:38 -0300

    add new error page

--- src/frontend/src/pages/MainPage/index.tsx
+++ src/frontend/src/pages/MainPage/index.tsx
@@ -6,6 +6,7 @@ import IconComponent from "../../components/genericIconComponent";
 import PageLayout from "../../components/pageLayout";
 import SidebarNav from "../../components/sidebarComponent";
 import { Button } from "../../components/ui/button";
+import { USER_PROJECTS_HEADER } from "../../constants/constants";
 import useAlertStore from "../../stores/alertStore";
 import useFlowsManagerStore from "../../stores/flowsManagerStore";
 import { downloadFlows } from "../../utils/reactflowUtils";
@@ -69,7 +70,7 @@ export default function HomePage(): JSX.Element {
   // Personal flows display
   return (
     <PageLayout
-      title={USER_PROJECTS_HEADERdasd}
+      title={USER_PROJECTS_HEADER}
       description="Manage your personal projects. Download or upload your collection."
       button={
         <div className="flex gap-2">
