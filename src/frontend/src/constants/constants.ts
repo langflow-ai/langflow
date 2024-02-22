138aa1c5fea102a7fa70bdca4e9a46af63ea0a90
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-02-22 13:08:52 +0100

    Removed prompt template from outputs

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -681,4 +681,4 @@ export const LANGFLOW_SUPPORTED_TYPES = new Set([
 export const priorityFields = new Set(["code", "template"]);
 
 export const INPUT_TYPES = new Set(["ChatInput", "TextInput"]);
-export const OUTPUT_TYPES = new Set(["ChatOutput", "PromptTemplate"]);
+export const OUTPUT_TYPES = new Set(["ChatOutput"]);
