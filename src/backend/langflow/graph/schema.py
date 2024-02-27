9beadd70f19ef8bb794c2469d949603857e9a5f1
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-02-27 11:37:27 -0300

    Move ResultData to graph

--- src/backend/langflow/graph/schema.py
+++ src/backend/langflow/graph/schema.py
@@ -1,8 +1,37 @@
 from enum import Enum
+from typing import Any, Optional
+from langflow.graph.utils import serialize_field
+
+from pydantic import BaseModel, Field, field_serializer
 
 
-class InterfaceComponentTypes(Enum):
+class ResultData(BaseModel):
+    results: Optional[Any] = Field(default_factory=dict)
+    artifacts: Optional[Any] = Field(default_factory=dict)
+    timedelta: Optional[float] = None
+    duration: Optional[str] = None
+
+    @field_serializer("results")
+    def serialize_results(self, value):
+        if isinstance(value, dict):
+            return {key: serialize_field(val) for key, val in value.items()}
+        return serialize_field(value)
+
+
+class InterfaceComponentTypes(str, Enum):
     # ChatInput and ChatOutput are the only ones that are
     # power components
     ChatInput = "ChatInput"
     ChatOutput = "ChatOutput"
+    TextInput = "TextInput"
+    TextOutput = "TextOutput"
+
+
+INPUT_COMPONENTS = [
+    InterfaceComponentTypes.ChatInput,
+    InterfaceComponentTypes.TextInput,
+]
+OUTPUT_COMPONENTS = [
+    InterfaceComponentTypes.ChatOutput,
+    InterfaceComponentTypes.TextOutput,
+]
