8e163cdd519fc189ac20257e0b26981a406c0ba4
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-02-27 15:34:25 -0300

    Refactor ChatInput and ChatOutput components

--- src/backend/langflow/components/io/ChatOutput.py
+++ src/backend/langflow/components/io/ChatOutput.py
@@ -9,55 +9,18 @@ class ChatOutput(CustomComponent):
     display_name = "Chat Output"
     description = "Used to send a message to the chat."
 
-    field_config = {
-        "code": {
-            "show": True,
-        }
-    }
-
-    def build_config(self):
-        return {
-            "input_value": {"input_types": ["Text"], "display_name": "Message"},
-            "sender": {
-                "options": ["Machine", "User"],
-                "display_name": "Sender Type",
-            },
-            "sender_name": {"display_name": "Sender Name"},
-            "session_id": {
-                "display_name": "Session ID",
-                "info": "Session ID of the chat history.",
-                "input_types": ["Text"],
-            },
-            "return_record": {
-                "display_name": "Return Record",
-                "info": "Return the message as a record containing the sender, sender_name, and session_id.",
-            },
-        }
-
     def build(
         self,
-        sender: Optional[str] = "Machine",
-        sender_name: Optional[str] = "AI",
-        session_id: Optional[str] = None,
+        sender: Optional[str] = "User",
+        sender_name: Optional[str] = "User",
         input_value: Optional[str] = None,
+        session_id: Optional[str] = None,
         return_record: Optional[bool] = False,
     ) -> Union[Text, Record]:
-        if return_record:
-            if isinstance(input_value, Record):
-                # Update the data of the record
-                input_value.data["sender"] = sender
-                input_value.data["sender_name"] = sender_name
-                input_value.data["session_id"] = session_id
-            else:
-                input_value = Record(
-                    text=input_value,
-                    data={
-                        "sender": sender,
-                        "sender_name": sender_name,
-                        "session_id": session_id,
-                    },
-                )
-        if not input_value:
-            input_value = ""
-        self.status = input_value
-        return input_value
+        return super().build(
+            sender=sender,
+            sender_name=sender_name,
+            input_value=input_value,
+            session_id=session_id,
+            return_record=return_record,
+        )
