92c29c793e89d848c411a7f3fe8f58d8d758a033
anovazzi1 <otavio2204@gmail.com>
2024-02-28 17:59:43 -0300

    Add VertexAI icon to ChatVertexAI and VertexAI components

--- src/backend/langflow/components/models/VertexAiModel.py
+++ src/backend/langflow/components/models/VertexAiModel.py
@@ -9,6 +9,8 @@ from langflow.field_typing import Text
 class ChatVertexAIComponent(LCModelComponent):
     display_name = "ChatVertexAIModel"
     description = "Generate text using Vertex AI Chat large language models API."
+    icon="VertexAI"
+
 
     def build_config(self):
         return {
