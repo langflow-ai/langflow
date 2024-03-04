07d2f52e9eefd946b0b201b391fe2b1fad0d3e62
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-04 10:11:54 -0300

    Add ListFlows and RunFlow components

---
+++ src/backend/langflow/components/utilities/ListFlows.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from langflow import CustomComponent
+from langflow.schema import Record
+
+
+class ListFlowsComponent(CustomComponent):
+    display_name = "List Flows"
+    description = "A component to list all available flows."
+
+    def build_config(self):
+        return {}
+
+    def build(
+        self,
+    ) -> List[Record]:
+        flows = self.list_flows()
+        self.status = flows
+        return flows
