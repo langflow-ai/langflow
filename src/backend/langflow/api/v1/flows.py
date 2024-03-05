064e16cb7758db7283b031e7978a5dd806fd7122
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-05 17:41:39 -0300

    Refactor flow reading logic and handle exception when retrieving example flows

--- src/backend/langflow/api/v1/flows.py
+++ src/backend/langflow/api/v1/flows.py
@@ -5,17 +5,14 @@ from uuid import UUID
 import orjson
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from fastapi.encoders import jsonable_encoder
+from loguru import logger
 from sqlmodel import Session, select
 
 from langflow.api.utils import remove_api_keys, validate_is_component
 from langflow.api.v1.schemas import FlowListCreate, FlowListRead
 from langflow.services.auth.utils import get_current_active_user
-from langflow.services.database.models.flow import (
-    Flow,
-    FlowCreate,
-    FlowRead,
-    FlowUpdate,
-)
+from langflow.services.database.models.flow import (Flow, FlowCreate, FlowRead,
+                                                    FlowUpdate)
 from langflow.services.database.models.user.model import User
 from langflow.services.deps import get_session, get_settings_service
 
@@ -54,8 +51,11 @@ def read_flows(
         flows = current_user.flows
         flows = validate_is_component(flows)
         # with the session get the flows that DO NOT have a user_id
-        example_flows = session.exec(select(Flow).where(Flow.user_id == None)).all()
-        flows.extend(example_flows)
+        try:
+            example_flows = session.exec(select(Flow).where(Flow.user_id == None)).all()
+            flows.extend(example_flows)
+        except Exception as e:
+            logger.error(e)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
     return [jsonable_encoder(flow) for flow in flows]
