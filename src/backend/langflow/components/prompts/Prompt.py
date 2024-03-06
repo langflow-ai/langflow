0b3f079e3233d8c58178c96193203c2be122bb0b
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-06 18:45:51 -0300

    Refactor PromptComponent to convert dictionary values to strings

--- src/backend/langflow/components/prompts/Prompt.py
+++ src/backend/langflow/components/prompts/Prompt.py
@@ -1,6 +1,7 @@
 from langchain_core.prompts import PromptTemplate
 
 from langflow import CustomComponent
+from langflow.components.prompts.base.utils import dict_values_to_string
 from langflow.field_typing import Prompt, TemplateField, Text
 
 
@@ -21,16 +22,13 @@ class PromptComponent(CustomComponent):
         **kwargs,
     ) -> Text:
         prompt_template = PromptTemplate.from_template(Text(template))
-
-        attributes_to_check = ["text", "page_content"]
-        for key, value in kwargs.copy().items():
-            for attribute in attributes_to_check:
-                if hasattr(value, attribute):
-                    kwargs[key] = getattr(value, attribute)
-
+        kwargs = dict_values_to_string(kwargs)
+        kwargs = {
+            k: "\n".join(v) if isinstance(v, list) else v for k, v in kwargs.items()
+        }
         try:
             formated_prompt = prompt_template.format(**kwargs)
         except Exception as exc:
             raise ValueError(f"Error formatting prompt: {exc}") from exc
-        self.status = f'Prompt: "{formated_prompt}"'
+        self.status = f'Prompt:\n"{formated_prompt}"'
         return formated_prompt
