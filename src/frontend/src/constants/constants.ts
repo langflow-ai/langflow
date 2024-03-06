f1eb6ee6730e6fb7ab03e7a2a3ec95f0f5f6010f
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-05 21:54:48 -0300

    Add STARTER_FOLDER_NAME constant

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -727,7 +727,7 @@ export const STATUS_BUILD = "Build to validate status.";
 export const STATUS_BUILDING = "Building...";
 export const SAVED_HOVER = "Last saved at ";
 export const RUN_TIMESTAMP_PREFIX = "Last Run: ";
-
+export const STARTER_FOLDER_NAME = "Starter Projects";
 export const PRIORITY_SIDEBAR_ORDER = [
   "saved_components",
   "inputs",
