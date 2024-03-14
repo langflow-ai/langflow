f3a2e300b849c8b8c714068874cbb1754b8cefd5
cristhianzl <cristhian.lousa@gmail.com>
2024-03-14 17:49:27 -0300

    separate function csv convertion

--- src/frontend/src/components/csvOutputComponent/index.tsx
+++ src/frontend/src/components/csvOutputComponent/index.tsx
@@ -2,6 +2,7 @@ import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the 
 import "ag-grid-community/styles/ag-theme-balham.css"; // Optional Theme applied to the grid
 import { AgGridReact } from "ag-grid-react";
 import { useState } from "react";
+import { convertCSVToData } from "./helpers/convert-data-function";
 
 function CsvOutputComponent() {
   const [rowData, setRowData] = useState([]);
@@ -12,35 +13,12 @@ function CsvOutputComponent() {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = (e) => {
-      const contents = e.target.result;
+      const contents = e.target!.result;
       const { rowData: data, colDefs: columns } = convertCSVToData(contents);
       setRowData(data);
       setColDefs(columns);
     };
     reader.readAsText(file);
-  };
-
-  // Function to convert CSV to data
-  const convertCSVToData = (csvFile) => {
-    const lines = csvFile.split("\n");
-    const headers = lines[0].trim().split(";");
-
-    const initialRowData = [];
-    const initialColDefs = headers.map((header) => ({ field: header.trim() }));
-
-    for (let i = 1; i < lines.length; i++) {
-      const data = lines[i].trim().split(";");
-      const rowDataEntry = {};
-
-      for (let j = 0; j < headers.length; j++) {
-        const value = isNaN(data[j]) ? data[j] : parseFloat(data[j]);
-        rowDataEntry[headers[j].trim()] = value;
-      }
-
-      initialRowData.push(rowDataEntry);
-    }
-
-    return { rowData: initialRowData, colDefs: initialColDefs };
   };
 
   return (
