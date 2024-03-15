b702e52f2ca0a1b5ca43d48145d53fa2c06b5bb3
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-03-15 09:25:44 +0100

    Working on mongodb

--- src/backend/langflow/components/vectorstores/MongoDBAtlasVectorSearch.py
+++ src/backend/langflow/components/vectorstores/MongoDBAtlasVectorSearch.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.vectorstores.mongodb_atlas import MongoDBAtlasVectorSearch
 
 from langflow import CustomComponent
 from langflow.field_typing import (
@@ -12,11 +12,11 @@ from langflow.field_typing import (
 
 class MongoDBAtlasComponent(CustomComponent):
     display_name = "MongoDB Atlas"
-    description = "Construct a `MongoDB Atlas Vector Search` vector store from raw documents."
+    description = "a `MongoDB Atlas Vector Search` vector store from raw documents."
 
     def build_config(self):
         return {
-            "documents": {"display_name": "Documents"},
+            "documents": {"display_name": "Documents", "is_list": True},
             "embedding": {"display_name": "Embedding"},
             "collection_name": {"display_name": "Collection Name"},
             "db_name": {"display_name": "Database Name"},
@@ -27,8 +27,8 @@ class MongoDBAtlasComponent(CustomComponent):
 
     def build(
         self,
-        documents: List[Document],
         embedding: Embeddings,
+        documents: Optional[List[Document]] = None,
         collection_name: str = "",
         db_name: str = "",
         index_name: str = "",
@@ -36,12 +36,25 @@ class MongoDBAtlasComponent(CustomComponent):
         search_kwargs: Optional[NestedDict] = None,
     ) -> MongoDBAtlasVectorSearch:
         search_kwargs = search_kwargs or {}
-        return MongoDBAtlasVectorSearch(
-            documents=documents,
+        vector_store = MongoDBAtlasVectorSearch.from_connection_string(
+            connection_string=mongodb_atlas_cluster_uri,
+            namespace=f"{db_name}.{collection_name}",
             embedding=embedding,
-            collection_name=collection_name,
-            db_name=db_name,
             index_name=index_name,
-            mongodb_atlas_cluster_uri=mongodb_atlas_cluster_uri,
-            search_kwargs=search_kwargs,
         )
+
+        if documents:
+            pass
+
+        return vector_store
+
+
+        # return MongoDBAtlasVectorSearch(
+        #     documents=documents,
+        #     embedding=embedding,
+        #     collection_name=collection_name,
+        #     db_name=db_name,
+        #     index_name=index_name,
+        #     mongodb_atlas_cluster_uri=mongodb_atlas_cluster_uri,
+        #     search_kwargs=search_kwargs,
+        # )
