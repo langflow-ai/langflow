6ab4e90f771cdeee410e3e5113538ac6a828008d
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-03-22 17:18:29 +0100

    Added global input component to modularize

--- src/frontend/src/CustomNodes/GenericNode/components/parameterComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/parameterComponent/index.tsx
@@ -2,17 +2,13 @@ import { cloneDeep } from "lodash";
 import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { Handle, Position, useUpdateNodeInternals } from "reactflow";
 import ShadTooltip from "../../../../components/ShadTooltipComponent";
-import AddNewVariableButton from "../../../../components/addNewVariableButtonComponent/addNewVariableButton";
 import CodeAreaComponent from "../../../../components/codeAreaComponent";
 import DictComponent from "../../../../components/dictComponent";
 import Dropdown from "../../../../components/dropdownComponent";
 import FloatComponent from "../../../../components/floatComponent";
-import {
-  default as ForwardedIconComponent,
-  default as IconComponent,
-} from "../../../../components/genericIconComponent";
-import InputComponent from "../../../../components/inputComponent";
+import { default as IconComponent } from "../../../../components/genericIconComponent";
 import InputFileComponent from "../../../../components/inputFileComponent";
+import InputGlobalComponent from "../../../../components/inputGlobalComponent";
 import InputListComponent from "../../../../components/inputListComponent";
 import IntComponent from "../../../../components/intComponent";
 import KeypairListComponent from "../../../../components/keypairListComponent";
@@ -20,7 +16,6 @@ import PromptAreaComponent from "../../../../components/promptComponent";
 import TextAreaComponent from "../../../../components/textAreaComponent";
 import ToggleShadComponent from "../../../../components/toggleShadComponent";
 import { Button } from "../../../../components/ui/button";
-import { CommandItem } from "../../../../components/ui/command";
 import { RefreshButton } from "../../../../components/ui/refreshButton";
 import {
   INPUT_HANDLER_HOVER,
@@ -28,18 +23,11 @@ import {
   OUTPUT_HANDLER_HOVER,
   TOOLTIP_EMPTY,
 } from "../../../../constants/constants";
-import { deleteGlobalVariable } from "../../../../controllers/API";
-import DeleteConfirmationModal from "../../../../modals/DeleteConfirmationModal";
 import useAlertStore from "../../../../stores/alertStore";
 import useFlowStore from "../../../../stores/flowStore";
 import useFlowsManagerStore from "../../../../stores/flowsManagerStore";
-import { useGlobalVariablesStore } from "../../../../stores/globalVariables";
 import { useTypesStore } from "../../../../stores/typesStore";
-import {
-  APIClassType,
-  ResponseErrorDetailAPI,
-  ResponseErrorTypeAPI,
-} from "../../../../types/api";
+import { APIClassType, ResponseErrorTypeAPI } from "../../../../types/api";
 import { ParameterComponentType } from "../../../../types/components";
 import {
   handleUpdateValues,
@@ -57,7 +45,7 @@ import {
   nodeIconsLucide,
   nodeNames,
 } from "../../../../utils/styleUtils";
-import { classNames, cn, groupByFamily } from "../../../../utils/utils";
+import { classNames, groupByFamily } from "../../../../utils/utils";
 
 export default function ParameterComponent({
   left,
@@ -83,49 +71,6 @@ export default function ParameterComponent({
   const nodes = useFlowStore((state) => state.nodes);
   const edges = useFlowStore((state) => state.edges);
   const setNode = useFlowStore((state) => state.setNode);
-  const globalVariablesEntries = useGlobalVariablesStore(
-    (state) => state.globalVariablesEntries
-  );
-
-  const getVariableId = useGlobalVariablesStore((state) => state.getVariableId);
-  const removeGlobalVariable = useGlobalVariablesStore(
-    (state) => state.removeGlobalVariable
-  );
-  function handleDelete(key: string) {
-    const id = getVariableId(key);
-    if (id !== undefined) {
-      deleteGlobalVariable(id)
-        .then((_) => {
-          removeGlobalVariable(key);
-          if (
-            data?.node?.template[name].value === key &&
-            data?.node?.template[name].load_from_db
-          ) {
-            handleOnNewValue("");
-            setNode(data.id, (oldNode) => {
-              let newNode = cloneDeep(oldNode);
-              newNode.data = {
-                ...newNode.data,
-              };
-              newNode.data.node.template[name].load_from_db = false;
-              return newNode;
-            });
-          }
-        })
-        .catch((error) => {
-          let responseError = error as ResponseErrorDetailAPI;
-          setErrorData({
-            title: "Error deleting variable",
-            list: [responseError.response.data.detail ?? "Unknown error"],
-          });
-        });
-    } else {
-      setErrorData({
-        title: "Error deleting variable",
-        list: [cn("ID not found for variable: ", key)],
-      });
-    }
-  }
 
   const [isLoading, setIsLoading] = useState(false);
   const flow = currentFlow?.data?.nodes ?? null;
@@ -168,24 +113,6 @@ export default function ParameterComponent({
     setIsLoading(false);
     renderTooltips();
   };
-
-  useEffect(() => {
-    if (data.node?.template[name])
-      if (
-        !globalVariablesEntries.includes(data.node?.template[name].value) &&
-        data.node?.template[name].load_from_db
-      ) {
-        handleOnNewValue("");
-        setNode(data.id, (oldNode) => {
-          let newNode = cloneDeep(oldNode);
-          newNode.data = {
-            ...newNode.data,
-          };
-          newNode.data.node.template[name].load_from_db = false;
-          return newNode;
-        });
-      }
-  }, [globalVariablesEntries]);
 
   useEffect(() => {
     async function fetchData() {
@@ -609,80 +536,21 @@ export default function ParameterComponent({
                     (data.node?.template[name].refresh_button ? "w-5/6" : "")
                   }
                 >
-                  <InputComponent
-                    id={"input-" + name}
+                  <InputGlobalComponent
                     disabled={disabled}
-                    password={data.node?.template[name].password ?? false}
-                    value={data.node?.template[name].value ?? ""}
-                    options={globalVariablesEntries}
-                    optionsPlaceholder={"Global Variables"}
-                    optionsIcon="Globe"
-                    optionsButton={
-                      <AddNewVariableButton>
-                        <CommandItem value="doNotFilter-addNewVariable">
-                          <IconComponent
-                            name="Plus"
-                            className={cn("mr-2 h-4 w-4 text-primary")}
-                            aria-hidden="true"
-                          />
-                          <span>Add New Variable</span>
-                        </CommandItem>
-                      </AddNewVariableButton>
-                    }
-                    optionButton={(option) => (
-                      <DeleteConfirmationModal
-                        onConfirm={(e) => {
-                          e.stopPropagation();
-                          e.preventDefault();
-                          handleDelete(option);
-                        }}
-                        description={'variable "' + option + '"'}
-                        asChild
-                      >
-                        <button
-                          onClick={(e) => {
-                            e.stopPropagation();
-                          }}
-                          className="pr-1"
-                        >
-                          <ForwardedIconComponent
-                            name="Trash2"
-                            className={cn(
-                              "h-4 w-4 text-primary opacity-0 hover:text-status-red group-hover:opacity-100"
-                            )}
-                            aria-hidden="true"
-                          />
-                        </button>
-                      </DeleteConfirmationModal>
-                    )}
-                    selectedOption={
-                      data?.node?.template[name].load_from_db ?? false
-                        ? data?.node?.template[name].value
-                        : ""
-                    }
-                    setSelectedOption={(value) => {
-                      handleOnNewValue(value);
+                    onChange={handleOnNewValue}
+                    setDb={(value) => {
                       setNode(data.id, (oldNode) => {
                         let newNode = cloneDeep(oldNode);
                         newNode.data = {
                           ...newNode.data,
                         };
-                        newNode.data.node.template[name].load_from_db =
-                          value === "" ? false : true;
+                        newNode.data.node.template[name].load_from_db = value;
                         return newNode;
                       });
                     }}
-                    onChange={(value) => {
-                      handleOnNewValue(value);
-                      setNode(data.id, (oldNode) => {
-                        let newNode = cloneDeep(oldNode);
-                        newNode.data = {
-                          ...newNode.data,
-                        };
-                        newNode.data.node.template[name].load_from_db = false;
-                        return newNode;
-                      });
-                    }}
+                    name={name}
+                    data={data}
                   />
                 </div>
                 {data.node?.template[name].refresh_button && (
