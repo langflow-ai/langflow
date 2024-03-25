2316beea93e82a9832b966ddec4c54adb62592b9
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-25 10:27:27 -0300

    Refactor code for initializing services and socketio server

--- src/backend/langflow/services/monitor/factory.py
+++ src/backend/langflow/services/monitor/factory.py
@@ -1,6 +1,5 @@
 from langflow.services.factory import ServiceFactory
 from langflow.services.monitor.service import MonitorService
-from langflow.services.schema import ServiceType
 
 
 class MonitorServiceFactory(ServiceFactory):
