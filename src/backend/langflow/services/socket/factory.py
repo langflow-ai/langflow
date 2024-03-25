c7345450660fc8f185139cac7d764ecd63459ffb
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-25 10:00:39 -0300

    Refactor ServiceFactory and Dependencies (#1560)
    * Update dependencies for OpenTelemetry
    
    * Update service dependency logic and add first version of telemetry service
    
    * Remove telemetry service and related code

--- src/backend/langflow/services/socket/factory.py
+++ src/backend/langflow/services/socket/factory.py
@@ -1,15 +1,18 @@
 from typing import TYPE_CHECKING
 
 from langflow.services.factory import ServiceFactory
+from langflow.services.schema import ServiceType
 from langflow.services.socket.service import SocketIOService
 
 if TYPE_CHECKING:
-    from langflow.services.cache.service import BaseCacheService
+    from langflow.services.cache.service import CacheService
 
 
 class SocketIOFactory(ServiceFactory):
     def __init__(self):
-        super().__init__(service_class=SocketIOService)
+        super().__init__(
+            service_class=SocketIOService,
+        )
 
-    def create(self, cache_service: "BaseCacheService"):
+    def create(self, cache_service: "CacheService"):
         return SocketIOService(cache_service)
