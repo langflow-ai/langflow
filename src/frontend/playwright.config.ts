48f9f5e1bb9ad30ed9988c23ec830d7b656e3368
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-27 10:13:10 -0300

    Update GitHub Actions workflow and Playwright configuration

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -20,9 +20,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["html", { open: "never", outputFolder: "playwright-report/test-results" }],
-  ],
+  // reporter: [
+  //   ["html", { open: "never", outputFolder: "playwright-report/test-results" }],
+  // ],
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -51,26 +52,6 @@ export default defineConfig({
     // {
     //   name: "webkit",
     //   use: { ...devices["Desktop Safari"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
   /* Run your local dev server before starting the tests */
