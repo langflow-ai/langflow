76dbaf3b215f28e8ab462fb23600d5e441f27ffe
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-28 20:16:16 -0300

    Refactor API generation tests

--- src/frontend/tests/end-to-end/codeAreaModalComponent.spec.ts
+++ src/frontend/tests/end-to-end/codeAreaModalComponent.spec.ts
@@ -28,21 +28,31 @@ test("CodeAreaModalComponent", async ({ page }) => {
   await page.getByTestId("code-button-modal").click();
 
   let value = await page.locator('//*[@id="codeValue"]').inputValue();
-  const code = 'def python_function(text: str) -> str:\n    """This is a default python function that returns the input text"""\n    return text'
-  const wCode = 'def python_function(text: str) -> st:    """This is a default python function that returns the input text"""    return text'
-  const assertCode ='def python_function(text: str) -> str:    """This is a default python function that returns the input text"""    return text'
-  await page.locator('#CodeEditor div').filter({ hasText: 'def python_function(text: str' }).nth(1).click();
-  await page.locator('textarea').press('Control+a');
-  await page.locator('textarea').fill(wCode);
+  const code =
+    'def python_function(text: str) -> str:\n    """This is a default python function that returns the input text"""\n    return text';
+  const wCode =
+    'def python_function(text: str) -> st:    """This is a default python function that returns the input text"""    return text';
+  const assertCode =
+    'def python_function(text: str) -> str:    """This is a default python function that returns the input text"""    return text';
+  await page
+    .locator("#CodeEditor div")
+    .filter({ hasText: "def python_function(text: str" })
+    .nth(1)
+    .click();
+  await page.locator("textarea").press("Control+a");
+  await page.locator("textarea").fill(wCode);
   await page.locator('//*[@id="checkAndSaveBtn"]').click();
   await page.waitForTimeout(1000);
-  expect(await page.getByText("invalid syntax (<unknown>, line 1)").isVisible()).toBeTruthy();
-  await page.locator('textarea').press('Control+a');
-  await page.locator('textarea').fill(wCode);
-  await page.locator('textarea').fill(code);
+  expect(
+    await page.getByText("invalid syntax (<unknown>, line 1)").isVisible()
+  ).toBeTruthy();
+  await page.locator("textarea").press("Control+a");
+  await page.locator("textarea").fill(wCode);
+  await page.locator("textarea").fill(code);
   await page.locator('//*[@id="checkAndSaveBtn"]').click();
   expect(await page.getByText("Code is ready to run").isVisible()).toBeTruthy();
   await page.getByTestId("code-button-modal").click();
-  expect(await page.locator('//*[@id="codeValue"]').inputValue()).toBe(assertCode);
-
+  expect(await page.locator('//*[@id="codeValue"]').inputValue()).toBe(
+    assertCode
+  );
 });
--- src/frontend/tests/end-to-end/python_api_generation.spec.ts
+++ src/frontend/tests/end-to-end/python_api_generation.spec.ts
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-
-test('python_api_generation', async ({ page,context }) => {
+test("python_api_generation", async ({ page, context }) => {
   await page.goto("/");
   await page.locator('//*[@id="new-project-btn"]').click();
   await context.grantPermissions(["clipboard-read", "clipboard-write"]);
-  await page.getByRole('heading', { name: 'Data Ingestion' }).click();
+  await page.getByRole("heading", { name: "Data Ingestion" }).click();
   await page.waitForTimeout(2000);
-  await page.getByText('API', { exact: true }).click();
-  await page.getByRole('tab', { name: 'Python API' }).click();
-  await page.getByRole('button', { name: 'Copy Code' }).click();
-  const handle = await page.evaluateHandle(() => navigator.clipboard.readText());
+  await page.getByText("API", { exact: true }).click();
+  await page.getByRole("tab", { name: "Python API" }).click();
+  await page.getByRole("button", { name: "Copy Code" }).click();
+  const handle = await page.evaluateHandle(() =>
+    navigator.clipboard.readText()
+  );
   const clipboardContent = await handle.jsonValue();
   expect(clipboardContent.length).toBeGreaterThan(0);
-
 });
