ff3bf645fd67e95492665d084562e72d4f333f21
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-03-31 21:48:33 -0300

    Update BaiduQianfanChatModel and OpenAIModel configurations

--- src/backend/base/langflow/components/models/OpenAIModel.py
+++ src/backend/base/langflow/components/models/OpenAIModel.py
@@ -29,30 +29,28 @@ class OpenAIModelComponent(LCModelComponent):
             "max_tokens": {
                 "display_name": "Max Tokens",
                 "advanced": True,
-                "required": False,
             },
             "model_kwargs": {
                 "display_name": "Model Kwargs",
                 "advanced": True,
-                "required": False,
             },
             "model_name": {
                 "display_name": "Model Name",
                 "advanced": False,
-                "required": False,
                 "options": [
                     "gpt-4-turbo-preview",
+                    "gpt-3.5-turbo",
                     "gpt-4-0125-preview",
                     "gpt-4-1106-preview",
                     "gpt-4-vision-preview",
                     "gpt-3.5-turbo-0125",
                     "gpt-3.5-turbo-1106",
                 ],
+                "value": "gpt-4-turbo-preview",
             },
             "openai_api_base": {
                 "display_name": "OpenAI API Base",
                 "advanced": True,
-                "required": False,
                 "info": (
                     "The base URL of the OpenAI API. Defaults to https://api.openai.com/v1.\n\n"
                     "You can change this to use other APIs like JinaChat, LocalAI and Prem."
@@ -60,14 +58,13 @@ class OpenAIModelComponent(LCModelComponent):
             },
             "openai_api_key": {
                 "display_name": "OpenAI API Key",
+                "info": "The OpenAI API Key to use for the OpenAI model.",
                 "advanced": False,
-                "required": True,
                 "password": True,
             },
             "temperature": {
                 "display_name": "Temperature",
                 "advanced": False,
-                "required": False,
                 "value": 0.1,
             },
             "stream": {
@@ -85,12 +82,12 @@ class OpenAIModelComponent(LCModelComponent):
     def build(
         self,
         input_value: Text,
+        openai_api_key: str,
+        temperature: float,
+        model_name: str,
         max_tokens: Optional[int] = 256,
         model_kwargs: NestedDict = {},
-        model_name: str = "gpt-4-1106-preview",
         openai_api_base: Optional[str] = None,
-        openai_api_key: Optional[str] = None,
-        temperature: float = 0.7,
         stream: bool = False,
         system_message: Optional[str] = None,
     ) -> Text:
