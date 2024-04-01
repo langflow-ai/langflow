b2cfe3d750c706ee99ee713f8117e50d5fcd10d7
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-04-01 10:38:01 -0300

    Add new schema for simplified API request

--- src/backend/base/langflow/api/v1/schemas.py
+++ src/backend/base/langflow/api/v1/schemas.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel, ConfigDict, Field, RootModel, field_validator, m
 
 from langflow.graph.schema import RunOutputs
 from langflow.schema import dotdict
+from langflow.schema.schema import InputType, OutputType
+from langflow.schema.schema import InputType, OutputType
 from langflow.services.database.models.api_key.model import ApiKeyRead
 from langflow.services.database.models.base import orjson_dumps
 from langflow.services.database.models.flow import FlowCreate, FlowRead
@@ -259,7 +261,7 @@ class VerticesBuiltResponse(BaseModel):
 class InputValueRequest(BaseModel):
     components: Optional[List[str]] = []
     input_value: Optional[str] = None
-    type: Optional[Literal["chat", "text", "any"]] = Field(
+    type: Optional[InputType] = Field(
         "any",
         description="Defines on which components the input value should be applied. 'any' applies to all input components.",
     )
@@ -310,3 +312,11 @@ class Tweaks(RootModel):
 
     def items(self):
         return self.root.items()
+
+
+class SimplifiedAPIRequest(BaseModel):
+    input_value: Optional[str] = Field(default="", description="The input value")
+    input_type: Optional[InputType] = Field(default="chat", description="The input type")
+    output_type: Optional[OutputType] = Field(default="chat", description="The output type")
+    tweaks: Optional[Tweaks] = Field(default=None, description="The tweaks")
+    session_id: Optional[str] = Field(default=None, description="The session id")
