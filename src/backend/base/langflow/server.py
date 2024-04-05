5ed2c59c2a17999816a2460408285b5e481e6eaf
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2024-04-05 16:20:17 -0300

    Add version check and new version notice (#1616)
    * Add version check and new version notice
    
    * Add function to extract letter from pre-release version
    
    * Update version banner and add package update notice
    
    * Change log level from error to debug
    
    * Refactor logging configuration and add InterceptHandler
    
    * update banner
    
    * Update launch.json path in .vscode directory

--- src/backend/base/langflow/server.py
+++ src/backend/base/langflow/server.py
@@ -1,11 +1,28 @@
-import os
+import logging
 
+from gunicorn import glogging
 from gunicorn.app.base import BaseApplication  # type: ignore
 from uvicorn.workers import UvicornWorker
+
+from langflow.utils.logger import InterceptHandler  # type: ignore
 
 
 class LangflowUvicornWorker(UvicornWorker):
     CONFIG_KWARGS = {"loop": "asyncio"}
+
+
+class Logger(glogging.Logger):
+    """Implements and overrides the gunicorn logging interface.
+
+    This class inherits from the standard gunicorn logger and overrides it by
+    replacing the handlers with `InterceptHandler` in order to route the
+    gunicorn logs to loguru.
+    """
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        logging.getLogger("gunicorn.error").handlers = [InterceptHandler()]
+        logging.getLogger("gunicorn.access").handlers = [InterceptHandler()]
 
 
 class LangflowApplication(BaseApplication):
@@ -13,12 +30,16 @@ class LangflowApplication(BaseApplication):
         self.options = options or {}
 
         self.options["worker_class"] = "langflow.server.LangflowUvicornWorker"
-        self.options["loglevel"] = os.getenv("LANGFLOW_LOG_LEVEL", "error").lower()
+        self.options["logger_class"] = Logger
         self.application = app
         super().__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
