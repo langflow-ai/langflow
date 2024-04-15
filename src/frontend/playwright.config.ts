17654742147fbdbcac043814255b94868d748ad9
cristhianzl <cristhian.lousa@gmail.com>
2024-04-15 12:23:17 -0300

    📝 (playwright.config.ts): comment out the forbidOnly and retries options to disable them 📝 (playwright.config.ts): change the reporter option to always use the "blob" reporter 📝 (playwright.config.ts): comment out the globalTeardown option to disable it 📝 (playwright.config.ts): set the reuseExistingServer option to always be true

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -19,9 +19,9 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,7 +29,7 @@ export default defineConfig({
   // reporter: [
   //   ["html", { open: "never", outputFolder: "playwright-report/test-results" }],
   // ],
-  reporter: process.env.CI ? "blob" : "html",
+  reporter: "blob",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -39,7 +39,7 @@ export default defineConfig({
     trace: "on-first-retry",
   },
 
-  globalTeardown: require.resolve("./tests/globalTeardown.ts"),
+  // globalTeardown: require.resolve("./tests/globalTeardown.ts"),
 
   /* Configure projects for major browsers */
   projects: [
@@ -70,7 +70,7 @@ export default defineConfig({
       },
       stdout: "ignore",
 
-      reuseExistingServer: !process.env.CI,
+      reuseExistingServer: true,
       timeout: 120 * 1000,
     },
     {
