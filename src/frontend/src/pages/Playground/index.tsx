bc66dc1e6f0968f89e3a06449427da4445da3d10
anovazzi1 <otavio2204@gmail.com>
2024-04-26 17:33:56 -0300

    Fix useEffect dependency in PlaygroundPage component

--- src/frontend/src/pages/Playground/index.tsx
+++ src/frontend/src/pages/Playground/index.tsx
@@ -35,7 +35,7 @@ export default function PlaygroundPage() {
                 setCurrentFlow(flow);
             });
         }
-    }, [id]);
+    }, []);
 
     useEffect(() => {
         if (currentFlow) {
