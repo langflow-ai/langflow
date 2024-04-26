1e1d56ec2034d37c1bf2fdc785cac67502113eec
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2024-04-26 16:39:34 -0400

    bump-whats-new

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,27 +2,34 @@ module.exports = {
   docs: [
     {
       type: "category",
-      label: " Getting Started",
+      label: "What's New?",
+      collapsed: false,
+      items: [
+        "whats-new/a-new-chapter-langflow"
+      ]
+    },
+    {
+      type: "category",
+      label: "Getting Started",
       collapsed: false,
       items: [
         "index",
         "getting-started/install-langflow",
         "getting-started/quickstart",
         "getting-started/huggingface-spaces",
-        "getting-started/new-to-llms",
-      ],
+        "getting-started/new-to-llms"
+      ]
     },
-
     {
       type: "category",
-      label: " Starter Projects",
+      label: "Starter Projects",
       collapsed: false,
       items: [
         "starter-projects/basic-prompting",
         "starter-projects/blog-writer",
         "starter-projects/document-qa",
-        "starter-projects/memory-chatbot",
-      ],
+        "starter-projects/memory-chatbot"
+      ]
     },
     {
       type: "category",
@@ -33,14 +40,10 @@ module.exports = {
         "administration/api",
         "administration/cli",
         "administration/components",
-        // "guidelines/features",
         "administration/collection",
         "administration/prompt-customization",
         "administration/langfuse_integration"
-        // "guidelines/chat-interface",
-        // "guidelines/chat-widget",
-        // "guidelines/custom-component",
-      ],
+      ]
     },
     {
       type: "category",
@@ -54,8 +57,8 @@ module.exports = {
         "components/helpers",
         "components/vector-stores",
         "components/embeddings",
-        "components/custom",
-      ],
+        "components/custom"
+      ]
     },
     {
       type: "category",
@@ -70,11 +73,9 @@ module.exports = {
         "components/retrievers",
         "components/text-splitters",
         "components/toolkits",
-        "components/tools",
-        // "components/memories",
-      ],
+        "components/tools"
+      ]
     },
-
     {
       type: "category",
       label: "Example Components",
@@ -86,37 +87,22 @@ module.exports = {
         "examples/csv-loader",
         "examples/searchapi-tool",
         "examples/serp-api-tool",
-        "examples/python-function",
-      ],
+        "examples/python-function"
+      ]
     },
-
     {
       type: "category",
-      label: " Migration Guides",
+      label: "Migration Guides",
       collapsed: false,
       items: [
         "migration/possible-installation-issues",
         "migration/migrating-to-one-point-zero",
-        // "migration/flow-of-data",
         "migration/inputs-and-outputs",
-        // "migration/supported-frameworks",
-        // "migration/sidebar-and-interaction-panel",
-        // "migration/new-categories-and-components",
         "migration/text-and-record",
-        // "migration/custom-component",
         "migration/compatibility",
-        // "migration/multiple-flows",
-        // "migration/component-status-and-data-passing",
-        // "migration/connecting-output-components",
-        // "migration/renaming-and-editing-components",
-        // "migration/passing-tweaks-and-inputs",
-        "migration/global-variables",
-        // "migration/experimental-components",
-        // "migration/state-management",
-        //"guides/rag-with-astradb",
-      ],
+        "migration/global-variables"
+      ]
     },
-
     {
       type: "category",
       label: "Tutorials",
@@ -124,26 +110,17 @@ module.exports = {
       items: [
         "tutorials/chatprompttemplate_guide",
         "tutorials/loading_document",
-        "tutorials/rag-with-astradb",
-      ],
+        "tutorials/rag-with-astradb"
+      ]
     },
-
     {
       type: "category",
       label: "Deployment",
       collapsed: true,
-      items: ["deployment/gcp-deployment"],
-    },
-
-    {
-      type: "category",
-      label: " What's New",
-      collapsed: false,
       items: [
-        "whats-new/a-new-chapter-langflow",
-      ],
+        "deployment/gcp-deployment"
+      ]
     },
-
     {
       type: "category",
       label: "Contributing",
@@ -151,8 +128,8 @@ module.exports = {
       items: [
         "contributing/how-contribute",
         "contributing/github-issues",
-        "contributing/community",
-      ],
-    },
-  ],
+        "contributing/community"
+      ]
+    }
+  ]
 };
