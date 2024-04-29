bc1650f50d1883af2d935b9ccdd690f0d2f661fa
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-04-30 00:10:33 +0200

    Passed Table props to page

--- src/frontend/src/components/tableComponent/index.tsx
+++ src/frontend/src/components/tableComponent/index.tsx
@@ -1,89 +1,29 @@
-import { ColDef, ColGroupDef } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import { AgGridReact } from "ag-grid-react";
-import { ColDef,ColGroupDef } from 'ag-grid-community';
-import { useState } from "react";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { useDarkStore } from "../../stores/darkStore";
 import "../../style/ag-theme-shadcn.css"; // Custom CSS applied to the grid
 import { cn } from "../../utils/utils";
-import { Button } from "../ui/button";
 
-export default function TableComponent() {
-  // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState<(ColDef<any> | ColGroupDef<any>)[]>([
-    { headerName: "Variable Name", field: "name", flex: 1 }, //This column will be twice as wide as the others
-    {
-      
-      field: "type",
-      cellEditor: "agSelectCellEditor",
-      cellEditorParams: {
-        values: ["Prompt", "Credential"],
-        valueListGap: 10,
-      },
-      flex: 1,
-      editable: true,
-    },
-    {
-      field: "value",
-      cellEditor: "agLargeTextCellEditor",
-      cellEditorPopup: true,
-      flex: 2,
-      editable: true,
-    },
-    {
-      headerName: "Apply To Fields",
-      field: "defaultFields",
-      flex: 1,
-      editable: true,
-    },
-  ]);
-
-  const [rowData, setRowData] = useState([
-    {
-      name: "OpenAI Key",
-      type: "Credential",
-      value: "apijpioj09u302j0982ejf",
-      defaultFields: "Open AI API Key",
-    },
-    {
-      name: "Prompt",
-      type: "Prompt",
-      value: `Answer user's questions based on the document below:
-
-    ---
-    
-    {Document}
-    
-    ---
-    
-    Question:
-    {Question}
-    
-    Answer:
-    `,
-      defaultFields: ["Prompt"],
-    },
-    {
-      name: "Azure Key",
-      type: "Credential",
-      value: "awowkdenvoaimojndofunoweoij0293u0n2e08n23",
-      defaultFields: ["Azure API Key"],
-    },
-  ]);
-
+const TableComponent = forwardRef<
+  ElementRef<typeof AgGridReact>,
+  ComponentPropsWithoutRef<typeof AgGridReact>
+>(({ ...props }, ref) => {
   const dark = useDarkStore((state) => state.dark);
 
   return (
     <div className="flex h-full flex-col">
-        <div
-          className={cn(
-            dark ? "ag-theme-quartz-dark" : "ag-theme-quartz",
-            "ag-theme-shadcn flex h-full flex-col pb-8"
-          )} // applying the grid theme
-        >
-          <AgGridReact pagination={true} columnDefs={colDefs} rowData={rowData} />
-        </div>
+      <div
+        className={cn(
+          dark ? "ag-theme-quartz-dark" : "ag-theme-quartz",
+          "ag-theme-shadcn flex h-full flex-col pb-8"
+        )} // applying the grid theme
+      >
+        <AgGridReact ref={ref} pagination={true} {...props} />
+      </div>
     </div>
   );
-}
+});
+
+export default TableComponent;
