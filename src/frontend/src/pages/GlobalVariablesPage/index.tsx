f14b8831bfac329d0b18c84be5fd7128731a9cd6
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-04-29 19:18:03 +0200

    Modified title to be on pair with modals

--- src/frontend/src/pages/GlobalVariablesPage/index.tsx
+++ src/frontend/src/pages/GlobalVariablesPage/index.tsx
@@ -1,6 +1,7 @@
 import IconComponent from "../../components/genericIconComponent";
 import { Button } from "../../components/ui/button";
 
+import ForwardedIconComponent from "../../components/genericIconComponent";
 import TableComponent from "../../components/tableComponent";
 
 export default function GlobalVariablesPage() {
@@ -8,8 +9,11 @@ export default function GlobalVariablesPage() {
     <div className="flex h-full w-full flex-col justify-between gap-6">
       <div className="flex w-full items-center justify-between gap-4 space-y-0.5">
         <div className="flex w-full flex-col">
-          <h2 className="text-xl font-bold tracking-tight">Global Variables</h2>
-          <p className="text-muted-foreground">
+          <h2 className="flex items-center text-lg font-semibold tracking-tight">
+            Global Variables
+            <ForwardedIconComponent name="Globe" className="ml-3 w-6 h-6 text-primary"  />
+          </h2>
+          <p className="text-sm text-muted-foreground">
             Manage and assign global variables to default fields. You can add
             new global variables by clicking the button.
           </p>
@@ -17,7 +21,7 @@ export default function GlobalVariablesPage() {
         <div className="flex-shrink-0">
           <Button data-testid="api-key-button-store" variant="primary">
             <IconComponent name="Plus" className="mr-2 w-4" />
-            Add Global Variables
+            Add New
           </Button>
         </div>
       </div>
