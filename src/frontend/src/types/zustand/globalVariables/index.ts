174fbe92a7a46c87b59ab819ef2d176e0755b11f
anovazzi1 <otavio2204@gmail.com>
2024-04-30 13:34:44 -0300

    Refactored GlobalVariablesStore to include a value property in globalVariables object

--- src/frontend/src/types/zustand/globalVariables/index.ts
+++ src/frontend/src/types/zustand/globalVariables/index.ts
@@ -1,10 +1,23 @@
 export type GlobalVariablesStore = {
   globalVariablesEntries: Array<string>;
-  globalVariables: { [name: string]: { id: string; type?: string, default_fields?: string[] } };
+  globalVariables: {
+    [name: string]: {
+      id: string;
+      type?: string;
+      default_fields?: string[];
+      value?: string;
+    };
+  };
   setGlobalVariables: (variables: {
-    [name: string]: { id: string; type?: string, default_fields?: string[]};
+    [name: string]: { id: string; type?: string; default_fields?: string[],value?: string };
   }) => void;
-  addGlobalVariable: (name: string, id: string, type?: string, default_fields?: string[]) => void;
+  addGlobalVariable: (
+    name: string,
+    id: string,
+    type?: string,
+    default_fields?: string[],
+    value?: string
+  ) => void;
   removeGlobalVariable: (name: string) => Promise<void>;
   getVariableId: (name: string) => string | undefined;
   unavaliableFields: Set<string>;
