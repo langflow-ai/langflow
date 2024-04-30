4a5acec2972d9c39b22963a9b01f2065781a4f51
anovazzi1 <otavio2204@gmail.com>
2024-04-29 21:43:56 -0300

    Fix cleanOnClose behavior in IOModal component

--- src/frontend/src/modals/IOModal/index.tsx
+++ src/frontend/src/modals/IOModal/index.tsx
@@ -31,10 +31,8 @@ export default function IOModal({
   open,
   setOpen,
   disable,
-  cleanOnClose = false,
 }: IOModalPropsType): JSX.Element {
   const allNodes = useFlowStore((state) => state.nodes);
-  const cleanFlowPool = useFlowStore((state) => state.CleanFlowPool);
   const inputs = useFlowStore((state) => state.inputs).filter(
     (input) => input.type !== "ChatInput"
   );
@@ -123,12 +121,6 @@ export default function IOModal({
       open={open}
       setOpen={setOpen}
       disable={disable}
-      onChangeOpenModal={(open) => {
-        if (!open && cleanOnClose) {
-          console.log("cleaning flow pool");
-          cleanFlowPool();
-        }
-      }}
     >
       <BaseModal.Trigger>{children}</BaseModal.Trigger>
       {/* TODO ADAPT TO ALL TYPES OF INPUTS AND OUTPUTS */}
