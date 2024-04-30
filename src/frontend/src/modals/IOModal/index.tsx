9c0ae350bf16c858d1d9017f5d42dddf222f78c7
Cristhian Zanforlin Lousa <72977554+Cristhianzl@users.noreply.github.com>
2024-04-30 09:39:50 -0300

    Fix modal opening bug when not supposed to (#1795)
    * fix opening modal error
    
    * Fix cleanOnClose behavior in IOModal component
    
    * Refactor useEffect dependency in CollectionCardComponent
    
    * remove console
    
    * add console.log for debugging
    
    ---------
    
    Co-authored-by: anovazzi1 <otavio2204@gmail.com>

--- src/frontend/src/modals/IOModal/index.tsx
+++ src/frontend/src/modals/IOModal/index.tsx
@@ -31,10 +31,8 @@ export default function IOModal({
   open,
   setOpen,
   disable,
-  cleanOnClose=false,
 }: IOModalPropsType): JSX.Element {
   const allNodes = useFlowStore((state) => state.nodes);
-  const cleanFlowPool = useFlowStore((state) => state.CleanFlowPool);
   const inputs = useFlowStore((state) => state.inputs).filter(
     (input) => input.type !== "ChatInput"
   );
@@ -85,12 +83,6 @@ export default function IOModal({
     return updateVerticesOrder(currentFlow!.id, null);
   }
 
-  // useEffect(() => {
-  //   if (open) {
-  //     updateVertices();
-  //   }
-  // }, [open, currentFlow]);
-
   async function sendMessage(count = 1): Promise<void> {
     if (isBuilding) return;
     setIsBuilding(true);
@@ -129,13 +121,6 @@ export default function IOModal({
       open={open}
       setOpen={setOpen}
       disable={disable}
-      onChangeOpenModal={(open)=>{
-        if(!open && cleanOnClose){
-          console.log("cleaning flow pool") 
-          cleanFlowPool();
-        }
-      
-      }}
     >
       <BaseModal.Trigger>{children}</BaseModal.Trigger>
       {/* TODO ADAPT TO ALL TYPES OF INPUTS AND OUTPUTS */}
