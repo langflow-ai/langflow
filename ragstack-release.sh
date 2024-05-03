3fa9605bf1784b2b4cda725fe035490c892e1cc6
Nicolò Boschi <boschi1997@gmail.com>
2024-05-03 10:16:30 +0200

    RAGStack setup

---
+++ ragstack-release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+version=$1
+package=$2
+if [[ -z "$version" || -z "$package" ]]; then
+    echo "Usage: $0 <version> <package>"
+    echo "Packages: ragstack-ai-langflow-base, ragstack-ai-langflow."
+    exit 1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Working directory not clean"
+    exit 1
+fi
+
+tag="$package-$version"
+directory=""
+if [ "$package" == "ragstack-ai-langflow" ]; then
+    directory="."
+elif [ "$package" == "ragstack-ai-langflow-base" ]; then
+    directory="src/backend/base"
+else
+    echo "Invalid package. Please choose from: ragstack-ai-langflow, ragstack-ai-langflow-base."
+    exit 1
+fi
+
+cd $directory
+
+git checkout main
+git pull
+echo ":: Bumping version to $version for package $package"
+poetry version $version
+git commit -am "Release $package $version"
+git tag $tag
+git push origin main
+git push origin $tag
+echo "done."
---
+++ src/backend/base/README.md
@@ -0,0 +1,5 @@
+# RAGStack Langflow base
+
+## Documentation
+
+[DataStax RAGStack Documentation](https://docs.datastax.com/en/ragstack/docs/index.html)
