cd23b276d8d868d9a2de2eb3884dfad17d972442
cristhianzl <cristhian.lousa@gmail.com>
2024-05-08 17:20:27 -0300

    refactor: Update SidebarNav component to handle both link and button items

--- src/frontend/src/components/sidebarComponent/index.tsx
+++ src/frontend/src/components/sidebarComponent/index.tsx
@@ -4,15 +4,17 @@ import { buttonVariants } from "../ui/button";
 
 interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
   items: {
-    href: string;
+    href?: string;
     title: string;
     icon: React.ReactNode;
   }[];
+  handleOpenNewFolderModal: () => void;
 }
 
 export default function SidebarNav({
   className,
   items,
+  handleOpenNewFolderModal,
   ...props
 }: SidebarNavProps) {
   const location = useLocation();
@@ -22,27 +24,45 @@ export default function SidebarNav({
     <nav
       className={cn(
         "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
-        className
+        className,
       )}
       {...props}
     >
-      {items.map((item) => (
-        <Link
-          data-testid={`sidebar-nav-${item.title}`}
-          key={item.href}
-          to={item.href}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            pathname === item.href
-              ? "border border-border bg-muted hover:bg-muted"
-              : "border border-transparent hover:border-border hover:bg-transparent",
-            "justify-start gap-2"
-          )}
-        >
-          {item.icon}
-          {item.title}
-        </Link>
-      ))}
+      {items.map((item) =>
+        item.href ? (
+          <Link
+            data-testid={`sidebar-nav-${item.title}`}
+            key={item.href}
+            to={item.href}
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              pathname === item.href
+                ? "border border-border bg-muted hover:bg-muted"
+                : "border border-transparent hover:border-border hover:bg-transparent",
+              "justify-start gap-2",
+            )}
+          >
+            {item.icon}
+            {item.title}
+          </Link>
+        ) : (
+          <div
+            key={item.title}
+            data-testid={`sidebar-nav-${item.title}`}
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              pathname === item.href
+                ? "border border-border bg-muted hover:bg-muted"
+                : "border border-transparent hover:border-border hover:bg-transparent",
+              "cursor-pointer justify-start gap-2",
+            )}
+            onClick={handleOpenNewFolderModal}
+          >
+            {item.icon}
+            {item.title}
+          </div>
+        ),
+      )}
     </nav>
   );
 }
