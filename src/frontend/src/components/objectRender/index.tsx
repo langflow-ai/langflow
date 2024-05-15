ca7f5fb948097609c463c3b34e7a2e4d518a2c1c
anovazzi1 <otavio2204@gmail.com>
2024-05-15 17:29:26 -0300

    style(objectRender): add hover effect to object render component for better user experience style(tailwind.config.js): add slow-wiggle animation to tailwind config for smoother animation effect

--- src/frontend/src/components/objectRender/index.tsx
+++ src/frontend/src/components/objectRender/index.tsx
@@ -2,11 +2,11 @@ import DictAreaModal from "../../modals/dictAreaModal";
 
 export default function ObjectRender({ object }: { object: any }): JSX.Element {
   //TODO check object type
+
   return (
     <DictAreaModal value={object}>
-      <div className="flex gap-1">
+      <div className="hover:animate-slow-wiggle">
         <div className="truncate">{JSON.stringify(object)}</div>
-        see more
       </div>
     </DictAreaModal>
   );
