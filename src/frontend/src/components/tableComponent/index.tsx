d49e72f9af01bffe383a15a4cf05cdda510504a9
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-05-15 18:19:26 -0300

    Refactor: use shadcn alert when there is no data

--- src/frontend/src/components/tableComponent/index.tsx
+++ src/frontend/src/components/tableComponent/index.tsx
@@ -5,32 +5,59 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { useDarkStore } from "../../stores/darkStore";
 import "../../style/ag-theme-shadcn.css"; // Custom CSS applied to the grid
 import { cn } from "../../utils/utils";
-import noDataTemplate from "./utils/no-data-template";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import ForwardedIconComponent from "../genericIconComponent";
+import {
+  DEFAULT_TABLE_ALERT_MSG,
+  DEFAULT_TABLE_ALERT_TITLE,
+} from "../../constants/constants";
 
 interface TableComponentProps extends AgGridReactProps {
   columnDefs: NonNullable<AgGridReactProps["columnDefs"]>;
   rowData: NonNullable<AgGridReactProps["rowData"]>;
+  alertTitle?: string;
+  alertDescription?: string;
 }
 
 const TableComponent = forwardRef<
   ElementRef<typeof AgGridReact>,
   TableComponentProps
->(({ ...props }, ref) => {
-  const dark = useDarkStore((state) => state.dark);
-  return (
-    <div
-      className={cn(
-        dark ? "ag-theme-quartz-dark" : "ag-theme-quartz",
-        "ag-theme-shadcn flex h-full flex-col",
-      )} // applying the grid theme
-    >
-      <AgGridReact
-        ref={ref}
-        {...props}
-        overlayNoRowsTemplate={noDataTemplate()}
-      />
-    </div>
-  );
-});
+>(
+  (
+    {
+      alertTitle = DEFAULT_TABLE_ALERT_TITLE,
+      alertDescription = DEFAULT_TABLE_ALERT_MSG,
+      ...props
+    },
+    ref,
+  ) => {
+    const dark = useDarkStore((state) => state.dark);
+
+    if (props.rowData.length === 0) {
+      return (
+        <div className="flex h-full w-full items-center justify-center">
+          <Alert variant={"default"} className="w-[50%]">
+            <ForwardedIconComponent
+              name="AlertCircle"
+              className="h-5 w-5 text-primary"
+            />
+            <AlertTitle>{alertTitle}</AlertTitle>
+            <AlertDescription>{alertDescription}</AlertDescription>
+          </Alert>
+        </div>
+      );
+    }
+    return (
+      <div
+        className={cn(
+          dark ? "ag-theme-quartz-dark" : "ag-theme-quartz",
+          "ag-theme-shadcn flex h-full flex-col",
+        )} // applying the grid theme
+      >
+        <AgGridReact ref={ref} {...props} />
+      </div>
+    );
+  },
+);
 
 export default TableComponent;
