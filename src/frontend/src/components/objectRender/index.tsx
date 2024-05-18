f87a4f6681b9cf4f64f5963b046dc524e98fbb01
anovazzi1 <otavio2204@gmail.com>
2024-05-17 22:31:56 -0300

    refactor: Update hover animation in ObjectRender component

--- src/frontend/src/components/objectRender/index.tsx
+++ src/frontend/src/components/objectRender/index.tsx
@@ -5,7 +5,7 @@ export default function ObjectRender({ object }: { object: any }): JSX.Element {
 
   return (
     <DictAreaModal value={object}>
-      <div className="flex h-full w-full items-center align-middle hover:animate-slow-wiggle">
+      <div className="flex h-full w-full items-center align-middle transition-all hover:scale-105">
         <div className="truncate">{JSON.stringify(object)}</div>
       </div>
     </DictAreaModal>
