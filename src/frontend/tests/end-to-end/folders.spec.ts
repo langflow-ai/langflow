c1fe6b80a74e030a52239a0103fa8994806c2f34
cristhianzl <cristhian.lousa@gmail.com>
2024-05-21 15:07:44 -0300

    ✅ (folders.spec.ts): remove unnecessary code that was clicking on elements and pressing the Escape key ♻️ (folders.spec.ts): refactor code to improve readability and remove unused variables ✨ (folders.spec.ts): add test to verify the ability to change the flow folder

--- src/frontend/tests/end-to-end/folders.spec.ts
+++ src/frontend/tests/end-to-end/folders.spec.ts
@@ -36,9 +36,6 @@ test("CRUD folders", async ({ page }) => {
   await page
     .getByPlaceholder("Insert a description for the folder")
     .fill("test");
-  await page.getByTestId("object-input-flow").first().click();
-  await page.getByTestId("option-0").first().click();
-  await page.keyboard.press("Escape");
   await page.getByText("Save Folder").click();
 
   await page.waitForTimeout(1000);
@@ -78,7 +75,7 @@ test("add folder by drag and drop", async ({ page }) => {
 
   const jsonContent = readFileSync(
     "tests/end-to-end/assets/collection.json",
-    "utf-8",
+    "utf-8"
   );
 
   // Create the DataTransfer and File
@@ -98,8 +95,58 @@ test("add folder by drag and drop", async ({ page }) => {
     "drop",
     {
       dataTransfer,
-    },
+    }
   );
 
   await page.getByText("Getting Started").first().isVisible();
+});
+
+test("change flow folder", async ({ page }) => {
+  await page.goto("/");
+  await page.waitForTimeout(2000);
+
+  let modalCount = 0;
+  try {
+    const modalTitleElement = await page?.getByTestId("modal-title");
+    if (modalTitleElement) {
+      modalCount = await modalTitleElement.count();
+    }
+  } catch (error) {
+    modalCount = 0;
+  }
+
+  while (modalCount === 0) {
+    await page.getByText("New Project", { exact: true }).click();
+    await page.waitForTimeout(5000);
+    modalCount = await page.getByTestId("modal-title")?.count();
+  }
+  await page.getByRole("heading", { name: "Basic Prompting" }).click();
+
+  await page.getByTestId("icon-ChevronLeft").first().click();
+
+  await page.getByText("My Collection").nth(2).isVisible();
+  await page.getByPlaceholder("Search flows").isVisible();
+  await page.getByText("Flows").isVisible();
+  await page.getByText("Components").isVisible();
+  await page.getByText("All").first().isVisible();
+  await page.getByText("Select All").isVisible();
+
+  await page.getByText("New Folder", { exact: true }).last().click();
+  await page.getByPlaceholder("Insert a name for the folder").fill("test");
+  await page
+    .getByPlaceholder("Insert a description for the folder")
+    .fill("test");
+
+  await page.getByText("Save Folder").click();
+
+  await page.waitForTimeout(1000);
+  await page.getByText("Folder created succefully").isVisible();
+  await page.getByText("test").last().isVisible();
+
+  await page.getByText("My Projects").last().click();
+  await page.getByText("Basic Prompting").first().hover();
+  await page.mouse.down();
+  await page.getByText("test").first().hover();
+  await page.mouse.up();
+  await page.getByText("Basic Prompting").first().isVisible();
 });
