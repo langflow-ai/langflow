69f3b16fb73231c07728c721a7e1d5b2194b6d65
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-05-21 19:56:02 -0300

    Refactor: move getPythonCode to apiModal folder

--- src/frontend/src/modals/apiModal/views/index.tsx
+++ src/frontend/src/modals/apiModal/views/index.tsx
@@ -14,7 +14,6 @@ import { TemplateVariableType } from "../../../types/api";
 import { uniqueTweakType } from "../../../types/components";
 import { FlowType } from "../../../types/flow/index";
 import {
-  getPythonCode,
   getWidgetCode,
   tabsArray,
 } from "../../../utils/utils";
@@ -27,6 +26,7 @@ import { getNodesWithDefaultValue } from "../utils/get-nodes-with-default-value"
 import { getValue } from "../utils/get-value";
 import getPythonApiCode from "../utils/get-python-api-code";
 import getCurlCode from "../utils/get-curl-code";
+import getPythonCode from "../utils/get-python-code";
 
 const ApiModal = forwardRef(
   (
