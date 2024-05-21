e2c902f47b13f5630936a4f809b00b04586854f0
cristhianzl <cristhian.lousa@gmail.com>
2024-05-20 21:48:20 -0300

    Refactor: Update folder_id when moving a flow to a different folder

--- src/backend/base/langflow/api/v1/folders.py
+++ src/backend/base/langflow/api/v1/folders.py
@@ -166,6 +166,37 @@ def delete_folder(
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.patch("/move_to_folder/{flow_id}/{folder_id}", status_code=200)
+def update_folder(
+    *,
+    session: Session = Depends(get_session),
+    folder_id: UUID,
+    flow_id: UUID,
+    current_user: User = Depends(get_current_active_user),
+):
+    try:
+        update_flow = session.exec(select(Flow).where(Flow.id == flow_id, Flow.user_id == current_user.id)).first()
+        if not update_flow:
+            raise HTTPException(status_code=404, detail="Flow not found")
+        
+        existing_folder = session.exec(
+            select(Folder).where(Folder.id == folder_id, Folder.user_id == current_user.id)
+        ).first()
+        if not existing_folder:
+            raise HTTPException(status_code=404, detail="Folder not found")
+        
+        setattr(update_flow, "folder_id", folder_id)
+        session.add(update_flow)
+        session.commit()
+        session.refresh(update_flow)
+        
+        return update_flow
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 
 @router.get("/download/{folder_id}", response_model=FlowListReadWithFolderName, status_code=200)
