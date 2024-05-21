53a3ca5f311d35b4b7a8d2eefdebe24e6e48ad2c
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-05-21 19:54:01 -0300

    Refactor: move getCurlCode to apiModal folder

--- src/frontend/src/utils/utils.ts
+++ src/frontend/src/utils/utils.ts
@@ -141,33 +141,6 @@ export function getChatInputField(flowState?: FlowState) {
   return chat_input_field;
 }
 
-/**
- * Function to get the curl code for the API
- * @param {string} flowId - The id of the flow
- * @param {boolean} isAuth - If the API is authenticated
- * @returns {string} - The curl code
- */
-export function getCurlCode(
-  flowId: string,
-  isAuth: boolean,
-  tweaksBuildedObject,
-): string {
-  const tweaksObject = tweaksBuildedObject[0];
-
-  return `curl -X POST \\
-  ${window.location.protocol}//${
-    window.location.host
-  }/api/v1/run/${flowId}?stream=false \\
-  -H 'Content-Type: application/json'\\${
-    !isAuth ? `\n  -H 'x-api-key: <your api key>'\\` : ""
-  }
-  -d '{"input_value": "message",
-  "output_type": "chat",
-  "input_type": "chat",
-  "tweaks": ${JSON.stringify(tweaksObject, null, 2)}'
-  `;
-}
-
 export function getOutputIds(flow) {
   const nodes = flow.data!.nodes;
 
