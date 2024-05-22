45c54cea3fefb543d1e00a5aa7f1c0d33c681240
ogabrielluiz <gabriel@langflow.org>
2024-05-22 16:26:19 -0300

    feat: Update webhook columns revision ID

--- src/backend/base/langflow/alembic/versions/631faacf5da2_add_webhook_columns.py
+++ src/backend/base/langflow/alembic/versions/631faacf5da2_add_webhook_columns.py
@@ -1,21 +1,20 @@
 """Add webhook columns
 
 Revision ID: 631faacf5da2
-Revises: 58b28437a398
+Revises: 29fe8f1f806b
 Create Date: 2024-04-22 15:14:43.454784
 
 """
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.engine.reflection import Inspector
-
 
 # revision identifiers, used by Alembic.
 revision: str = "631faacf5da2"
-down_revision: Union[str, None] = "58b28437a398"
+down_revision: Union[str, None] = "29fe8f1f806b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
