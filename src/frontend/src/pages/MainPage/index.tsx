8ec406102eeab74be8137f64c1a19778ff1c3412
Prashanth P <42766214+prashudev@users.noreply.github.com>
2024-05-30 18:11:48 +0530

    Update index.tsx

--- src/frontend/src/pages/MainPage/index.tsx
+++ src/frontend/src/pages/MainPage/index.tsx
@@ -71,7 +71,7 @@ export default function HomePage(): JSX.Element {
   return (
     <PageLayout
       title={USER_PROJECTS_HEADER}
-      description="Manage your personal projects. Download or upload your collection for StackRoute"
+      description="Manage your personal projects. Download or upload your collection for StackRoute, NIIT Ltd"
       title= Stackroute
       button={
         <div className="flex gap-2">
