d091bf9611438f38b4b3ff693b86cb2bf873145c
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-05-30 17:14:39 -0300

    Fix: formatFileName function cant truncate when numberToTruncate are bigger than name length

--- src/frontend/src/modals/IOModal/components/chatView/filePreviewChat/utils/format-file-name.tsx
+++ src/frontend/src/modals/IOModal/components/chatView/filePreviewChat/utils/format-file-name.tsx
@@ -2,6 +2,9 @@ export default function formatFileName(
   name: string,
   numberToTruncate: number = 29,
 ): string {
+  if (name[numberToTruncate] === undefined) {
+    return name;
+  }
   const fileExtension = name.split(".").pop(); // Get the file extension
   const baseName = name.slice(0, name.lastIndexOf(".")); // Get the base name without the extension
   if (baseName.length > 6) {
