f1555c41d518da4d0df05b6c41530c5bdb21fd4f
anovazzi1 <otavio2204@gmail.com>
2024-05-31 17:44:29 -0300

    refactor: remove console.log statement and update condition for displaying "NO OUTPUT" message

--- src/frontend/src/customNodes/genericNode/components/outputModal/components/switchOutputView/components/index.tsx
+++ src/frontend/src/customNodes/genericNode/components/outputModal/components/switchOutputView/components/index.tsx
@@ -1,7 +1,6 @@
 import { Textarea } from "../../../../../../../components/ui/textarea";
 
 export default function ErrorOutput({ value }: { value: string }) {
-  console.log(value);
   return (
     <Textarea
       className={`h-full w-full text-destructive custom-scroll`}
