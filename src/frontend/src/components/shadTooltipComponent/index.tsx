ede690be95331d4a6d1785e05dd4fe5c189aefc5
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-02 23:16:04 -0300

    Added description column to advanced tab

--- src/frontend/src/components/shadTooltipComponent/index.tsx
+++ src/frontend/src/components/shadTooltipComponent/index.tsx
@@ -1,6 +1,11 @@
 import { ShadToolTipType } from "../../types/components";
 import { cn } from "../../utils/utils";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipContentWithoutPortal,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export default function ShadTooltip({
   content,
@@ -8,20 +13,23 @@ export default function ShadTooltip({
   asChild = true,
   children,
   styleClasses,
+  portal = true,
   delayDuration = 500,
 }: ShadToolTipType): JSX.Element {
+  const TooltipContentComponent = portal
+    ? TooltipContent
+    : TooltipContentWithoutPortal;
   return (
     <Tooltip delayDuration={delayDuration}>
       <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
-
-      <TooltipContent
+      <TooltipContentComponent
         className={cn(styleClasses, "max-w-96")}
         side={side}
         avoidCollisions={false}
         sticky="always"
       >
         {content}
-      </TooltipContent>
+      </TooltipContentComponent>
     </Tooltip>
   );
 }
