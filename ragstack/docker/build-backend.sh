e45efdb494d0e6e7d6bfc34faa161263fa8a9435
ming <itestmycode@gmail.com>
2024-06-03 11:54:52 -0400

    docker image with endpoint entry (#22)
    * build backend with endpoint entry
    
    * add curl to the entrypoint based image
    
    * poetry version from pyproject.xml

--- ragstack/docker/build-backend.sh
+++ ragstack/docker/build-backend.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 set -e
 RAGSTACK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+cd $RAGSTACK_DIR
+if command -v poetry &> /dev/null
+then
+    version=$(poetry version | awk '{print $2}')
+else
+    pyproject="$RAGSTACK_DIR/../pyproject.toml"
+    echo "pyproject file $pyproject"
+    if [[ -f "$pyproject" ]]; then
+        version=$(grep "^version" $pyproject | awk -F'=' '{gsub(/ /,"",$2); print $2}' | tr -d '"')
+        if [[ -n "$version" ]]; then
+            echo "Poetry is not installed. Version from pyproject.toml: $version"
+        else
+            echo "Version not found in $pyproject."
+            exit 1
+        fi
+    else
+        echo "pyproject.toml file $pyproject not found."
+        exit 1
+    fi
+fi
+
+echo "build docker image version $version ..."
+
 cd $RAGSTACK_DIR/docker/backend
 echo "Building backend image"
-docker build -t ragstack-ai-langflow-backend:latest -f Dockerfile ../../..
+docker build --build-arg VERSION=${version} -t ragstack-ai-langflow-backend:latest -f Dockerfile ../../..
 echo "Done ragstack-ai-langflow-backend:latest "
+
+cd $RAGSTACK_DIR/docker/backend-ep
+docker build --build-arg VERSION=${version} -t ragstack-ai-langflow-backend-ep:latest -f Dockerfile ../../..
+echo "Done ragstack-ai-langflow-backend-ep:latest "
+
