a1e521f2c6b6be8f9e6c3654a743fdffdc3e9d7e
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-05 18:39:36 -0300

    Fixed edit node modal not being updated with opening of modal

--- src/frontend/src/modals/editNodeModal/hooks/use-column-defs.tsx
+++ src/frontend/src/modals/editNodeModal/hooks/use-column-defs.tsx
@@ -10,6 +10,7 @@ const useColumnDefs = (
   myData: any,
   handleOnNewValue: (newValue: any, name: string) => void,
   changeAdvanced: (n: string) => void,
+  open: boolean,
 ) => {
   const columnDefs: ColDef[] = useMemo(
     () => [
@@ -81,7 +82,7 @@ const useColumnDefs = (
         cellClass: "no-border",
       },
     ],
-    [],
+    [open],
   );
 
   return columnDefs;
