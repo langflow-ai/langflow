079645e4c037d919107f5ba87c35d2d6571ec71d
joaoguilhermeS <j.guilherme.s.oliveira2@gmail.com>
2024-06-06 11:59:35 -0300

    <update> bringing streamlit icon from source commit

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -189,6 +189,7 @@ import SvgWolfram from "../icons/Wolfram/Wolfram";
 import { HackerNewsIcon } from "../icons/hackerNews";
 import { SupabaseIcon } from "../icons/supabase";
 import { iconsType } from "../types/components";
+import { Streamlit } from "../icons/Streamlit";
 
 export const gradients = [
   "bg-gradient-to-br from-gray-800 via-rose-700 to-violet-900",
@@ -517,4 +518,5 @@ export const nodeIconsLucide: iconsType = {
   Command,
   ArrowBigUp,
   Dot,
+  Streamlit
 };
