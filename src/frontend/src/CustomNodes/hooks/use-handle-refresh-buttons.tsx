472c8737ddda97f4e20d12d42ae9f73dcadcb354
ogabrielluiz <gabriel@langflow.org>
2024-06-07 14:39:27 -0300

    move folder

---
+++ src/frontend/src/CustomNodes/hooks/use-handle-refresh-buttons.tsx
@@ -0,0 +1,38 @@
+import { cloneDeep } from "lodash";
+import useAlertStore from "../../stores/alertStore";
+import { ResponseErrorDetailAPI } from "../../types/api";
+import { handleUpdateValues } from "../../utils/parameterUtils";
+
+const useHandleRefreshButtonPress = (setIsLoading, setNode) => {
+  const setErrorData = useAlertStore((state) => state.setErrorData);
+
+  const handleRefreshButtonPress = async (name, data) => {
+    setIsLoading(true);
+    try {
+      let newTemplate = await handleUpdateValues(name, data);
+
+      if (newTemplate) {
+        setNode(data.id, (oldNode) => {
+          let newNode = cloneDeep(oldNode);
+          newNode.data = {
+            ...newNode.data,
+          };
+          newNode.data.node.template = newTemplate;
+          return newNode;
+        });
+      }
+    } catch (error) {
+      let responseError = error as ResponseErrorDetailAPI;
+
+      setErrorData({
+        title: "Error while updating the Component",
+        list: [responseError?.response?.data?.detail ?? "Unknown error"],
+      });
+    }
+    setIsLoading(false);
+  };
+
+  return { handleRefreshButtonPress };
+};
+
+export default useHandleRefreshButtonPress;
