4d11c76476360ca58d86d2b738cb069f4925c343
ogabrielluiz <gabriel@langflow.org>
2024-06-10 19:00:09 -0300

    refactor: Update ChatInput and ChatOutput to use Message instead of Record for input and output

--- src/backend/base/langflow/components/inputs/Prompt.py
+++ src/backend/base/langflow/components/inputs/Prompt.py
@@ -1,9 +1,9 @@
-from langflow.custom import CustomComponent
-from langflow.field_typing import Input
+from langflow.custom import Component
+from langflow.field_typing import Input, Output
 from langflow.field_typing.prompt import Prompt
 
 
-class PromptComponent(CustomComponent):
+class PromptComponent(Component):
     display_name: str = "Prompt"
     description: str = "Create a prompt template with dynamic variables."
     icon = "prompts"
@@ -11,14 +11,26 @@ class PromptComponent(CustomComponent):
     def build_config(self):
         return {
             "template": Input(display_name="Template"),
-            "code": Input(advanced=True),
         }
 
-    async def build(
+    inputs = [
+        Input(name="template", type=Prompt, display_name="Template"),
+    ]
+
+    outputs = [
+        Output(display_name="Prompt", name="prompt", method="build_prompt"),
+        Output(display_name="Text", name="text", method="format_prompt"),
+    ]
+
+    async def format_prompt(self) -> str:
+        prompt = await self.build_prompt()
+        formatted_text = prompt.format_text()
+        self.status = formatted_text
+        return formatted_text
+
+    async def build_prompt(
         self,
-        template: Prompt,
-        **kwargs,
     ) -> Prompt:
-        prompt = await Prompt.from_template_and_variables(template, kwargs)
+        prompt = await Prompt.from_template_and_variables(self.template, self.kwargs)
         self.status = prompt.format_text()
         return prompt
