6985b179d9be9fdd5a9cff04855c58ba820f8e69
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-10 17:19:19 -0300

    Fixed margin on buttons

--- src/frontend/src/pages/SettingsPage/pages/GlobalVariablesPage/index.tsx
+++ src/frontend/src/pages/SettingsPage/pages/GlobalVariablesPage/index.tsx
@@ -144,7 +144,7 @@ export default function GlobalVariablesPage() {
         <div className="flex flex-shrink-0 items-center gap-2">
           <AddNewVariableButton>
             <Button data-testid="api-key-button-store" variant="primary">
-              <IconComponent name="Plus" className="mr-2 w-4" />
+              <IconComponent name="Plus" className="w-4" />
               Add New
             </Button>
           </AddNewVariableButton>
