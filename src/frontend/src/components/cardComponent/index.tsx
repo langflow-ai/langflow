6554add0c3c5d91908cfa1eba36576ab5f3eb943
Rodrigo <rodrigosilvanader@gmail.com>
2024-06-09 23:28:24 -0300

    chore: Update cardComponent and button styles

--- src/frontend/src/components/cardComponent/index.tsx
+++ src/frontend/src/components/cardComponent/index.tsx
@@ -217,7 +217,7 @@ export default function CollectionCardComponent({
         data-testid={`card-${convertTestName(data.name)}`}
         //TODO check color schema
         className={cn(
-          "group relative flex h-[11rem] flex-col justify-between overflow-hidden transition-all hover:bg-muted/50 hover:shadow-md hover:dark:bg-[#ffffff10]",
+          "group relative flex h-[11rem] flex-col justify-between overflow-hidden hover:bg-muted/50 hover:shadow-md hover:dark:bg-[#5f5f5f0e]",
           disabled ? "pointer-events-none opacity-50" : "",
           onClick ? "cursor-pointer" : "",
           isSelectedCard ? "border border-selected" : "",
