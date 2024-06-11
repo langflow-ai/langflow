c3e2ad670ebcfe315a9bfeab085392bf7dce417a
ogabrielluiz <gabriel@langflow.org>
2024-06-11 20:49:34 -0300

    refactor: Remove PromptComponent and update langflow components to use Prompt field type

--- src/backend/base/langflow/components/prompts/Prompt.py
+++ src/backend/base/langflow/components/prompts/Prompt.py
@@ -1,24 +1,37 @@
-from langflow.custom import CustomComponent
-from langflow.field_typing import TemplateField
+from langflow.custom import Component
+from langflow.field_typing import Input, Output
 from langflow.field_typing.prompt import Prompt
 
 
-class PromptComponent(CustomComponent):
-    display_name: str = "Empty Prompt"
+class PromptComponent(Component):
+    display_name: str = "Prompt"
     description: str = "Create a prompt template with dynamic variables."
     icon = "prompts"
 
     def build_config(self):
         return {
-            "template": TemplateField(display_name="Template"),
-            "code": TemplateField(advanced=True),
+            "template": Input(display_name="Template"),
         }
 
-    async def build(
+    inputs = [
+        Input(name="template", type=Prompt, display_name="Template"),
+    ]
+
+    outputs = [
+        Output(display_name="Prompt", name="prompt", method="build_prompt"),
+        Output(display_name="Text", name="text", method="format_prompt"),
+    ]
+
+    async def format_prompt(self) -> str:
+        prompt = await self.build_prompt()
+        formatted_text = prompt.format_text()
+        self.status = formatted_text
+        return formatted_text
+
+    async def build_prompt(
         self,
-        template: Prompt,
-        **kwargs,
     ) -> Prompt:
-        prompt = await Prompt.from_template_and_variables(template, kwargs)  # type: ignore
+        kwargs = {k: v for k, v in self._arguments.items() if k != "template"}
+        prompt = await Prompt.from_template_and_variables(self.template, kwargs)
         self.status = prompt.format_text()
         return prompt
