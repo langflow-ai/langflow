489bc77896a1f9fadd10c8087b898e12b720889d
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-11 12:32:58 -0300

    Added Status button and fixed positioning of Node Header elements

--- src/frontend/src/CustomNodes/hooks/use-icons-status.tsx
+++ src/frontend/src/CustomNodes/hooks/use-icons-status.tsx
@@ -1,7 +1,4 @@
-import IconComponent from "../../components/genericIconComponent";
-import Checkmark from "../../components/ui/checkmark";
 import Loading from "../../components/ui/loading";
-import Xmark from "../../components/ui/xmark";
 import { BuildStatus } from "../../constants/enums";
 import { VertexBuildTypeAPI } from "../../types/api";
 import { cn } from "../../utils/utils";
@@ -24,35 +21,18 @@ const useIconStatus = (
       return <Loading className="text-medium-indigo" />;
     } else {
       return (
-        <>
-          <IconComponent
-            name="Play"
-            className={cn(
-              !conditionSuccess && !conditionInactive && !conditionError
-                ? "opacity-100"
-                : "opacity-0",
-              "absolute ml-0.5 h-5 fill-current stroke-2 text-muted-foreground transition-all group-hover:text-medium-indigo group-hover/node:opacity-100",
-            )}
-          />
-          {conditionSuccess ? (
-            <Checkmark
-              className="absolute ml-0.5 h-5 stroke-2 text-status-green opacity-100 transition-all group-hover/node:opacity-0"
-              isVisible={true}
-            />
-          ) : conditionInactive ? (
-            <IconComponent
-              name="Play"
-              className="absolute ml-0.5 h-5 fill-current stroke-2 text-status-gray opacity-30 transition-all group-hover/node:opacity-0"
-            />
-          ) : conditionError ? (
-            <Xmark
-              isVisible={true}
-              className="absolute ml-0.5 h-5 fill-current stroke-2 text-status-red opacity-100 transition-all group-hover/node:opacity-0"
-            />
-          ) : (
-            <></>
+        <div
+          className={cn(
+            "h-4 w-4 shrink-0 rounded-full",
+            conditionSuccess
+              ? "bg-status-green"
+              : conditionInactive
+                ? "bg-status-gray"
+                : conditionError
+                  ? "bg-status-red"
+                  : "bg-muted-foreground/40",
           )}
-        </>
+        />
       );
     }
   };
