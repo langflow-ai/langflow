a5752cdc14043df19142917a4ffa8ce24edbbbc3
Jordan Frazier <122494242+jordanrfrazier@users.noreply.github.com>
2024-06-11 05:05:31 -0700

    fix: ensure tweaks object exists (#2134)
    * ensure tweaks object exists
    
    * Make similar change for python code

--- src/frontend/src/modals/apiModal/utils/get-python-api-code.tsx
+++ src/frontend/src/modals/apiModal/utils/get-python-api-code.tsx
@@ -2,19 +2,27 @@
  * Function to get the python code for the API
  * @param {string} flowId - The id of the flow
  * @param {boolean} isAuth - If the API is authenticated
- * @param {any[]} tweak - The tweaks
+ * @param {any[]} tweaksBuildedObject - The tweaks
+ * @param {string} [endpointName] - The optional endpoint name
  * @returns {string} - The python code
  */
 export default function getPythonApiCode(
   flowId: string,
   isAuth: boolean,
-  tweaksBuildedObject,
-  endpointName?: string
+  tweaksBuildedObject: any[],
+  endpointName?: string,
 ): string {
-  const tweaksObject = tweaksBuildedObject[0];
-  const tweaksString = JSON.stringify(tweaksObject, null, 2)
-    .replace(/true/g, "True")
-    .replace(/false/g, "False");
+  let tweaksString = "{}";
+  if (tweaksBuildedObject && tweaksBuildedObject.length > 0) {
+    const tweaksObject = tweaksBuildedObject[0];
+    if (!tweaksObject) {
+      throw new Error("expected tweaks");
+    }
+    tweaksString = JSON.stringify(tweaksObject, null, 2)
+      .replace(/true/g, "True")
+      .replace(/false/g, "False");
+  }
+
   return `import argparse
 import json
 from argparse import RawTextHelpFormatter
