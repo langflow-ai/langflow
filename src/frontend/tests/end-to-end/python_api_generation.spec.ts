f21f2002b81085fa502f64ad3c930224b6a46a0c
cristhianzl <cristhian.lousa@gmail.com>
2024-06-12 11:31:59 -0300

    fix python api test

--- src/frontend/tests/end-to-end/python_api_generation.spec.ts
+++ src/frontend/tests/end-to-end/python_api_generation.spec.ts
@@ -21,7 +21,7 @@ test("python_api_generation", async ({ page, context }) => {
   await page.waitForTimeout(2000);
   await page.getByText("API", { exact: true }).click();
   await page.getByRole("tab", { name: "Python API" }).click();
-  await page.getByRole("button", { name: "Copy Code" }).click();
+  await page.getByTestId("icon-Copy").click();
   const handle = await page.evaluateHandle(() =>
     navigator.clipboard.readText(),
   );
