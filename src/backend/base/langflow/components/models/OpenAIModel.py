605729cd000d41592616d757c623a4162726a805
ogabrielluiz <gabriel@langflow.org>
2024-06-12 13:54:12 -0300

    refactor: Update OpenAIModelComponent inputs and model response method
    This commit updates the inputs of the `OpenAIModelComponent` class in the `OpenAIModel.py` file. It replaces the `Input` class with specific input classes such as `StrInput`, `IntInput`, `DictInput`, `DropdownInput`, `BoolInput`, and `SecretStrInput`. This change improves the organization and separation of concerns in the codebase, making it easier to understand and maintain.
    
    Additionally, the commit renames the `model_response` method to `build_model` for better clarity and consistency. The method is responsible for constructing the language model using the specified input values and returning the built model.
    
    These updates enhance the functionality and maintainability of the `OpenAIModelComponent` class.

--- src/backend/base/langflow/components/models/OpenAIModel.py
+++ src/backend/base/langflow/components/models/OpenAIModel.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from langchain_openai import ChatOpenAI
 from pydantic.v1 import SecretStr
 
@@ -7,7 +5,9 @@ from langflow.base.constants import STREAM_INFO_TEXT
 from langflow.base.models.model import LCModelComponent
 from langflow.base.models.openai_constants import MODEL_NAMES
 from langflow.field_typing import BaseLanguageModel, Text
-from langflow.template import Input, Output
+from langflow.inputs import BoolInput, DictInput, DropdownInput, FloatInput, SecretStrInput, StrInput
+from langflow.inputs.inputs import IntInput
+from langflow.template import Output
 
 
 class OpenAIModelComponent(LCModelComponent):
@@ -16,36 +16,34 @@ class OpenAIModelComponent(LCModelComponent):
     icon = "OpenAI"
 
     inputs = [
-        Input(name="input_value", type=str, display_name="Input", input_types=["Text", "Record", "Prompt"]),
-        Input(
+        StrInput(name="input_value", display_name="Input", input_types=["Text", "Record", "Prompt"]),
+        IntInput(
             name="max_tokens",
-            type=Optional[int],
             display_name="Max Tokens",
             advanced=True,
             info="The maximum number of tokens to generate. Set to 0 for unlimited tokens.",
         ),
-        Input(name="model_kwargs", type=dict, display_name="Model Kwargs", advanced=True),
-        Input(name="model_name", type=str, display_name="Model Name", advanced=False, options=MODEL_NAMES),
-        Input(
+        DictInput(name="model_kwargs", display_name="Model Kwargs", advanced=True),
+        DropdownInput(
+            name="model_name", display_name="Model Name", advanced=False, options=MODEL_NAMES, value=MODEL_NAMES[0]
+        ),
+        StrInput(
             name="openai_api_base",
-            type=Optional[str],
             display_name="OpenAI API Base",
             advanced=True,
             info="The base URL of the OpenAI API. Defaults to https://api.openai.com/v1.\n\nYou can change this to use other APIs like JinaChat, LocalAI and Prem.",
         ),
-        Input(
+        SecretStrInput(
             name="openai_api_key",
-            type=str,
             display_name="OpenAI API Key",
             info="The OpenAI API Key to use for the OpenAI model.",
             advanced=False,
-            password=True,
+            value="OPENAI_API_KEY",
         ),
-        Input(name="temperature", type=float, display_name="Temperature", advanced=False, default=0.1),
-        Input(name="stream", type=bool, display_name="Stream", info=STREAM_INFO_TEXT, advanced=True),
-        Input(
+        FloatInput(name="temperature", display_name="Temperature", value=0.1),
+        BoolInput(name="stream", display_name="Stream", info=STREAM_INFO_TEXT, advanced=True),
+        StrInput(
             name="system_message",
-            type=Optional[str],
             display_name="System Message",
             info="System message to pass to the model.",
             advanced=True,
@@ -53,19 +51,19 @@ class OpenAIModelComponent(LCModelComponent):
     ]
     outputs = [
         Output(display_name="Text", name="text_output", method="text_response"),
-        Output(display_name="Language Model", name="model_output", method="model_response"),
+        Output(display_name="Language Model", name="model_output", method="build_model"),
     ]
 
     def text_response(self) -> Text:
         input_value = self.input_value
         stream = self.stream
         system_message = self.system_message
-        output = self.model_response()
+        output = self.build_model()
         result = self.get_chat_result(output, stream, input_value, system_message)
         self.status = result
         return result
 
-    def model_response(self) -> BaseLanguageModel:
+    def build_model(self) -> BaseLanguageModel:
         openai_api_key = self.openai_api_key
         temperature = self.temperature
         model_name = self.model_name
@@ -80,7 +78,7 @@ class OpenAIModelComponent(LCModelComponent):
 
         output = ChatOpenAI(
             max_tokens=max_tokens or None,
-            model_kwargs=model_kwargs,
+            model_kwargs=model_kwargs or {},
             model=model_name,
             base_url=openai_api_base,
             api_key=api_key,
