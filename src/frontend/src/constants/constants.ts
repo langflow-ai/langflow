0e69693852f85bbb9a9ac586b7746e87a44e66a9
Rodrigo <rodrigosilvanader@gmail.com>
2024-06-13 01:59:29 -0300

    refactor: Update CHAT_FIRST_INITIAL_TEXT and CHAT_SECOND_INITIAL_TEXT constants

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -669,12 +669,12 @@ export const OUTPUT_TYPES = new Set([
 ]);
 
 export const CHAT_FIRST_INITIAL_TEXT =
-  "Start a conversation and click the agent's thoughts";
+  "Start a conversation and click the agent's memories";
 
 export const TOOLTIP_OUTDATED_NODE =
   "Your component is outdated. Click to update (data may be lost)";
 
-export const CHAT_SECOND_INITIAL_TEXT = "to inspect the chaining process.";
+export const CHAT_SECOND_INITIAL_TEXT = "to inspect previous messages.";
 
 export const ZERO_NOTIFICATIONS = "No new notifications";
 
