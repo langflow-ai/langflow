6f53c9c06f8f094099d0fddbeaba81cc536eef2a
cristhianzl <cristhian.lousa@gmail.com>
2024-06-13 17:45:41 -0300

    chore: Remove unused code in HeaderTabsSearchComponent

--- src/frontend/src/pages/MainPage/components/myCollectionComponent/components/headerTabsSearchComponent/index.tsx
+++ src/frontend/src/pages/MainPage/components/myCollectionComponent/components/headerTabsSearchComponent/index.tsx
@@ -1,38 +1,18 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
-import useAlertStore from "../../../../../../stores/alertStore";
 import useFlowsManagerStore from "../../../../../../stores/flowsManagerStore";
-import { useFolderStore } from "../../../../../../stores/foldersStore";
-import { handleDownloadFolderFn } from "../../../../utils/handle-download-folder";
 import InputSearchComponent from "../inputSearchComponent";
 import TabsSearchComponent from "../tabsComponent";
 
 type HeaderTabsSearchComponentProps = {};
 
 const HeaderTabsSearchComponent = ({}: HeaderTabsSearchComponentProps) => {
-  const location = useLocation();
-  const myCollectionId = useFolderStore((state) => state.myCollectionId);
-  const folderId = location?.state?.folderId || myCollectionId;
   const isLoading = useFlowsManagerStore((state) => state.isLoading);
   const [tabActive, setTabActive] = useState("Flows");
-  const setErrorData = useAlertStore((state) => state.setErrorData);
-  const allFlows = useFlowsManagerStore((state) => state.allFlows);
   const [inputValue, setInputValue] = useState("");
 
   const setSearchFlowsComponents = useFlowsManagerStore(
-    (state) => state.setSearchFlowsComponents
+    (state) => state.setSearchFlowsComponents,
   );
-
-  const handleDownloadFolder = () => {
-    if (allFlows.length === 0) {
-      setErrorData({
-        title: "Folder is empty",
-        list: [],
-      });
-      return;
-    }
-    handleDownloadFolderFn(folderId);
-  };
 
   return (
     <>
