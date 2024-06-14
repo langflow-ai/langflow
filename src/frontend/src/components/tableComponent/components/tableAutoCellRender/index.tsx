3a056ca5e722689a816d5aa20014aae5567e7a57
anovazzi1 <otavio2204@gmail.com>
2024-06-14 18:32:44 -0300

    refactor: Add badge rendering for success and failure values in TableAutoCellRender

--- src/frontend/src/components/tableComponent/components/tableAutoCellRender/index.tsx
+++ src/frontend/src/components/tableComponent/components/tableAutoCellRender/index.tsx
@@ -30,7 +30,19 @@ export default function TableAutoCellRender({
               variant="outline"
               size="sq"
               className={cn(
-                "min-w-min bg-success-background text-success-foreground hover:bg-success-background"
+                "min-w-min bg-success-background text-success-foreground hover:bg-success-background",
+              )}
+            >
+              {value}
+            </Badge>
+          );
+        } else if (value === "failure") {
+          return (
+            <Badge
+              variant="outline"
+              size="sq"
+              className={cn(
+                "min-w-min bg-error-background text-error-foreground hover:bg-error-background",
               )}
             >
               {value}
