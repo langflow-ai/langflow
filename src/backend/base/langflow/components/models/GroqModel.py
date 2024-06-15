60806556b145ff134a08c78bf8712a6f96b6f4e4
namastex888 <felipe@namastex.io>
2024-06-15 00:44:16 +0000

    upd: llm models

--- src/backend/base/langflow/components/models/GroqModel.py
+++ src/backend/base/langflow/components/models/GroqModel.py
@@ -1,102 +1,106 @@
 from typing import Optional
 
 from langchain_groq import ChatGroq
-from pydantic.v1 import SecretStr
-
-from langflow.base.constants import STREAM_INFO_TEXT
 from langflow.base.models.groq_constants import MODEL_NAMES
+from pydantic.v1 import SecretStr
+from langflow.base.constants import STREAM_INFO_TEXT
 from langflow.base.models.model import LCModelComponent
 from langflow.field_typing import BaseLanguageModel, Text
-from langflow.template import Input, Output
+from langflow.inputs import BoolInput, FloatInput, IntInput, SecretStrInput, StrInput, DropdownInput
+from langflow.template import Output
 
-
-class GroqModelComponent(LCModelComponent):
+class GroqModel(LCModelComponent):
     display_name: str = "Groq"
     description: str = "Generate text using Groq."
     icon = "Groq"
 
     inputs = [
-        Input(
+        SecretStrInput(
             name="groq_api_key",
-            field_type=str,
             display_name="Groq API Key",
             info="API key for the Groq API.",
-            password=True,
         ),
-        Input(
+        StrInput(
             name="groq_api_base",
-            field_type=Optional[str],
             display_name="Groq API Base",
-            advanced=True,
             info="Base URL path for API requests, leave blank if not using a proxy or service emulator.",
+            advanced=True,
         ),
-        Input(
+        IntInput(
             name="max_tokens",
-            field_type=Optional[int],
             display_name="Max Output Tokens",
-            advanced=True,
             info="The maximum number of tokens to generate.",
-        ),
-        Input(
-            name="temperature",
-            field_type=float,
-            display_name="Temperature",
-            info="Run inference with this temperature. Must be in the closed interval [0.0, 1.0].",
-        ),
-        Input(
-            name="n",
-            field_type=Optional[int],
-            display_name="N",
             advanced=True,
-            info="Number of chat completions to generate for each prompt. Note that the API may not return the full n completions if duplicates are generated.",
         ),
-        Input(
+        FloatInput(
+            name="temperature",
+            display_name="Temperature",
+            info="Run inference with this temperature. Must by in the closed interval [0.0, 1.0].",
+            value=0.1,
+        ),
+        IntInput(
+            name="n",
+            display_name="N",
+            info="Number of chat completions to generate for each prompt. Note that the API may not return the full n completions if duplicates are generated.",
+            advanced=True,
+        ),
+        DropdownInput(
             name="model_name",
-            field_type=str,
             display_name="Model",
-            info="The name of the model to use. Supported examples: gemini-pro",
+            info="The name of the model to use.",
             options=MODEL_NAMES,
         ),
-        Input(name="input_value", field_type=str, display_name="Input", input_types=["Text", "Data", "Prompt"]),
-        Input(name="stream", field_type=bool, display_name="Stream", advanced=True, info=STREAM_INFO_TEXT),
-        Input(
-            name="system_message",
-            field_type=Optional[str],
-            display_name="System Message",
+        StrInput(
+            name="input_value",
+            display_name="Input",
+            info="The input to the model.",
+            input_types=["Text", "Data", "Prompt"],
+        ),
+        BoolInput(
+            name="stream",
+            display_name="Stream",
+            info=STREAM_INFO_TEXT,
             advanced=True,
+        ),
+        StrInput(
+            name="system_message",
+            display_name="System Message",
             info="System message to pass to the model.",
+            advanced=True,
         ),
     ]
+
     outputs = [
         Output(display_name="Text", name="text_output", method="text_response"),
-        Output(display_name="Language Model", name="model_output", method="model_response"),
+        Output(display_name="Language Model", name="model_output", method="build_model"),
     ]
 
     def text_response(self) -> Text:
         input_value = self.input_value
         stream = self.stream
         system_message = self.system_message
-        output = self.model_response()
+        output = self.build_model()
         result = self.get_chat_result(output, stream, input_value, system_message)
         self.status = result
         return result
 
-    def model_response(self) -> BaseLanguageModel:
+    def build_model(self) -> BaseLanguageModel:
         groq_api_key = self.groq_api_key
         model_name = self.model_name
-        groq_api_base = self.groq_api_base or None
         max_tokens = self.max_tokens
         temperature = self.temperature
-        n = self.n or 1
+        groq_api_base = self.groq_api_base
+        n = self.n
         stream = self.stream
 
         output = ChatGroq(
             model_name=model_name,
-            max_tokens=max_tokens or None,  # type: ignore
+            max_tokens=max_tokens or None,
             temperature=temperature,
             groq_api_base=groq_api_base,
-            n=n,
+            n=n or 1,
             groq_api_key=SecretStr(groq_api_key),
             streaming=stream,
         )
+
         return output
