1de0767eaaa281ba80926204b8169d13f588526f
ogabrielluiz <gabriel@langflow.org>
2024-06-17 16:32:55 -0300

    refactor: Update StrictBoolean to CoalesceBool in inputs.py and validators.py

--- src/backend/base/langflow/inputs/validators.py
+++ src/backend/base/langflow/inputs/validators.py
@@ -2,4 +2,18 @@ from typing import Annotated
 
 from pydantic import PlainValidator
 
-StrictBoolean = Annotated[bool, PlainValidator(lambda v: v is True or v is False)]
+
+def validate_boolean(value: bool) -> bool:
+    valid_trues = ["True", "true", "1", "yes"]
+    valid_falses = ["False", "false", "0", "no"]
+    if value in valid_trues:
+        return True
+    if value in valid_falses:
+        return False
+    if isinstance(value, bool):
+        return value
+    else:
+        raise ValueError("Value must be a boolean")
+
+
+CoalesceBool = Annotated[bool, PlainValidator(validate_boolean)]
