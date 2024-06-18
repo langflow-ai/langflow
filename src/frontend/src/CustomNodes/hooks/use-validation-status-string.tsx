33af41d328678c1fb56c04c4feb6004673347ac1
ogabrielluiz <gabriel@langflow.org>
2024-06-18 10:33:29 -0300

    refactor: Improve useValidationStatusString hook to handle multiple log items

--- src/frontend/src/CustomNodes/hooks/use-validation-status-string.tsx
+++ src/frontend/src/CustomNodes/hooks/use-validation-status-string.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
+import { LogType, VertexBuildTypeAPI } from "../../types/api";
 
-const useValidationStatusString = (validationStatus, setValidationString) => {
+const useValidationStatusString = (validationStatus: VertexBuildTypeAPI, setValidationString) => {
   useEffect(() => {
     if (validationStatus?.data?.logs) {
       // if it is not a string turn it into a string
       console.log("validationStatus", validationStatus);
       let newValidationString = "";
-      Object.values(validationStatus?.data?.logs).forEach((log: any) => {
+      Object.values(validationStatus?.data?.logs).forEach((log: LogType | LogType[]) => {
+        if (!Array.isArray(log)) {
+          log = [log];
+        }
         log.forEach((logItem) => {
           if (logItem.type === "error" || logItem.type === "ValueError") {
             newValidationString += `${logItem.message}\n`;
