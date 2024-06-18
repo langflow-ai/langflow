3d710b155dd8c06504ad5a383df0ac32fccf4a40
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-18 19:48:39 -0300

    removed console.log

--- src/frontend/src/components/dataOutputComponent/index.tsx
+++ src/frontend/src/components/dataOutputComponent/index.tsx
@@ -13,9 +13,7 @@ function DataOutputComponent({
   rows: any;
   columnMode?: "intersection" | "union";
 }) {
-  console.log("rows", rows);
   const columns = extractColumnsFromRows(rows, columnMode);
-  console.log("columns", columns);
 
   const columnDefs = columns.map((col, idx) => ({
     ...col,
