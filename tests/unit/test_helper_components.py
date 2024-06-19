412a793a4c5f915f57a5dda41868774a5eb009e4
ogabrielluiz <gabriel@langflow.org>
2024-06-18 22:42:06 -0300

    refactor: Update DocumentsToDataComponent class name in test_helper_components.py
    The code changes in test_helper_components.py update the class name from DocumentToDataComponent to DocumentsToDataComponent. This refactor improves the clarity and consistency of the code.

--- tests/unit/test_helper_components.py
+++ tests/unit/test_helper_components.py
@@ -1,5 +1,4 @@
 from langchain_core.documents import Document
-
 from langflow.components import helpers
 from langflow.custom.utils import build_custom_component_template
 from langflow.schema import Data
@@ -20,7 +19,7 @@ def test_update_data_component():
 
 def test_document_to_data_component():
     # Arrange
-    document_to_data_component = helpers.DocumentToDataComponent()
+    document_to_data_component = helpers.DocumentsToDataComponent()
 
     # Act
     # Replace with your actual test data
@@ -54,17 +53,18 @@ def test_uuid_generator_component():
 
 def test_data_as_text_component():
     # Arrange
-    data_as_text_component = helpers.DataToTextComponent()
+    data_as_text_component = helpers.ParseDataComponent()
 
     # Act
     # Replace with your actual test data
     data = [Data(data={"key": "value", "bacon": "eggs"})]
     template = "Data:{data} -- Bacon:{bacon}"
-    result = data_as_text_component.build(data, template=template)
+    data_as_text_component.set_attributes({"data": data, "template": template})
+    result = data_as_text_component.parse_data()
 
     # Assert
     # Replace with your actual expected result
-    assert result == "Data:{'key': 'value', 'bacon': 'eggs'} -- Bacon:eggs"
+    assert result.text == "Data:{'key': 'value', 'bacon': 'eggs'} -- Bacon:eggs"
 
 
 def test_text_to_data_component():
