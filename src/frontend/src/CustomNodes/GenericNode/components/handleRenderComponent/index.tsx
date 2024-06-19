1625d448bdb309cb4f0b12672590fa61ffa162a9
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-19 15:46:17 -0300

    Fixed handle tooltip being on the wrong place

--- src/frontend/src/CustomNodes/GenericNode/components/handleRenderComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/handleRenderComponent/index.tsx
@@ -39,77 +39,65 @@ export default function HandleRenderComponent({
       unstyled
       className="h-7 truncate bg-muted p-0 text-sm font-normal text-black hover:bg-muted"
     >
-      <div className="flex">
-        <ShadTooltip
-          styleClasses={"tooltip-fixed-width custom-scroll nowheel"}
-          delayDuration={1000}
-          content={
-            <HandleTooltips
-              left={left}
-              nodes={nodes}
-              tooltipTitle={tooltipTitle!}
-            />
+      <ShadTooltip
+        styleClasses={"tooltip-fixed-width custom-scroll nowheel"}
+        delayDuration={1000}
+        content={
+          <HandleTooltips
+            left={left}
+            nodes={nodes}
+            tooltipTitle={tooltipTitle!}
+          />
+        }
+        side={left ? "left" : "right"}
+      >
+        <Handle
+          data-test-id={`handle-${title.toLowerCase()}-${
+            !showNode ? (left ? "target" : "source") : left ? "left" : "right"
+          }`}
+          type={left ? "target" : "source"}
+          position={left ? Position.Left : Position.Right}
+          key={scapedJSONStringfy(proxy ? { ...id, proxy } : id)}
+          id={scapedJSONStringfy(proxy ? { ...id, proxy } : id)}
+          isValidConnection={(connection) =>
+            isValidConnection(connection, nodes, edges)
           }
-          side={left ? "left" : "right"}
-        >
-          <div>
-            <Handle
-              data-test-id={`handle-${title.toLowerCase()}-${
-                !showNode
-                  ? left
-                    ? "target"
-                    : "source"
-                  : left
-                    ? "left"
-                    : "right"
-              }`}
-              type={left ? "target" : "source"}
-              position={left ? Position.Left : Position.Right}
-              key={scapedJSONStringfy(proxy ? { ...id, proxy } : id)}
-              id={scapedJSONStringfy(proxy ? { ...id, proxy } : id)}
-              isValidConnection={(connection) =>
-                isValidConnection(connection, nodes, edges)
-              }
-              className={classNames(
-                left ? "-ml-0.5" : "-mr-0.5",
-                "z-20 h-3 w-3 rounded-full border-none bg-background",
-              )}
-              style={{
-                background:
-                  "conic-gradient(" +
-                  colors
-                    .concat(colors[0])
-                    .map(
-                      (color, index) =>
-                        color +
-                        " " +
-                        ((360 / colors.length) * index -
-                          360 / (colors.length * 4)) +
-                        "deg " +
-                        ((360 / colors.length) * index +
-                          360 / (colors.length * 4)) +
-                        "deg",
-                    )
-                    .join(" ,") +
-                  ")",
-                WebkitMaskImage: "radial-gradient(transparent 40%, black 44%)",
-                maskImage: "radial-gradient(transparent 40%, black 44%)",
-              }}
-              onClick={() => {
-                setFilterEdge(
-                  groupByFamily(myData, tooltipTitle!, left, nodes!),
-                );
-              }}
-            />
-            <div
-              className={cn(
-                "absolute top-[50%] z-10 h-3 w-3 translate-y-[-50%] rounded-full bg-background",
-                left ? "-left-[4px] -ml-0.5" : "-right-[4px] -mr-0.5",
-              )}
-            />
-          </div>
-        </ShadTooltip>
-      </div>
+          className={classNames(
+            left ? "-ml-0.5" : "-mr-0.5",
+            "z-20 h-3 w-3 rounded-full border-none bg-background",
+          )}
+          style={{
+            background:
+              "conic-gradient(" +
+              colors
+                .concat(colors[0])
+                .map(
+                  (color, index) =>
+                    color +
+                    " " +
+                    ((360 / colors.length) * index -
+                      360 / (colors.length * 4)) +
+                    "deg " +
+                    ((360 / colors.length) * index +
+                      360 / (colors.length * 4)) +
+                    "deg",
+                )
+                .join(" ,") +
+              ")",
+            WebkitMaskImage: "radial-gradient(transparent 40%, black 44%)",
+            maskImage: "radial-gradient(transparent 40%, black 44%)",
+          }}
+          onClick={() => {
+            setFilterEdge(groupByFamily(myData, tooltipTitle!, left, nodes!));
+          }}
+        />
+      </ShadTooltip>
+      <div
+        className={cn(
+          "absolute top-[50%] z-10 h-3 w-3 translate-y-[-50%] rounded-full bg-background",
+          left ? "-left-[4px] -ml-0.5" : "-right-[4px] -mr-0.5",
+        )}
+      />
     </Button>
   );
 }
