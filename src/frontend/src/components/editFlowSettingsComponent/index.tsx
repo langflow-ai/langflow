59477dd8e72e68028edcab09dba495a0f706f3d8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-19 14:39:06 -0300

    Changed description height

--- src/frontend/src/components/editFlowSettingsComponent/index.tsx
+++ src/frontend/src/components/editFlowSettingsComponent/index.tsx
@@ -108,8 +108,8 @@ export const EditFlowSettings: React.FC<InputProps> = ({
             onChange={handleDescriptionChange}
             value={description!}
             placeholder="Flow description"
-            className="mt-2 max-h-[100px] resize-none font-normal"
-            rows={3}
+            className="mt-2 max-h-[250px] resize-none font-normal"
+            rows={5}
             onDoubleClickCapture={(event) => {
               handleFocus(event);
             }}
