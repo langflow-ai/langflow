8ef4b090f411f0026c8aa178ffc41516d755ca9f
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-06-20 13:36:38 -0300

    🔧 (constants.py): Replace TypeAlias with TypeVar for LanguageModel in field_typing module
    📝 (model.py): Import LanguageModel from field_typing module to use in LCModelComponent class
    🔧 (utils.py): Replace get_all_types_from_type function with format_type function in custom module
    🔧 (custom.py): Remove unused import get_args in format_type function in helpers module

--- src/backend/base/langflow/helpers/custom.py
+++ src/backend/base/langflow/helpers/custom.py
@@ -1,4 +1,4 @@
-from typing import Any, get_args
+from typing import Any
 
 
 def format_type(type_: Any) -> str:
@@ -11,13 +11,3 @@ def format_type(type_: Any) -> str:
     else:
         type_ = str(type_)
     return type_
-
-
-def get_all_types_from_type(type_: Any) -> str:
-    args = get_args(type_)
-    if args:
-        formatted_types = [format_type(arg) for arg in args]
-        formatted_types.insert(0, format_type(type_))
-        return formatted_types
-    else:
-        return [format_type(type_)]
