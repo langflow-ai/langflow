b4598f1b1fc03fc5cee8a292bfb6f483d6390e96
anovazzi1 <otavio2204@gmail.com>
2024-06-20 12:22:28 -0300

    refactor: Remove unused code and comments in OutputComponent/index.tsx

--- src/frontend/src/CustomNodes/GenericNode/components/OutputComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/OutputComponent/index.tsx
@@ -38,55 +38,54 @@ export default function OutputComponent({
     }
   };
 
-  if (types.length < 2) {
     return displayProxy(
       <span className={cn(frozen ? "text-ice" : "")}>{name}</span>,
     );
-  }
 
-  return (
-    <div className="nocopy nopan nodelete nodrag noundo flex items-center gap-2">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            disabled={frozen}
-            variant="primary"
-            size="xs"
-            className={cn(
-              frozen ? "text-ice" : "",
-              "items-center gap-1 pl-2 pr-1.5 align-middle text-xs font-normal",
-            )}
-          >
-            <span className="pb-px">{selected}</span>
-            <ForwardedIconComponent name="ChevronDown" className="h-3 w-3" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {types.map((type) => (
-            <DropdownMenuItem
-              onSelect={() => {
-                // TODO: UDPDATE SET NODE TO NEW NODE FORM
-                setNode(nodeId, (node) => {
-                  const newNode = cloneDeep(node);
-                  (newNode.data as NodeDataType).node!.outputs![idx].selected =
-                    type;
-                  return newNode;
-                });
-                updateNodeInternals(nodeId);
-              }}
-            >
-              {type}
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-      {proxy ? (
-        <ShadTooltip content={<span>{proxy.nodeDisplayName}</span>}>
-          <span>{name}</span>
-        </ShadTooltip>
-      ) : (
-        <span>{name}</span>
-      )}
-    </div>
-  );
+  // ! DEACTIVATED UNTIL BETTER IMPLEMENTATION
+  // return (
+  //   <div className="nocopy nopan nodelete nodrag noundo flex items-center gap-2">
+  //     <DropdownMenu>
+  //       <DropdownMenuTrigger asChild>
+  //         <Button
+  //           disabled={frozen}
+  //           variant="primary"
+  //           size="xs"
+  //           className={cn(
+  //             frozen ? "text-ice" : "",
+  //             "items-center gap-1 pl-2 pr-1.5 align-middle text-xs font-normal",
+  //           )}
+  //         >
+  //           <span className="pb-px">{selected}</span>
+  //           <ForwardedIconComponent name="ChevronDown" className="h-3 w-3" />
+  //         </Button>
+  //       </DropdownMenuTrigger>
+  //       <DropdownMenuContent>
+  //         {types.map((type) => (
+  //           <DropdownMenuItem
+  //             onSelect={() => {
+  //               // TODO: UDPDATE SET NODE TO NEW NODE FORM
+  //               setNode(nodeId, (node) => {
+  //                 const newNode = cloneDeep(node);
+  //                 (newNode.data as NodeDataType).node!.outputs![idx].selected =
+  //                   type;
+  //                 return newNode;
+  //               });
+  //               updateNodeInternals(nodeId);
+  //             }}
+  //           >
+  //             {type}
+  //           </DropdownMenuItem>
+  //         ))}
+  //       </DropdownMenuContent>
+  //     </DropdownMenu>
+  //     {proxy ? (
+  //       <ShadTooltip content={<span>{proxy.nodeDisplayName}</span>}>
+  //         <span>{name}</span>
+  //       </ShadTooltip>
+  //     ) : (
+  //       <span>{name}</span>
+  //     )}
+  //   </div>
+  // );
 }
