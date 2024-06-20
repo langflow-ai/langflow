58f2bf4919eb04d5af20dcbf8db91e6dbc3dd911
berrytern <berrytern@gmail.com>
2024-06-20 20:53:27 -0300

    <fix>: Adjustments on MongoVectorStoreComponent   - Remove unnecessary kwargs;   - Post-process to Serialize ObjectId data type;

--- src/backend/base/langflow/components/vectorstores/MongoDBAtlasVector.py
+++ src/backend/base/langflow/components/vectorstores/MongoDBAtlasVector.py
@@ -68,9 +68,7 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
                     documents=documents,
                     embedding=self.embedding,
                     collection=collection,
-                    db_name=self.db_name,
-                    index_name=self.index_name,
-                    mongodb_atlas_cluster_uri=self.mongodb_atlas_cluster_uri,
+                    index_name=self.index_name
                 )
             else:
                 vector_store = MongoDBAtlasVectorSearch(
@@ -88,14 +86,23 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
         return vector_store
 
     def search_documents(self) -> List[Data]:
+        from typing import Optional, Union
+        from bson import ObjectId
+        from langchain.schema import Document
+        import json
         vector_store = self._build_mongodb_atlas()
-
-        if self.search_input and isinstance(self.search_input, str) and self.search_input.strip():
+        class JSONEncoder(json.JSONEncoder):
+            def default(self, o):
+                if isinstance(o, Union[ObjectId, Document]):
+                    return str(o)
+                return super(JSONEncoder, self).default(o)
+        if self.search_input and isinstance(self.search_input, str):
             docs = vector_store.similarity_search(
                 query=self.search_input,
                 k=self.number_of_results,
             )
-
+            for index in range(len(docs)):
+                docs[index].metadata = {key:str(docs[index].metadata[key]) if isinstance(docs[index].metadata[key], ObjectId) else docs[index].metadata[key] for key in docs[index].metadata}
             data = docs_to_data(docs)
             self.status = data
             return data
