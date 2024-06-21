2269e241e0bd9afdb188fe895c54ebecf62463f9
anovazzi1 <otavio2204@gmail.com>
2024-06-21 12:31:53 -0300

    updating inside text modal but with bugs

--- src/frontend/src/components/stringReaderComponent/index.tsx
+++ src/frontend/src/components/stringReaderComponent/index.tsx
@@ -2,11 +2,13 @@ import TextModal from "../../modals/textModal";
 
 export default function StringReader({
   string,
+  setValue,
 }: {
   string: string;
+  setValue: (value: string) => void;
 }): JSX.Element {
   return (
-    <TextModal value={string}>
+    <TextModal setValue={setValue} value={string}>
       <span className="truncate">{string}</span>
     </TextModal>
   );
