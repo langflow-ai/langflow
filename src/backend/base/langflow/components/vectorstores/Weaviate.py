2b3e91ce62d284d7d55f0c09bfa0ad50b8426664
joaoguilhermeS <j.guilherme.s.oliveira2@gmail.com>
2024-06-22 17:46:13 -0300

    chore: Updating Weaviate Vector Store parameters format

--- src/backend/base/langflow/components/vectorstores/Weaviate.py
+++ src/backend/base/langflow/components/vectorstores/Weaviate.py
@@ -20,18 +20,13 @@ class WeaviateVectorStoreComponent(LCVectorStoreComponent):
         SecretStrInput(name="api_key", display_name="API Key", required=False),
         StrInput(name="index_name", display_name="Index Name", required=True),
         StrInput(name="text_key", display_name="Text Key", value="text", advanced=True),
-        HandleInput(name="embedding", display_name="Embedding", input_types=["Embeddings"]),
+        MultilineInput(name="search_query", display_name="Search Query"),
         DataInput(
-            name="vector_store_inputs",
-            display_name="Vector Store Inputs",
+            name="ingest_data",
+            display_name="Ingest Data",
             is_list=True,
         ),
-        BoolInput(
-            name="add_to_vector_store",
-            display_name="Add to Vector Store",
-            info="If true, the Vector Store Inputs will be added to the Vector Store.",
-        ),
-        MultilineInput(name="search_input", display_name="Search Input"),
+        HandleInput(name="embedding", display_name="Embedding", input_types=["Embeddings"]),
         IntInput(
             name="number_of_results",
             display_name="Number of Results",
@@ -52,22 +47,21 @@ class WeaviateVectorStoreComponent(LCVectorStoreComponent):
         else:
             client = weaviate.Client(url=self.url)
 
-        if self.add_to_vector_store:
-            documents = []
-            for _input in self.vector_store_inputs or []:
-                if isinstance(_input, Data):
-                    documents.append(_input.to_lc_document())
-                else:
-                    documents.append(_input)
+        documents = []
+        for _input in self.ingest_data or []:
+            if isinstance(_input, Data):
+                documents.append(_input.to_lc_document())
+            else:
+                documents.append(_input)
 
-            if documents and self.embedding:
-                return Weaviate.from_documents(
-                    client=client,
-                    index_name=self.index_name,
-                    documents=documents,
-                    embedding=self.embedding,
-                    by_text=self.search_by_text,
-                )
+        if documents and self.embedding:
+            return Weaviate.from_documents(
+                client=client,
+                index_name=self.index_name,
+                documents=documents,
+                embedding=self.embedding,
+                by_text=self.search_by_text,
+            )
 
         return Weaviate(
             client=client,
@@ -80,9 +74,9 @@ class WeaviateVectorStoreComponent(LCVectorStoreComponent):
     def search_documents(self) -> List[Data]:
         vector_store = self._build_weaviate()
 
-        if self.search_input and isinstance(self.search_input, str) and self.search_input.strip():
+        if self.search_query and isinstance(self.search_query, str) and self.search_query.strip():
             docs = vector_store.similarity_search(
-                query=self.search_input,
+                query=self.search_query,
                 k=self.number_of_results,
             )
 
