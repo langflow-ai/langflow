833693cdf09675a6fdaec0787b31ef764e0f70ea
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-06-23 18:14:56 -0300

    refactor: Update build_config parameter type in AgentComponent

--- src/backend/base/langflow/field_typing/__init__.py
+++ src/backend/base/langflow/field_typing/__init__.py
@@ -3,11 +3,11 @@ from typing import Any
 from .constants import (
     AgentExecutor,
     BaseChatMemory,
+    BaseChatModel,
     BaseLanguageModel,
     BaseLLM,
     BaseLoader,
     BaseMemory,
-    BaseChatModel,
     BaseOutputParser,
     BasePromptTemplate,
     BaseRetriever,
@@ -21,10 +21,11 @@ from .constants import (
     NestedDict,
     Object,
     PromptTemplate,
+    Retriever,
+    Text,
     TextSplitter,
     Tool,
     VectorStore,
-    Retriever,
 )
 from .range_spec import RangeSpec
 
@@ -82,4 +83,5 @@ __all__ = [
     "VectorStore",
     "BaseChatModel",
     "Retriever",
+    "Text",
 ]
--- src/backend/base/langflow/field_typing/constants.py
+++ src/backend/base/langflow/field_typing/constants.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, TypeAlias, TypeVar, Union
+from typing import Callable, Dict, Text, TypeAlias, TypeVar, Union
 
 from langchain.agents.agent import AgentExecutor
 from langchain.chains.base import Chain
@@ -65,6 +65,7 @@ CUSTOM_COMPONENT_SUPPORTED_TYPES = {
     "NestedDict": NestedDict,
     "Data": Data,
     "Message": Message,
+    "Text": Text,
     "Object": Object,
     "Callable": Callable,
     "LanguageModel": LanguageModel,
