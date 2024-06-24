0412208ca33493a0de7479b96f57636def97a01c
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-06-24 08:52:52 -0300

    chore: Update imports and remove unused components in deactivated and prototypes folders

--- src/backend/base/langflow/components/deactivated/__init__.py
+++ src/backend/base/langflow/components/deactivated/__init__.py
@@ -1,17 +1,8 @@
 from .AgentComponent import AgentComponent
-from .ConditionalRouter import ConditionalRouterComponent  # type: ignore
 from .ExtractKeyFromData import ExtractKeyFromDataComponent
-from .FlowTool import FlowToolComponent  # type: ignore
-from .Listen import ListenComponent  # type: ignore
 from .ListFlows import ListFlowsComponent
 from .MergeData import MergeDataComponent
-from .Notify import NotifyComponent  # type: ignore
-from .PythonFunction import PythonFunctionComponent  # type: ignore
-from .RunFlow import RunFlowComponent   # type: ignore
-from .RunnableExecutor import RunnableExecComponent
 from .SelectivePassThrough import SelectivePassThroughComponent
-from .SplitText import SplitTextComponent  # type: ignore
-from .SQLExecutor import SQLExecutorComponent  # type: ignore
 from .SubFlow import SubFlowComponent
 
 __all__ = [
@@ -25,7 +16,6 @@ __all__ = [
     "NotifyComponent",
     "PythonFunctionComponent",
     "RunFlowComponent",
-    "RunnableExecComponent",
     "SplitTextComponent",
     "SQLExecutorComponent",
     "SubFlowComponent",
