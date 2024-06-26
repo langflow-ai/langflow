e9322c234f28d2ddca2800f535c4b73bbc27457f
Rodrigo <rodrigosilvanader@gmail.com>
2024-06-26 12:02:18 -0300

    Update StoreMessageComponent for enhanced message handling

--- src/backend/base/langflow/components/helpers/StoreMessage.py
+++ src/backend/base/langflow/components/helpers/StoreMessage.py
@@ -1,5 +1,5 @@
 from langflow.custom import Component
-from langflow.inputs import MessageTextInput, StrInput
+from langflow.inputs import MessageInput, StrInput
 from langflow.schema.message import Message
 from langflow.template import Output
 from langflow.memory import get_messages, store_message
@@ -11,22 +11,25 @@ class StoreMessageComponent(Component):
     icon = "save"
 
     inputs = [
-        MessageTextInput(
+        MessageInput(
             name="message",
             display_name="Message",
             info="The chat message to be stored.",
-            input_types=["Message", "str"],
-            required=True,
+            required=True
         ),
         StrInput(
             name="sender",
             display_name="Sender",
             info="The sender of the message.",
-            value="",
+            value="AI",
             advanced=True,
         ),
         StrInput(
-            name="sender_name", display_name="Sender Name", info="The name of the sender.", value="", advanced=True
+            name="sender_name",
+            display_name="Sender Name",
+            info="The name of the sender.",
+            value="AI",
+            advanced=True
         ),
         StrInput(
             name="session_id",
@@ -41,26 +44,13 @@ class StoreMessageComponent(Component):
     ]
 
     def store_message(self) -> Message:
-        if isinstance(self.message, str):
-            if not self.session_id:
-                raise ValueError("If passing a text, Session ID cannot be empty.")
-            message = Message(
-                text=self.message, sender=self.sender, sender_name=self.sender_name, session_id=self.session_id
-            )
+        message = self.message
 
-        elif isinstance(self.message, Message):
-            message = self.message
-            if self.session_id:
-                message.session_id = self.session_id
-            if self.sender:
-                message.sender = self.sender
-            if self.sender_name:
-                message.sender_name = self.sender_name
-        else:
-            raise ValueError("Message should be either string or Message.")
+        message.session_id = self.session_id or message.session_id
+        message.sender = self.sender or message.sender
+        message.sender_name = self.sender_name or message.sender_name
 
         store_message(message, flow_id=self.graph.flow_id)
-
         stored = get_messages(session_id=message.session_id, sender_name=message.sender_name, sender=message.sender)
         self.status = stored
         return stored
