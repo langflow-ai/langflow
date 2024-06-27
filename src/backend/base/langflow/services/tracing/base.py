4c763e8b1d0d890badb338203f67aee3c810d2bc
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-06-27 09:22:30 -0300

    refactor: Remove unused methods and abstract class from BaseTracer

--- src/backend/base/langflow/services/tracing/base.py
+++ src/backend/base/langflow/services/tracing/base.py
@@ -1,27 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import Any, Dict
 
 
-class BaseTrace(ABC):
-
+class BaseTracer(ABC):
     @abstractmethod
     def ready(self):
         raise NotImplementedError
 
     @abstractmethod
-    def setup_langsmith(self):
-        raise NotImplementedError
-
-    @abstractmethod
     def add_trace(self, trace_name: str, trace_type: str, inputs: Dict[str, Any], metadata: Dict[str, Any] = None):
-        raise NotImplementedError
-
-    @abstractmethod
-    def _convert_to_langchain_types(self, io_dict: Dict[str, Any]):
-        raise NotImplementedError
-
-    @abstractmethod
-    def _convert_to_langchain_type(self, value):
         raise NotImplementedError
 
     @abstractmethod
