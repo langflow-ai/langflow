d2916db1a678bb36a425a10ea895fe43aeb3da86
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-06-28 16:38:08 -0300

    Formatted

--- src/frontend/src/utils/buildUtils.ts
+++ src/frontend/src/utils/buildUtils.ts
@@ -99,7 +99,9 @@ export async function updateVerticesOrder(
     const runId = orderResponse.data.run_id;
     const verticesToRun = orderResponse.data.vertices_to_run;
 
-    useFlowStore.getState().updateBuildStatus(verticesToRun, BuildStatus.TO_BUILD)
+    useFlowStore
+      .getState()
+      .updateBuildStatus(verticesToRun, BuildStatus.TO_BUILD);
 
     const verticesIds = orderResponse.data.ids;
     useFlowStore.getState().updateVerticesBuild({
