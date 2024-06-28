90a908a817af08fbe0083a54795ea18f4c0fa60f
cristhianzl <cristhian.lousa@gmail.com>
2024-06-28 17:15:50 -0300

    removing wrong code pushed

--- src/frontend/src/components/codeTabsComponent/index.tsx
+++ src/frontend/src/components/codeTabsComponent/index.tsx
@@ -147,7 +147,7 @@ export default function CodeTabsComponent({
             </div>
           )}
 
-          {Number(activeTab) < 6 && (
+          {Number(activeTab) < 5 && (
             <>
               <Button
                 variant="ghost"
@@ -172,7 +172,7 @@ export default function CodeTabsComponent({
           className="api-modal-tabs-content overflow-hidden"
           key={idx} // Remember to add a unique key prop
         >
-          {idx < 6 ? (
+          {idx < 5 ? (
             <div className="flex h-full w-full flex-col">
               {tab.description && (
                 <div
