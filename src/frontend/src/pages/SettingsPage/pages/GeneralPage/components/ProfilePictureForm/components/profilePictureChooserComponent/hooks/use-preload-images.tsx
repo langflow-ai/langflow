932c06e0cf3666dae897ba9846600712cab2f488
Nicolò Boschi <boschi1997@gmail.com>
2024-06-28 11:36:50 +0200

    fronted: do not use backend url in <img>

--- src/frontend/src/pages/SettingsPage/pages/GeneralPage/components/ProfilePictureForm/components/profilePictureChooserComponent/hooks/use-preload-images.tsx
+++ src/frontend/src/pages/SettingsPage/pages/GeneralPage/components/ProfilePictureForm/components/profilePictureChooserComponent/hooks/use-preload-images.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import {
-  BACKEND_URL,
   BASE_URL_API,
 } from "../../../../../../../../../constants/constants";
 
@@ -24,12 +23,11 @@ const usePreloadImages = (
 
   useEffect(() => {
     const imageArray: string[] = [];
-    const firstUrl = `${BACKEND_URL.slice(0, BACKEND_URL.length - 1)}`;
 
     Object.keys(profilePictures).flatMap((folder) =>
       profilePictures[folder].map((path) =>
         imageArray.push(
-          `${firstUrl}${BASE_URL_API}files/profile_pictures/${folder}/${path}`,
+          `${BASE_URL_API}files/profile_pictures/${folder}/${path}`,
         ),
       ),
     );
