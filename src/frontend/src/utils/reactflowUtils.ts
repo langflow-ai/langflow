c06c2aef7634871650d14dc95be0cf423e4f6a74
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-07-03 12:44:25 -0300

    Refactor: Show why group button is disabled

--- src/frontend/src/utils/reactflowUtils.ts
+++ src/frontend/src/utils/reactflowUtils.ts
@@ -858,7 +858,7 @@ export function validateSelection(
     )
   ) {
     errorsArray.push(
-      "Please select only nodes that are not input or output nodes",
+      "Select non-input/output nodes only",
     );
   }
   //check if there are two or more nodes with free outputs
@@ -867,7 +867,7 @@ export function validateSelection(
       (n) => !clonedSelection.edges.some((e) => e.source === n.id),
     ).length > 1
   ) {
-    errorsArray.push("Please select only one node with free outputs");
+    errorsArray.push("Select only one node with free outputs");
   }
 
   // check if there is any node that does not have any connection
@@ -878,7 +878,7 @@ export function validateSelection(
         !clonedSelection.edges.some((edge) => edge.source === node.id),
     )
   ) {
-    errorsArray.push("Please select only nodes that are connected");
+    errorsArray.push("Select only connected nodes");
   }
   return errorsArray;
 }
