09a09c8f333fda700000cc399688f7b969944434
anovazzi1 <otavio2204@gmail.com>
2024-07-04 20:14:53 -0300

    refactor useGetMessages

--- src/frontend/src/controllers/API/queries/messages/use-get-messages.ts
+++ src/frontend/src/controllers/API/queries/messages/use-get-messages.ts
@@ -1,3 +1,4 @@
+import { useMessagesStore } from "@/stores/messagesStore";
 import { keepPreviousData } from "@tanstack/react-query";
 import { ColDef, ColGroupDef } from "ag-grid-community";
 import { useQueryFunctionType } from "../../../../types/api";
@@ -5,48 +6,24 @@ import { extractColumnsFromRows } from "../../../../utils/utils";
 import { api } from "../../api";
 import { getURL } from "../../helpers/constants";
 import { UseRequestProcessor } from "../../services/request-processor";
-import { useMessagesStore } from "@/stores/messagesStore";
 
 interface MessagesQueryParams {
   id?: string;
-  mode: "intersection" | "union",
-  excludedFields?: string[],
+  mode: "intersection" | "union";
+  excludedFields?: string[];
   params?: object;
 }
 
 interface MessagesResponse {
   rows: Array<object>;
   columns: Array<ColDef | ColGroupDef>;
-};
+}
 
 export const useGetMessagesQuery: useQueryFunctionType<
   MessagesQueryParams,
   MessagesResponse
-> = ({ id, mode,excludedFields,params }, onFetch) => {
+> = ({ id, mode, excludedFields, params }, onFetch) => {
   const { query } = UseRequestProcessor();
-
-  const responseFn = (
-    data: any,
-    mode: "intersection" | "union",
-    excludedFields?: string[]
-) => {
-    if (!onFetch) return data;
-    if (typeof onFetch === "function") return onFetch(data);
-    
-    switch (onFetch) {
-      case "Table": {
-        const columns = extractColumnsFromRows(data.data, mode, excludedFields);
-        return { rows: data, columns };
-      }
-      case "TableSaveState":
-        const columns = extractColumnsFromRows(data.data, mode, excludedFields);
-        useMessagesStore.getState().setMessages(data.data);
-        useMessagesStore.getState().setColumns(columns);
-        return { rows: data, columns };
-      default:
-        return data;
-    }
-  };
 
   const getMessagesFn = async (id?: string, params = {}) => {
     const config = {};
@@ -59,14 +36,19 @@ export const useGetMessagesQuery: useQueryFunctionType<
     return await api.get<any>(`${getURL("MESSAGES")}`, config);
   };
 
+  const responseFn = async (
+  ) => {
+    const data = await getMessagesFn(id, params);
+    const columns = extractColumnsFromRows(data.data, mode, excludedFields);
+    useMessagesStore.getState().setMessages(data.data);
+    useMessagesStore.getState().setColumns(columns);
+    return { rows: data, columns };
+  };
+  
   const queryResult = query(
     ["useGetMessagesQuery"],
-    async () => {
-      const data = await getMessagesFn(id, params);
-      return responseFn(data.data,mode,excludedFields);
-    },
-    {
-    },
+    responseFn,
+    {},
   );
 
   return queryResult;
