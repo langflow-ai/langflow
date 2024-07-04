cb4f0d03608a6725cc4cb7758722e5d5cf52d937
Nicolò Boschi <boschi1997@gmail.com>
2024-07-04 14:08:24 +0200

    feat: migrate chains and memories to Component syntax

--- src/backend/base/langflow/components/chains/ConversationChain.py
+++ src/backend/base/langflow/components/chains/ConversationChain.py
@@ -2,40 +2,48 @@ from typing import Optional
 
 from langchain.chains import ConversationChain
 
-from langflow.custom import CustomComponent
-from langflow.field_typing import BaseMemory, LanguageModel, Text
+from langflow.custom import Component
+from langflow.field_typing import Message
+from langflow.inputs import MultilineInput, HandleInput
+from langflow.template import Output
 
 
-class ConversationChainComponent(CustomComponent):
+class ConversationChainComponent(Component):
     display_name = "ConversationChain"
     description = "Chain to have a conversation and load context from memory."
     name = "ConversationChain"
 
-    def build_config(self):
-        return {
-            "prompt": {"display_name": "Prompt"},
-            "llm": {"display_name": "LLM"},
-            "memory": {
-                "display_name": "Memory",
-                "info": "Memory to load context from. If none is provided, a ConversationBufferMemory will be used.",
-            },
-            "input_value": {
-                "display_name": "Input Value",
-                "info": "The input value to pass to the chain.",
-            },
-        }
+    inputs = [
+        MultilineInput(
+            name="input_value",
+            display_name="Input",
+            info="The input value to pass to the chain.",
+            required=True
+        ),
+        HandleInput(
+            name="llm",
+            display_name="Language Model",
+            input_types=["LanguageModel"],
+            required=True
+        ),
+        HandleInput(
+            name="memory",
+            display_name="Memory",
+            input_types=["BaseChatMemory"],
+        )
+    ]
 
-    def build(
-        self,
-        input_value: Text,
-        llm: LanguageModel,
-        memory: Optional[BaseMemory] = None,
-    ) -> Text:
-        if memory is None:
-            chain = ConversationChain(llm=llm)
+    outputs = [
+        Output(display_name="Text", name="text", method="invoke_chain")
+    ]
+
+    def invoke_chain(self) -> Message:
+        if not self.memory:
+            chain = ConversationChain(llm=self.llm)
         else:
-            chain = ConversationChain(llm=llm, memory=memory)
-        result = chain.invoke({"input": input_value})
+            chain = ConversationChain(llm=self.llm, memory=self.memory)
+
+        result = chain.invoke({"input": self.input_value})
         if isinstance(result, dict):
             result = result.get(chain.output_key, "")  # type: ignore
 
@@ -43,5 +51,6 @@ class ConversationChainComponent(CustomComponent):
             result = result
         else:
             result = result.get("response")
+        result = str(result)
         self.status = result
-        return str(result)
+        return Message(text=result)
