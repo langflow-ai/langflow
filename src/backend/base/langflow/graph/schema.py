cb92120e3d600afbaa9c2c6a129b1a6f27c07bb3
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-16 15:24:55 -0300

    feat: add logs field to ResultData and Vertex class (#2732)
    * feat: add logs to ResultDataResponse in schemas.py
    
    * feat(schema.py): add logs field to ResultData class to store log messages for better debugging and monitoring
    
    * feat(vertex): add logs attribute to Vertex class to store logs for each vertex operation

--- src/backend/base/langflow/graph/schema.py
+++ src/backend/base/langflow/graph/schema.py
@@ -4,7 +4,7 @@ from typing import Any, List, Optional
 from pydantic import BaseModel, Field, field_serializer, model_validator
 
 from langflow.graph.utils import serialize_field
-from langflow.schema.schema import OutputLog, StreamURL
+from langflow.schema.schema import OutputValue, StreamURL
 from langflow.utils.schemas import ChatOutputResponse, ContainsEnumMeta
 
 
@@ -12,6 +12,7 @@ class ResultData(BaseModel):
     results: Optional[Any] = Field(default_factory=dict)
     artifacts: Optional[Any] = Field(default_factory=dict)
     outputs: Optional[dict] = Field(default_factory=dict)
+    logs: Optional[dict] = Field(default_factory=dict)
     messages: Optional[list[ChatOutputResponse]] = Field(default_factory=list)
     timedelta: Optional[float] = None
     duration: Optional[str] = None
@@ -40,9 +41,9 @@ class ResultData(BaseModel):
 
                 if "stream_url" in message and "type" in message:
                     stream_url = StreamURL(location=message["stream_url"])
-                    values["outputs"].update({key: OutputLog(message=stream_url, type=message["type"])})
+                    values["outputs"].update({key: OutputValue(message=stream_url, type=message["type"])})
                 elif "type" in message:
-                    values["outputs"].update({OutputLog(message=message, type=message["type"])})
+                    values["outputs"].update({OutputValue(message=message, type=message["type"])})
         return values
 
 
