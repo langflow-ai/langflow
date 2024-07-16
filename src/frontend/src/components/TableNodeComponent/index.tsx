0dbf0127c56cbfdce004669b10f4a9ed42e579e8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-16 15:10:14 -0300

    Used the TableModal on the TableNodeComponent

--- src/frontend/src/components/TableNodeComponent/index.tsx
+++ src/frontend/src/components/TableNodeComponent/index.tsx
@@ -1,4 +1,5 @@
 import BaseModal from "@/modals/baseModal";
+import TableModal from "@/modals/tableModal";
 import { FormatColumns } from "@/utils/utils";
 import { SelectionChangedEvent } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
@@ -70,40 +71,29 @@ export default function TableNodeComponent({
   return (
     <div className={"flex w-full items-center"}>
       <div className="flex w-full items-center gap-3" data-testid={"div-" + id}>
-        <BaseModal>
-          <BaseModal.Header description={"Add or edit your data"}>
-            <div className="flex justify-center gap-2 align-baseline">
-              <IconComponent name="Table" />
-              {tableTitle}
-            </div>
-          </BaseModal.Header>
-          <BaseModal.Content>
-            <TableComponent
-              ref={agGrid}
-              onSelectionChanged={(event: SelectionChangedEvent) => {
-                setSelectedNodes(event.api.getSelectedNodes());
-              }}
-              rowSelection="multiple"
-              suppressRowClickSelection={true}
-              editable={editable}
-              pagination={true}
-              addRow={addRow}
-              onDelete={deleteRow}
-              onDuplicate={duplicateRow}
-              displayEmptyAlert={false}
-              className="h-full w-full"
-              columnDefs={AgColumns}
-              rowData={value}
-            ></TableComponent>
-          </BaseModal.Content>
-          <BaseModal.Footer submit={{ label: "close" }}></BaseModal.Footer>
-          <BaseModal.Trigger asChild>
-            <Button variant="primary" className="w-full">
-              <ForwardedIconComponent name="Table" className="mt-px h-4 w-4" />
-              <span className="font-normal">Open Table</span>
-            </Button>
-          </BaseModal.Trigger>
-        </BaseModal>
+        <TableModal
+          tableTitle={tableTitle}
+          ref={agGrid}
+          onSelectionChanged={(event: SelectionChangedEvent) => {
+            setSelectedNodes(event.api.getSelectedNodes());
+          }}
+          rowSelection="multiple"
+          suppressRowClickSelection={true}
+          editable={editable}
+          pagination={true}
+          addRow={addRow}
+          onDelete={deleteRow}
+          onDuplicate={duplicateRow}
+          displayEmptyAlert={false}
+          className="h-full w-full"
+          columnDefs={AgColumns}
+          rowData={value}
+        >
+          <Button variant="primary" className="w-full">
+            <ForwardedIconComponent name="Table" className="mt-px h-4 w-4" />
+            <span className="font-normal">Open Table</span>
+          </Button>
+        </TableModal>
       </div>
     </div>
   );
