8ddabc2100923bcb9b1e35ea33c36a6d01dcb563
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-17 16:18:09 -0300

    Refactored logout to use the onSuccess and onError inside the API hook instead of in a new hook.

--- src/frontend/src/controllers/API/queries/auth/use-logout.ts
+++ src/frontend/src/controllers/API/queries/auth/use-logout.ts
@@ -5,14 +5,17 @@ import {
   useMutationFunctionType,
 } from "@/types/api";
 import { UseMutationResult } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { api } from "../../api";
 import { getURL } from "../../helpers/constants";
 import { UseRequestProcessor } from "../../services/request-processor";
 
-export const useLogout: useMutationFunctionType<undefined, undefined> = (
+export const useLogout: useMutationFunctionType<undefined, void> = (
   options?,
 ) => {
   const { mutate } = UseRequestProcessor();
+  const navigate = useNavigate();
+  const logout = useAuthStore((state) => state.logout);
 
   async function logoutUser(): Promise<any> {
     const autoLogin = useAuthStore.getState().autoLogin;
@@ -23,7 +26,16 @@ export const useLogout: useMutationFunctionType<undefined, undefined> = (
     return res.data;
   }
 
-  const mutation = mutate(["useLogout"], logoutUser, options);
+  const mutation = mutate(["useLogout"], logoutUser, {
+    onSuccess: () => {
+      logout();
+      navigate("/login");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+    ...options,
+  });
 
   return mutation;
 };
