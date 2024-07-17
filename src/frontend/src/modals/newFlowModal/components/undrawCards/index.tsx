435fa938b4a23844ab19e5f86c645a26fc86b7ea
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-07-17 19:25:00 -0300

    feat: add undraw cards for new flows (#2772)
    Added cards for Complex Agent, Sequential Tasks and HierarchicalTasks

--- src/frontend/src/modals/newFlowModal/components/undrawCards/index.tsx
+++ src/frontend/src/modals/newFlowModal/components/undrawCards/index.tsx
@@ -3,6 +3,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import BlogPost from "../../../../assets/undraw_blog_post_re_fy5x.svg?react";
 import ChatBot from "../../../../assets/undraw_chat_bot_re_e2gj.svg?react";
 import PromptChaining from "../../../../assets/undraw_cloud_docs_re_xjht.svg?react";
+import HierarchicalTasks from "../../../../assets/undraw_educator_re_ju47.svg?react";
+import ComplexAgent from "../../../../assets/undraw_firmware_re_fgdy.svg?react";
+import SequentialTasks from "../../../../assets/undraw_project_completed_re_jr7u.svg?react";
 import APIRequest from "../../../../assets/undraw_real_time_analytics_re_yliv.svg?react";
 import BasicPrompt from "../../../../assets/undraw_short_bio_re_fmx0.svg?react";
 import TransferFiles from "../../../../assets/undraw_transfer_files_re_a2a9.svg?react";
@@ -85,6 +88,36 @@ export default function UndrawCardComponent({
       case "Vector Store RAG":
         return (
           <PromptChaining
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        );
+      case "Sequential Tasks Agent":
+        return (
+          <SequentialTasks
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        );
+      case "Hierarchical Tasks Agent":
+        return (
+          <HierarchicalTasks
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        );
+      case "Complex Agent":
+        return (
+          <ComplexAgent
             style={{
               width: "80%",
               height: "80%",
