b9685e961649b9d1f28621f7caa37c6502220c05
cristhianzl <cristhian.lousa@gmail.com>
2024-07-18 16:31:18 -0300

    ✨ (frontend): Add new API queries for user authentication and messages handling 📝 (frontend): Update API queries for user authentication to include new functionalities like logout, reset password, update user, add user, login user, and refresh access token 📝 (frontend): Update API queries for messages to change the update messages functionality to use PUT method instead of PATCH

---
+++ src/frontend/src/controllers/API/queries/auth/use-patch-update-user.ts
@@ -0,0 +1,27 @@
+import { changeUser, useMutationFunctionType } from "@/types/api";
+import { UseMutationResult } from "@tanstack/react-query";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+
+interface UpdateUserParams {
+  user_id: string;
+  user: changeUser;
+}
+
+export const useUpdateUser: useMutationFunctionType<
+  undefined,
+  UpdateUserParams
+> = (options?) => {
+  const { mutate } = UseRequestProcessor();
+
+  async function updateUser({ user_id, user }: UpdateUserParams): Promise<any> {
+    const res = await api.patch(`${getURL("USERS")}/${user_id}`, user);
+    return res.data;
+  }
+
+  const mutation: UseMutationResult<UpdateUserParams, any, UpdateUserParams> =
+    mutate(["useUpdateUser"], updateUser, options);
+
+  return mutation;
+};
