3ace46feec294658e3b62db0f525caaf333ee998
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-18 12:31:32 -0300

    fix: update import in retriever tool

--- src/backend/base/langflow/components/tools/RetrieverTool.py
+++ src/backend/base/langflow/components/tools/RetrieverTool.py
@@ -1,4 +1,4 @@
-from langchain.tools.retriever import create_retriever_tool
+from langchain_core.tools import create_retriever_tool
 
 from langflow.custom import CustomComponent
 from langflow.field_typing import BaseRetriever, Tool
