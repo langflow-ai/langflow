1306605226be6e0bf3467aadaf1c16af322ec61d
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-07-19 18:52:19 -0300

    feat: create useGetCheckApiKeyQuery hook to handle api key validation

---
+++ src/frontend/src/controllers/API/queries/store/use-get-check-api-key.ts
@@ -0,0 +1,34 @@
+import { useStoreStore } from "@/stores/storeStore";
+import { useQueryFunctionType } from "@/types/api";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+
+interface ICheckQueryResponse {
+  has_api_key: boolean;
+  is_valid: boolean;
+}
+
+export const useGetCheckApiKeysQuery: useQueryFunctionType<
+  undefined,
+  ICheckQueryResponse
+> = (_, options) => {
+  const { query } = UseRequestProcessor();
+
+  const getCheckFn = async () => {
+    return await api.get<ICheckQueryResponse>(`${getURL("STORE")}/check/api_key/`);
+  };
+
+  const responseFn = async () => {
+    const { data } = await getCheckFn();
+    const { fetchApiData, updateLoadingApiKey } = useStoreStore.getState();
+    updateLoadingApiKey(true);
+    fetchApiData(data);
+    updateLoadingApiKey(false);
+    return data;
+  };
+
+  const queryResult = query(["useGetCheckApiKeysQuery"], responseFn, { ...options });
+
+  return queryResult;
+};
