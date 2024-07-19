a7f43939680ed4a5f361a2bb8d815513a48a5be8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-19 14:53:31 -0300

    Refactor EditNodeModal component to use new handleNodeClass and columnDefs

--- src/frontend/src/modals/editNodeModal/index.tsx
+++ src/frontend/src/modals/editNodeModal/index.tsx
@@ -1,106 +1,81 @@
 import { ColDef } from "ag-grid-community";
-import { forwardRef, useState } from "react";
-import { useUpdateNodeInternals } from "reactflow";
+import { useState } from "react";
 import useHandleNodeClass from "../../CustomNodes/hooks/use-handle-node-class";
 import TableComponent from "../../components/tableComponent";
 import { Badge } from "../../components/ui/badge";
 import { Button } from "../../components/ui/button";
 import { useDarkStore } from "../../stores/darkStore";
-import useFlowStore from "../../stores/flowStore";
-import useFlowsManagerStore from "../../stores/flowsManagerStore";
 import { APIClassType } from "../../types/api";
 import { NodeDataType } from "../../types/flow";
 import BaseModal from "../baseModal";
 import useColumnDefs from "./hooks/use-column-defs";
-import useHandleChangeAdvanced from "./hooks/use-handle-change-advanced";
 import useRowData from "./hooks/use-row-data";
 
-const EditNodeModal = forwardRef(
-  (
-    {
-      nodeLength,
-      open,
-      setOpen,
-      data,
-    }: {
-      nodeLength: number;
-      open: boolean;
-      setOpen: (open: boolean) => void;
-      data: NodeDataType;
-    },
-    ref,
-  ) => {
-    const isDark = useDarkStore((state) => state.dark);
-    const setNode = useFlowStore((state) => state.setNode);
-    const takeSnapshot = useFlowsManagerStore((state) => state.takeSnapshot);
-    const updateNodeInternals = useUpdateNodeInternals();
+const EditNodeModal = ({
+  nodeLength,
+  open,
+  setOpen,
+  data,
+}: {
+  nodeLength: number;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  data: NodeDataType;
+}) => {
+  const isDark = useDarkStore((state) => state.dark);
 
-    const { handleNodeClass: handleNodeClassHook } = useHandleNodeClass(
-      takeSnapshot,
-      setNode,
-      data.id,
-    );
+  const { handleNodeClass: handleNodeClassHook } = useHandleNodeClass(data.id);
 
-    const [nodeClass, setNodeClass] = useState<APIClassType>(data.node!);
+  const [nodeClass, setNodeClass] = useState<APIClassType>(data.node!);
 
-    const handleNodeClass = (
-      newNodeClass: APIClassType,
-      name: string,
-      code: string,
-      type?: string,
-    ) => {
-      handleNodeClassHook(newNodeClass, name, code, type);
-      setNodeClass(newNodeClass);
-    };
+  const handleNodeClass = (newNodeClass: APIClassType, type?: string) => {
+    handleNodeClassHook(newNodeClass, type);
+    setNodeClass(newNodeClass);
+  };
 
-    const { handleChangeAdvanced: handleChangeAdvancedHook } =
-      useHandleChangeAdvanced(data, takeSnapshot, setNode, updateNodeInternals);
+  const rowData = useRowData(data, nodeClass, open);
 
-    const rowData = useRowData(data, nodeClass, open);
+  const columnDefs: ColDef[] = useColumnDefs(
+    nodeClass,
+    handleNodeClass,
+    data.id,
+    open,
+  );
 
-    const columnDefs: ColDef[] = useColumnDefs(
-      nodeClass,
-      handleNodeClass,
-      data.id,
-      handleChangeAdvancedHook,
-      open,
-    );
-
-    return (
-      <BaseModal key={data.id} open={open} setOpen={setOpen}>
-        <BaseModal.Trigger>
-          <></>
-        </BaseModal.Trigger>
-        <BaseModal.Header description={data.node?.description!}>
-          <span className="pr-2">{data.node?.display_name ?? data.type}</span>
-          <div>
-            <Badge size="sm" variant={isDark ? "gray" : "secondary"}>
-              ID: {data.id}
-            </Badge>
+  return (
+    <BaseModal key={data.id} open={open} setOpen={setOpen}>
+      <BaseModal.Trigger>
+        <></>
+      </BaseModal.Trigger>
+      <BaseModal.Header description={data.node?.description!}>
+        <span className="pr-2">{data.node?.display_name ?? data.type}</span>
+        <div>
+          <Badge size="sm" variant={isDark ? "gray" : "secondary"}>
+            ID: {data.id}
+          </Badge>
+        </div>
+      </BaseModal.Header>
+      <BaseModal.Content>
+        <div className="flex h-full flex-col">
+          <div className="h-full">
+            {nodeLength > 0 && (
+              <TableComponent
+                key={"editNode"}
+                tooltipShowDelay={0.5}
+                columnDefs={columnDefs}
+                rowData={rowData}
+              />
+            )}
           </div>
-        </BaseModal.Header>
-        <BaseModal.Content>
-          <div className="flex h-full flex-col">
-            <div className="h-full">
-              {nodeLength > 0 && (
-                <TableComponent
-                  key={"editNode"}
-                  tooltipShowDelay={0.5}
-                  columnDefs={columnDefs}
-                  rowData={rowData}
-                />
-              )}
-            </div>
-          </div>
-        </BaseModal.Content>
-        <BaseModal.Footer>
-          <div className="flex w-full justify-end gap-2 pt-2">
-            <Button onClick={() => setOpen(false)}>Close</Button>
-          </div>
-        </BaseModal.Footer>
-      </BaseModal>
-    );
-  },
-);
+        </div>
+      </BaseModal.Content>
+      <BaseModal.Footer>
+        <div className="flex w-full justify-end gap-2 pt-2">
+          <Button onClick={() => setOpen(false)}>Close</Button>
+        </div>
+      </BaseModal.Footer>
+    </BaseModal>
+  );
+};
 
 export default EditNodeModal;
