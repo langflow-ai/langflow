48cef19aa413a84b15371f99306be53af18c665b
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-20 10:50:01 -0300

    refactor(test_base.py): add tests for graph preparation and execution
    Add tests to the 'test_base.py' file in the 'graph' directory to ensure proper graph preparation and execution. The tests cover scenarios where the graph is not prepared, ensuring that a 'ValueError' is raised, and scenarios where the graph is prepared and executed successfully. This addition improves the reliability and stability of the codebase.

--- src/backend/tests/unit/graph/graph/test_base.py
+++ src/backend/tests/unit/graph/graph/test_base.py
@@ -1,14 +1,41 @@
+from collections import deque
+
+import pytest
+
 from langflow import components
 from langflow.graph.graph.base import Graph
 
 
-def test_graph():
+@pytest.fixture
+def client():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_graph_not_prepared():
     chat_input = components.inputs.ChatInput()
     chat_output = components.outputs.ChatOutput()
     graph = Graph()
     graph.add_component("chat_input", chat_input)
     graph.add_component("chat_output", chat_output)
     graph.add_component_edge("chat_input", (chat_input.outputs[0].name, chat_input.inputs[0].name), "chat_output")
+    with pytest.raises(ValueError):
+        await graph.astep()
+
+
+@pytest.mark.asyncio
+async def test_graph():
+    chat_input = components.inputs.ChatInput()
+    chat_output = components.outputs.ChatOutput()
+    graph = Graph()
+    graph.add_component("chat_input", chat_input)
+    graph.add_component("chat_output", chat_output)
+    graph.add_component_edge("chat_input", (chat_input.outputs[0].name, chat_input.inputs[0].name), "chat_output")
+    graph.prepare()
+    assert graph._run_queue == deque(["chat_input"])
+    await graph.astep()
+    assert graph._run_queue == deque(["chat_output"])
+
     assert graph.vertices[0].id == "chat_input"
     assert graph.vertices[1].id == "chat_output"
     assert graph.edges[0].source_id == "chat_input"
