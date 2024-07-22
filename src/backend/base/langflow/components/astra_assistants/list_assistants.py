0d5228d68158a21877af739b25f5bc2b4c3a2399
Sebastián Estévez <estevezsebastian@gmail.com>
2024-07-22 13:26:30 -0400

    Astra Assistants Support (#2041)
    * ruff
    
    * revert config.yaml
    
    * add build_config
    
    * ruff
    
    * add build_config

---
+++ src/backend/base/langflow/components/astra_assistants/list_assistants.py
@@ -0,0 +1,18 @@
+from typing import List
+from langflow.custom import CustomComponent
+from openai import OpenAI
+from astra_assistants import patch
+
+
+class AssistantsListAssistants(CustomComponent):
+    display_name = "List Assistants"
+    description = "Returns a list of assistant id's"
+
+    def build_config(self):
+        return {}
+
+    def build(self) -> List[str]:
+        client = patch(OpenAI())
+        assistants = client.beta.assistants.list()
+        id_list = [assistant.id for assistant in assistants]
+        return id_list
