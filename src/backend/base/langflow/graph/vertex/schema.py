7e71427cc64f35c16a5a608e919ab7fa253d9bb6
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-23 12:46:40 -0300

    feat: Add new schema module for vertex data
    Add a new module, `schema.py`, under the `src/backend/base/langflow/graph/vertex` directory. This module defines the `Position` and `NodeData` types using `TypedDict` and `NotRequired` from the `typing_extensions` module. These types are used to represent the position and data of a vertex node in the graph. This addition enhances the codebase by providing a structured and type-safe way to handle vertex data.

---
+++ src/backend/base/langflow/graph/vertex/schema.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from typing_extensions import NotRequired, TypedDict
+
+
+class Position(TypedDict):
+    x: float
+    y: float
+
+
+class NodeData(TypedDict):
+    id: str
+    data: Dict
+    dragging: NotRequired[bool]
+    height: NotRequired[int]
+    width: NotRequired[int]
+    position: NotRequired[Position]
+    positionAbsolute: NotRequired[Position]
+    selected: NotRequired[bool]
+    type: str
