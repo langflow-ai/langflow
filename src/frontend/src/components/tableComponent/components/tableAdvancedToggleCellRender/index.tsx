2cd2d2f51fd838b84f397f491c0b96e01cd26fa2
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-24 14:45:38 -0300

    Removed external state on tableAdvancedToggle, making every state come from the Stores

--- src/frontend/src/components/tableComponent/components/tableAdvancedToggleCellRender/index.tsx
+++ src/frontend/src/components/tableComponent/components/tableAdvancedToggleCellRender/index.tsx
@@ -3,48 +3,50 @@ import ShadTooltip from "@/components/shadTooltipComponent";
 import useFlowStore from "@/stores/flowStore";
 import { isTargetHandleConnected } from "@/utils/reactflowUtils";
 import { CustomCellRendererProps } from "ag-grid-react";
-import { useState } from "react";
 import ToggleShadComponent from "../../../toggleShadComponent";
 
 export default function TableAdvancedToggleCellRender({
   node: { data },
-  value: { value: enabled, nodeId, nodeClass },
+  value: { nodeId },
 }: CustomCellRendererProps) {
-  const [value, setValue] = useState(enabled);
   const edges = useFlowStore((state) => state.edges);
+  const node = useFlowStore((state) => state.getNode(nodeId));
+  const parameter = node?.data?.node?.template?.[data.key];
 
   const disabled = isTargetHandleConnected(edges, data.key, data, nodeId);
 
   const { handleOnNewValue } = useHandleOnNewValue({
-    node: nodeClass,
+    node: node?.data.node,
     nodeId,
     name: data.key,
   });
 
   return (
-    <ShadTooltip
-      content={
-        disabled
-          ? "Cannot change visibility of connected handles"
-          : "Change visibility of the field"
-      }
-      styleClasses="z-50"
-    >
-      <div>
-        <div className="flex h-full items-center">
-          <ToggleShadComponent
-            id={"show" + name}
-            disabled={disabled}
-            enabled={value}
-            setEnabled={(e) => {
-              setValue(e);
-              handleOnNewValue({ advanced: value });
-            }}
-            size="small"
-            editNode={true}
-          />
+    parameter && (
+      <ShadTooltip
+        content={
+          disabled
+            ? "Cannot change visibility of connected handles"
+            : "Change visibility of the field"
+        }
+        styleClasses="z-50"
+      >
+        <div>
+          <div className="flex h-full items-center">
+            <ToggleShadComponent
+              id={"show" + data.key}
+              disabled={disabled}
+              enabled={!parameter?.advanced ?? true}
+              setEnabled={(e) => {
+                console.log(e, parameter);
+                handleOnNewValue({ advanced: !e });
+              }}
+              size="small"
+              editNode={true}
+            />
+          </div>
         </div>
-      </div>
-    </ShadTooltip>
+      </ShadTooltip>
+    )
   );
 }
