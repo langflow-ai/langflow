5915f91c55fb913738c97d20966a84803aa9eae5
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-24 14:50:12 -0300

    removed unused state and passed isTweaks to value getters

--- src/frontend/src/modals/editNodeModal/hooks/use-column-defs.tsx
+++ src/frontend/src/modals/editNodeModal/hooks/use-column-defs.tsx
@@ -1,24 +1,14 @@
-import { NodeType } from "@/types/flow";
+import TableAdvancedToggleCellRender from "@/components/tableComponent/components/tableAdvancedToggleCellRender";
 import { ColDef, ValueGetterParams } from "ag-grid-community";
 import { useMemo } from "react";
-import TableAdvancedToggleCellRender from "../../../components/tableComponent/components/tableAdvancedToggleCellRender";
 import TableNodeCellRender from "../../../components/tableComponent/components/tableNodeCellRender";
 import { APIClassType } from "../../../types/api";
 
 const useColumnDefs = (
   nodeClass: APIClassType,
-  handleNodeClass: (
-    newNodeClass: APIClassType,
-    name: string,
-    code: string,
-    type?: string,
-  ) => void,
   nodeId: string,
   open: boolean,
-  setNode?: (
-    id: string,
-    update: NodeType | ((oldState: NodeType) => NodeType),
-  ) => void,
+  isTweaks?: boolean,
   hideVisibility?: boolean,
 ) => {
   const columnDefs: ColDef[] = useMemo(() => {
@@ -56,11 +46,8 @@ const useColumnDefs = (
         cellRenderer: TableNodeCellRender,
         valueGetter: (params: ValueGetterParams) => {
           return {
-            value: params.data.value,
             nodeId: nodeId,
-            nodeClass: nodeClass,
-            handleNodeClass,
-            setNode,
+            isTweaks,
           };
         },
         minWidth: 340,
@@ -77,9 +64,7 @@ const useColumnDefs = (
         cellRenderer: TableAdvancedToggleCellRender,
         valueGetter: (params: ValueGetterParams) => {
           return {
-            value: !params.data.advanced,
-            nodeId: nodeId,
-            nodeClass,
+            nodeId,
           };
         },
         editable: false,
