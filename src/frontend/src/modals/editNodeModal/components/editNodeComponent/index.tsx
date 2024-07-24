a817072987042ba67d65dc5dbdca199e2ca2b944
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-24 15:38:37 -0300

    Fix performance issues on edit node modal

--- src/frontend/src/modals/editNodeModal/components/editNodeComponent/index.tsx
+++ src/frontend/src/modals/editNodeModal/components/editNodeComponent/index.tsx
@@ -1,6 +1,7 @@
 import TableComponent from "@/components/tableComponent";
 import { APIClassType } from "@/types/api";
 import { ColDef } from "ag-grid-community";
+import { useMemo } from "react";
 import useColumnDefs from "../../hooks/use-column-defs";
 import useRowData from "../../hooks/use-row-data";
 
@@ -27,19 +28,22 @@ export function EditNodeComponent({
     isTweaks,
     hideVisibility,
   );
-  return (
-    <div className="flex h-full flex-col">
-      <div className="h-full">
-        {nodeClass && (
-          <TableComponent
-            domLayout={autoHeight ? "autoHeight" : undefined}
-            key={"editNode"}
-            tooltipShowDelay={0.5}
-            columnDefs={columnDefs}
-            rowData={rowData}
-          />
-        )}
+  return useMemo(
+    () => (
+      <div className="flex h-full flex-col">
+        <div className="h-full">
+          {nodeClass && (
+            <TableComponent
+              domLayout={autoHeight ? "autoHeight" : undefined}
+              key={"editNode"}
+              tooltipShowDelay={0.5}
+              columnDefs={columnDefs}
+              rowData={rowData}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    ),
+    [nodeClass],
   );
 }
