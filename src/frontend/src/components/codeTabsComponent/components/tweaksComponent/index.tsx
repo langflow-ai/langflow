e88386b8874f39ff2cb21c4aaf3ddb1967c1d372
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-07-24 15:44:09 -0300

    Fixed scroll overflow issues on tweaksComponent

--- src/frontend/src/components/codeTabsComponent/components/tweaksComponent/index.tsx
+++ src/frontend/src/components/codeTabsComponent/components/tweaksComponent/index.tsx
@@ -1,23 +1,16 @@
 import { useTweaksStore } from "@/stores/tweaksStore";
 import { NodeType } from "@/types/flow";
-import { classNames } from "@/utils/utils";
 import { TweakComponent } from "../tweakComponent";
 
 export function TweaksComponent({ open }: { open: boolean }) {
   const nodes = useTweaksStore((state) => state.nodes);
   return (
-    <div className="api-modal-according-display">
-      <div
-        className={classNames(
-          "h-[70vh] w-full overflow-y-auto overflow-x-hidden rounded-lg bg-muted custom-scroll",
-        )}
-      >
-        {nodes?.map((node: NodeType, i) => (
-          <div className="px-3" key={i}>
-            <TweakComponent open={open} node={node} />
-          </div>
-        ))}
-      </div>
+    <div className="h-full w-full overflow-y-auto overflow-x-hidden rounded-lg bg-muted custom-scroll">
+      {nodes?.map((node: NodeType, i) => (
+        <div className="px-3" key={i}>
+          <TweakComponent open={open} node={node} />
+        </div>
+      ))}
     </div>
   );
 }
