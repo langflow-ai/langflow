f7c065f3c9c5e8c09a048904e7468c9f3a0cb18f
Jordan Frazier <jordan.frazier@datastax.com>
2024-07-24 14:22:47 -0700

    prints

--- src/backend/base/langflow/components/embeddings/util/AIMLEmbeddingsImpl.py
+++ src/backend/base/langflow/components/embeddings/util/AIMLEmbeddingsImpl.py
@@ -14,13 +14,18 @@ class AIMLEmbeddingsImpl(BaseModel, Embeddings):
     model: str
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        embeddings = []
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key.get_secret_value()}",
         }
 
+        import time
+        start = time.time()
+
         with httpx.Client() as client:
             for text in texts:
+                print(f"Text: {text}")
                 # This is inefficient - we should be batching up to the maximum length
                 # allowed by the model. However, that requires maintaining a mapping of the
                 # model and input length.
@@ -33,14 +38,19 @@ class AIMLEmbeddingsImpl(BaseModel, Embeddings):
                 }
 
                 try:
+                    start_req = time.time()
                     response = client.post(
                         self.embeddings_completion_url,
                         headers=headers,
                         json=payload,
                     )
+                    print(f"Time taken for request: {time.time() - start_req}")
                     response.raise_for_status()
                     result_data = response.json()
-                    return [item["embedding"] for item in result_data["data"]]
+                    print(f"Result data: {result_data}")
+                    assert len(result_data["data"]) == 1, "Expected one embedding"
+                    # embeddings.append([item["embedding"] for item in result_data["data"]])
+                    embeddings.append(result_data["data"][0]["embedding"])
                 except (
                     httpx.HTTPStatusError,
                     httpx.RequestError,
@@ -49,6 +59,10 @@ class AIMLEmbeddingsImpl(BaseModel, Embeddings):
                 ) as e:
                     logger.error(f"Error occurred: {e}")
                     raise
+
+        end = time.time()
+        print(f"Time taken: {end - start}")
+        return embeddings
 
     def embed_query(self, text: str) -> List[float]:
         return self.embed_documents([text])[0]
