a963ea2773c85d2cbbe05245b016d7a5d5df2f04
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-25 10:46:46 -0300

    refactor: update StateVertex class to inherit from ComponentVertex
    The StateVertex class in types.py has been updated to inherit from the ComponentVertex class instead of the Vertex class. This change ensures that the StateVertex class has access to the methods and attributes of the ComponentVertex class, improving code organization and maintainability.

--- src/backend/base/langflow/graph/vertex/types.py
+++ src/backend/base/langflow/graph/vertex/types.py
@@ -404,7 +404,7 @@ class InterfaceVertex(ComponentVertex):
         return self.vertex_type == InterfaceComponentTypes.ChatInput and self.is_input
 
 
-class StateVertex(Vertex):
+class StateVertex(ComponentVertex):
     def __init__(self, data: Dict, graph):
         super().__init__(data, graph=graph, base_type="custom_components")
         self.steps = [self._build]
@@ -412,8 +412,9 @@ class StateVertex(Vertex):
 
     @property
     def successors_ids(self) -> List[str]:
-        successors = self.graph.successor_map.get(self.id, [])
-        return successors + self.graph.activated_vertices
+        if self._successors_ids is None:
+            return super().successors_ids
+        return self._successors_ids
 
     def _built_object_repr(self):
         if self.artifacts and "repr" in self.artifacts:
