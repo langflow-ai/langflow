d3985f3f834e42de348e30822620497aad1bc76f
Nicolò Boschi <boschi1997@gmail.com>
2024-07-26 14:11:54 +0200

    feat: migrate vertex_builds to sql database

---
+++ src/backend/base/langflow/alembic/versions/0d60fcbd4e8e_create_vertex_builds_table.py
@@ -0,0 +1,50 @@
+"""create vertex_builds table
+
+Revision ID: 0d60fcbd4e8e
+Revises: 90be8e2ed91e
+Create Date: 2024-07-26 11:41:31.274271
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+from sqlalchemy.engine.reflection import Inspector
+from langflow.utils import migration
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0d60fcbd4e8e'
+down_revision: Union[str, None] = '90be8e2ed91e'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    if not migration.table_exists("vertex_build", conn):
+        op.create_table(
+            "vertex_build",
+            sa.Column("timestamp", sa.DateTime(), nullable=False),
+            sa.Column("id", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+            sa.Column("data", sa.JSON(), nullable=True),
+            sa.Column("artifacts", sa.JSON(), nullable=True),
+            sa.Column("params", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+            sa.Column("build_id", sqlmodel.sql.sqltypes.GUID(), nullable=False),
+            sa.Column("flow_id", sqlmodel.sql.sqltypes.GUID(), nullable=False),
+            sa.Column("valid", sqlmodel.sql.sqltypes.BOOLEAN(), nullable=False),
+            sa.ForeignKeyConstraint(
+                ["flow_id"],
+                ["flow.id"],
+            ),
+            sa.PrimaryKeyConstraint("build_id"),
+        )
+    pass
+
+
+def downgrade() -> None:
+    conn = op.get_bind()
+    if migration.table_exists("vertex_build", conn):
+        op.drop_table("vertex_build")
+    pass
---
+++ src/backend/base/langflow/services/database/models/vertex_builds/crud.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import Session, select
+from sqlalchemy import delete
+
+from langflow.services.database.models.transactions.model import TransactionBase, TransactionTable
+from langflow.services.database.models.vertex_builds.model import VertexBuildBase, VertexBuildTable
+
+
+def get_vertex_builds_by_flow_id(db: Session, flow_id: UUID, limit: Optional[int] = 1000) -> list[VertexBuildTable]:
+    stmt = (
+        select(VertexBuildTable)
+        .where(VertexBuildTable.flow_id == flow_id)
+        .order_by(VertexBuildTable.timestamp)
+        .limit(limit)
+    )
+
+    builds = db.exec(stmt)
+    return [t for t in builds]
+
+
+def log_vertex_build(db: Session, vertex_build: VertexBuildBase) -> VertexBuildTable:
+    table = VertexBuildTable(**vertex_build.model_dump())
+    db.add(table)
+    try:
+        db.commit()
+        return table
+    except IntegrityError as e:
+        db.rollback()
+        raise e
+
+
+def delete_vertex_builds_by_flow_id(db: Session, flow_id: UUID) -> None:
+    delete(VertexBuildTable).where(VertexBuildTable.flow_id == flow_id)
+    db.commit()
