cec4eb2c0c11bc3cf1c49dafe89823c8d449f55c
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-26 19:41:37 -0300

    refactor: Update langflow.inputs import in prompt.py for backward compatibility

--- src/backend/base/langflow/template/field/prompt.py
+++ src/backend/base/langflow/template/field/prompt.py
@@ -1,5 +1,3 @@
 # For backward compatibility
-from langflow.inputs import (
-    DEFAULT_PROMPT_INTUT_TYPES,  # noqa
-    DefaultPromptField,  # noqa
-)
+from langflow.inputs.inputs import DEFAULT_PROMPT_INTUT_TYPES  # noqa
+from langflow.inputs.inputs import DefaultPromptField  # noqa
