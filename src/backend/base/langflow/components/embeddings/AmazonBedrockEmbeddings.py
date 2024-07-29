1bcbe9e7a2118802487d0de30d76146219273deb
Nicolò Boschi <boschi1997@gmail.com>
2024-07-29 23:18:21 +0200

    feat: bedrock access with aws access key (#3032)
    * feat: bedrock access with aws access key
    
    * [autofix.ci] apply automated fixes
    
    * chore: ignore type import in AmazonBedrockEmbeddings.py and AmazonBedrockModel.py
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>

--- src/backend/base/langflow/components/embeddings/AmazonBedrockEmbeddings.py
+++ src/backend/base/langflow/components/embeddings/AmazonBedrockEmbeddings.py
@@ -2,6 +2,7 @@ from langchain_community.embeddings import BedrockEmbeddings
 
 from langflow.base.models.model import LCModelComponent
 from langflow.field_typing import Embeddings
+from langflow.inputs import SecretStrInput
 from langflow.io import DropdownInput, MessageTextInput, Output
 
 
@@ -19,18 +20,15 @@ class AmazonBedrockEmbeddingsComponent(LCModelComponent):
             options=["amazon.titan-embed-text-v1"],
             value="amazon.titan-embed-text-v1",
         ),
+        SecretStrInput(name="aws_access_key", display_name="Access Key"),
+        SecretStrInput(name="aws_secret_key", display_name="Secret Key"),
         MessageTextInput(
             name="credentials_profile_name",
             display_name="Credentials Profile Name",
+            advanced=True,
         ),
-        MessageTextInput(
-            name="endpoint_url",
-            display_name="Bedrock Endpoint URL",
-        ),
-        MessageTextInput(
-            name="region_name",
-            display_name="AWS Region",
-        ),
+        MessageTextInput(name="region_name", display_name="Region Name", value="us-east-1"),
+        MessageTextInput(name="endpoint_url", display_name=" Endpoint URL", advanced=True),
     ]
 
     outputs = [
@@ -38,13 +36,34 @@ class AmazonBedrockEmbeddingsComponent(LCModelComponent):
     ]
 
     def build_embeddings(self) -> Embeddings:
-        try:
-            output = BedrockEmbeddings(
-                credentials_profile_name=self.credentials_profile_name,
-                model_id=self.model_id,
-                endpoint_url=self.endpoint_url,
-                region_name=self.region_name,
-            )  # type: ignore
-        except Exception as e:
-            raise ValueError("Could not connect to Amazon Bedrock API.") from e
+        if self.aws_access_key:
+            import boto3  # type: ignore
+
+            session = boto3.Session(
+                aws_access_key_id=self.aws_access_key,
+                aws_secret_access_key=self.aws_secret_key,
+            )
+        elif self.credentials_profile_name:
+            import boto3
+
+            session = boto3.Session(profile_name=self.credentials_profile_name)
+        else:
+            import boto3
+
+            session = boto3.Session()
+
+        client_params = {}
+        if self.endpoint_url:
+            client_params["endpoint_url"] = self.endpoint_url
+        if self.region_name:
+            client_params["region_name"] = self.region_name
+
+        boto3_client = session.client("bedrock-runtime", **client_params)
+        output = BedrockEmbeddings(
+            credentials_profile_name=self.credentials_profile_name,
+            client=boto3_client,
+            model_id=self.model_id,
+            endpoint_url=self.endpoint_url,
+            region_name=self.region_name,
+        )  # type: ignore
         return output
--- src/frontend/tests/end-to-end/keyPairListComponent.spec.ts
+++ src/frontend/tests/end-to-end/keyPairListComponent.spec.ts
@@ -88,10 +88,6 @@ test("KeypairListComponent", async ({ page }) => {
   await page.getByTestId("more-options-modal").click();
   await page.getByTestId("edit-button-modal").click();
 
-  await page.locator('//*[@id="showcredentials_profile_name"]').click();
-  expect(
-    await page.locator('//*[@id="showcredentials_profile_name"]').isChecked(),
-  ).toBeFalsy();
   await page.getByText("Close").last().click();
 
   const plusButtonLocator = page.locator('//*[@id="plusbtn0"]');
@@ -102,11 +98,6 @@ test("KeypairListComponent", async ({ page }) => {
 
     await page.getByTestId("more-options-modal").click();
     await page.getByTestId("edit-button-modal").click();
-
-    await page.locator('//*[@id="showcredentials_profile_name"]').click();
-    expect(
-      await page.locator('//*[@id="showcredentials_profile_name"]').isChecked(),
-    ).toBeTruthy();
 
     await page.locator('//*[@id="editNodekeypair0"]').click();
     await page.locator('//*[@id="editNodekeypair0"]').fill("testtesttesttest");
