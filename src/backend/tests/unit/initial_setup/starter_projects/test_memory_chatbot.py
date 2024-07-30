4986062fdebcb449a955401ec12cae739ca445a0
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-30 15:08:40 -0300

    refactor: update langflow components imports in test_memory_chatbot.py and use .connect

--- src/backend/tests/unit/initial_setup/starter_projects/test_memory_chatbot.py
+++ src/backend/tests/unit/initial_setup/starter_projects/test_memory_chatbot.py
@@ -1,6 +1,10 @@
 from collections import deque
 
-from langflow import components
+from langflow.components.helpers.Memory import MemoryComponent
+from langflow.components.inputs.ChatInput import ChatInput
+from langflow.components.models.OpenAIModel import OpenAIModelComponent
+from langflow.components.outputs.ChatOutput import ChatOutput
+from langflow.components.prompts.Prompt import PromptComponent
 from langflow.graph import Graph
 from langflow.graph.graph.constants import Finish
 
@@ -11,16 +15,19 @@ def test_memory_chatbot():
 
 User: {user_message}
 AI: """
-    memory_component = components.helpers.MemoryComponent(_id="chat_memory")(session_id=session_id)
-    chat_input = components.inputs.ChatInput(_id="chat_input")
-    prompt_component = components.prompts.PromptComponent(_id="prompt")(
+    memory_component = MemoryComponent(_id="chat_memory")
+    memory_component.connect(session_id=session_id)
+    chat_input = ChatInput(_id="chat_input")
+    prompt_component = PromptComponent(_id="prompt")
+    prompt_component.connect(
         template=template, user_message=chat_input.message_response, context=memory_component.retrieve_messages_as_text
     )
-    openai_component = components.models.OpenAIModelComponent(_id="openai")(
-        input_value=prompt_component.build_prompt, max_tokens=100, temperature=0.1
-    )
+    openai_component = OpenAIModelComponent(_id="openai")
+    openai_component.connect(input_value=prompt_component.build_prompt, max_tokens=100, temperature=0.1)
     openai_component.get_output("text_output").value = "Mock response"
-    chat_output = components.outputs.ChatOutput(_id="chat_output")(input_value=openai_component.text_response)
+
+    chat_output = ChatOutput(_id="chat_output")
+    chat_output.connect(input_value=openai_component.text_response)
 
     graph = Graph(chat_input, chat_output)
     # Now we run step by step
