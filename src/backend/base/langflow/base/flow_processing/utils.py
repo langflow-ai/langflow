1a7f32e932b42e90ea66499242799761824b6327
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-07-31 08:57:25 -0300

    fixed dataobj with wrong name

--- src/backend/base/langflow/base/flow_processing/utils.py
+++ src/backend/base/langflow/base/flow_processing/utils.py
@@ -68,7 +68,7 @@ def build_data_from_result_data(result_data: ResultData, get_final_results_only:
 
     if isinstance(result_data.results, dict):
         for name, result in result_data.results.items():
-            dataoj: Data | Message | None = None
+            dataobj: Data | Message | None = None
             if isinstance(result, Message):
                 dataobj = result
             else:
