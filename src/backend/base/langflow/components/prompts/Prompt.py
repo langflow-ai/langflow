941bcc43ed84a0d59c365fa4b23389e7ffdb4656
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-07-31 21:04:07 -0300

    refactor(prompts): Update PromptComponent to support custom fields and template updates
    The `PromptComponent` class in `Prompt.py` has been updated to support custom fields and template updates. The `_update_template` method has been added to update the prompt template with custom fields. The `post_code_processing` method has been modified to update the template and improve backwards compatibility. The `_get_fallback_input` method has been added to provide a default prompt field. These changes improve the functionality and flexibility of the codebase.

--- src/backend/base/langflow/components/prompts/Prompt.py
+++ src/backend/base/langflow/components/prompts/Prompt.py
@@ -2,6 +2,7 @@ from langflow.base.prompts.api_utils import process_prompt_template
 from langflow.custom import Component
 from langflow.io import Output, PromptInput
 from langflow.schema.message import Message
+from langflow.template.field.prompt import DefaultPromptField
 from langflow.template.utils import update_template_values
 
 
@@ -27,12 +28,25 @@ class PromptComponent(Component):
         self.status = prompt.text
         return prompt
 
+    def _update_template(self, frontend_node: dict):
+        prompt_template = frontend_node["template"]["template"]["value"]
+        custom_fields = frontend_node["custom_fields"]
+        frontend_node_template = frontend_node["template"]
+        _ = process_prompt_template(
+            template=prompt_template,
+            name="template",
+            custom_fields=custom_fields,
+            frontend_node_template=frontend_node_template,
+        )
+        return frontend_node
+
     def post_code_processing(self, new_frontend_node: dict, current_frontend_node: dict):
         """
         This function is called after the code validation is done.
         """
         frontend_node = super().post_code_processing(new_frontend_node, current_frontend_node)
         template = frontend_node["template"]["template"]["value"]
+        # Kept it duplicated for backwards compatibility
         _ = process_prompt_template(
             template=template,
             name="template",
@@ -43,3 +57,6 @@ class PromptComponent(Component):
         # and update the frontend_node with those values
         update_template_values(new_template=frontend_node, previous_template=current_frontend_node["template"])
         return frontend_node
+
+    def _get_fallback_input(self, **kwargs):
+        return DefaultPromptField(**kwargs)
