fc4805449f96cd3bc0f181ae060a328ce55455a2
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-01 15:30:16 -0300

    removed unused variables from addFlow

--- src/frontend/src/hooks/flows/use-add-flow.ts
+++ src/frontend/src/hooks/flows/use-add-flow.ts
@@ -1,8 +1,6 @@
-import { BROKEN_EDGES_WARNING } from "@/constants/constants";
 import { usePostSaveFlow } from "@/controllers/API/queries/flows/use-post-save-flow";
 import useAlertStore from "@/stores/alertStore";
 import useFlowsManagerStore from "@/stores/flowsManagerStore";
-import useFlowStore from "@/stores/flowStore";
 import { useFolderStore } from "@/stores/foldersStore";
 import { useGlobalVariablesStore } from "@/stores/globalVariablesStore/globalVariables";
 import { useTypesStore } from "@/stores/typesStore";
@@ -10,14 +8,11 @@ import { FlowType } from "@/types/flow";
 import {
   addVersionToDuplicates,
   createNewFlow,
-  detectBrokenEdgesEdges,
   extractFieldsFromComponenents,
   processDataFromFlow,
   processFlows,
   updateGroupRecursion,
 } from "@/utils/reactflowUtils";
-import { brokenEdgeMessage } from "@/utils/utils";
-import { XYPosition } from "reactflow";
 
 const useAddFlow = () => {
   const unavaliableFields = useGlobalVariablesStore(
@@ -36,15 +31,11 @@ const useAddFlow = () => {
   const { mutate: saveFlow } = usePostSaveFlow();
 
   const addFlow = async ({
-    newProject = true,
     flow,
     override,
-    position,
   }: {
-    newProject?: Boolean;
     flow?: FlowType;
     override?: boolean;
-    position?: XYPosition;
   }) => {
     let flowData = flow
       ? processDataFromFlow(flow)
@@ -57,78 +48,59 @@ const useAddFlow = () => {
         globalVariablesEntries,
       );
     });
-    if (newProject) {
-      // Create a new flow with a default name if no flow is provided.
-      const folder_id = useFolderStore.getState().folderUrl;
-      const my_collection_id = useFolderStore.getState().myCollectionId;
+    // Create a new flow with a default name if no flow is provided.
+    const folder_id = useFolderStore.getState().folderUrl;
+    const my_collection_id = useFolderStore.getState().myCollectionId;
 
-      if (override) {
-        await deleteComponent(flow!.name);
-      }
-      const newFlow = createNewFlow(
-        flowData!,
-        flow!,
-        folder_id || my_collection_id!,
-      );
-
-      const newName = addVersionToDuplicates(newFlow, flows);
-      newFlow.name = newName;
-      newFlow.folder_id = useFolderStore.getState().folderUrl;
-
-      return new Promise((resolve, reject) =>
-        saveFlow(newFlow, {
-          onSuccess: ({ id }) => {
-            newFlow.id = id;
-            // Add the new flow to the list of flows.
-            const { data, flows: myFlows } = processFlows([newFlow, ...flows]);
-            setFlows(myFlows);
-            setIsLoading(false);
-            useTypesStore.setState((state) => ({
-              data: { ...state.data, ["saved_components"]: data },
-              ComponentFields: extractFieldsFromComponenents({
-                ...state.data,
-                ["saved_components"]: data,
-              }),
-            }));
-            resolve(id);
-          },
-          onError: (error) => {
-            if (error.response?.data?.detail) {
-              useAlertStore.getState().setErrorData({
-                title: "Could not load flows from database",
-                list: [error.response?.data?.detail],
-              });
-            } else {
-              useAlertStore.getState().setErrorData({
-                title: "Could not load flows from database",
-                list: [
-                  error.message ??
-                    "An unexpected error occurred, please try again",
-                ],
-              });
-            }
-            reject(error); // Re-throw the error so the caller can handle it if needed},
-          },
-        }),
-      );
-    } else {
-      let brokenEdges = detectBrokenEdgesEdges(
-        flow!.data!.nodes,
-        flow!.data!.edges,
-      );
-      if (brokenEdges.length > 0) {
-        useAlertStore.getState().setErrorData({
-          title: BROKEN_EDGES_WARNING,
-          list: brokenEdges.map((edge) => brokenEdgeMessage(edge)),
-        });
-      }
-      useFlowStore
-        .getState()
-        .paste(
-          { nodes: flow!.data!.nodes, edges: flow!.data!.edges },
-          position ?? { x: 10, y: 10 },
-        );
+    if (override) {
+      await deleteComponent(flow!.name);
     }
+    const newFlow = createNewFlow(
+      flowData!,
+      flow!,
+      folder_id || my_collection_id!,
+    );
+
+    const newName = addVersionToDuplicates(newFlow, flows);
+    newFlow.name = newName;
+    newFlow.folder_id = useFolderStore.getState().folderUrl;
+
+    return new Promise((resolve, reject) =>
+      saveFlow(newFlow, {
+        onSuccess: ({ id }) => {
+          newFlow.id = id;
+          // Add the new flow to the list of flows.
+          const { data, flows: myFlows } = processFlows([newFlow, ...flows]);
+          setFlows(myFlows);
+          setIsLoading(false);
+          useTypesStore.setState((state) => ({
+            data: { ...state.data, ["saved_components"]: data },
+            ComponentFields: extractFieldsFromComponenents({
+              ...state.data,
+              ["saved_components"]: data,
+            }),
+          }));
+          resolve(id);
+        },
+        onError: (error) => {
+          if (error.response?.data?.detail) {
+            useAlertStore.getState().setErrorData({
+              title: "Could not load flows from database",
+              list: [error.response?.data?.detail],
+            });
+          } else {
+            useAlertStore.getState().setErrorData({
+              title: "Could not load flows from database",
+              list: [
+                error.message ??
+                  "An unexpected error occurred, please try again",
+              ],
+            });
+          }
+          reject(error); // Re-throw the error so the caller can handle it if needed},
+        },
+      }),
+    );
   };
 
   return addFlow;
