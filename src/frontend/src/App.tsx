7c75ad07283068ac8b9330e8a1e62e328f3e7e5f
Cristhian Zanforlin Lousa <72977554+Cristhianzl@users.noreply.github.com>
2024-08-01 16:45:32 -0300

    bugfix: automatically refresh user's session (#3148)
    📝 (App.tsx): Update imports and add new constants for access token expiration time
    🔧 (App.tsx): Add support for refreshing access token and setting expiration time
    📝 (authAdminGuard/index.tsx): Remove unnecessary logout function and add conditional check for login page
    📝 (authGuard/index.tsx): Add conditional check for login page
    📝 (constants.ts): Add constants for access token expiration time
    📝 (authContext.tsx): Remove logout function and update related logic
    📝 (api.tsx): Add conditional check for login page
    📝 (use-post-logout.ts): Remove navigate function call
    📝 (LoginPage/index.tsx): Add logic to reset flows and selected folder on successful login
    
    ✨ (LoginPage/index.tsx): Add support for managing folders in the login page to improve user experience
    🔧 (authStore.ts): Update logout function to clear user data and redirect to login page for better security and user experience
    📝 (contexts/auth.ts): Remove unnecessary logout function from AuthContextType for consistency
    🔧 (vite.config.mts): Add environment variables for ACCESS_TOKEN_EXPIRE_SECONDS and CI to improve configuration flexibility
    
    Co-authored-by: Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { Cookies } from "react-cookie";
 import { ErrorBoundary } from "react-error-boundary";
-import { useNavigate } from "react-router-dom";
 import "reactflow/dist/style.css";
 import "./App.css";
 import AlertDisplayArea from "./alerts/displayArea";
@@ -11,10 +10,14 @@ import LoadingComponent from "./components/loadingComponent";
 import {
   FETCH_ERROR_DESCRIPION,
   FETCH_ERROR_MESSAGE,
-  LANGFLOW_AUTO_LOGIN_OPTION,
+  LANGFLOW_ACCESS_TOKEN_EXPIRE_SECONDS,
+  LANGFLOW_ACCESS_TOKEN_EXPIRE_SECONDS_ENV,
 } from "./constants/constants";
 import { AuthContext } from "./contexts/authContext";
-import { useAutoLogin } from "./controllers/API/queries/auth";
+import {
+  useAutoLogin,
+  useRefreshAccessToken,
+} from "./controllers/API/queries/auth";
 import { useGetHealthQuery } from "./controllers/API/queries/health";
 import { useGetGlobalVariables } from "./controllers/API/queries/variables";
 import { useGetVersionQuery } from "./controllers/API/queries/version";
@@ -31,21 +34,27 @@ import { useFolderStore } from "./stores/foldersStore";
 export default function App() {
   useTrackLastVisitedPath();
   const isLoading = useFlowsManagerStore((state) => state.isLoading);
-  const { login, setUserData, getUser, logout } = useContext(AuthContext);
+  const { login, setUserData, getUser } = useContext(AuthContext);
   const setAutoLogin = useAuthStore((state) => state.setAutoLogin);
   const setLoading = useAlertStore((state) => state.setLoading);
   const refreshStars = useDarkStore((state) => state.refreshStars);
   const dark = useDarkStore((state) => state.dark);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const cookies = new Cookies();
+  const logout = useAuthStore((state) => state.logout);
+
+  const refreshToken = cookies.get("refresh_token");
 
   const { mutate: mutateAutoLogin } = useAutoLogin();
 
   useGetVersionQuery();
-  const cookies = new Cookies();
 
   const isLoadingFolders = useFolderStore((state) => state.isLoadingFolders);
 
   const { mutate: mutateGetGlobalVariables } = useGetGlobalVariables();
+  const { mutate: mutateRefresh } = useRefreshAccessToken();
+
+  const isLoginPage = location.pathname.includes("login");
 
   const {
     data: healthData,
@@ -63,8 +72,6 @@ export default function App() {
   }, [dark]);
 
   useEffect(() => {
-    const isLoginPage = location.pathname.includes("login");
-
     mutateAutoLogin(undefined, {
       onSuccess: async (user) => {
         if (user && user["access_token"]) {
@@ -74,30 +81,44 @@ export default function App() {
           setUserData(user);
           setAutoLogin(true);
           fetchAllData();
+          // mutateRefresh({ refresh_token: refreshToken });
         }
       },
       onError: (error) => {
         if (error.name !== "CanceledError") {
           setAutoLogin(false);
-          if (
-            cookies.get(LANGFLOW_AUTO_LOGIN_OPTION) === "auto" &&
-            isAuthenticated
-          ) {
-            logout();
-            return;
-          }
-
-          if (isAuthenticated && !isLoginPage) {
-            getUser();
-            fetchAllData();
-          } else {
-            setLoading(false);
-            useFlowsManagerStore.setState({ isLoading: false });
+          if (!isLoginPage) {
+            if (!isAuthenticated) {
+              setLoading(false);
+              useFlowsManagerStore.setState({ isLoading: false });
+              logout();
+            } else {
+              mutateRefresh({ refresh_token: refreshToken });
+              fetchAllData();
+              getUser();
+            }
           }
         }
       },
     });
   }, []);
+
+  useEffect(() => {
+    const envRefreshTime = LANGFLOW_ACCESS_TOKEN_EXPIRE_SECONDS_ENV;
+    const automaticRefreshTime = LANGFLOW_ACCESS_TOKEN_EXPIRE_SECONDS;
+
+    const accessTokenTimer = isNaN(envRefreshTime)
+      ? automaticRefreshTime
+      : envRefreshTime;
+
+    const intervalId = setInterval(() => {
+      if (isAuthenticated && !isLoginPage) {
+        mutateRefresh({ refresh_token: refreshToken });
+      }
+    }, accessTokenTimer * 1000);
+
+    return () => clearInterval(intervalId);
+  }, [isLoginPage]);
 
   const fetchAllData = async () => {
     setTimeout(async () => {
