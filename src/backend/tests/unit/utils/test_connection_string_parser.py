3408a988396260fe6897db31ddcfb31ba00aa9bb
italojohnny <italojohnnydosanjos@gmail.com>
2024-08-03 09:10:17 -0300

    test: fix import

--- src/backend/tests/unit/utils/test_connection_string_parser.py
+++ src/backend/tests/unit/utils/test_connection_string_parser.py
@@ -1,20 +1,10 @@
 import pytest
-from urllib.parse import quote
+from langflow.utils.connection_string_parser import transform_connection_string
 
 
 @pytest.fixture
 def client():
     pass
-
-
-def transform_connection_string(connection_string):
-    db_url_name = connection_string.split("@")[-1]
-    password_url = connection_string.split(":")[-1]
-    password_string = password_url.replace(f"@{db_url_name}", "")
-    encoded_password = quote(password_string)
-    protocol_user = connection_string.split(":")[:-1]
-    transformed_connection_string = f'{":".join(protocol_user)}:{encoded_password}@{db_url_name}'
-    return transformed_connection_string
 
 
 @pytest.mark.parametrize(
