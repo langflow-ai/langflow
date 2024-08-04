74f465c393126b73592f28a5c498e91055faba4e
bruno.reis <bruno.rreiss11@gmail.com>
2024-08-04 02:49:42 +0000

    refactor: update Component class to handle inputs and outputs
    This commit refactors the `Component` class in `component.py` to handle inputs and outputs. It adds a new method `map_outputs` to map a list of outputs to the component. The `__init__` method is also updated to properly initialize the inputs, outputs, and other attributes. This change improves the flexibility and extensibility of the `Component` class.

--- src/backend/tests/data/component_with_templatefield.py
+++ src/backend/tests/data/component_with_templatefield.py
@@ -4,6 +4,7 @@ from langflow.custom import CustomComponent
 from langflow.field_typing import Input
 
 
+# This is a custom component that will be used in the tests
 class TestComponent(CustomComponent):
     def refresh_values(self):
         # This is a function that will be called every time the component is updated
