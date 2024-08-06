165c0d512f57f03bef754c52e167be0c8c6b6158
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-06 18:26:16 -0300

    test: Add pytest fixture for CustomComponent in unit tests to enhance test structure and readability

--- src/backend/tests/unit/test_custom_component_with_client.py
+++ src/backend/tests/unit/test_custom_component_with_client.py
@@ -1,4 +1,21 @@
+import pytest
+
+from langflow.custom.custom_component.custom_component import CustomComponent
 from langflow.services.database.models.flow import Flow
+
+
+@pytest.fixture
+def component(client, active_user):
+    return CustomComponent(
+        user_id=active_user.id,
+        field_config={
+            "fields": {
+                "llm": {"type": "str"},
+                "url": {"type": "str"},
+                "year": {"type": "int"},
+            }
+        },
+    )
 
 
 def test_list_flows_flow_objects(component):
