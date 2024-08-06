b818e9b6850c2d176de07ddf248e4d70279c2dbe
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-06 11:39:42 -0300

    feat: Enhance ValidateComponentTool with additional input types and implement code validation functionality for components

--- src/backend/base/langflow/components/tools/ValidateComponentTool.py
+++ src/backend/base/langflow/components/tools/ValidateComponentTool.py
@@ -2,28 +2,49 @@ from typing import cast
 
 from langchain.tools import StructuredTool
 
+from langflow.base.langchain_utilities.model import LCToolComponent
 from langflow.components.helpers.component_code_validator import ComponentCodeValidator
-from langflow.custom import Component
 from langflow.field_typing import Tool
-from langflow.inputs import MessageTextInput
+from langflow.io import MessageTextInput, MultilineInput
+from langflow.schema import Data
 
 
-class ValidateComponentTool(Component):
+class ValidateComponentTool(LCToolComponent):
     display_name = "Validate Component Tool"
     description = "Validates the code of a component."
     name = "ValidateComponentTool"
 
     inputs = [
-        MessageTextInput(name="tool_name", display_name="Tool Name", required=True),
-        MessageTextInput(name="tool_description", display_name="Tool Description", required=True),
+        MessageTextInput(
+            name="tool_name", display_name="Tool Name", required=True, value="Langflow Component Code Validator"
+        ),
+        MessageTextInput(
+            name="tool_description",
+            display_name="Tool Description",
+            required=True,
+            value="Validates the code of a component.",
+        ),
+        MultilineInput(
+            name="component_code",
+            display_name="Component Code",
+            required=False,
+            info="Only required if you plan to use the Data output.",
+        ),
     ]
+
+    def run_model(self) -> Data | list[Data]:
+        tool = self.build_tool()
+        data_json = tool.invoke(self.component_code)
+        return Data.from_json(data_json)
 
     def build_tool(self) -> Tool:
         def validate_code(component_code: str) -> str:
             """Validates the code of a component."""
             component = ComponentCodeValidator()
-            component.initialize(parameters={"component_code": component_code})
-            component.validate_code()
+
+            component.set(component_code=component_code)
+            data_obj = component.validate_code()
+            return data_obj.model_dump_json()
 
         tool = StructuredTool.from_function(func=validate_code, name=self.tool_name, description=self.tool_description)
         return cast(Tool, tool)
