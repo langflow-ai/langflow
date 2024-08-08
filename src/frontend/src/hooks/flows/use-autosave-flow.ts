65f62fbb8a7bfe54ac32d5e6809bf881118da37f
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-08 11:44:04 -0300

    use debounce hook to save flow

--- src/frontend/src/hooks/flows/use-autosave-flow.ts
+++ src/frontend/src/hooks/flows/use-autosave-flow.ts
@@ -1,22 +1,16 @@
 import { SAVE_DEBOUNCE_TIME } from "@/constants/constants";
 import { FlowType } from "@/types/flow";
-import { debounce } from "lodash";
-import { useMemo } from "react";
+import { useDebounce } from "../use-debounce";
 import useSaveFlow from "./use-save-flow";
 
 const useAutoSaveFlow = () => {
   const saveFlow = useSaveFlow();
-
-  const shouldAutosave = Boolean(process.env.LANGFLOW_AUTO_SAVE) ?? true;
+  const shouldAutosave = process.env.LANGFLOW_AUTO_SAVE !== "false";
 
   const autoSaveFlow = shouldAutosave
-    ? useMemo(
-        () =>
-          debounce((flow?: FlowType) => {
-            saveFlow(flow);
-          }, SAVE_DEBOUNCE_TIME),
-        [],
-      )
+    ? useDebounce((flow?: FlowType) => {
+        saveFlow(flow);
+      }, SAVE_DEBOUNCE_TIME)
     : () => {};
 
   return autoSaveFlow;
