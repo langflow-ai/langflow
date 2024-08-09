8dea891498545a2abf238d43fcaa5eb55dba6ea3
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-09 18:46:16 -0300

    feat: add unit tests for state model creation and validation in test_state_model.py

---
+++ src/backend/tests/unit/graph/graph/test_graph_state_model.py
@@ -0,0 +1,49 @@
+import pytest
+
+from langflow.components.helpers.Memory import MemoryComponent
+from langflow.components.inputs.ChatInput import ChatInput
+from langflow.components.models.OpenAIModel import OpenAIModelComponent
+from langflow.components.outputs.ChatOutput import ChatOutput
+from langflow.components.prompts.Prompt import PromptComponent
+from langflow.graph import Graph
+from langflow.graph.graph.state_model import create_state_model_from_graph
+
+
+@pytest.fixture
+def client():
+    pass
+
+
+def test_graph_state_model():
+    session_id = "test_session_id"
+    template = """{context}
+
+User: {user_message}
+AI: """
+    memory_component = MemoryComponent(_id="chat_memory")
+    memory_component.set(session_id=session_id)
+    chat_input = ChatInput(_id="chat_input")
+    prompt_component = PromptComponent(_id="prompt")
+    prompt_component.set(
+        template=template, user_message=chat_input.message_response, context=memory_component.retrieve_messages_as_text
+    )
+    openai_component = OpenAIModelComponent(_id="openai")
+    openai_component.set(
+        input_value=prompt_component.build_prompt, max_tokens=100, temperature=0.1, api_key="test_api_key"
+    )
+    openai_component.get_output("text_output").value = "Mock response"
+
+    chat_output = ChatOutput(_id="chat_output")
+    chat_output.set(input_value=openai_component.text_response)
+
+    graph = Graph(chat_input, chat_output)
+
+    GraphStateModel = create_state_model_from_graph(graph)
+    assert GraphStateModel.__name__ == "GraphStateModel"
+    assert list(GraphStateModel.model_computed_fields.keys()) == [
+        "chat_input",
+        "chat_output",
+        "openai",
+        "prompt",
+        "chat_memory",
+    ]
