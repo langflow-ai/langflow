023e673d2dc9350f3d34c49fbb7ac52362ca4151
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-09 19:10:03 -0300

    refactor: serialize and deserialize GraphStateModel in test_graph_state_model.py

--- src/backend/tests/unit/graph/graph/test_graph_state_model.py
+++ src/backend/tests/unit/graph/graph/test_graph_state_model.py
@@ -75,3 +75,33 @@ def test_graph_functional_start_graph_state_update():
     assert graph_state_model.__class__.__name__ == "GraphStateModel"
     assert graph_state_model.chat_input.message.get_text() == "Test Sender Name"
     assert graph_state_model.chat_output.message.get_text() == "test"
+
+
+def test_graph_state_model_serialization():
+    chat_input = ChatInput(_id="chat_input")
+    chat_input.set(input_value="Test Sender Name")
+    chat_output = ChatOutput(input_value="test", _id="chat_output")
+    chat_output.set(sender_name=chat_input.message_response)
+
+    graph = Graph(chat_input, chat_output)
+    graph.prepare()
+    # Now iterate through the graph
+    # and check that the graph is running
+    # correctly
+    GraphStateModel = create_state_model_from_graph(graph)
+    graph_state_model = GraphStateModel()
+    ids = ["chat_input", "chat_output"]
+    results = []
+    for result in graph.start():
+        results.append(result)
+
+    assert len(results) == 3
+    assert all(result.vertex.id in ids for result in results if hasattr(result, "vertex"))
+    assert results[-1] == Finish()
+
+    assert graph_state_model.__class__.__name__ == "GraphStateModel"
+    assert graph_state_model.chat_input.message.get_text() == "Test Sender Name"
+    assert graph_state_model.chat_output.message.get_text() == "test"
+
+    serialized_state_model = graph_state_model.model_dump()
+    assert serialized_state_model["chat_input"]["message"]["text"] == "Test Sender Name"
