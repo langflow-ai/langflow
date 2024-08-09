ad586640d9fbf04c4acfd43692d153013132b9d2
neeraj gupta <neeraj111261cse@gmail.com>
2024-08-10 05:10:33 +0530

    src/frontend/src/CustomNodes/GenericNode/components/NodeInputField/index.tsx

--- src/frontend/src/CustomNodes/GenericNode/components/NodeInputField/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/NodeInputField/index.tsx
@@ -137,7 +137,6 @@ export default function NodeInputField({
             )}
           </div>
         </div>
-
         {(Array.isArray(displayHandle)
           ? displayHandle.length > 0
           : displayHandle) && (
