b0c226d6db35158d182acc759d6025c11b1628ee
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-12 18:35:59 -0300

    Changed on file drop to use saveFlow

--- src/frontend/src/components/sidebarComponent/hooks/use-on-file-drop.tsx
+++ src/frontend/src/components/sidebarComponent/hooks/use-on-file-drop.tsx
@@ -1,8 +1,8 @@
+import useSaveFlow from "@/hooks/flows/use-save-flow";
 import {
   UPLOAD_ALERT_LIST,
   WRONG_FILE_ERROR_ALERT,
 } from "../../../constants/alerts_constants";
-import { updateFlowInDatabase } from "../../../controllers/API";
 import { uploadFlowToFolder } from "../../../pages/MainPage/services";
 import useAlertStore from "../../../stores/alertStore";
 import useFlowsManagerStore from "../../../stores/flowsManagerStore";
@@ -21,6 +21,7 @@ const useFileDrop = (
   const setErrorData = useAlertStore((state) => state.setErrorData);
   const refreshFolders = useFolderStore((state) => state.refreshFolders);
   const flows = useFlowsManagerStore((state) => state.flows);
+  const saveFlow = useSaveFlow();
 
   const triggerFolderChange = (folderId) => {
     if (folderChangeCallback) {
@@ -120,7 +121,7 @@ const useFileDrop = (
     setFolderDragging(false);
     setFolderIdDragging("");
 
-    updateFlowInDatabase(updatedFlow).then(() => {
+    saveFlow(updatedFlow).then(() => {
       refreshFolders();
       triggerFolderChange(folderId);
     });
