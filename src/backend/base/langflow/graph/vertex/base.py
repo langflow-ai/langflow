c6be9b3a19d153b8fd3db73862e3811f4ca31e8a
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-12 17:48:28 -0300

    feat: Add method to retrieve value from template dict and set params in Vertex class

--- src/backend/base/langflow/graph/vertex/base.py
+++ src/backend/base/langflow/graph/vertex/base.py
@@ -250,12 +250,40 @@ class Vertex:
                     self.base_type = base_type
                     break
 
+    def get_value_from_template_dict(self, key: str):
+        return self.data.get("node").get("template").get(key).get("value")
+
     def get_task(self):
         # using the task_id, get the task from celery
         # and return it
         from celery.result import AsyncResult  # type: ignore
 
         return AsyncResult(self.task_id)
+
+    def _set_params_from_normal_edge(self, params: dict, edge: "Edge", template_dict: dict):
+        param_key = edge.target_param
+
+        # If the param_key is in the template_dict and the edge.target_id is the current node
+        # We check this to make sure params with the same name but different target_id
+        # don't get overwritten
+        if param_key in template_dict and edge.target_id == self.id:
+            if template_dict[param_key].get("list"):
+                if param_key not in params:
+                    params[param_key] = []
+                params[param_key].append(self.graph.get_vertex(edge.source_id))
+            elif edge.target_id == self.id:
+                if isinstance(template_dict[param_key].get("value"), dict):
+                    # we don't know the key of the dict but we need to set the value
+                    # to the vertex that is the source of the edge
+                    param_dict = template_dict[param_key]["value"]
+                    if not param_dict or len(param_dict) != 1:
+                        params[param_key] = self.graph.get_vertex(edge.source_id)
+                    else:
+                        params[param_key] = {key: self.graph.get_vertex(edge.source_id) for key in param_dict.keys()}
+
+                else:
+                    params[param_key] = self.graph.get_vertex(edge.source_id)
+        return params
 
     def _build_params(self):
         # sourcery skip: merge-list-append, remove-redundant-if
