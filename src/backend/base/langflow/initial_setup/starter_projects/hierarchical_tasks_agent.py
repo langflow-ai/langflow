23ae5a8c7b5a2c646a1738673b2dd33d40db1fe4
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-15 16:22:05 -0300

    feat: Add hierarchical tasks agent graph to handle sequential tasks.

---
+++ src/backend/base/langflow/initial_setup/starter_projects/hierarchical_tasks_agent.py
@@ -0,0 +1,68 @@
+from langflow.components.agents.CrewAIAgent import CrewAIAgentComponent
+from langflow.components.agents.HierarchicalCrew import HierarchicalCrewComponent
+from langflow.components.helpers.HierarchicalTask import HierarchicalTaskComponent
+from langflow.components.inputs.ChatInput import ChatInput
+from langflow.components.models.OpenAIModel import OpenAIModelComponent
+from langflow.components.outputs.ChatOutput import ChatOutput
+from langflow.components.prompts.Prompt import PromptComponent
+from langflow.components.tools.SearchAPI import SearchAPIComponent
+from langflow.graph.graph.base import Graph
+
+
+def hierarchical_tasks_agent_graph():
+    llm = OpenAIModelComponent(model="gpt-4o-mini")
+    manager_llm = OpenAIModelComponent(model="gpt-4o")
+    search_api_tool = SearchAPIComponent()
+    researcher_agent = CrewAIAgentComponent()
+    chat_input = ChatInput()
+    researcher_agent.set(
+        tools=[search_api_tool.build_tool],
+        llm=llm,
+        role="Researcher",
+        goal="Search for information about the User's query and answer as best as you can",
+        backstory="You are a reliable researcher and journalist ",
+    )
+
+    editor_agent = CrewAIAgentComponent()
+
+    editor_agent.set(
+        llm=llm,
+        role="Editor",
+        goal="Evaluate the information for misleading or biased data.",
+        backstory="You are a reliable researcher and journalist ",
+    )
+
+    response_prompt = PromptComponent()
+    response_prompt.set(
+        template="""User's query:
+{query}
+
+Respond to the user with as much as information as you can about the topic. Delete if needed. If it is just a general query (e.g a greeting) you can respond them directly.""",
+        query=chat_input.message_response,
+    )
+    manager_agent = CrewAIAgentComponent()
+    manager_agent.set(
+        llm=manager_llm,
+        role="Manager",
+        goal="You can answer general questions from the User and may call others for help if needed.",
+        backstory="You are polite and helpful. You've always been a beacon of politeness.",
+    )
+    task = HierarchicalTaskComponent()
+    task.set(
+        task_description=response_prompt.build_prompt,
+        expected_output="Succinct response that answers the User's query.",
+    )
+    sequential_crew_component = HierarchicalCrewComponent()
+    sequential_crew_component.set(
+        tasks=task.build_task, agents=[researcher_agent, editor_agent], manager_agent=manager_agent
+    )
+    chat_output = ChatOutput()
+    chat_output.set(input_value=sequential_crew_component.build_output)
+
+    graph = Graph(
+        start=chat_input,
+        end=chat_output,
+        flow_name="Sequential Tasks Agent",
+        description="This Agent runs tasks in a predefined sequence.",
+    )
+    return graph
