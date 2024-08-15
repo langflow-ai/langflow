404139f0eda73c66f470dde77c4004e1a140ba36
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-15 10:20:24 -0300

    test: Add test case for fetching starter projects using API key.

--- src/backend/tests/test_endpoints.py
+++ src/backend/tests/test_endpoints.py
@@ -670,3 +670,9 @@ def test_invalid_flow_id(client, created_api_key):
     response = client.post(f"/api/v1/run/{flow_id}", headers=headers)
     assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
     # Check if the error detail is as expected
+
+
+def test_starter_projects(client, created_api_key):
+    headers = {"x-api-key": created_api_key.api_key}
+    response = client.get("/api/v1/flows/starter-projects/", headers=headers)
+    assert response.status_code == status.HTTP_200_OK, response.text
