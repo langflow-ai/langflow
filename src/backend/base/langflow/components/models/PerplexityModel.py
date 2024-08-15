22879355ea50d1f532508959bfab134bc7a4417c
Sai Kolasani <sai.kolasani@datastax.com>
2024-08-15 15:28:29 -0700

    wip

--- src/backend/base/langflow/components/models/PerplexityModel.py
+++ src/backend/base/langflow/components/models/PerplexityModel.py
@@ -3,7 +3,7 @@ from pydantic.v1 import SecretStr
 
 from langflow.base.models.model import LCModelComponent
 from langflow.field_typing import LanguageModel
-from langflow.io import FloatInput, SecretStrInput, DropdownInput
+from langflow.io import FloatInput, SecretStrInput, DropdownInput, IntInput
 
 
 class PerplexityComponent(LCModelComponent):
@@ -29,6 +29,11 @@ class PerplexityComponent(LCModelComponent):
             ],
             value="llama-3.1-sonar-small-128k-online",
         ),
+        IntInput(
+            name="max_output_tokens",
+            display_name="Max Output Tokens",
+            info="The maximum number of tokens to generate.",
+        ),
         SecretStrInput(
             name="api_key",
             display_name="Perplexity API Key",
@@ -36,17 +41,43 @@ class PerplexityComponent(LCModelComponent):
             advanced=False,
         ),
         FloatInput(name="temperature", display_name="Temperature", value=0.75),
+        FloatInput(
+            name="top_p",
+            display_name="Top P",
+            info="The maximum cumulative probability of tokens to consider when sampling.",
+            advanced=True,
+        ),
+        IntInput(
+            name="n",
+            display_name="N",
+            info="Number of chat completions to generate for each prompt. Note that the API may not return the full n completions if duplicates are generated.",
+            advanced=True,
+        ),
+        IntInput(
+            name="top_k",
+            display_name="Top K",
+            info="Decode using top-k sampling: consider the set of top_k most probable tokens. Must be positive.",
+            advanced=True,
+        ),
     ]
 
     def build_model(self) -> LanguageModel:  # type: ignore[type-var]
         api_key = SecretStr(self.api_key).get_secret_value()
         temperature = self.temperature
         model = self.model_name
+        max_output_tokens = self.max_output_tokens
+        top_k = self.top_k
+        top_p = self.top_p
+        n = self.n
 
         output = ChatPerplexity(
             model=model,
             temperature=temperature or 0.75,
             pplx_api_key=api_key,
+            top_k=top_k or None,
+            top_p=top_p or None,
+            n=n or 1,
+            max_output_tokens=max_output_tokens,
         )
 
         return output  # type: ignore
