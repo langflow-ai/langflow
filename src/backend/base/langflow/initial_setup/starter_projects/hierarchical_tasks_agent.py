b01d635f71d5941bd347f3b04968d82acde3fa58
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-15 16:26:20 -0300

    Refactor: Update the hierarchical task agent to use builder methods for agents and models.

--- src/backend/base/langflow/initial_setup/starter_projects/hierarchical_tasks_agent.py
+++ src/backend/base/langflow/initial_setup/starter_projects/hierarchical_tasks_agent.py
@@ -10,14 +10,14 @@ from langflow.graph.graph.base import Graph
 
 
 def hierarchical_tasks_agent_graph():
-    llm = OpenAIModelComponent(model="gpt-4o-mini")
-    manager_llm = OpenAIModelComponent(model="gpt-4o")
+    llm = OpenAIModelComponent(model_name="gpt-4o-mini")
+    manager_llm = OpenAIModelComponent(model_name="gpt-4o")
     search_api_tool = SearchAPIComponent()
     researcher_agent = CrewAIAgentComponent()
     chat_input = ChatInput()
     researcher_agent.set(
         tools=[search_api_tool.build_tool],
-        llm=llm,
+        llm=llm.build_model,
         role="Researcher",
         goal="Search for information about the User's query and answer as best as you can",
         backstory="You are a reliable researcher and journalist ",
@@ -26,7 +26,7 @@ def hierarchical_tasks_agent_graph():
     editor_agent = CrewAIAgentComponent()
 
     editor_agent.set(
-        llm=llm,
+        llm=llm.build_model,
         role="Editor",
         goal="Evaluate the information for misleading or biased data.",
         backstory="You are a reliable researcher and journalist ",
@@ -42,7 +42,7 @@ Respond to the user with as much as information as you can about the topic. Dele
     )
     manager_agent = CrewAIAgentComponent()
     manager_agent.set(
-        llm=manager_llm,
+        llm=manager_llm.build_model,
         role="Manager",
         goal="You can answer general questions from the User and may call others for help if needed.",
         backstory="You are polite and helpful. You've always been a beacon of politeness.",
@@ -52,12 +52,14 @@ Respond to the user with as much as information as you can about the topic. Dele
         task_description=response_prompt.build_prompt,
         expected_output="Succinct response that answers the User's query.",
     )
-    sequential_crew_component = HierarchicalCrewComponent()
-    sequential_crew_component.set(
-        tasks=task.build_task, agents=[researcher_agent, editor_agent], manager_agent=manager_agent
+    crew_component = HierarchicalCrewComponent()
+    crew_component.set(
+        tasks=task.build_task,
+        agents=[researcher_agent.build_output, editor_agent.build_output],
+        manager_agent=manager_agent.build_output,
     )
     chat_output = ChatOutput()
-    chat_output.set(input_value=sequential_crew_component.build_output)
+    chat_output.set(input_value=crew_component.build_output)
 
     graph = Graph(
         start=chat_input,
