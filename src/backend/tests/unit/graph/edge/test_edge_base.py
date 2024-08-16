cdc6772cf47c951a8ace294671e060411dc52c6b
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-16 11:52:43 -0300

    fix(edge): Handle invalid input types when creating TargetHandle (#3368)
    * fix(edge): Handle invalid input types when creating TargetHandle
    
    * test(edge): add test for raising error on invalid target handle
    
    * fix: mypy error union-attr
    
    ---------
    
    Co-authored-by: italojohnny <italojohnnydosanjos@gmail.com>

---
+++ src/backend/tests/unit/graph/edge/test_edge_base.py
@@ -0,0 +1,35 @@
+import pytest
+
+from langflow.components.inputs.ChatInput import ChatInput
+from langflow.components.models.OpenAIModel import OpenAIModelComponent
+from langflow.components.outputs.ChatOutput import ChatOutput
+from langflow.components.prompts.Prompt import PromptComponent
+from langflow.graph.graph.base import Graph
+
+
+@pytest.fixture
+def client():
+    pass
+
+
+def test_edge_raises_error_on_invalid_target_handle(client):
+    template = """Answer the user as if you were a pirate.
+
+User: {user_input}
+
+Answer:
+"""
+    chat_input = ChatInput()
+    prompt_component = PromptComponent()
+    prompt_component.set(
+        template=template,
+        user_input=chat_input.message_response,
+    )
+
+    openai_component = OpenAIModelComponent()
+    openai_component.set(input_values=prompt_component.build_prompt)
+
+    chat_output = ChatOutput()
+    chat_output.set(input_value=openai_component.text_response)
+    with pytest.raises(ValueError, match="Component OpenAI field 'input_values' might not be a valid input."):
+        Graph(start=chat_input, end=chat_output)
