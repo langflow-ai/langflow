8fb1a54b56d1efab5a457c55f0ccae8fd51c563b
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-16 16:26:15 -0300

    add get config query

---
+++ src/frontend/src/controllers/API/queries/config/use-get-config.ts
@@ -0,0 +1,25 @@
+import { useQueryFunctionType } from "../../../../types/api";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+
+export interface ConfigResponse {
+  frontend_timeout: number;
+  auto_saving: boolean;
+}
+
+export const useGetConfigQuery: useQueryFunctionType<
+  undefined,
+  ConfigResponse
+> = (options) => {
+  const { query } = UseRequestProcessor();
+
+  const getConfigFn = async () => {
+    const response = await api.get<ConfigResponse>(`${getURL("CONFIG")}/`);
+    return response["data"];
+  };
+
+  const queryResult = query(["useGetConfigQuery"], getConfigFn, options);
+
+  return queryResult;
+};
