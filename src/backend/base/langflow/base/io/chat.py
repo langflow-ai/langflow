20a1727aed52fc81ba8217588e5e59c274fcf989
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-19 16:21:18 -0300

    refactor(chat): Update imports and remove unnecessary config method in ChatComponent.

--- src/backend/base/langflow/base/io/chat.py
+++ src/backend/base/langflow/base/io/chat.py
@@ -1,54 +1,16 @@
-from typing import Optional, Union
+import asyncio
+from typing import AsyncIterator, Iterator, Optional, Union
 
-from langflow.base.data.utils import IMG_FILE_TYPES, TEXT_FILE_TYPES
 from langflow.custom import Component
 from langflow.memory import store_message
 from langflow.schema import Data
 from langflow.schema.message import Message
-from langflow.utils.constants import MESSAGE_SENDER_USER, MESSAGE_SENDER_AI
+from langflow.services.database.models.message.crud import update_message
 
 
 class ChatComponent(Component):
     display_name = "Chat Component"
     description = "Use as base for chat components."
-
-    def build_config(self):
-        return {
-            "input_value": {
-                "input_types": ["Text"],
-                "display_name": "Text",
-                "multiline": True,
-            },
-            "sender": {
-                "options": [MESSAGE_SENDER_AI, MESSAGE_SENDER_USER],
-                "display_name": "Sender Type",
-                "advanced": True,
-            },
-            "sender_name": {"display_name": "Sender Name", "advanced": True},
-            "session_id": {
-                "display_name": "Session ID",
-                "info": "If provided, the message will be stored in the memory.",
-                "advanced": True,
-            },
-            "return_message": {
-                "display_name": "Return Message",
-                "info": "Return the message as a Message containing the sender, sender_name, and session_id.",
-                "advanced": True,
-            },
-            "data_template": {
-                "display_name": "Data Template",
-                "multiline": True,
-                "info": "In case of Message being a Data, this template will be used to convert it to text.",
-                "advanced": True,
-            },
-            "files": {
-                "field_type": "file",
-                "display_name": "Files",
-                "file_types": TEXT_FILE_TYPES + IMG_FILE_TYPES,
-                "info": "Files to be sent with the message.",
-                "advanced": True,
-            },
-        }
 
     # Keep this method for backward compatibility
     def store_message(
