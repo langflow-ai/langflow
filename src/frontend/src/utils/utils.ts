401e15b00497439d770c39e8f7261ebc351ab9c7
anovazzi1 <otavio2204@gmail.com>
2024-08-20 16:30:34 -0300

    feat: Add support functions for custom drag event types

--- src/frontend/src/utils/utils.ts
+++ src/frontend/src/utils/utils.ts
@@ -4,6 +4,7 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import TableAutoCellRender from "../components/tableComponent/components/tableAutoCellRender";
 import {
+  DRAG_EVENTS_CUSTOM_TYPESS,
   MESSAGES_TABLE_ORDER,
   MODAL_CLASSES,
   SHORTCUT_KEYS,
@@ -620,4 +621,13 @@ export function addPlusSignes(array: string[]): string[] {
 
     return "+" + key;
   });
+}
+
+
+export function isSupportedNodeTypes(type:string) {
+  return Object.keys(DRAG_EVENTS_CUSTOM_TYPESS).some(key=>key===type);
+}
+
+export function getNodeRenderType(MIMEtype:string) {
+  return DRAG_EVENTS_CUSTOM_TYPESS[MIMEtype];
 }
