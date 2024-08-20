2d7a3fdc5bbb71cd0bf27d08ea27a90a76271ada
anovazzi1 <otavio2204@gmail.com>
2024-08-20 19:35:47 -0300

    add initial resize to note component

--- src/frontend/src/CustomNodes/NoteNode/index.tsx
+++ src/frontend/src/CustomNodes/NoteNode/index.tsx
@@ -3,11 +3,14 @@ import { noteDataType } from "@/types/flow";
 import IconComponent from "../../components/genericIconComponent";
 import NodeName from "../GenericNode/components/NodeName";
 import NodeDescription from "../GenericNode/components/NodeDescription";
+import { NodeResizer } from "reactflow";
 
 function NoteNode({ data, selected }:{data: noteDataType, selected: boolean}) {
 
   return (
     <>
+    <NodeResizer minWidth={100} minHeight={30} isVisible={selected} lineClassName="border border-border" />
+
       <div className="generic-node-div">
         <div className="flex w-18">
           <IconComponent name="StickyNote"/>
