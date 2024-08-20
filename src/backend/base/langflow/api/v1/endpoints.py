5e0301b41695795afc6a947943b3b58a7f30ad0e
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-20 08:20:14 -0300

    refactor(endpoints.py): remove duplicate imports of loguru and sqlmodel to improve code readability and maintainability

--- src/backend/base/langflow/api/v1/endpoints.py
+++ src/backend/base/langflow/api/v1/endpoints.py
@@ -6,9 +6,6 @@ from uuid import UUID
 
 import sqlalchemy as sa
 from fastapi import APIRouter, BackgroundTasks, Body, Depends, HTTPException, Request, UploadFile, status
-from loguru import logger
-from sqlmodel import Session, select
-
 from langflow.api.v1.schemas import (
     ConfigResponse,
     CustomComponentRequest,
@@ -48,6 +45,8 @@ from langflow.services.task.service import TaskService
 from langflow.services.telemetry.schema import RunPayload
 from langflow.services.telemetry.service import TelemetryService
 from langflow.utils.version import get_version_info
+from loguru import logger
+from sqlmodel import Session, select
 
 if TYPE_CHECKING:
     from langflow.services.cache.base import CacheService
@@ -595,9 +594,9 @@ async def custom_component_update(
                 key: value_dict.get("value")
                 if value_dict.get("_input_type") != "IntInput"
                 else (
-                    int(value_dict.get("value"))
+                    int(value_dict.get("value"))  # type: ignore
                     if value_dict.get("_input_type") != "FloatInput"
-                    else float(value_dict.get("value"))
+                    else float(value_dict.get("value"))  # type: ignore
                 )
                 for key, value_dict in template.items()
                 if isinstance(value_dict, dict)
