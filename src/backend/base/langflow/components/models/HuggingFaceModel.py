bdbcc4cac966422d738d71ab260adbbab1f10842
Rodrigo <rodrigosilvanader@gmail.com>
2024-08-21 15:35:40 -0300

    fix: :bug: fix hf api component

--- src/backend/base/langflow/components/models/HuggingFaceModel.py
+++ src/backend/base/langflow/components/models/HuggingFaceModel.py
@@ -1,11 +1,9 @@
 from tenacity import retry, stop_after_attempt, wait_fixed
-
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 
 from langflow.base.models.model import LCModelComponent
 from langflow.field_typing import LanguageModel
 from langflow.io import DictInput, DropdownInput, SecretStrInput, StrInput, IntInput
-
 
 class HuggingFaceEndpointsComponent(LCModelComponent):
     display_name: str = "Hugging Face API"
@@ -15,9 +13,9 @@ class HuggingFaceEndpointsComponent(LCModelComponent):
 
     inputs = LCModelComponent._base_inputs + [
         StrInput(
-            name="endpoint_url",
-            display_name="Endpoint URL",
-            value="https://api-inference.huggingface.co/models/openai-community/gpt2",
+            name="model_id",
+            display_name="Model ID",
+            value="openai-community/gpt2",
         ),
         DropdownInput(
             name="task",
@@ -31,9 +29,10 @@ class HuggingFaceEndpointsComponent(LCModelComponent):
     ]
 
     def create_huggingface_endpoint(
-        self, endpoint_url: str, task: str, huggingfacehub_api_token: str, model_kwargs: dict
+        self, model_id: str, task: str, huggingfacehub_api_token: str, model_kwargs: dict
     ) -> HuggingFaceEndpoint:
         retry_attempts = self.retry_attempts  # Access the retry attempts input
+        endpoint_url = f"https://api-inference.huggingface.co/models/{model_id}"
 
         @retry(stop=stop_after_attempt(retry_attempts), wait=wait_fixed(2))
         def _attempt_create():
@@ -47,14 +46,14 @@ class HuggingFaceEndpointsComponent(LCModelComponent):
         return _attempt_create()
 
     def build_model(self) -> LanguageModel:  # type: ignore[type-var]
-        endpoint_url = self.endpoint_url
+        model_id = self.model_id
         task = self.task
         huggingfacehub_api_token = self.huggingfacehub_api_token
         model_kwargs = self.model_kwargs or {}
 
         try:
-            llm = self.create_huggingface_endpoint(endpoint_url, task, huggingfacehub_api_token, model_kwargs)
+            llm = self.create_huggingface_endpoint(model_id, task, huggingfacehub_api_token, model_kwargs)
         except Exception as e:
             raise ValueError("Could not connect to HuggingFace Endpoints API.") from e
 
-        return llm  # Return the LLM directly without wrapping in ChatHuggingFace
+        return llm
