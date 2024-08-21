cc13c7718904c0804e48c1c847916cbb43915144
anovazzi1 <otavio2204@gmail.com>
2024-08-21 17:36:59 -0300

    fix overflow issue

--- src/frontend/src/CustomNodes/NoteNode/index.tsx
+++ src/frontend/src/CustomNodes/NoteNode/index.tsx
@@ -27,13 +27,13 @@ function NoteNode({
       />
 
       <div style={{
-        maxHeight:NOTE_NODE_MAX_HEIGHT,
-        maxWidth:NOTE_NODE_MAX_WIDTH,
-        minWidth:NOTE_NODE_MIN_WIDTH,
-        minHeight:NOTE_NODE_MIN_HEIGHT,
+        maxHeight: NOTE_NODE_MAX_HEIGHT,
+        maxWidth: NOTE_NODE_MAX_WIDTH,
+        minWidth: NOTE_NODE_MIN_WIDTH,
+        minHeight: NOTE_NODE_MIN_HEIGHT,
       }}
-       className={cn("generic-node-div gap-3 p-5 h-full border border-b rounded-md",selected?"":"shadow-sm")}>
-        <div className="w-full flex align-middle items-center">
+        className={cn("flex flex-col bg-background transition-all gap-3 p-5 h-full border border-b rounded-md", selected ? "" : "shadow-sm")}>
+        <div className="w-full flex align-middle items-center h-fit">
           <div className="flex w-full gap-2">
             <IconComponent name="StickyNote" />
             <div className="w-4/5">
@@ -45,21 +45,20 @@ function NoteNode({
 
             </div>
           </div>
-          <div onClick={()=>{
-              console.log("clicked")
-              setExpanded((prev)=>!prev)
-            }}>
-            <IconComponent className="w-4 h-4 cursor-pointer" name={expanded?"ChevronsDownUp":"ChevronsUpDown"} />
+          <div onClick={() => {
+            setExpanded((prev) => !prev)
+          }}>
+            <IconComponent className="w-4 h-4 cursor-pointer" name={expanded ? "ChevronsDownUp" : "ChevronsUpDown"} />
           </div>
         </div>
-        <div className=" h-full nowheel">
-        <NodeDescription
-        charLimit={2500}
-          nodeId={data.id}
-          selected={selected}
-          description={data.node?.description}
-          emptyPlaceholder="Double Click to Edit Note"
-        />
+        <div className="h-full nowheel overflow-auto">
+          <NodeDescription
+            charLimit={2500}
+            nodeId={data.id}
+            selected={selected}
+            description={data.node?.description}
+            emptyPlaceholder="Double Click to Edit Note"
+          />
         </div>
       </div>
     </>
