d2cd85c53c130a6b7373f50cda4e284d6108e33a
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-21 18:11:51 -0300

    Added compression handling to backend

--- src/backend/base/langflow/main.py
+++ src/backend/base/langflow/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import gzip
 import json
 import os
 import warnings
@@ -11,6 +12,7 @@ from urllib.parse import urlencode
 import nest_asyncio  # type: ignore
 from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from loguru import logger
@@ -28,7 +30,11 @@ from langflow.initial_setup.setup import (
 )
 from langflow.interface.types import get_and_cache_all_types_dict
 from langflow.interface.utils import setup_llm_caching
-from langflow.services.deps import get_cache_service, get_settings_service, get_telemetry_service
+from langflow.services.deps import (
+    get_cache_service,
+    get_settings_service,
+    get_telemetry_service,
+)
 from langflow.services.plugins.langfuse_plugin import LangfuseInstance
 from langflow.services.utils import initialize_services, teardown_services
 from langflow.logging.logger import configure
@@ -53,7 +59,9 @@ class RequestCancelledMiddleware(BaseHTTPMiddleware):
         handler_task = asyncio.create_task(call_next(request))
         cancel_task = asyncio.create_task(cancel_handler())
 
-        done, pending = await asyncio.wait([handler_task, cancel_task], return_when=asyncio.FIRST_COMPLETED)
+        done, pending = await asyncio.wait(
+            [handler_task, cancel_task], return_when=asyncio.FIRST_COMPLETED
+        )
 
         for task in pending:
             task.cancel()
@@ -72,9 +80,15 @@ class JavaScriptMIMETypeMiddleware(BaseHTTPMiddleware):
             if isinstance(exc, PydanticSerializationError):
                 message = "Something went wrong while serializing the response. Please share this error on our GitHub repository."
                 error_messages = json.dumps([message, str(exc)])
-                raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=error_messages) from exc
+                raise HTTPException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=error_messages
+                ) from exc
             raise exc
-        if "files/" not in request.url.path and request.url.path.endswith(".js") and response.status_code == 200:
+        if (
+            "files/" not in request.url.path
+            and request.url.path.endswith(".js")
+            and response.status_code == 200
+        ):
             response.headers["Content-Type"] = "text/javascript"
         return response
 
@@ -89,11 +103,17 @@ def get_lifespan(fix_migration=False, socketio_server=None, version=None):
         else:
             rprint("[bold green]Starting Langflow...[/bold green]")
         try:
-            initialize_services(fix_migration=fix_migration, socketio_server=socketio_server)
+            initialize_services(
+                fix_migration=fix_migration, socketio_server=socketio_server
+            )
             setup_llm_caching()
             LangfuseInstance.update()
             initialize_super_user_if_needed()
-            task = asyncio.create_task(get_and_cache_all_types_dict(get_settings_service(), get_cache_service()))
+            task = asyncio.create_task(
+                get_and_cache_all_types_dict(
+                    get_settings_service(), get_cache_service()
+                )
+            )
             await create_or_update_starter_projects(task)
             asyncio.create_task(get_telemetry_service().start())
             load_flows_from_directory()
@@ -132,8 +152,20 @@ def create_app():
         allow_headers=["*"],
     )
     app.add_middleware(JavaScriptMIMETypeMiddleware)
+    app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
+
     # ! Deactivating this until we find a better solution
     # app.add_middleware(RequestCancelledMiddleware)
+
+    @app.middleware("http")
+    async def decompress_if_gzip(request: Request, call_next):
+        if request.headers.get("content-encoding", "") == "gzip":
+            # the request's body is compressed, so we need to decompress it
+            body = await request.body()
+            dec = gzip.decompress(body)
+            request._body = dec  # <-- if only things were that easy
+        response = await call_next(request)
+        return response
 
     @app.middleware("http")
     async def flatten_query_string_lists(request: Request, call_next):
@@ -150,7 +182,9 @@ def create_app():
         # set here for create_app() entry point
         prome_port = int(prome_port_str)
         if prome_port > 0 or prome_port < 65535:
-            rprint(f"[bold green]Starting Prometheus server on port {prome_port}...[/bold green]")
+            rprint(
+                f"[bold green]Starting Prometheus server on port {prome_port}...[/bold green]"
+            )
             settings.prometheus_enabled = True
             settings.prometheus_port = prome_port
         else:
@@ -227,7 +261,9 @@ def get_static_files_dir():
     return frontend_path / "frontend"
 
 
-def setup_app(static_files_dir: Optional[Path] = None, backend_only: bool = False) -> FastAPI:
+def setup_app(
+    static_files_dir: Optional[Path] = None, backend_only: bool = False
+) -> FastAPI:
     """Setup the FastAPI app."""
     # get the directory of the current file
     logger.info(f"Setting up app with static files directory {static_files_dir}")
