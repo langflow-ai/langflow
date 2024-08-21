855c9741d4af14db4fe6cb6882236bb91cd3c108
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-08-21 12:12:45 -0300

    refactor: prompt validation functionality (#3473)
    * Added post validate prompt mutation
    
    * Updated prompt modal to use mutation to validate prompt
    
    * Removed post validate prompt from API.ts

---
+++ src/frontend/src/controllers/API/queries/nodes/use-post-validate-prompt.ts
@@ -0,0 +1,48 @@
+import {
+  APIClassType,
+  PromptTypeAPI,
+  ResponseErrorDetailAPI,
+  useMutationFunctionType,
+} from "@/types/api";
+import { UseMutationResult } from "@tanstack/react-query";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+
+interface IPostValidatePrompt {
+  name: string;
+  template: string;
+  frontend_node: APIClassType;
+}
+
+export const usePostValidatePrompt: useMutationFunctionType<
+  undefined,
+  IPostValidatePrompt,
+  PromptTypeAPI,
+  ResponseErrorDetailAPI
+> = (options?) => {
+  const { mutate } = UseRequestProcessor();
+
+  const postValidatePromptFn = async (
+    payload: IPostValidatePrompt,
+  ): Promise<PromptTypeAPI> => {
+    const response = await api.post<PromptTypeAPI>(
+      getURL("VALIDATE", { 1: "prompt" }),
+      {
+        name: payload.name,
+        template: payload.template,
+        frontend_node: payload.frontend_node,
+      },
+    );
+
+    return response.data;
+  };
+
+  const mutation: UseMutationResult<
+    PromptTypeAPI,
+    ResponseErrorDetailAPI,
+    IPostValidatePrompt
+  > = mutate(["usePostValidatePrompt"], postValidatePromptFn, options);
+
+  return mutation;
+};
