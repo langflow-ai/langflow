ee96a0451684054f4dae3b5d67af220e94707683
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-22 18:31:59 -0300

    refactor: Refactor ComponentTool to use Component instance instead of component_call_method. Update forward refs.

--- src/backend/base/langflow/base/tools/component_tool.py
+++ src/backend/base/langflow/base/tools/component_tool.py
@@ -1,27 +1,24 @@
-from typing import Any, Callable
+from typing import Any
 
 from langchain_core.tools import BaseTool, ToolException
+
+from langflow.custom.custom_component.component import Component
 
 
 class ComponentTool(BaseTool):
     name: str
     description: str
-    component_call_method: Callable | None = None
+    component: "Component"
 
-    def __init__(self, component) -> None:
+    def __init__(self, component: "Component") -> None:
         """Initialize the tool."""
         from langflow.io.schema import create_input_schema
 
         name = component.name or component.__class__.__name__
         description = component.description or ""
         args_schema = create_input_schema(component.inputs)
-        super().__init__(name=name, description=description, args_schema=args_schema)
-        self.component_call_method = component.__call__
+        super().__init__(name=name, description=description, args_schema=args_schema, component=component)
         # self.component = component
-
-    def __copy__(self):
-        self.component_call_method = None
-        return super().__copy__()
 
     @property
     def args(self) -> dict:
@@ -35,7 +32,10 @@ class ComponentTool(BaseTool):
     ) -> dict:
         """Use the tool."""
         try:
-            results, _ = self.component_call_method(**kwargs)
+            results, _ = self.component(**kwargs)
             return results
         except Exception as e:
             raise ToolException(f"Error running {self.name}: {e}")
+
+
+ComponentTool.update_forward_refs()
