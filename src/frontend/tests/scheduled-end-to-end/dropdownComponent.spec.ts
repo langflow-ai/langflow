0e330d94b4bafa3468636eca2579a43ca4b43dcf
cristhianzl <cristhian.lousa@gmail.com>
2024-08-22 10:22:17 -0300

    ✅ (dropdownComponent.spec.ts): update mouse wheel scroll distance to 800 to improve test reliability 📝 (dropdownComponent.spec.ts): add additional wait time to ensure stability in test execution

--- src/frontend/tests/scheduled-end-to-end/dropdownComponent.spec.ts
+++ src/frontend/tests/scheduled-end-to-end/dropdownComponent.spec.ts
@@ -152,8 +152,10 @@ test("dropDownComponent", async ({ page }) => {
     .hover()
     .then(async () => {
       await page.waitForTimeout(1000);
-      await page.mouse.wheel(0, 1000);
+      await page.mouse.wheel(0, 800);
     });
+
+  await page.waitForTimeout(1000);
 
   const element = page
     .locator("#CodeEditor div")
