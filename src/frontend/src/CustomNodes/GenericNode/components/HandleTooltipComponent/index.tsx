8b2627d931a17327480a451687a9db0c5eca3339
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-23 12:46:24 -0300

    Added text to instruct user to click to filter on handles

--- src/frontend/src/CustomNodes/GenericNode/components/HandleTooltipComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/HandleTooltipComponent/index.tsx
@@ -6,14 +6,18 @@ export default function HandleTooltipComponent({
   tooltipTitle: string;
 }) {
   return (
-    <div className="py-1.5 text-xs">
+    <div className="py-1.5">
       <span className="mr-1">Type: </span>
       <span
-        className="rounded-md px-2 pb-1 pt-0.5 text-background"
+        className="rounded-md px-2 pb-1 pt-0.5 text-xs text-background"
         style={{ backgroundColor: color }}
       >
         {tooltipTitle}
       </span>
+
+      <div className="mt-3 text-xs text-muted-foreground">
+        Click to filter by this type
+      </div>
     </div>
   );
 }
