65b20581a7275291cbc30761af86fe6e1249b9c6
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-26 18:29:10 -0300

    refactor(worker.py): refactor run_flow_task function to improve readability and maintainability by simplifying the code and removing unnecessary complexity

--- src/backend/base/langflow/worker.py
+++ src/backend/base/langflow/worker.py
@@ -1,12 +1,15 @@
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Optional
 
-from asgiref.sync import async_to_sync
-from celery.exceptions import SoftTimeLimitExceeded  # type: ignore
+from asyncer import syncify
 
+from langflow.api.run_utils import simple_run_flow
+from langflow.api.v1.schemas import SimplifiedAPIRequest
 from langflow.core.celery_app import celery_app
+from langflow.services.database.models.flow.model import FlowRead
+from langflow.services.database.models.user.model import User
 
 if TYPE_CHECKING:
-    from langflow.graph.vertex.base import Vertex
+    pass
 
 
 @celery_app.task(acks_late=True)
@@ -14,24 +17,21 @@ def test_celery(word: str) -> str:
     return f"test task return {word}"
 
 
-@celery_app.task(bind=True, soft_time_limit=30, max_retries=3)
-def build_vertex(self, vertex: "Vertex") -> "Vertex":
-    """
-    Build a vertex
-    """
+@celery_app.task(bind=True, max_retries=3)
+def run_flow_task(
+    self,
+    flow: FlowRead,
+    input_request: SimplifiedAPIRequest,
+    stream: bool = False,
+    api_key_user: Optional[User] = None,
+):
     try:
-        vertex.task_id = self.request.id
-        async_to_sync(vertex.build)()
-        return vertex
-    except SoftTimeLimitExceeded as e:
-        raise self.retry(exc=SoftTimeLimitExceeded("Task took too long"), countdown=2) from e
-
-
-@celery_app.task(acks_late=True)
-def process_graph_cached_task(
-    data_graph: Dict[str, Any],
-    inputs: Optional[Union[dict, List[dict]]] = None,
-    clear_cache=False,
-    session_id=None,
-) -> Dict[str, Any]:
-    raise NotImplementedError("This task is not implemented yet")
+        result = syncify(simple_run_flow)(
+            flow=flow,
+            input_request=input_request,
+            stream=stream,
+            api_key_user=api_key_user,
+        )
+        return result
+    except Exception as e:
+        raise self.retry(exc=e)
