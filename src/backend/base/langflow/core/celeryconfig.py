cc89ec9402b11b40631b4d806459a5e85b517a28
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-27 19:48:46 -0300

    refactor(celeryconfig.py): restructure broker_url and result_backend assignment logic to prioritize langflow_redis_host and langflow_redis_port values if available, fallback to default values if not provided

--- src/backend/base/langflow/core/celeryconfig.py
+++ src/backend/base/langflow/core/celeryconfig.py
@@ -1,10 +1,19 @@
 # celeryconfig.py
 import os
 
-mq_user = os.environ.get("RABBITMQ_DEFAULT_USER", "langflow")
-mq_password = os.environ.get("RABBITMQ_DEFAULT_PASS", "langflow")
-# RabbitMQ
-broker_url = os.environ.get("BROKER_URL", f"amqp://{mq_user}:{mq_password}@localhost:5672//")
-result_backend = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
+langflow_redis_host = os.environ.get("LANGFLOW_REDIS_HOST")
+langflow_redis_port = os.environ.get("LANGFLOW_REDIS_PORT")
+broker_url = os.environ.get("BROKER_URL")
+# broker default user
+
+if langflow_redis_host and langflow_redis_port and not broker_url:
+    broker_url = f"redis://{langflow_redis_host}:{langflow_redis_port}/0"
+    result_backend = f"redis://{langflow_redis_host}:{langflow_redis_port}/0"
+else:
+    # RabbitMQ
+    mq_user = os.environ.get("RABBITMQ_DEFAULT_USER", "langflow")
+    mq_password = os.environ.get("RABBITMQ_DEFAULT_PASS", "langflow")
+    broker_url = broker_url or f"amqp://{mq_user}:{mq_password}@localhost:5672//"
+    result_backend = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
 # tasks should be json or pickle
 accept_content = ["json"]
