d556d2210af195ae477b38ddb2341b4f7f6b44cd
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-08-27 17:36:53 -0300

    fix(celeryconfig.py): fix broker_url to include double slash after port number for correct connection URL format

--- src/backend/base/langflow/core/celeryconfig.py
+++ src/backend/base/langflow/core/celeryconfig.py
@@ -12,7 +12,7 @@ else:
     # RabbitMQ
     mq_user = os.environ.get("RABBITMQ_DEFAULT_USER", "langflow")
     mq_password = os.environ.get("RABBITMQ_DEFAULT_PASS", "langflow")
-    broker_url = os.environ.get("BROKER_URL", f"amqp://{mq_user}:{mq_password}@localhost:5672")
+    broker_url = os.environ.get("BROKER_URL", f"amqp://{mq_user}:{mq_password}@localhost:5672//")
     result_backend = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
 # tasks should be json or pickle
 accept_content = ["json", "pickle"]
