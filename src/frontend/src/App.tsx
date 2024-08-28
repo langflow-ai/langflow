cd7f71697bea457d816d175972f0d18635bf7427
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-28 15:34:36 -0300

    Updated App.tsx with new gets and configurations and added a loader before loading the router

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,36 +1,26 @@
-import { Suspense, useContext, useEffect } from "react";
-import { Cookies } from "react-cookie";
+import { Suspense, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import "reactflow/dist/style.css";
-import { AuthContext } from "./contexts/authContext";
-import {
-  useAutoLogin,
-  useRefreshAccessToken,
-} from "./controllers/API/queries/auth";
+import { useGetAutoLogin } from "./controllers/API/queries/auth";
 import { useGetConfig } from "./controllers/API/queries/config/use-get-config";
 import { useGetVersionQuery } from "./controllers/API/queries/version";
 import { LoadingPage } from "./pages/LoadingPage";
 import router from "./routes";
-import useAuthStore from "./stores/authStore";
 import { useDarkStore } from "./stores/darkStore";
 
 export default function App() {
-  const { login, setUserData, getUser } = useContext(AuthContext);
-  const setAutoLogin = useAuthStore((state) => state.setAutoLogin);
-  const autoLogin = useAuthStore((state) => state.autoLogin);
-  const refreshStars = useDarkStore((state) => state.refreshStars);
   const dark = useDarkStore((state) => state.dark);
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  const cookies = new Cookies();
-  const logout = useAuthStore((state) => state.logout);
+  const refreshStars = useDarkStore((state) => state.refreshStars);
 
-  const refreshToken = cookies.get("refresh_token");
+  const { isFetched } = useGetAutoLogin();
+  useGetVersionQuery({ enabled: isFetched });
+  useGetConfig({ enabled: isFetched });
 
-  const { mutate: mutateAutoLogin } = useAutoLogin();
-
-  const { mutate: mutateRefresh } = useRefreshAccessToken();
-
-  const isLoginPage = location.pathname.includes("login");
+  useEffect(() => {
+    if (isFetched) {
+      refreshStars();
+    }
+  }, [isFetched]);
 
   useEffect(() => {
     if (!dark) {
@@ -40,42 +30,10 @@ export default function App() {
     }
   }, [dark]);
 
-  useEffect(() => {
-    mutateAutoLogin(undefined, {
-      onSuccess: async (user) => {
-        if (user && user["access_token"]) {
-          user["refresh_token"] = "auto";
-          login(user["access_token"], "auto");
-          setUserData(user);
-          setAutoLogin(true);
-          refreshStars();
-          // mutateRefresh({ refresh_token: refreshToken });
-        }
-      },
-      onError: (error) => {
-        if (error.name !== "CanceledError") {
-          setAutoLogin(false);
-          if (!isLoginPage) {
-            if (!isAuthenticated) {
-              logout();
-            } else {
-              mutateRefresh({ refresh_token: refreshToken });
-              refreshStars();
-              getUser();
-            }
-          }
-        }
-      },
-    });
-  }, []);
-
-  useGetVersionQuery();
-  useGetConfig();
-
   return (
     //need parent component with width and height
     <Suspense fallback={<LoadingPage />}>
-      {autoLogin == null ? <LoadingPage /> : <RouterProvider router={router} />}
+      {isFetched ? <RouterProvider router={router} /> : <LoadingPage />}
     </Suspense>
   );
 }
