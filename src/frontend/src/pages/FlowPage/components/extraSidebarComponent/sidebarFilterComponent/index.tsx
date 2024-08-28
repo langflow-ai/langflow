ed7148f5a8f2a088ab105c2b505ca463458c78a0
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-28 17:28:20 -0300

    Changed sidebar filter to be gray

--- src/frontend/src/pages/FlowPage/components/extraSidebarComponent/sidebarFilterComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/extraSidebarComponent/sidebarFilterComponent/index.tsx
@@ -12,20 +12,22 @@ export function SidebarFilterComponent({
   resetFilters: () => void;
 }) {
   return (
-    <div className="mb-0.5 flex w-full items-center justify-between rounded bg-filter-background p-1 px-2 text-xs font-medium text-filter-foreground">
-      <div className="flex items-center gap-1.5">
+    <div className="mb-0.5 flex w-full items-center justify-between rounded border bg-muted p-1 px-2 text-xs font-medium text-primary">
+      <div className="flex flex-1 items-center gap-1.5">
         <ForwardedIconComponent
           name="ListFilter"
-          className="h-4 w-4 stroke-2"
+          className="h-4 w-4 shrink-0 stroke-2"
         />
-        {isInput ? "Input" : "Output"}: {type}
+        <div className="flex-1 overflow-hidden truncate">
+          {isInput ? "Input" : "Output"}: {type}
+        </div>
       </div>
       <ShadTooltip
         side="right"
         styleClasses="max-w-full"
         content="Remove filter"
       >
-        <Button unstyled className="" onClick={resetFilters}>
+        <Button unstyled className="shrink-0" onClick={resetFilters}>
           <ForwardedIconComponent
             name="X"
             className="h-4 w-4 stroke-2"
