0f245a584413b25d95ddc5ecea9a5b01012bc0d5
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-28 11:48:11 -0300

    Changed routes to be contained by only one ProtectedRoute

--- src/frontend/src/routes.tsx
+++ src/frontend/src/routes.tsx
@@ -1,17 +1,18 @@
-import React, { lazy } from "react";
+import { lazy } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
+  Outlet,
   Route,
 } from "react-router-dom";
 import { ProtectedAdminRoute } from "./components/authAdminGuard";
 import { ProtectedRoute } from "./components/authGuard";
 import { ProtectedLoginRoute } from "./components/authLoginGuard";
 import { AuthSettingsGuard } from "./components/authSettingsGuard";
-import { CatchAllRoute } from "./components/catchAllRoutes";
 import { StoreGuard } from "./components/storeGuard";
 import { AppWrapperPage } from "./pages/AppWrapperPage";
+import { DashboardWrapperPage } from "./pages/DashboardWrapperPage";
 import FlowPage from "./pages/FlowPage";
 import LoginPage from "./pages/LoginPage";
 import MyCollectionComponent from "./pages/MainPage/components/myCollectionComponent";
@@ -34,132 +35,104 @@ const PlaygroundPage = lazy(() => import("./pages/Playground"));
 const SignUp = lazy(() => import("./pages/SignUpPage"));
 const router = createBrowserRouter(
   createRoutesFromElements([
-    <Route path="" element={<AppWrapperPage />}>
+    <Route path="/" element={<AppWrapperPage />}>
       <Route
-        path="/"
+        path=""
         element={
           <ProtectedRoute>
-            <HomePage />
+            <Outlet />
           </ProtectedRoute>
         }
       >
-        <Route index element={<Navigate replace to={"all"} />} />
-        <Route
-          path="flows/"
-          element={<MyCollectionComponent key="flows" type="flow" />}
-        >
+        <Route path="" element={<DashboardWrapperPage />}>
+          <Route path="" element={<HomePage />}>
+            <Route index element={<Navigate replace to={"all"} />} />
+            <Route
+              path="flows/"
+              element={<MyCollectionComponent key="flows" type="flow" />}
+            >
+              <Route
+                path="folder/:folderId"
+                element={<MyCollectionComponent key="flows" type="flow" />}
+              />
+            </Route>
+            <Route
+              path="components/"
+              element={
+                <MyCollectionComponent key="components" type="component" />
+              }
+            >
+              <Route
+                path="folder/:folderId"
+                element={
+                  <MyCollectionComponent key="components" type="component" />
+                }
+              />
+            </Route>
+            <Route
+              path="all/"
+              element={<MyCollectionComponent key="all" type="all" />}
+            >
+              <Route
+                path="folder/:folderId"
+                element={<MyCollectionComponent key="all" type="all" />}
+              />
+            </Route>
+          </Route>
+          <Route path="/settings" element={<SettingsPage />}>
+            <Route index element={<Navigate replace to={"general"} />} />
+            <Route path="global-variables" element={<GlobalVariablesPage />} />
+            <Route path="api-keys" element={<ApiKeysPage />} />
+            <Route
+              path="general/:scrollId?"
+              element={
+                <AuthSettingsGuard>
+                  <GeneralPage />
+                </AuthSettingsGuard>
+              }
+            />
+            <Route path="shortcuts" element={<ShortcutsPage />} />
+            <Route path="messages" element={<MessagesPage />} />
+          </Route>
           <Route
-            path="folder/:folderId"
-            element={<MyCollectionComponent key="flows" type="flow" />}
-          />
-        </Route>
-        <Route
-          path="components/"
-          element={<MyCollectionComponent key="components" type="component" />}
-        >
-          <Route
-            path="folder/:folderId"
+            path="/store"
             element={
-              <MyCollectionComponent key="components" type="component" />
+              <StoreGuard>
+                <StorePage />
+              </StoreGuard>
+            }
+          />
+          <Route
+            path="/store/:id/"
+            element={
+              <StoreGuard>
+                <StorePage />
+              </StoreGuard>
+            }
+          />
+          <Route path="/account">
+            <Route path="delete" element={<DeleteAccountPage />}></Route>
+          </Route>
+          <Route
+            path="/admin"
+            element={
+              <ProtectedAdminRoute>
+                <AdminPage />
+              </ProtectedAdminRoute>
             }
           />
         </Route>
-        <Route
-          path="all/"
-          element={<MyCollectionComponent key="all" type="all" />}
-        >
-          <Route
-            path="folder/:folderId"
-            element={<MyCollectionComponent key="all" type="all" />}
-          />
+        <Route path="/flow/:id/">
+          <Route path="" element={<DashboardWrapperPage />}>
+            <Route path="folder/:folderId/" element={<FlowPage />} />
+            <Route path="" element={<FlowPage />} />
+          </Route>
+          <Route path="view" element={<ViewPage />} />
+        </Route>
+        <Route path="/playground/:id/">
+          <Route path="" element={<PlaygroundPage />} />
         </Route>
       </Route>
-      <Route
-        path="/settings"
-        element={
-          <ProtectedRoute>
-            <SettingsPage />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<Navigate replace to={"general"} />} />
-        <Route path="global-variables" element={<GlobalVariablesPage />} />
-        <Route path="api-keys" element={<ApiKeysPage />} />
-        <Route
-          path="general/:scrollId?"
-          element={
-            <AuthSettingsGuard>
-              <GeneralPage />
-            </AuthSettingsGuard>
-          }
-        />
-        <Route path="shortcuts" element={<ShortcutsPage />} />
-        <Route path="messages" element={<MessagesPage />} />
-      </Route>
-      <Route
-        path="/store"
-        element={
-          <ProtectedRoute>
-            <StoreGuard>
-              <StorePage />
-            </StoreGuard>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/store/:id/"
-        element={
-          <ProtectedRoute>
-            <StoreGuard>
-              <StorePage />
-            </StoreGuard>
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/playground/:id/">
-        <Route
-          path=""
-          element={
-            <ProtectedRoute>
-              <PlaygroundPage />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
-      <Route path="/flow/:id/">
-        <Route
-          path="folder/:folderId/"
-          element={
-            <ProtectedRoute>
-              <FlowPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path=""
-          element={
-            <ProtectedRoute>
-              <FlowPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="view"
-          element={
-            <ProtectedRoute>
-              <ViewPage />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
-      <Route
-        path="*"
-        element={
-          <ProtectedRoute>
-            <CatchAllRoute />
-          </ProtectedRoute>
-        }
-      />
       <Route
         path="/login"
         element={
@@ -184,24 +157,7 @@ const router = createBrowserRouter(
           </ProtectedLoginRoute>
         }
       />
-      <Route
-        path="/admin"
-        element={
-          <ProtectedAdminRoute>
-            <AdminPage />
-          </ProtectedAdminRoute>
-        }
-      />
-      <Route path="/account">
-        <Route
-          path="delete"
-          element={
-            <ProtectedRoute>
-              <DeleteAccountPage />
-            </ProtectedRoute>
-          }
-        ></Route>
-      </Route>
+      <Route path="*" element={<Navigate replace to="/" />} />
     </Route>,
   ]),
 );
