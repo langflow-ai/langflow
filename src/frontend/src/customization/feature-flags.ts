557b590d22b21098d4f80d5637f9a07cb063adad
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-28 18:59:03 -0300

    Added customization files

---
+++ src/frontend/src/customization/feature-flags.ts
@@ -0,0 +1,7 @@
+export const ENABLE_DARK_MODE = true;
+export const ENABLE_API = true;
+export const ENABLE_LANGFLOW_STORE = true;
+export const ENABLE_PROFILE_ICONS = true;
+export const ENABLE_SOCIAL_LINKS = true;
+export const ENABLE_BRANDING = true;
+export const ENABLE_MVPS = false;
