018ced4439d3ce0df52e95c8927a36ef7f2f3aec
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-28 11:51:21 -0300

    changed viewPage to not set onFlowPage since it's used only on header

--- src/frontend/src/pages/ViewPage/index.tsx
+++ src/frontend/src/pages/ViewPage/index.tsx
@@ -1,6 +1,5 @@
 import { useGetRefreshFlows } from "@/controllers/API/queries/flows/use-get-refresh-flows";
 import { useGetGlobalVariables } from "@/controllers/API/queries/variables";
-import useFlowStore from "@/stores/flowStore";
 import { useTypesStore } from "@/stores/typesStore";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -10,7 +9,6 @@ import Page from "../FlowPage/components/PageComponent";
 export default function ViewPage() {
   const setCurrentFlow = useFlowsManagerStore((state) => state.setCurrentFlow);
 
-  const setOnFlowPage = useFlowStore((state) => state.setOnFlowPage);
   const { id } = useParams();
   const navigate = useNavigate();
   useGetGlobalVariables();
@@ -42,15 +40,6 @@ export default function ViewPage() {
     };
     awaitgetTypes();
   }, [id, flows]);
-
-  useEffect(() => {
-    setOnFlowPage(true);
-
-    return () => {
-      setOnFlowPage(false);
-      setCurrentFlow(undefined);
-    };
-  }, [id]);
 
   return (
     <div className="flow-page-positioning">
