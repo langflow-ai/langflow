2c64c17fdab00b4b7ae7501d0a4d7815545db2d6
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-08-29 15:41:59 -0300

    Added custom health check

---
+++ src/frontend/src/customization/hooks/use-custom-health-check.ts
@@ -0,0 +1,13 @@
+export function useCustomHealthCheck() {
+  const message = "";
+  const description = "";
+  const isFetching = false;
+  const isError = false;
+
+  return {
+    message,
+    description,
+    isFetching,
+    isError,
+  };
+}
