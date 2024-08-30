3bef9aa633618ed8343c0fe746046c0fa87b8824
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-08-30 14:53:11 -0300

    refactor: custom post loading and add AppAuthenticatedPage (#3632)
    * Added post loading hook to appInitPage
    
    * Removed custom post loading
    
    * Changed custom post loading to cusotm post auth
    
    * Adds AppAuthenticatedPage
    
    * Uses AppAuthenticatedPage inside protected route

---
+++ src/frontend/src/customization/hooks/use-custom-post-auth.ts
@@ -0,0 +1,16 @@
+import { UseRequestProcessor } from "@/controllers/API/services/request-processor";
+import { useQueryFunctionType } from "@/types/api";
+
+export const useCustomPostAuth: useQueryFunctionType<undefined, null> = (
+  options,
+) => {
+  const { query } = UseRequestProcessor();
+
+  const getPostAuthFn = async () => {
+    return null;
+  };
+
+  const queryResult = query(["usePostAuth"], getPostAuthFn, options);
+
+  return queryResult;
+};
