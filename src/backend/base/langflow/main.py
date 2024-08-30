f17ac257229f2e8018bcd34bc5ef663e55ea56c9
Eric Hare <ericrhare@gmail.com>
2024-08-29 21:39:15 -0700

    Update main.py

--- src/backend/base/langflow/main.py
+++ src/backend/base/langflow/main.py
@@ -183,7 +183,7 @@ def create_app():
 
     FastAPIInstrumentor.instrument_app(app)
 
-    # Get necessary NLTK packages (PDF parsing support)
+    # Get necessary NLTK packages
     download_nltk_resources()
 
     return app
