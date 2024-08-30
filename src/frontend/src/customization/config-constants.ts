27b5f55ef91f9eb7d3cfde53620ddcd83ec629cd
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-08-30 17:40:16 -0300

    refactor: health check endpoint (#3635)
    * Changed health check to use /health_check and to be modularized
    
    * Added default value
    
    * Added health constant to default export
    
    * check cors before inserting custom headers
    
    * Added get global variables check on init
    
    * Updated global variables to start with undefined
    
    * Fixed redirect when showGeneralSettings is disabled
    
    * Added isOpen props to custom api generator

--- src/frontend/src/customization/config-constants.ts
+++ src/frontend/src/customization/config-constants.ts
@@ -3,6 +3,7 @@ export const PORT = 3000;
 export const PROXY_TARGET = "http://127.0.0.1:7860";
 export const API_ROUTES = ["^/api/v1/", "/health"];
 export const BASE_URL_API = "/api/v1/";
+export const HEALTH_CHECK_URL = "/health_check";
 
 export default {
   BASENAME,
@@ -10,4 +11,5 @@ export default {
   PROXY_TARGET,
   API_ROUTES,
   BASE_URL_API,
+  HEALTH_CHECK_URL,
 };
--- src/frontend/src/types/zustand/globalVariables/index.ts
+++ src/frontend/src/types/zustand/globalVariables/index.ts
@@ -1,5 +1,5 @@
 export type GlobalVariablesStore = {
-  globalVariablesEntries: Array<string>;
+  globalVariablesEntries: Array<string> | undefined;
   setGlobalVariablesEntries: (entries: Array<string>) => void;
   unavailableFields: { [name: string]: string };
   setUnavailableFields: (fields: { [name: string]: string }) => void;
