21065e565a43b33a784070c3df7b0691599e68b8
Nicolò Boschi <boschi1997@gmail.com>
2024-09-02 15:57:59 +0200

    ci: add tests for text input/output components

---
+++ src/backend/tests/integration/components/outputs/test_text_output.py
@@ -0,0 +1,24 @@
+from langflow.components.outputs import ChatOutput, TextOutput, TextOutputComponent
+from langflow.memory import get_messages
+from langflow.schema.message import Message
+from tests.integration.utils import run_single_component
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test():
+    outputs = await run_single_component(TextOutputComponent, inputs={"input_value": "hello"})
+    assert isinstance(outputs["text"], Message)
+    assert outputs["text"].text == "hello"
+    assert outputs["text"].sender is None
+    assert outputs["text"].sender_name is None
+
+
+@pytest.mark.asyncio
+async def test_message():
+    outputs = await run_single_component(TextOutputComponent, inputs={"input_value": Message(text="hello")})
+    assert isinstance(outputs["text"], Message)
+    assert outputs["text"].text == "hello"
+    assert outputs["text"].sender is None
+    assert outputs["text"].sender_name is None
