1c7ef6ee60008b963f95213458247951837874c5
Nicolò Boschi <boschi1997@gmail.com>
2024-09-03 18:36:30 +0200

    feat: add Parse JSON component (#3167)
    * feat: add Parse JSON component
    
    * feat: Update ParseJSONDataComponent to handle JSON decoding errors
    
    * fix tests
    
    * [autofix.ci] apply automated fixes
    
    * add string check in _parse_data
    
    ---------
    
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ src/backend/tests/integration/components/helpers/test_parse_json_data.py
@@ -0,0 +1,57 @@
+import pytest
+
+from langflow.components.helpers.ParseJSONData import ParseJSONDataComponent
+from langflow.components.inputs import ChatInput
+from langflow.schema import Data
+from tests.integration.components.mock_components import TextToData
+from tests.integration.utils import run_single_component, ComponentInputHandle
+
+
+@pytest.mark.asyncio
+async def test_from_data():
+    outputs = await run_single_component(
+        ParseJSONDataComponent,
+        inputs={
+            "input_value": ComponentInputHandle(
+                clazz=TextToData, inputs={"text_data": ['{"key":"value1"}'], "is_json": True}, output_name="from_text"
+            ),
+            "query": ".[0].key",
+        },
+    )
+    assert outputs["filtered_data"] == [Data(text="value1")]
+
+    outputs = await run_single_component(
+        ParseJSONDataComponent,
+        inputs={
+            "input_value": ComponentInputHandle(
+                clazz=TextToData,
+                inputs={"text_data": ['{"key":[{"field1": 1, "field2": 2}]}'], "is_json": True},
+                output_name="from_text",
+            ),
+            "query": ".[0].key.[0].field2",
+        },
+    )
+    assert outputs["filtered_data"] == [Data(text="2")]
+
+
+@pytest.mark.asyncio
+async def test_from_message():
+    outputs = await run_single_component(
+        ParseJSONDataComponent,
+        inputs={
+            "input_value": ComponentInputHandle(clazz=ChatInput, inputs={}, output_name="message"),
+            "query": ".[0].key",
+        },
+        run_input="{'key':'value1'}",
+    )
+    assert outputs["filtered_data"] == [Data(text="value1")]
+
+    outputs = await run_single_component(
+        ParseJSONDataComponent,
+        inputs={
+            "input_value": ComponentInputHandle(clazz=ChatInput, inputs={}, output_name="message"),
+            "query": ".[0].key.[0].field2",
+        },
+        run_input='{"key":[{"field1": 1, "field2": 2}]}',
+    )
+    assert outputs["filtered_data"] == [Data(text="2")]
