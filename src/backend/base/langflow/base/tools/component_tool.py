2bf16cc9ec72c7c14bbd10a588702d9204d1bb3b
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-03 16:54:40 -0300

    Add tool name formatting to ComponentToolkit to ensure valid characters

--- src/backend/base/langflow/base/tools/component_tool.py
+++ src/backend/base/langflow/base/tools/component_tool.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 from typing import TYPE_CHECKING, Callable
 
 from langchain_core.tools import BaseTool
@@ -42,6 +43,12 @@ def _build_output_function(component: "Component", output_method: Callable):
     return output_function
 
 
+def _format_tool_name(name: str):
+    # format to '^[a-zA-Z0-9_-]+$'."
+    # to do that we must remove all non-alphanumeric characters
+    return re.sub(r"[^a-zA-Z0-9_-]", "-", name)
+
+
 class ComponentToolkit:  # type: ignore
     def __init__(self, component: "Component"):
         self.component = component
@@ -62,9 +69,11 @@ class ComponentToolkit:  # type: ignore
                 args_schema = create_input_schema(inputs)
             else:
                 args_schema = create_input_schema(self.component.inputs)
+            name = f"{self.component.name}.{output.method}"
+            formatted_name = _format_tool_name(name)
             tools.append(
                 StructuredTool(
-                    name=f"{self.component.name}.{output.method}",
+                    name=formatted_name,
                     description=build_description(self.component, output),
                     func=_build_output_function(self.component, output_method),
                     args_schema=args_schema,
