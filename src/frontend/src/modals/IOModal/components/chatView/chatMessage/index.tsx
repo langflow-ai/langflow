5dbe6ff84da869fabbe288851adefffd146ff1b1
cristhianzl <cristhian.lousa@gmail.com>
2024-09-04 11:27:08 -0300

    refactor: Remove unused 'open' prop from CodeTabsComponent

--- src/frontend/src/modals/IOModal/components/chatView/chatMessage/index.tsx
+++ src/frontend/src/modals/IOModal/components/chatView/chatMessage/index.tsx
@@ -244,7 +244,6 @@ export default function ChatMessage({
 
                                 return !inline ? (
                                   <CodeTabsComponent
-                                    open={undefined}
                                     isMessage
                                     tabs={[
                                       {
