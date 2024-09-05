0af95c948cb1199d4af50b63d0679da3b0cc1877
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-05 16:12:51 -0300

    Add unit test to ensure output cache is disabled in graph cycles

--- src/backend/tests/unit/graph/graph/test_cycles.py
+++ src/backend/tests/unit/graph/graph/test_cycles.py
@@ -6,6 +6,7 @@ from langflow.components.outputs.TextOutput import TextOutputComponent
 from langflow.components.prototypes.ConditionalRouter import ConditionalRouterComponent
 from langflow.custom.custom_component.component import Component
 from langflow.graph.graph.base import Graph
+from langflow.graph.graph.utils import find_cycle_vertices
 from langflow.io import MessageTextInput, Output
 from langflow.schema.message import Message
 
@@ -109,3 +110,35 @@ def test_cycle_in_graph_max_iterations():
     with pytest.raises(ValueError, match="Max iterations reached"):
         for result in graph.start(max_iterations=2, config={"output": {"cache": False}}):
             results.append(result)
+
+
+def test_that_outputs_cache_is_set_to_false_in_cycle():
+    chat_input = ChatInput(_id="chat_input")
+    router = ConditionalRouterComponent(_id="router")
+    chat_input.set(input_value=router.false_response)
+    concat_component = Concatenate(_id="concatenate")
+    concat_component.set(text=chat_input.message_response)
+    router.set(
+        input_text=chat_input.message_response,
+        match_text="testtesttesttest",
+        operator="equals",
+        message=concat_component.concatenate,
+    )
+    text_output = TextOutputComponent(_id="text_output")
+    text_output.set(input_value=router.true_response)
+    chat_output = ChatOutput(_id="chat_output")
+    chat_output.set(input_value=text_output.text_response)
+
+    graph = Graph(chat_input, chat_output)
+    cycle_vertices = find_cycle_vertices(graph._get_edges_as_list_of_tuples())
+    cycle_outputs_lists = [graph.vertex_map[vertex_id]._custom_component.outputs for vertex_id in cycle_vertices]
+    cycle_outputs = [output for outputs in cycle_outputs_lists for output in outputs]
+    for output in cycle_outputs:
+        assert output.cache is False
+
+    non_cycle_outputs_lists = [
+        vertex._custom_component.outputs for vertex in graph.vertices if vertex.id not in cycle_vertices
+    ]
+    non_cycle_outputs = [output for outputs in non_cycle_outputs_lists for output in outputs]
+    for output in non_cycle_outputs:
+        assert output.cache is True
