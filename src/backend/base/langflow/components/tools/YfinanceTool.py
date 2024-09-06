8ee6816cb24247975f3ffee2d71a94710ff04d07
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-06 09:37:43 -0300

    Refactor YfinanceToolComponent to inherit from LCToolComponent and remove unused outputs

--- src/backend/base/langflow/components/tools/YfinanceTool.py
+++ src/backend/base/langflow/components/tools/YfinanceTool.py
@@ -2,19 +2,14 @@ from typing import cast
 
 from langchain_community.tools.yahoo_finance_news import YahooFinanceNewsTool
 
-from langflow.custom import Component
+from langflow.base.langchain_utilities.model import LCToolComponent
 from langflow.field_typing import Tool
-from langflow.io import Output
 
 
-class YfinanceToolComponent(Component):
+class YfinanceToolComponent(LCToolComponent):
     display_name = "Yahoo Finance News Tool"
     description = "Tool for interacting with Yahoo Finance News."
     name = "YFinanceTool"
-
-    outputs = [
-        Output(display_name="Tool", name="tool", method="build_tool"),
-    ]
 
     def build_tool(self) -> Tool:
         return cast(Tool, YahooFinanceNewsTool())
