1d54fdbbfaba8556496c45bf588300fdcde4ccad
anovazzi1 <otavio2204@gmail.com>
2024-09-09 15:10:47 -0300

    Refactor IOModal component to remove redundant code

--- src/frontend/src/modals/IOModal/index.tsx
+++ src/frontend/src/modals/IOModal/index.tsx
@@ -170,7 +170,6 @@ export default function IOModal({
         sessions.add(row.session_id);
       });
     setSessions(Array.from(sessions));
-    setvisibleSessions(Array.from(sessions));
   }, [messages]);
 
   useEffect(() => {
