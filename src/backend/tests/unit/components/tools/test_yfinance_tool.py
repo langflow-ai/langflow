caf739fad040cbabf61268f8694666e204571040
italojohnny <italojohnnydosanjos@gmail.com>
2024-09-09 19:52:05 -0300

    test: fix test units

--- src/backend/tests/unit/components/tools/test_yfinance_tool.py
+++ src/backend/tests/unit/components/tools/test_yfinance_tool.py
@@ -1,6 +1,6 @@
 import pytest
 
-from langflow.components.tools.YfinanceTool import YfinanceToolComponent
+from langflow.components.tools.YfinanceTool import YahooFinanceToolComponent
 from langflow.custom.custom_component.component import Component
 from langflow.custom.utils import build_custom_component_template
 
@@ -11,11 +11,11 @@ def client():
 
 
 def test_yfinance_tool_template():
-    yf_tool = YfinanceToolComponent()
+    yf_tool = YahooFinanceToolComponent()
     component = Component(_code=yf_tool._code)
     frontend_node, _ = build_custom_component_template(component)
     assert "outputs" in frontend_node
     output_names = [output["name"] for output in frontend_node["outputs"]]
     assert "api_run_model" in output_names
-    assert "tool" in output_names
+    assert "api_build_tool" in output_names
     assert all(output["types"] != [] for output in frontend_node["outputs"])
