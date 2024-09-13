6b7b9db19deab6c27fe090a8f5cb688054a2d7b8
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-09-13 14:29:19 -0300

    refactor: add custom banner and refactor pagelayout to include it (#3803)
    * Added custom banner
    
    * Refactored pageLayout to include custom banner

--- src/frontend/src/components/pageLayout/index.tsx
+++ src/frontend/src/components/pageLayout/index.tsx
@@ -1,3 +1,4 @@
+import { CustomBanner } from "@/customization/components/custom-banner";
 import { Separator } from "../ui/separator";
 
 export default function PageLayout({
@@ -14,19 +15,22 @@ export default function PageLayout({
   betaIcon?: boolean;
 }) {
   return (
-    <div className="flex h-full w-full flex-col justify-between overflow-auto bg-background px-16">
-      <div className="flex w-full items-center justify-between gap-4 space-y-0.5 py-8 pb-2">
-        <div className="flex w-full flex-col">
-          <h2
-            className="text-2xl font-bold tracking-tight"
-            data-testid="mainpage_title"
-          >
-            {title}
-            {betaIcon && <span className="store-beta-icon">BETA</span>}
-          </h2>
-          <p className="text-muted-foreground">{description}</p>
+    <div className="flex h-full w-full flex-col justify-between overflow-auto bg-background px-16 pt-6">
+      <div className="flex flex-col gap-4">
+        <CustomBanner />
+        <div className="flex w-full items-center justify-between gap-4 space-y-0.5 py-2">
+          <div className="flex w-full flex-col">
+            <h2
+              className="text-2xl font-bold tracking-tight"
+              data-testid="mainpage_title"
+            >
+              {title}
+              {betaIcon && <span className="store-beta-icon">BETA</span>}
+            </h2>
+            <p className="text-muted-foreground">{description}</p>
+          </div>
+          <div className="flex-shrink-0">{button && button}</div>
         </div>
-        <div className="flex-shrink-0">{button && button}</div>
       </div>
       <Separator className="my-6 flex" />
       {children}
