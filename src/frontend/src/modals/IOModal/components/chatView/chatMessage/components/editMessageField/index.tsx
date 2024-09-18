de4f3fc78585cbc36c7994270ce015d10c074ca9
anovazzi1 <otavio2204@gmail.com>
2024-09-18 15:49:55 -0300

    create edit message component for chat view

---
+++ src/frontend/src/modals/IOModal/components/chatView/chatMessage/components/editMessageField/index.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useRef, useState } from "react";
+
+export default function EditMessageField({
+    message: initialMessage,
+    onEdit,
+    onCancel,
+}: {
+    message: string;
+    onEdit: (message: string) => void;
+    onCancel: () => void;
+}) {
+    const [message, setMessage] = useState(initialMessage);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const adjustTextareaHeight = () => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight+3}px`;
+        }
+    };
+    useEffect(() => {
+        adjustTextareaHeight();
+    }, []);
+
+    return (
+        <div className="flex flex-col w-full h-fit">
+            <Textarea ref={textareaRef} className="h-mx-full" onBlur={()=>onCancel()}  value={message} autoFocus={true} onChange={
+                (e) => {
+                    setMessage(e.target.value);
+                }
+            }/>
+            <div>
+                <Button onClick={
+                    () => {
+                        onEdit(message);
+                    }
+                } className="btn btn-primary mt-2">Save</Button>
+                <Button
+                onClick={onCancel}
+                 className="btn btn-secondary mt-2">Cancel</Button>
+            </div>
+        </div>
+    );
+}
