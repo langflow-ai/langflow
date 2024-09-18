bad009c0fab91dfdde20872995f65da9b551d994
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-09-18 10:30:49 -0300

    fix: update project description, update alerts animation, fix delete and duplicate loading (#3835)
    * Deleted "personal" from manage your projects
    
    * Removed animations from alerts on the dropdown
    
    * Changed useDeleteFlow to return isPending as well
    
    * Added delete loading to components and added filter of components not present in "flows" for deletion
    
    * added isDuplicating as a isMutating with the postAddFlow

--- src/frontend/src/alerts/alertDropDown/components/singleAlertComponent/index.tsx
+++ src/frontend/src/alerts/alertDropDown/components/singleAlertComponent/index.tsx
@@ -1,5 +1,4 @@
 import { CustomLink } from "@/customization/components/custom-link";
-import { Transition } from "@headlessui/react";
 import { useState } from "react";
 import IconComponent from "../../../../components/genericIconComponent";
 import { SingleAlertComponentType } from "../../../../types/alerts";
@@ -11,161 +10,147 @@ export default function SingleAlert({
   const [show, setShow] = useState(true);
   const type = dropItem.type;
 
-  return (
-    <Transition
-      className="noflow nowheel nopan nodelete nodrag relative"
-      show={show}
-      appear={true}
-      enter="transition-transform duration-500 ease-out"
-      enterFrom={"transform translate-x-[-100%]"}
-      enterTo={"transform translate-x-0"}
-      leave="transition-transform duration-500 ease-in"
-      leaveFrom={"transform translate-x-0"}
-      leaveTo={"transform translate-x-[-100%]"}
+  return type === "error" ? (
+    <div
+      className="mx-2 mb-2 flex rounded-md bg-error-background p-3"
+      key={dropItem.id}
     >
-      {type === "error" ? (
-        <div
-          className="mx-2 mb-2 flex rounded-md bg-error-background p-3"
-          key={dropItem.id}
-        >
-          <div className="flex-shrink-0">
+      <div className="flex-shrink-0">
+        <IconComponent
+          name="XCircle"
+          className="h-5 w-5 text-status-red"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="ml-3">
+        <h3 className="text-sm font-medium text-error-foreground word-break-break-word">
+          {dropItem.title}
+        </h3>
+        {dropItem.list ? (
+          <div className="mt-2 text-sm text-error-foreground">
+            <ul className="list-disc space-y-1 pl-5">
+              {dropItem.list.map((item, idx) => (
+                <li className="word-break-break-word" key={idx}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="ml-auto pl-3">
+        <div className="-mx-1.5 -my-1.5">
+          <button
+            type="button"
+            onClick={() => {
+              setShow(false);
+              setTimeout(() => {
+                removeAlert(dropItem.id);
+              }, 500);
+            }}
+            className="inline-flex rounded-md p-1.5 text-status-red"
+          >
+            <span className="sr-only">Dismiss</span>
             <IconComponent
-              name="XCircle"
-              className="h-5 w-5 text-status-red"
+              name="X"
+              className="h-4 w-4 text-error-foreground"
               aria-hidden="true"
             />
-          </div>
-          <div className="ml-3">
-            <h3 className="text-sm font-medium text-error-foreground word-break-break-word">
-              {dropItem.title}
-            </h3>
-            {dropItem.list ? (
-              <div className="mt-2 text-sm text-error-foreground">
-                <ul className="list-disc space-y-1 pl-5">
-                  {dropItem.list.map((item, idx) => (
-                    <li className="word-break-break-word" key={idx}>
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              <></>
-            )}
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              <button
-                type="button"
-                onClick={() => {
-                  setShow(false);
-                  setTimeout(() => {
-                    removeAlert(dropItem.id);
-                  }, 500);
-                }}
-                className="inline-flex rounded-md p-1.5 text-status-red"
-              >
-                <span className="sr-only">Dismiss</span>
-                <IconComponent
-                  name="X"
-                  className="h-4 w-4 text-error-foreground"
-                  aria-hidden="true"
-                />
-              </button>
-            </div>
-          </div>
+          </button>
         </div>
-      ) : type === "notice" ? (
-        <div
-          className="mx-2 mb-2 flex rounded-md bg-info-background p-3"
-          key={dropItem.id}
-        >
-          <div className="flex-shrink-0 cursor-help">
+      </div>
+    </div>
+  ) : type === "notice" ? (
+    <div
+      className="mx-2 mb-2 flex rounded-md bg-info-background p-3"
+      key={dropItem.id}
+    >
+      <div className="flex-shrink-0 cursor-help">
+        <IconComponent
+          name="Info"
+          className="h-5 w-5 text-status-blue"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="ml-3 flex-1 md:flex md:justify-between">
+        <p className="text-sm font-medium text-info-foreground">
+          {dropItem.title}
+        </p>
+        <p className="mt-3 text-sm md:ml-6 md:mt-0">
+          {dropItem.link ? (
+            <CustomLink
+              to={dropItem.link}
+              className="whitespace-nowrap font-medium text-info-foreground hover:text-accent-foreground"
+            >
+              Details
+            </CustomLink>
+          ) : (
+            <></>
+          )}
+        </p>
+      </div>
+      <div className="ml-auto pl-3">
+        <div className="-mx-1.5 -my-1.5">
+          <button
+            type="button"
+            onClick={() => {
+              setShow(false);
+              setTimeout(() => {
+                removeAlert(dropItem.id);
+              }, 500);
+            }}
+            className="inline-flex rounded-md p-1.5 text-info-foreground"
+          >
+            <span className="sr-only">Dismiss</span>
             <IconComponent
-              name="Info"
-              className="h-5 w-5 text-status-blue"
+              name="X"
+              className="h-4 w-4 text-info-foreground"
               aria-hidden="true"
             />
-          </div>
-          <div className="ml-3 flex-1 md:flex md:justify-between">
-            <p className="text-sm font-medium text-info-foreground">
-              {dropItem.title}
-            </p>
-            <p className="mt-3 text-sm md:ml-6 md:mt-0">
-              {dropItem.link ? (
-                <CustomLink
-                  to={dropItem.link}
-                  className="whitespace-nowrap font-medium text-info-foreground hover:text-accent-foreground"
-                >
-                  Details
-                </CustomLink>
-              ) : (
-                <></>
-              )}
-            </p>
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              <button
-                type="button"
-                onClick={() => {
-                  setShow(false);
-                  setTimeout(() => {
-                    removeAlert(dropItem.id);
-                  }, 500);
-                }}
-                className="inline-flex rounded-md p-1.5 text-info-foreground"
-              >
-                <span className="sr-only">Dismiss</span>
-                <IconComponent
-                  name="X"
-                  className="h-4 w-4 text-info-foreground"
-                  aria-hidden="true"
-                />
-              </button>
-            </div>
-          </div>
+          </button>
         </div>
-      ) : (
-        <div
-          className="mx-2 mb-2 flex rounded-md bg-success-background p-3"
-          key={dropItem.id}
-        >
-          <div className="flex-shrink-0">
+      </div>
+    </div>
+  ) : (
+    <div
+      className="mx-2 mb-2 flex rounded-md bg-success-background p-3"
+      key={dropItem.id}
+    >
+      <div className="flex-shrink-0">
+        <IconComponent
+          name="CheckCircle2"
+          className="h-5 w-5 text-status-green"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="ml-3">
+        <p className="text-sm font-medium text-success-foreground">
+          {dropItem.title}
+        </p>
+      </div>
+      <div className="ml-auto pl-3">
+        <div className="-mx-1.5 -my-1.5">
+          <button
+            type="button"
+            onClick={() => {
+              setShow(false);
+              setTimeout(() => {
+                removeAlert(dropItem.id);
+              }, 500);
+            }}
+            className="inline-flex rounded-md p-1.5 text-status-green"
+          >
+            <span className="sr-only">Dismiss</span>
             <IconComponent
-              name="CheckCircle2"
-              className="h-5 w-5 text-status-green"
+              name="X"
+              className="h-4 w-4 text-success-foreground"
               aria-hidden="true"
             />
-          </div>
-          <div className="ml-3">
-            <p className="text-sm font-medium text-success-foreground">
-              {dropItem.title}
-            </p>
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              <button
-                type="button"
-                onClick={() => {
-                  setShow(false);
-                  setTimeout(() => {
-                    removeAlert(dropItem.id);
-                  }, 500);
-                }}
-                className="inline-flex rounded-md p-1.5 text-status-green"
-              >
-                <span className="sr-only">Dismiss</span>
-                <IconComponent
-                  name="X"
-                  className="h-4 w-4 text-success-foreground"
-                  aria-hidden="true"
-                />
-              </button>
-            </div>
-          </div>
+          </button>
         </div>
-      )}
-    </Transition>
+      </div>
+    </div>
   );
 }
