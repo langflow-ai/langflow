0ba7d8b72669ec05fb5b4bcce41e274aa105a685
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-24 18:21:31 -0300

    Add refetch interval and URL helper to useGetTasks query function

--- src/frontend/src/controllers/API/queries/tasks/use-get-tasks.ts
+++ src/frontend/src/controllers/API/queries/tasks/use-get-tasks.ts
@@ -1,21 +1,25 @@
 import { Task } from "@/types/Task";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
 
 interface GetTasksParams {
   skip?: number;
   limit?: number;
+  refetchInterval?: number;
 }
 
 export function useGetTasks(params: GetTasksParams = {}) {
+  const { skip = 0, limit = 100, refetchInterval } = params;
+
   return useQuery({
-    queryKey: ["tasks", params],
+    queryKey: ["tasks", { skip, limit }],
     queryFn: async () => {
-      const { skip = 0, limit = 100 } = params;
-      const response = await api.get<Task[]>(
-        `/tasks?skip=${skip}&limit=${limit}`,
-      );
+      const response = await api.get<Task[]>(`${getURL("TASKS")}/`, {
+        params: { skip, limit },
+      });
       return response.data;
     },
+    refetchInterval: refetchInterval,
   });
 }
