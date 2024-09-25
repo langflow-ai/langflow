000cd41507705dfd693436d5a560c8728dba3805
anovazzi1 <otavio2204@gmail.com>
2024-09-25 16:39:02 -0300

    Update BUNDLES_SIDEBAR_FOLDER_NAMES in constants.ts

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -729,7 +729,7 @@ export const PRIORITY_SIDEBAR_ORDER = [
   "embeddings",
 ];
 
-export const BUNDLES_SIDEBAR_FOLDER_NAMES = ["notion", "Notion"];
+export const BUNDLES_SIDEBAR_FOLDER_NAMES = ["notion", "Notion","AssemblyAI","assemblyai"];
 
 export const AUTHORIZED_DUPLICATE_REQUESTS = [
   "/health",
