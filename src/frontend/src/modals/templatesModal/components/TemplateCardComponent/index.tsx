c67a9fc3dd29d7291b168933c79bf9f4e3da6d2a
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-09-26 17:57:39 -0300

    added template card

---
+++ src/frontend/src/modals/templatesModal/components/TemplateCardComponent/index.tsx
@@ -0,0 +1,56 @@
+import ForwardedIconComponent from "@/components/genericIconComponent";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+interface CardData {
+  bgImage: string;
+  spiralImage: string;
+  icon: string;
+  category: string;
+  title: string;
+  description: string;
+  onClick: () => void;
+}
+
+const TemplateCard: React.FC<CardData> = ({
+  bgImage,
+  spiralImage,
+  icon,
+  category,
+  title,
+  description,
+  onClick,
+}) => {
+  return (
+    <Button
+      unstyled
+      onClick={onClick}
+      className="group relative h-full w-full overflow-hidden rounded-3xl border text-left"
+    >
+      <img
+        src={bgImage}
+        alt={`${title} Background`}
+        className="absolute inset-2 h-[calc(100%-16px)] w-[calc(100%-16px)] rounded-2xl object-cover"
+      />
+      <div className="absolute inset-2 h-[calc(100%-16px)] w-[calc(100%-16px)] overflow-hidden rounded-2xl">
+        <img
+          src={spiralImage}
+          alt={`${title} Spiral`}
+          className="h-full w-full object-cover transition-all duration-300 group-hover:scale-105"
+        />
+      </div>
+      <div className="absolute inset-2 flex h-[calc(100%-16px)] w-[calc(100%-16px)] flex-col items-start gap-4 rounded-2xl p-4 py-6 text-white">
+        <div className="flex items-center gap-2 text-white mix-blend-overlay">
+          <ForwardedIconComponent name={icon} className="h-4 w-4" />
+          <span className="font-mono text-xs font-semibold uppercase tracking-wider">
+            {category}
+          </span>
+        </div>
+        <h3 className="text-xl font-bold">{title}</h3>
+        <p className="text-xs font-medium opacity-90">{description}</p>
+      </div>
+    </Button>
+  );
+};
+
+export default TemplateCard;
