f863a04f19ed5168811a27c3679cfca7c590ebfc
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-09-26 16:10:29 -0300

    added a Nav component to show the sidebar categories

---
+++ src/frontend/src/modals/templatesModal/components/navComponent/index.tsx
@@ -0,0 +1,41 @@
+import ForwardedIconComponent from "@/components/genericIconComponent";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/utils/utils";
+
+interface NavProps {
+  links: {
+    id: string;
+    title: string;
+    label?: string;
+    icon: string;
+  }[];
+  currentTab: string;
+  onClick?: (id: string) => void;
+}
+
+export function Nav({ links, currentTab, onClick }: NavProps) {
+  return (
+    <div className="group flex flex-col gap-4">
+      <nav className="grid">
+        {links.map((link, index) => (
+          <Button
+            variant={link.id === currentTab ? "menu-active" : "menu"}
+            size="sm"
+            key={index}
+            onClick={() => onClick?.(link.id)}
+            className="group"
+          >
+            <ForwardedIconComponent
+              name={link.icon}
+              className={cn(
+                "mr-2 h-4 w-4 stroke-2 text-muted-foreground",
+                link.id === currentTab && "text-pink-400",
+              )}
+            />
+            <span className="flex-1 text-left text-primary">{link.title}</span>
+          </Button>
+        ))}
+      </nav>
+    </div>
+  );
+}
