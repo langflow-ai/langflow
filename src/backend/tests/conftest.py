9e74a539e80a03c248dfb015e3b66fe2a8e3cc0b
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-26 10:20:49 -0300

    Add fixture for creating flow component in tests

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING
 
 import orjson
 import pytest
+from base.langflow.components.inputs.ChatInput import ChatInput
 from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
@@ -418,6 +419,19 @@ def added_webhook_test(client, json_webhook_test, logged_in_headers):
     assert response.json()["data"] == webhook_test.data
     yield response.json()
     client.delete(f"api/v1/flows/{response.json()['id']}", headers=logged_in_headers)
+
+
+@pytest.fixture
+def flow_component(client: TestClient, logged_in_headers):
+    chat_input = ChatInput()
+    graph = Graph(start=chat_input, end=chat_input)
+    graph_dict = graph.dump(name="Chat Input Component")
+    flow = FlowCreate(**graph_dict)
+    response = client.post("api/v1/flows/", json=flow.model_dump(), headers=logged_in_headers)
+    assert response.status_code == 201
+    assert response.json()["name"] == flow.name
+    assert response.json()["data"] == flow.data
+    return response.json()
 
 
 @pytest.fixture
