35f059e8d6adc76cb748d4222a0b78cb9ad89c12
anovazzi1 <otavio2204@gmail.com>
2024-10-02 14:17:21 -0300

    refactor: Update InputListComponent to use InputProps in parameterRenderComponent
    Refactor the InputListComponent in parameterRenderComponent to use the InputProps interface for better type checking and consistency. This refactor improves code organization and maintainability.

--- src/frontend/src/components/parameterRenderComponent/components/inputListComponent/index.tsx
+++ src/frontend/src/components/parameterRenderComponent/components/inputListComponent/index.tsx
@@ -1,24 +1,23 @@
 import { useEffect } from "react";
-import { InputListComponentType } from "../../../../types/components";
 
 import _ from "lodash";
 import { classNames, cn } from "../../../../utils/utils";
 import IconComponent from "../../../genericIconComponent";
 import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
+import { InputListComponentType, InputProps } from "../../types";
 
 export default function InputListComponent({
-  value,
-  onChange,
+  value=[""],
+  handleOnNewValue,
   disabled,
   editNode = false,
   componentName,
-  playgroundDisabled,
   id,
-}: InputListComponentType): JSX.Element {
+}: InputProps<string[], InputListComponentType>): JSX.Element {
   useEffect(() => {
     if (disabled && value.length > 0 && value[0] !== "") {
-      onChange([""], undefined, true);
+      handleOnNewValue({value: [""]}, {skipSnapshot: true});
     }
   }, [disabled]);
 
@@ -32,26 +31,26 @@ export default function InputListComponent({
   const handleInputChange = (index, newValue) => {
     const newInputList = _.cloneDeep(value);
     newInputList[index] = newValue;
-    onChange(newInputList);
+    handleOnNewValue({value: newInputList});
   };
 
   const addNewInput = (e) => {
     e.preventDefault();
     const newInputList = _.cloneDeep(value);
     newInputList.push("");
-    onChange(newInputList);
+    handleOnNewValue({value: newInputList});
   };
 
   const removeInput = (index, e) => {
     e.preventDefault();
     const newInputList = _.cloneDeep(value);
     newInputList.splice(index, 1);
-    onChange(newInputList);
+    handleOnNewValue({value: newInputList});
   };
 
   const getButtonClassName = () =>
     classNames(
-      disabled || playgroundDisabled
+      disabled
         ? "cursor-not-allowed text-muted-foreground"
         : "text-primary hover:text-accent-foreground",
     );
@@ -69,7 +68,7 @@ export default function InputListComponent({
       {value.map((singleValue, index) => (
         <div key={index} className="flex w-full gap-3">
           <Input
-            disabled={disabled || playgroundDisabled}
+            disabled={disabled}
             type="text"
             value={singleValue}
             className={editNode ? "input-edit-node" : ""}
@@ -82,7 +81,7 @@ export default function InputListComponent({
             className={getButtonClassName()}
             onClick={index === 0 ? addNewInput : (e) => removeInput(index, e)}
             data-testid={getTestId(index === 0 ? "plus" : "minus", index)}
-            disabled={disabled || playgroundDisabled}
+            disabled={disabled}
           >
             <IconComponent
               name={index === 0 ? "Plus" : "X"}
