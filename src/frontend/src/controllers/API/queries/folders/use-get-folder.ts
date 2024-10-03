957567d24edcc52fc8ed624e10cdefed35d90928
cristhianzl <cristhian.lousa@gmail.com>
2024-10-02 22:33:37 -0300

    ✨ (use-get-folder.ts): Add support for pagination and filtering options in the useGetFolderQuery function to enhance flexibility and customization for fetching folder data.

--- src/frontend/src/controllers/API/queries/folders/use-get-folder.ts
+++ src/frontend/src/controllers/API/queries/folders/use-get-folder.ts
@@ -1,30 +1,50 @@
-import { FolderType } from "@/pages/MainPage/entities";
+import { FolderType, PaginatedFolderType } from "@/pages/MainPage/entities";
 import { useQueryFunctionType } from "@/types/api";
+import { UseQueryOptions } from "@tanstack/react-query";
 import { api } from "../../api";
 import { getURL } from "../../helpers/constants";
 import { UseRequestProcessor } from "../../services/request-processor";
 
 interface IGetFolder {
   id: string;
+  page?: number;
+  size?: number;
+  is_component?: boolean;
+  is_flow?: boolean;
+  search?: string;
 }
 
-export const useGetFolderQuery: useQueryFunctionType<IGetFolder, FolderType> = (
-  params,
-  options,
-) => {
+const addQueryParams = (url: string, params: IGetFolder): string => {
+  const queryParams = new URLSearchParams();
+  if (params.page) queryParams.append("page", params.page.toString());
+  if (params.size) queryParams.append("size", params.size.toString());
+  if (params.is_component)
+    queryParams.append("is_component", params.is_component.toString());
+  if (params.is_flow) queryParams.append("is_flow", params.is_flow.toString());
+  if (params.search) queryParams.append("search", params.search);
+  const queryString = queryParams.toString();
+  return queryString ? `${url}?${queryString}` : url;
+};
+
+export const useGetFolderQuery: useQueryFunctionType<
+  IGetFolder,
+  PaginatedFolderType
+> = (params, options) => {
   const { query } = UseRequestProcessor();
 
-  const getFolderFn = async (): Promise<FolderType> => {
-    const res = await api.get(`${getURL("FOLDERS")}/${params.id}`);
-    const data = res.data;
-
+  const getFolderFn = async (
+    params: IGetFolder,
+  ): Promise<PaginatedFolderType> => {
+    const url = addQueryParams(`${getURL("FOLDERS")}/${params.id}`, params);
+    const { data } = await api.get<PaginatedFolderType>(url);
     return data;
   };
 
   const queryResult = query(
-    ["useGetFolder", { id: params.id }],
-    getFolderFn,
-    options,
+    ["useGetFolder", params],
+    () => getFolderFn(params),
+    options as UseQueryOptions<any, Error, PaginatedFolderType, any>,
   );
+
   return queryResult;
 };
