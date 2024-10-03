793c6d61e940eb6322df2db683dba033562d736f
cristhianzl <cristhian.lousa@gmail.com>
2024-10-02 22:33:31 -0300

    ✨ (use-get-refresh-flows.ts): introduce new functionality to fetch and process flows with query parameters for components_only, get_all, folder_id, remove_example_flows, page, and size. This allows for more flexible and specific retrieval of flow data.

--- src/frontend/src/controllers/API/queries/flows/use-get-refresh-flows.ts
+++ src/frontend/src/controllers/API/queries/flows/use-get-refresh-flows.ts
@@ -1,67 +1,90 @@
 import useAlertStore from "@/stores/alertStore";
 import useFlowsManagerStore from "@/stores/flowsManagerStore";
-import { useFolderStore } from "@/stores/foldersStore";
 import { useTypesStore } from "@/stores/typesStore";
 import { useMutationFunctionType } from "@/types/api";
-import { FlowType } from "@/types/flow";
+import { FlowType, PaginatedFlowsType } from "@/types/flow";
 import {
   extractFieldsFromComponenents,
   processFlows,
 } from "@/utils/reactflowUtils";
-import { UseMutationResult } from "@tanstack/react-query";
+import { UseMutationOptions } from "@tanstack/react-query";
 import { api } from "../../api";
 import { getURL } from "../../helpers/constants";
 import { UseRequestProcessor } from "../../services/request-processor";
 
+interface GetFlowsParams {
+  components_only?: boolean;
+  get_all?: boolean;
+  folder_id?: string;
+  remove_example_flows?: boolean;
+  page?: number;
+  size?: number;
+}
+
+const addQueryParams = (url: string, params: GetFlowsParams): string => {
+  const queryParams = new URLSearchParams();
+  if (params.components_only) queryParams.append("components_only", "true");
+  if (params.get_all) queryParams.append("get_all", "true");
+  if (params.folder_id) queryParams.append("folder_id", params.folder_id);
+  if (params.remove_example_flows)
+    queryParams.append("remove_example_flows", "true");
+  if (params.page) queryParams.append("page", params.page.toString());
+  if (params.size) queryParams.append("size", params.size.toString());
+  const queryString = queryParams.toString();
+  return queryString ? `${url}?${queryString}` : url;
+};
+
 export const useGetRefreshFlows: useMutationFunctionType<
   undefined,
-  undefined
-> = (options?) => {
+  GetFlowsParams
+> = (options) => {
   const { mutate } = UseRequestProcessor();
 
-  const setExamples = useFlowsManagerStore((state) => state.setExamples);
   const setFlows = useFlowsManagerStore((state) => state.setFlows);
   const setErrorData = useAlertStore((state) => state.setErrorData);
-  const starterProjectId = useFolderStore((state) => state.starterProjectId);
 
-  const getRefreshFlowsFn = async (): Promise<void> => {
+  const getRefreshFlowsFn = async (
+    params: GetFlowsParams,
+  ): Promise<FlowType[] | PaginatedFlowsType> => {
+    const url = addQueryParams(`${getURL("FLOWS")}/`, params);
+    const { data } = await api.get<FlowType[]>(url);
+    return data;
+  };
+
+  const mutationFn = async (
+    params?: GetFlowsParams,
+  ): Promise<FlowType[] | PaginatedFlowsType> => {
     try {
+      const dbDataFlows = await getRefreshFlowsFn(params!);
+
+      if (dbDataFlows) {
+        const { data, flows } = processFlows(
+          Array.isArray(dbDataFlows) ? dbDataFlows : dbDataFlows.items,
+        );
+
+        setFlows(flows);
+        useTypesStore.setState((state) => ({
+          data: { ...state.data, ["saved_components"]: data },
+          ComponentFields: extractFieldsFromComponenents({
+            ...state.data,
+            ["saved_components"]: data,
+          }),
+        }));
+
+        return flows;
+      }
+      return [];
     } catch (e) {
       setErrorData({
         title: "Could not load flows from database",
       });
       throw e;
     }
-    const response = await api.get<FlowType[]>(`${getURL("FLOWS")}/`);
-    const dbData = response.data;
-    if (dbData) {
-      const { data, flows } = processFlows(dbData);
-      const examples = flows.filter(
-        (flow) => flow.folder_id === starterProjectId,
-      );
-      setExamples(examples);
-
-      const flowsWithoutStarterFolder = flows.filter(
-        (flow) => flow.folder_id !== starterProjectId,
-      );
-
-      setFlows(flowsWithoutStarterFolder);
-      useTypesStore.setState((state) => ({
-        data: { ...state.data, ["saved_components"]: data },
-        ComponentFields: extractFieldsFromComponenents({
-          ...state.data,
-          ["saved_components"]: data,
-        }),
-      }));
-      return;
-    }
   };
 
-  const mutation: UseMutationResult<void, any, undefined> = mutate(
-    ["useGetRefreshFlows"],
-    getRefreshFlowsFn,
-    options,
-  );
+  const mutation = mutate(["useGetRefreshFlows"], mutationFn, {
+    ...(options as UseMutationOptions<any, any, void, unknown>),
+  });
 
   return mutation;
 };
