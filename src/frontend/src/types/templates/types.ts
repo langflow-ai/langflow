25a3ed5c818d3dccae46bd04afbd8b5cd18e10c4
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-10-03 18:24:21 -0300

    Added types

---
+++ src/frontend/src/types/templates/types.ts
@@ -0,0 +1,48 @@
+import { FlowType } from "@/types/flow";
+
+export interface NavItem {
+  title: string;
+  icon: string;
+  id: string;
+}
+
+export interface Category {
+  title: string;
+  items: NavItem[];
+}
+
+export interface CardData {
+  bgImage: string;
+  spiralImage: string;
+  icon: string;
+  category: string;
+  title: string;
+  description: string;
+  flow: FlowType | undefined;
+}
+
+export interface TemplateCategoryProps {
+  currentTab: NavItem;
+  examples: any[];
+  onCardClick: (example: any) => void;
+}
+
+export interface TemplateContentProps {
+  currentTab: string;
+  categories: NavItem[];
+}
+
+export interface TemplateCardComponentProps {
+  example: {
+    name: string;
+    description: string;
+    icon?: string;
+  };
+  onClick: () => void;
+}
+
+export interface NavProps {
+  links: NavItem[];
+  currentTab: string;
+  onClick?: (id: string) => void;
+}
