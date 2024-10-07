774d7973ee9b8e8099099a5f65bbe36410586a60
anovazzi1 <otavio2204@gmail.com>
2024-10-07 13:11:44 -0300

    add skip snapshot

--- src/frontend/src/components/parameterRenderComponent/components/dictComponent/index.tsx
+++ src/frontend/src/components/parameterRenderComponent/components/dictComponent/index.tsx
@@ -15,7 +15,7 @@ export default function DictComponent({
 }: InputProps<object | object[] | string>): JSX.Element {
   useEffect(() => {
     if (disabled) {
-      handleOnNewValue({ value: {} });
+      handleOnNewValue({ value: {} },{ skipSnapshot: true });
     }
   }, [disabled]);
 
