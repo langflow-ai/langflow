00936f5b1a509ba3235b38a6d7068cc676dbedd2
anovazzi1 <otavio2204@gmail.com>
2024-10-07 18:04:07 -0300

    fix: Add useUpdateNodeInternals hook to handle node class changes (#4058)
    * fix: Add useUpdateNodeInternals hook to handle node class changes
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>

--- src/frontend/src/CustomNodes/hooks/use-handle-node-class.tsx
+++ src/frontend/src/CustomNodes/hooks/use-handle-node-class.tsx
@@ -1,6 +1,7 @@
 import useFlowStore from "@/stores/flowStore";
 import { NodeType } from "@/types/flow";
 import { cloneDeep } from "lodash";
+import { useUpdateNodeInternals } from "reactflow";
 
 const useHandleNodeClass = (
   nodeId: string,
@@ -10,6 +11,7 @@ const useHandleNodeClass = (
   ) => void,
 ) => {
   const setNode = setMyNode ?? useFlowStore((state) => state.setNode);
+  const updateNodeInternals = useUpdateNodeInternals();
 
   const handleNodeClass = (newNodeClass, type?: string) => {
     setNode(nodeId, (oldNode) => {
@@ -22,6 +24,8 @@ const useHandleNodeClass = (
       if (type) {
         newNode.data.type = type;
       }
+
+      updateNodeInternals(nodeId);
 
       return newNode;
     });
