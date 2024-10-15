f3d912c1e085a787066e95c033e63fb7692ce4d6
phact <estevezsebastian@gmail.com>
2024-10-14 23:42:29 -0400

    do not override openai client outside of assistants

--- src/backend/base/langflow/base/astra_assistants/util.py
+++ src/backend/base/langflow/base/astra_assistants/util.py
@@ -7,7 +7,8 @@ import threading
 
 import astra_assistants.tools as astra_assistants_tools
 import requests
-from astra_assistants import OpenAI, patch
+from astra_assistants import OpenAI as AstraOpenAI
+from astra_assistants import patch
 from astra_assistants.tools.tool_interface import ToolInterface
 
 client_lock = threading.Lock()
@@ -18,7 +19,7 @@ def get_patched_openai_client(shared_component_cache):
     os.environ["ASTRA_ASSISTANTS_QUIET"] = "true"
     client = shared_component_cache.get("client")
     if client is None:
-        client = patch(OpenAI())
+        client = patch(AstraOpenAI())
         shared_component_cache.set("client", client)
     return client
 
