604e2d3b6db11e60fa1376f86700f51ec2652096
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2024-10-15 22:38:26 -0300

    fix: StoreMessage.py to prevent errors on Component  (#4162)
    * 📝 (StoreMessage.py): Update input types in StoreMessageComponent to include StrInput for sender, sender_name, and session_id to improve data handling and consistency.
    
    * 📝 (StoreMessage.py): Update imports to use the correct MessageTextInput class from langflow.inputs.inputs module for sender, sender_name, and session_id inputs in StoreMessageComponent class. This change ensures consistency and improves code readability.

--- src/backend/base/langflow/components/helpers/StoreMessage.py
+++ src/backend/base/langflow/components/helpers/StoreMessage.py
@@ -1,5 +1,6 @@
 from langflow.custom import Component
 from langflow.inputs import HandleInput, MessageInput
+from langflow.inputs.inputs import MessageTextInput
 from langflow.memory import get_messages, store_message
 from langflow.schema.message import Message
 from langflow.template import Output
@@ -20,20 +21,20 @@ class StoreMessageComponent(Component):
             input_types=["BaseChatMessageHistory"],
             info="The external memory to store the message. If empty, it will use the Langflow tables.",
         ),
-        MessageInput(
+        MessageTextInput(
             name="sender",
             display_name="Sender",
             info="The sender of the message. Might be Machine or User. "
             "If empty, the current sender parameter will be used.",
             advanced=True,
         ),
-        MessageInput(
+        MessageTextInput(
             name="sender_name",
             display_name="Sender Name",
             info="The name of the sender. Might be AI or User. If empty, the current sender parameter will be used.",
             advanced=True,
         ),
-        MessageInput(
+        MessageTextInput(
             name="session_id",
             display_name="Session ID",
             info="The session ID of the chat. If empty, the current session ID parameter will be used.",
