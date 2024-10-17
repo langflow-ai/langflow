5af8184d12564cdfe92ff40b82adaabcaf496473
cristhianzl <cristhian.lousa@gmail.com>
2024-10-17 15:31:35 -0300

    add inter font to project

--- src/frontend/tailwind.config.mjs
+++ src/frontend/tailwind.config.mjs
@@ -33,9 +33,12 @@ const config = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
-      },
+      }
     },
     extend: {
+            fontFamily: {
+              sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+            },
       keyframes: {
         wiggle: {
           "0%, 100%": { transform: "scale(100%)" },
