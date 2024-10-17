6cc8b0709fbe6536630c2bd578a2c5bd7f827c7f
Christophe Bornet <cbornet@hotmail.com>
2024-10-17 20:21:56 +0200

    ref: Add missing __init__.py files in tests (#4180)
    Add missing __init__.py files in tests

--- src/backend/tests/integration/components/output_parsers/test_output_parser.py
+++ src/backend/tests/integration/components/output_parsers/test_output_parser.py
@@ -23,7 +23,7 @@ async def test_csv_output_parser_openai():
     prompt_handler = ComponentInputHandle(
         clazz=PromptComponent,
         inputs={
-            "template": "List the first five positive integers.\n\n{format_instructions}",  # noqa: RUF027
+            "template": "List the first five positive integers.\n\n{format_instructions}",
             "format_instructions": format_instructions,
         },
         output_name="prompt",
