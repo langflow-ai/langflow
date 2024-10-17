08083ec90b418aee06c5b274c9f85ae627f1ac0f
Marcelo Nunes Alves <mnaweb@gmail.com>
2024-10-17 16:40:32 -0300

    fix: SQLDatabase has no attribute set_event_manager

--- src/backend/base/langflow/components/langchain_utilities/sql_database.py
+++ src/backend/base/langflow/components/langchain_utilities/sql_database.py
@@ -1,27 +1,33 @@
 from langchain_community.utilities.sql_database import SQLDatabase
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
+from langflow.io import (
+    StrInput,
+    Output,
+)
+from langflow.custom import Component
 
-from langflow.custom import CustomComponent
 
-
-class SQLDatabaseComponent(CustomComponent):
+class SQLDatabaseComponent(Component):
     display_name = "SQLDatabase"
     description = "SQL Database"
     name = "SQLDatabase"
 
-    def build_config(self):
-        return {
-            "uri": {"display_name": "URI", "info": "URI to the database."},
-        }
+    inputs = [
+        StrInput(name="uri", display_name="URI", info="URI to the database.", required=True),
+    ]
+
+    outputs = [
+        Output(display_name="SQLDatabase", name="SQLDatabase", method="build_sqldatabase"),
+    ]
 
     def clean_up_uri(self, uri: str) -> str:
         if uri.startswith("postgres://"):
             uri = uri.replace("postgres://", "postgresql://")
         return uri.strip()
 
-    def build(self, uri: str) -> SQLDatabase:
-        uri = self.clean_up_uri(uri)
+    def build_sqldatabase(self) -> SQLDatabase:
+        uri = self.clean_up_uri(self.uri)
         # Create an engine using SQLAlchemy with StaticPool
         engine = create_engine(uri, poolclass=StaticPool)
         return SQLDatabase(engine)
