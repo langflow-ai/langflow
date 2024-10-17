f2c23d547e5567dea1c5eae16dccbab36f6494a3
cristhianzl <cristhian.lousa@gmail.com>
2024-10-17 18:25:59 -0300

    ✨ (get-input-class-name.ts): Add a new helper function getInputClassName to dynamically generate class names based on input properties for better styling and customization.

---
+++ src/frontend/src/components/inputComponent/components/helpers/get-input-class-name.ts
@@ -0,0 +1,56 @@
+import { cn } from "@/utils/utils";
+
+export const getInputClassName = ({
+  disabled,
+  password,
+  setSelectedOption,
+  selectedOption,
+  pwdVisible,
+  value,
+  editNode,
+  setSelectedOptions,
+  isSelected,
+  areOptionsSelected,
+  nodeStyle,
+  className,
+}) => {
+  const classes = {
+    base: className || "",
+    disabled: disabled
+      ? "pointer-events-none bg-border text-muted-foreground"
+      : "",
+    password:
+      password &&
+      (!setSelectedOption || selectedOption === "") &&
+      !pwdVisible &&
+      value !== ""
+        ? "text-clip password"
+        : "",
+    editNode: editNode ? "input-edit-node" : "",
+    paddingRight: (() => {
+      if (password && (setSelectedOption || setSelectedOptions))
+        return "pr-[62.9px]";
+      if (
+        (!password && (setSelectedOption || setSelectedOptions)) ||
+        (password && !(setSelectedOption || setSelectedOptions))
+      )
+        return "pr-8";
+      return "";
+    })(),
+    selected:
+      isSelected || areOptionsSelected
+        ? "font-jetbrains text-sm font-medium text-foreground"
+        : "",
+    nodeStyle: selectedOption !== "" && nodeStyle ? "text-emerald-hard" : "",
+  };
+
+  return cn(
+    classes.base,
+    classes.disabled,
+    classes.password,
+    classes.editNode,
+    classes.paddingRight,
+    classes.selected,
+    classes.nodeStyle,
+  );
+};
