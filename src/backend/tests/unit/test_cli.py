4238acc53d808d85f563e6e211e15ddc12a0b0ed
Christophe Bornet <cbornet@hotmail.com>
2024-10-19 23:00:47 +0200

    fix: Fix some tests missing client fixture (#4213)
    Fix some tests missing client fixture

--- src/backend/tests/unit/test_cli.py
+++ src/backend/tests/unit/test_cli.py
@@ -25,7 +25,7 @@ def test_components_path(runner, default_settings, tmp_path):
     assert str(temp_dir) in settings_service.settings.components_path
 
 
-@pytest.mark.usefixtures("session")
+@pytest.mark.usefixtures("client")
 def test_superuser(runner):
     result = runner.invoke(app, ["superuser"], input="admin\nadmin\n")
     assert result.exit_code == 0, result.stdout
