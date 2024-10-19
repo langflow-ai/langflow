bdb4106ac0dbc21c6408df1e58636eb35265c13f
Christophe Bornet <cbornet@hotmail.com>
2024-10-19 17:52:04 +0200

    ref: Add ruff rules for naming (N) (#4187)
    Add ruff rules for naming (N)

--- src/backend/base/langflow/custom/tree_visitor.py
+++ src/backend/base/langflow/custom/tree_visitor.py
@@ -1,11 +1,14 @@
 import ast
 
+from typing_extensions import override
+
 
 class RequiredInputsVisitor(ast.NodeVisitor):
     def __init__(self, inputs):
         self.inputs = inputs
         self.required_inputs = set()
 
+    @override
     def visit_Attribute(self, node):
         if isinstance(node.value, ast.Name) and node.value.id == "self" and node.attr in self.inputs:
             self.required_inputs.add(node.attr)
