c04e63e2394d4e2e00d69a0dbafc6e5a01bc0090
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-10-21 15:43:54 -0300

    Added required colors

--- src/frontend/tailwind.config.mjs
+++ src/frontend/tailwind.config.mjs
@@ -145,6 +145,16 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        sidebar: {
+          background: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       borderRadius: {
         lg: `var(--radius)`,
