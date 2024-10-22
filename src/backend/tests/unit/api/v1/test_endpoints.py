14786b6778af820fac77af2f5ef81446ad8d1520
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-22 11:48:23 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_endpoints.py
+++ src/backend/tests/unit/api/v1/test_endpoints.py
@@ -24,3 +24,14 @@ async def test_get_config(client: AsyncClient):
     assert "auto_saving" in result.keys(), "The dictionary must contain a key called 'auto_saving'"
     assert "health_check_max_retries" in result.keys(), "The dictionary must contain a 'health_check_max_retries' key"
     assert "max_file_size_upload" in result.keys(), "The dictionary must contain a key called 'max_file_size_upload'"
+
+
+async def test_get_sidebar_categories(client: AsyncClient):
+    response = await client.get("api/v1/sidebar_categories")
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "categories" in result.keys(), "The dictionary must contain a key called 'categories'"
+    assert len(result["categories"]) > 0, "The categories list must not be empty"
+    assert isinstance(result["categories"], list), "The categories must be a list"
