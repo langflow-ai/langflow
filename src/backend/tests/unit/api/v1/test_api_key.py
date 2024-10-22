7ed3edb4dbf25b960c577d5493cbec34899c3185
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-22 15:55:46 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_api_key.py
+++ src/backend/tests/unit/api/v1/test_api_key.py
@@ -12,3 +12,25 @@ async def test_create_folder(client: AsyncClient, logged_in_headers):
     assert "api_keys" in result.keys(), "The dictionary must contain a key called 'api_keys'"
     assert "user_id" in result.keys(), "The dictionary must contain a key called 'user_id'"
     assert "total_count" in result.keys(), "The dictionary must contain a key called 'total_count'"
+
+
+async def test_create_api_key_route(client: AsyncClient, logged_in_headers, active_user):
+    basic_case = {
+        "name": "string",
+        "total_uses": 0,
+        "is_active": True,
+        "api_key": "string",
+        "user_id": str(active_user.id),
+    }
+    response = await client.post("api/v1/api_key/", json=basic_case, headers=logged_in_headers)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "api_key" in result.keys(), "The dictionary must contain a key called 'api_key'"
+    assert "id" in result.keys(), "The dictionary must contain a key called 'id'"
+    assert "is_active" in result.keys(), "The dictionary must contain a key called 'is_active'"
+    assert "last_used_at" in result.keys(), "The dictionary must contain a key called 'last_used_at'"
+    assert "name" in result.keys(), "The dictionary must contain a key called 'name'"
+    assert "total_uses" in result.keys(), "The dictionary must contain a key called 'total_uses'"
+    assert "user_id" in result.keys(), "The dictionary must contain a key called 'user_id'"
