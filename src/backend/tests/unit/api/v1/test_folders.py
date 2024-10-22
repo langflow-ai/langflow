205fb477b6783d498d9c3045241842d458721468
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-22 14:57:06 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_folders.py
+++ src/backend/tests/unit/api/v1/test_folders.py
@@ -46,3 +46,19 @@ async def test_read_folder(client: AsyncClient, logged_in_headers, basic_case):
     assert "description" in result.keys(), "The dictionary must contain a key called 'description'"
     assert "id" in result.keys(), "The dictionary must contain a key called 'id'"
     assert "parent_id" in result.keys(), "The dictionary must contain a key called 'parent_id'"
+
+
+async def test_update_folder(client: AsyncClient, logged_in_headers, basic_case):
+    update_case = basic_case.copy()
+    update_case["name"] = "Updated Folder"
+    _response = await client.post("api/v1/folders/", json=basic_case, headers=logged_in_headers)
+    _id = _response.json()["id"]
+    response = await client.patch(f"api/v1/folders/{_id}", json=update_case, headers=logged_in_headers)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "name" in result.keys(), "The dictionary must contain a key called 'name'"
+    assert "description" in result.keys(), "The dictionary must contain a key called 'description'"
+    assert "id" in result.keys(), "The dictionary must contain a key called 'id'"
+    assert "parent_id" in result.keys(), "The dictionary must contain a key called 'parent_id'"
