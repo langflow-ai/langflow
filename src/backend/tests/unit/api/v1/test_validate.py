22a55d16ae4115b8727a4bd154e03240f65ba6e5
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-22 10:45:05 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_validate.py
+++ src/backend/tests/unit/api/v1/test_validate.py
@@ -16,3 +16,42 @@ pprint(var)
     assert isinstance(result, dict), "The result must be a dictionary"
     assert "imports" in result.keys(), "The result must have an 'imports' key"
     assert "function" in result.keys(), "The result must have a 'function' key"
+
+
+async def test_post_validate_prompt(client: AsyncClient):
+    basic_case = {
+        "name": "string",
+        "template": "string",
+        "custom_fields": {},
+        "frontend_node": {
+            "template": {},
+            "description": "string",
+            "icon": "string",
+            "is_input": True,
+            "is_output": True,
+            "is_composition": True,
+            "base_classes": ["string"],
+            "name": "",
+            "display_name": "",
+            "documentation": "",
+            "custom_fields": {},
+            "output_types": [],
+            "full_path": "string",
+            "pinned": False,
+            "conditional_paths": [],
+            "frozen": False,
+            "outputs": [],
+            "field_order": [],
+            "beta": False,
+            "error": "string",
+            "edited": False,
+            "metadata": {},
+        },
+    }
+    response = await client.post("api/v1/validate/prompt", json=basic_case)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "frontend_node" in result.keys(), "The result must have a 'frontend_node' key"
+    assert "input_variables" in result.keys(), "The result must have an 'input_variables' key"
