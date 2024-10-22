6d790d0a5e39de1b62644dcdb66f0a7afd37d392
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-22 10:59:15 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_endpoints.py
+++ src/backend/tests/unit/api/v1/test_endpoints.py
@@ -12,3 +12,15 @@ async def test_get_version(client: AsyncClient):
     assert "version" in result.keys(), "The dictionary must contain a key called 'version'"
     assert "main_version" in result.keys(), "The dictionary must contain a key called 'main_version'"
     assert "package" in result.keys(), "The dictionary must contain a key called 'package'"
+
+
+async def test_get_config(client: AsyncClient):
+    response = await client.get("api/v1/config")
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "frontend_timeout" in result.keys(), "The dictionary must contain a key called 'frontend_timeout'"
+    assert "auto_saving" in result.keys(), "The dictionary must contain a key called 'auto_saving'"
+    assert "health_check_max_retries" in result.keys(), "The dictionary must contain a 'health_check_max_retries' key"
+    assert "max_file_size_upload" in result.keys(), "The dictionary must contain a key called 'max_file_size_upload'"
