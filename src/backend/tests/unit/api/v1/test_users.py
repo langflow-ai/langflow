b79d431c469509b42272de1346de6f7470754c11
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-23 10:58:24 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_users.py
+++ src/backend/tests/unit/api/v1/test_users.py
@@ -50,3 +50,29 @@ async def test_read_all_users(client: AsyncClient, logged_in_headers_super_user)
     assert isinstance(result, dict), "The result must be a dictionary"
     assert "total_count" in result, "The result must have an 'total_count' key"
     assert "users" in result, "The result must have an 'users' key"
+
+
+async def test_patch_user(client: AsyncClient, logged_in_headers_super_user):
+    name = "string"
+    updated_name = "string2"
+    basic_case = {
+        "username": name,
+        "password": "string"
+    }
+    _response = await client.post("api/v1/users/", json=basic_case)
+    _id = _response.json()["id"]
+    basic_case["username"] = updated_name
+    response = await client.patch(f"api/v1/users/{_id}", json=basic_case, headers=logged_in_headers_super_user)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "id" in result, "The result must have an 'id' key"
+    assert "is_active" in result, "The result must have an 'is_active' key"
+    assert "is_superuser" in result, "The result must have an 'is_superuser' key"
+    assert "last_login_at" in result, "The result must have an 'last_login_at' key"
+    assert "profile_image" in result, "The result must have an 'profile_image' key"
+    assert "store_api_key" in result, "The result must have an 'store_api_key' key"
+    assert "updated_at" in result, "The result must have an 'updated_at' key"
+    assert "username" in result, "The result must have an 'username' key"
+    assert result["username"] == updated_name, "The username must be updated"
