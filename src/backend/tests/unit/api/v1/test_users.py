2b3672fe77213628b03de9631b0d798f4f925680
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-23 10:04:20 -0300

    test: add test for api route

---
+++ src/backend/tests/unit/api/v1/test_users.py
@@ -0,0 +1,26 @@
+from unittest import mock
+from uuid import uuid4
+
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+
+
+async def test_add_user(client: AsyncClient):
+    basic_case = {
+        "username": "string",
+        "password": "string"
+    }
+    response = await client.post("api/v1/users/", json=basic_case)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "id" in result, "The result must have an 'id' key"
+    assert "is_active" in result, "The result must have an 'is_active' key"
+    assert "is_superuser" in result, "The result must have an 'is_superuser' key"
+    assert "last_login_at" in result, "The result must have an 'last_login_at' key"
+    assert "profile_image" in result, "The result must have an 'profile_image' key"
+    assert "store_api_key" in result, "The result must have an 'store_api_key' key"
+    assert "updated_at" in result, "The result must have an 'updated_at' key"
+    assert "username" in result, "The result must have an 'username' key"
