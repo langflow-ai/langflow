32e128506c685d2dc7916236a6612d94be0f7ce6
anovazzi1 <otavio2204@gmail.com>
2024-10-23 14:40:14 -0300

    Refactor styles in UploadFileButton component and add image attachment tooltip

--- src/frontend/src/modals/IOModal/components/chatView/chatInput/components/uploadFileButton/newUploadFileButton.tsx
+++ src/frontend/src/modals/IOModal/components/chatView/chatInput/components/uploadFileButton/newUploadFileButton.tsx
@@ -1,5 +1,7 @@
 import ForwardedIconComponent from "../../../../../../../components/genericIconComponent";
 import { Button } from "../../../../../../../components/ui/button";
+import ShadTooltip from "@/components/shadTooltipComponent";
+
 
 const UploadFileButton = ({
   fileInputRef,
@@ -8,25 +10,26 @@ const UploadFileButton = ({
   lockChat,
 }) => {
   return (
-    <div>
-      <input
-        disabled={lockChat}
-        type="file"
-        ref={fileInputRef}
-        style={{ display: "none" }}
-        onChange={handleFileChange}
-      />
-      <Button
-        disabled={lockChat}
-        className={`rounded-md bg-zinc-500 p-1 font-bold transition-all ${
-          lockChat ? "cursor-not-allowed" : "hover:text-muted-foreground"
-        }`}
-        onClick={handleButtonClick}
-        unstyled
-      >
-        <ForwardedIconComponent name="Image" />
-      </Button>
-    </div>
+    <ShadTooltip styleClasses="z-50" side="right" content="Attach image (png, jpg, jpeg)">
+      <div>
+        <input
+          disabled={lockChat}
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
+        <Button
+          disabled={lockChat}
+          className={`rounded-md bg-muted dark:bg-zinc-500 p-1 font-bold transition-all ${lockChat ? "cursor-not-allowed" : "hover:text-muted-foreground"
+            }`}
+          onClick={handleButtonClick}
+          unstyled
+        >
+          <ForwardedIconComponent name="Image" />
+        </Button>
+      </div>
+    </ShadTooltip>
   );
 };
 
