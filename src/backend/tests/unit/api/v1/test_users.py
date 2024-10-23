ae675078fb58fc8e2b14c3fc4893b2309b7561a8
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-23 10:22:09 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_users.py
+++ src/backend/tests/unit/api/v1/test_users.py
@@ -24,3 +24,19 @@ async def test_add_user(client: AsyncClient):
     assert "store_api_key" in result, "The result must have an 'store_api_key' key"
     assert "updated_at" in result, "The result must have an 'updated_at' key"
     assert "username" in result, "The result must have an 'username' key"
+
+
+async def test_read_current_user(client: AsyncClient, logged_in_headers):
+    response = await client.get("api/v1/users/whoami", headers=logged_in_headers)
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "id" in result, "The result must have an 'id' key"
+    assert "is_active" in result, "The result must have an 'is_active' key"
+    assert "is_superuser" in result, "The result must have an 'is_superuser' key"
+    assert "last_login_at" in result, "The result must have an 'last_login_at' key"
+    assert "profile_image" in result, "The result must have an 'profile_image' key"
+    assert "store_api_key" in result, "The result must have an 'store_api_key' key"
+    assert "updated_at" in result, "The result must have an 'updated_at' key"
+    assert "username" in result, "The result must have an 'username' key"
