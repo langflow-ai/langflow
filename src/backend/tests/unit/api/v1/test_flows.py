59cbf9d87f65cde2c7e1f1939de021b09ab74fa2
italojohnny <italojohnnydosanjos@gmail.com>
2024-10-25 14:17:10 -0300

    test: add test for api route

--- src/backend/tests/unit/api/v1/test_flows.py
+++ src/backend/tests/unit/api/v1/test_flows.py
@@ -94,3 +94,47 @@ async def test_read_flow(client: AsyncClient, logged_in_headers):
     assert "updated_at" in result.keys(), "The result must have a 'updated_at' key"
     assert "user_id" in result.keys(), "The result must have a 'user_id' key"
     assert "webhook" in result.keys(), "The result must have a 'webhook' key"
+
+
+async def test_update_flow(client: AsyncClient, logged_in_headers):
+    name = "first_name"
+    updated_name = "second_name"
+    basic_case = {
+        "description": "string",
+        "icon": "string",
+        "icon_bg_color": "#ff00ff",
+        "gradient": "string",
+        "data": {},
+        "is_component": False,
+        "webhook": False,
+        "endpoint_name": "string",
+        "tags": [
+            "string"
+        ],
+        "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "folder_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    }
+    basic_case["name"] = name
+    _response = await client.post("api/v1/flows/", json=basic_case, headers=logged_in_headers)
+    _id = _response.json()["id"]
+
+    basic_case["name"] = updated_name
+    response = await client.patch(f"api/v1/flows/{_id}", json=basic_case, headers=logged_in_headers)
+    result = response.json()
+
+    assert isinstance(result, dict), "The result must be a dictionary"
+    assert "data" in result.keys(), "The result must have a 'data' key"
+    assert "description" in result.keys(), "The result must have a 'description' key"
+    assert "endpoint_name" in result.keys(), "The result must have a 'endpoint_name' key"
+    assert "folder_id" in result.keys(), "The result must have a 'folder_id' key"
+    assert "gradient" in result.keys(), "The result must have a 'gradient' key"
+    assert "icon" in result.keys(), "The result must have a 'icon' key"
+    assert "icon_bg_color" in result.keys(), "The result must have a 'icon_bg_color' key"
+    assert "id" in result.keys(), "The result must have a 'id' key"
+    assert "is_component" in result.keys(), "The result must have a 'is_component' key"
+    assert "name" in result.keys(), "The result must have a 'name' key"
+    assert "tags" in result.keys(), "The result must have a 'tags' key"
+    assert "updated_at" in result.keys(), "The result must have a 'updated_at' key"
+    assert "user_id" in result.keys(), "The result must have a 'user_id' key"
+    assert "webhook" in result.keys(), "The result must have a 'webhook' key"
+    assert result["name"] == updated_name, "The name must be updated"
