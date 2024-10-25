b7609824340f99fa9aa18353c8c6510366e84aac
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-10-25 09:30:40 -0300

    Remove unused pytest fixture decorator from test_components_path function

--- src/backend/tests/unit/test_cli.py
+++ src/backend/tests/unit/test_cli.py
@@ -11,7 +11,6 @@ def default_settings():
     ]
 
 
-@pytest.mark.usefixtures("client")
 def test_components_path(runner, default_settings, tmp_path):
     # create a "components" folder
     temp_dir = tmp_path / "components"
