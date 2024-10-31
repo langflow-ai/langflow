a0397842b33146605da183e0c9d946be35cfa4c1
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-10-31 11:15:23 -0300

    fixed hover color of primary button

--- src/frontend/src/components/ui/button.tsx
+++ src/frontend/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground  hover:bg-primary-hover",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
