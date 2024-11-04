ba693d92e70e610d3d5eeff359e4686890de8eee
cristhianzl <cristhian.lousa@gmail.com>
2024-11-04 12:41:13 -0300

    refactor: update test to click on "New Flow" instead of "New Project"

--- src/frontend/tests/core/integrations/decisionFlow.spec.ts
+++ src/frontend/tests/core/integrations/decisionFlow.spec.ts
@@ -183,7 +183,7 @@ test("should create a flow with decision", async ({ page }) => {
     modalCount = 0;
   }
   while (modalCount === 0) {
-    await page.getByText("New Project", { exact: true }).click();
+    await page.getByText("New Flow", { exact: true }).click();
     await page.waitForTimeout(3000);
     modalCount = await page.getByTestId("modal-title")?.count();
   }
