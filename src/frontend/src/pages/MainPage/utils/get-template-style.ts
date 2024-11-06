87b2b9f82c94f6bbd624291f91d4b68a31102527
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-11-06 12:59:10 -0300

    Update default icon to Workflow

--- src/frontend/src/pages/MainPage/utils/get-template-style.ts
+++ src/frontend/src/pages/MainPage/utils/get-template-style.ts
@@ -5,6 +5,6 @@ export const getTemplateStyle = (flowData: {
 }): { icon: string; icon_bg_color: string } => {
   const { icon, icon_bg_color } = TEMPLATES_DATA.examples.find((example) =>
     flowData.name.includes(example.name),
-  ) ?? { icon: "circle-help", icon_bg_color: "bg-purple-300" };
+  ) ?? { icon: "Workflow", icon_bg_color: "bg-purple-300" };
   return { icon, icon_bg_color };
 };
