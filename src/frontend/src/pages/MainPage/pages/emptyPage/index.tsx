fc0dfd2276bd12c3de00ae7cf9e0540b8b278b89
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-11-07 14:00:23 -0300

    Removed folder buttons on emptyPage

--- src/frontend/src/pages/MainPage/pages/emptyPage/index.tsx
+++ src/frontend/src/pages/MainPage/pages/emptyPage/index.tsx
@@ -3,42 +3,17 @@ import ForwardedIconComponent from "@/components/genericIconComponent";
 import { Button } from "@/components/ui/button";
 import { ENABLE_NEW_LOGO } from "@/customization/feature-flags";
 import { useFolderStore } from "@/stores/foldersStore";
-import { PaginatedFolderType } from "../../entities";
 
 type EmptyPageProps = {
   setOpenModal: (open: boolean) => void;
-  setShowFolderModal: (open: boolean) => void;
-  folderData: PaginatedFolderType | null;
 };
 
-export const EmptyPage = ({
-  setOpenModal,
-  setShowFolderModal,
-  folderData,
-}: EmptyPageProps) => {
+export const EmptyPage = ({ setOpenModal }: EmptyPageProps) => {
   const folders = useFolderStore((state) => state.folders);
 
   return (
     <div className="m-0 h-full w-full bg-secondary p-0">
       <div className="text-container">
-        {(folderData?.folder?.name && folderData?.flows?.items?.length !== 0) ||
-          (folders?.length > 1 && (
-            <div className="absolute left-10 top-10 flex items-center text-2xl font-semibold dark:text-white">
-              <Button
-                variant="ghost"
-                size="icon"
-                onClick={() => setShowFolderModal(true)}
-                className="mr-2 bg-transparent lg:hidden"
-              >
-                <ForwardedIconComponent
-                  name="panel-left-open"
-                  aria-hidden="true"
-                  className="h-5 w-5 text-zinc-500 dark:text-zinc-400"
-                />
-              </Button>
-              {folderData?.folder?.name}
-            </div>
-          ))}
         <div className="relative z-20 flex w-full flex-col items-center justify-center gap-2">
           {ENABLE_NEW_LOGO ? (
             <ShortLangFlowIcon className="h-7 w-7 fill-black dark:fill-[white]" />
@@ -73,43 +48,37 @@ export const EmptyPage = ({
           </Button>
         </div>
       </div>
-      {folders?.length === 1 && (
-        <div className="gradient-bg">
-          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-            <defs>
-              <filter id="lf-balls">
-                <feGaussianBlur
-                  in="turbulence"
-                  stdDeviation="10"
-                  result="blur"
-                />
-                <feColorMatrix
-                  in="blur"
-                  type="matrix"
-                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                  result="color-matrix"
-                />
-                <feBlend in="SourceGraphic" in2="color-matrix" mode="normal" />
-              </filter>
-              <filter id="lf-noise">
-                <feTurbulence
-                  type="fractalNoise"
-                  baseFrequency="0.65"
-                  stitchTiles="stitch"
-                />
-              </filter>
-            </defs>
-          </svg>
-          <div className="gradients-container">
-            <div className="g1" />
-            <div className="g2" />
-            <div className="g3" />
-            <div className="g4" />
-            <div className="g5" />
-            <div className="g6" />
-          </div>
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          <defs>
+            <filter id="lf-balls">
+              <feGaussianBlur in="turbulence" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                type="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="color-matrix"
+              />
+              <feBlend in="SourceGraphic" in2="color-matrix" mode="normal" />
+            </filter>
+            <filter id="lf-noise">
+              <feTurbulence
+                type="fractalNoise"
+                baseFrequency="0.65"
+                stitchTiles="stitch"
+              />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1" />
+          <div className="g2" />
+          <div className="g3" />
+          <div className="g4" />
+          <div className="g5" />
+          <div className="g6" />
         </div>
-      )}
+      </div>
     </div>
   );
 };
