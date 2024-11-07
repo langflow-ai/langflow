883cd2e3250136b808c7fe2f1ed7b59dcc43b0b7
italojohnny <italojohnnydosanjos@gmail.com>
2024-11-07 08:38:41 -0300

    style: fix ruff erros

--- src/backend/base/langflow/__main__.py
+++ src/backend/base/langflow/__main__.py
@@ -518,17 +518,18 @@ def api_key(
     api_key_banner(unmasked_api_key)
 
 
-def show_version(value: bool):
+def show_version(*, value: bool):
     if value:
         default = "DEV"
         raw_info = get_version_info()
         version = raw_info.get("version", default) if raw_info else default
         typer.echo(f"langflow {version}")
-        raise typer.Exit()
+        raise typer.Exit
 
 
 @app.callback()
 def version_option(
+    *,
     version: bool = typer.Option(
         None,
         "--version",
