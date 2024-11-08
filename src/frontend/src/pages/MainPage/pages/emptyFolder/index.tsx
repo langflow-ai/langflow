f32360fea8f9fdb04ccf7667a5cbe60d45c2dd57
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2024-11-08 18:10:18 -0300

    fix: changed component colors name to not override existing colors, fixed responsiveness issues on main, playground and nav (#4482)
    * Changed description color
    
    * Updated edge colors
    
    * updated color of output inspection
    
    * Updated handle position to work with every case
    
    * Open sidebar on filter
    
    * Fixed empty folder on top of the sidebar
    
    * Fix app header responsitivity
    
    * Changed mobile playground showing
    
    * Changed color name to datatype
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/frontend/src/pages/MainPage/pages/emptyFolder/index.tsx
+++ src/frontend/src/pages/MainPage/pages/emptyFolder/index.tsx
@@ -11,32 +11,30 @@ export const EmptyFolder = ({ setOpenModal }: EmptyFolderProps) => {
 
   return (
     <div className="m-0 h-full w-full bg-secondary p-0">
-      <div className="text-container">
-        <div className="relative z-20 flex w-full flex-col items-center justify-center gap-2">
-          <h3
-            className="pt-5 font-chivo text-2xl font-semibold"
-            data-testid="mainpage_title"
-          >
-            {folders?.length > 1 ? "Empty folder" : "Start building"}
-          </h3>
-          <p className="pb-5 text-sm text-secondary-foreground">
-            Begin with a template, or start from scratch.
-          </p>
-          <Button
-            variant="default"
-            onClick={() => setOpenModal(true)}
-            id="new-project-btn"
-          >
-            <ForwardedIconComponent
-              name="plus"
-              aria-hidden="true"
-              className="h-4 w-4"
-            />
-            <span className="hidden whitespace-nowrap font-semibold md:inline">
-              New Flow
-            </span>
-          </Button>
-        </div>
+      <div className="absolute top-1/2 flex w-full -translate-y-1/2 flex-col items-center justify-center gap-2">
+        <h3
+          className="pt-5 font-chivo text-2xl font-semibold"
+          data-testid="mainpage_title"
+        >
+          {folders?.length > 1 ? "Empty folder" : "Start building"}
+        </h3>
+        <p className="pb-5 text-sm text-secondary-foreground">
+          Begin with a template, or start from scratch.
+        </p>
+        <Button
+          variant="default"
+          onClick={() => setOpenModal(true)}
+          id="new-project-btn"
+        >
+          <ForwardedIconComponent
+            name="plus"
+            aria-hidden="true"
+            className="h-4 w-4"
+          />
+          <span className="hidden whitespace-nowrap font-semibold md:inline">
+            New Flow
+          </span>
+        </Button>
       </div>
     </div>
   );
