9a5b3274ff517284e91865599ca444cbbfc12d18
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-11-08 18:38:20 -0300

    Allow 'size' prop to accept string values and update styling in BorderTrail component

--- src/frontend/src/components/core/border-trail.tsx
+++ src/frontend/src/components/core/border-trail.tsx
@@ -4,7 +4,7 @@ import { motion, Transition } from "framer-motion";
 
 type BorderTrailProps = {
   className?: string;
-  size?: number;
+  size?: number | string;
   transition?: Transition;
   delay?: number;
   onAnimationComplete?: () => void;
@@ -28,10 +28,12 @@ export function BorderTrail({
   return (
     <div className="pointer-events-none absolute inset-0 rounded-[inherit] border border-transparent [mask-clip:padding-box,border-box] [mask-composite:intersect] [mask-image:linear-gradient(transparent,transparent),linear-gradient(#000,#000)]">
       <motion.div
-        className={cn("absolute aspect-square bg-zinc-500", className)}
+        className={cn("absolute bg-zinc-500", className)}
         style={{
           width: size,
-          offsetPath: `rect(0 auto auto 0 round ${size}px)`,
+          offsetPath: `rect(0 auto auto 0 round 18px)`,
+          boxShadow:
+            "0px 0px 60px 30px rgb(255 255 255 / 50%), 0 0 100px 60px rgb(0 0 0 / 50%), 0 0 140px 90px rgb(0 0 0 / 50%)",
           ...style,
         }}
         animate={{
