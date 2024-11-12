6677a563de1569060d2ef3c999f54c7cbf210289
Eric Hare <ericrhare@gmail.com>
2024-11-12 08:03:29 -0800

    fix: KeyError in Langchain Hub component

--- src/backend/base/langflow/components/langchain_utilities/langchain_hub.py
+++ src/backend/base/langflow/components/langchain_utilities/langchain_hub.py
@@ -36,7 +36,9 @@ class LangChainHubPromptComponent(Component):
         Output(display_name="Build Prompt", name="prompt", method="build_prompt"),
     ]
 
-    def update_build_config(self, build_config: dict, field_value: str, field_name: str | None = None):
+    def update_build_config(
+        self, build_config: dict, field_value: str, field_name: str | None = None
+    ):
         # If the field is not langchain_hub_prompt or the value is empty, return the build config as is
         if field_name != "langchain_hub_prompt" or not field_value:
             return build_config
@@ -68,7 +70,9 @@ class LangChainHubPromptComponent(Component):
             full_template = full_template + "\n" + message.template
 
         # No need to reprocess if we have them already
-        if all("param_" + custom_field in build_config for custom_field in custom_fields):
+        if all(
+            "param_" + custom_field in build_config for custom_field in custom_fields
+        ):
             return build_config
 
         # Easter egg: Show template in info popup
@@ -99,9 +103,16 @@ class LangChainHubPromptComponent(Component):
         template = self._fetch_langchain_hub_template()
 
         # Get the parameters from the attributes
-        original_params = {k.removeprefix("param_"): v for k, v in self._attributes.items()}
+        params_dict = {
+            param: getattr(self, "param_" + param, f"{{{param}}}")
+            for param in template.input_variables
+        }
+        original_params = {
+            k: v.text if hasattr(v, "text") else v for k, v in params_dict.items()
+        }
         prompt_value = template.invoke(original_params)
 
+        # Update the template with the new value
         original_params["template"] = prompt_value.to_string()
 
         # Now pass the filtered attributes to the function
@@ -121,4 +132,6 @@ class LangChainHubPromptComponent(Component):
             raise ValueError(msg)
 
         # Pull the prompt from LangChain Hub
-        return langchain.hub.pull(self.langchain_hub_prompt, api_key=self.langchain_api_key)
+        return langchain.hub.pull(
+            self.langchain_hub_prompt, api_key=self.langchain_api_key
+        )
