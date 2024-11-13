51554bed3cedd25ac8897cb2ec27b096c4d086c7
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-11-13 11:58:12 -0300

    Handle undefined stars in GithubStarComponent to prevent errors

--- src/frontend/src/components/appHeaderComponent/components/GithubStarButton/index.tsx
+++ src/frontend/src/components/appHeaderComponent/components/GithubStarButton/index.tsx
@@ -3,7 +3,7 @@ import { useDarkStore } from "@/stores/darkStore";
 import { FaGithub } from "react-icons/fa";
 
 export const GithubStarComponent = () => {
-  const stars = useDarkStore((state) => state.stars);
+  const stars: number | undefined = useDarkStore((state) => state.stars);
 
   return (
     <ShadTooltip content="Go to Github repo" side="bottom" styleClasses="z-10">
@@ -11,7 +11,7 @@ export const GithubStarComponent = () => {
         <FaGithub className="h-4 w-4" />
         <div className="hidden text-xs font-semibold lg:block">Star</div>
         <div className="-mr-px ml-1 flex h-8 items-center justify-center rounded-md rounded-l-none border bg-background px-2 text-xs font-semibold text-secondary-foreground group-hover:border-input">
-          {stars.toLocaleString() ?? 0}
+          {stars?.toLocaleString() ?? 0}
         </div>
       </div>
     </ShadTooltip>
