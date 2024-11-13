5e5e25ca5b416860ee9bc5a545f2125e5960428e
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-11-13 16:04:00 -0300

    Fix sidebar not working some buttons in collapsed state

--- src/frontend/src/components/ui/sidebar.tsx
+++ src/frontend/src/components/ui/sidebar.tsx
@@ -441,7 +441,7 @@ const SidebarGroupLabel = React.forwardRef<
       data-sidebar="group-label"
       className={cn(
         "flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-semibold text-foreground/70 outline-none ring-ring transition-[margin,opa] duration-200 ease-linear focus-visible:ring-1 [&>svg]:size-4 [&>svg]:shrink-0",
-        "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
+        "group-data-[collapsible=icon]:pointer-events-none group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
         className,
       )}
       {...props}
