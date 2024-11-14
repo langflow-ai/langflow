3982fedd9ba178035f5d879b949ef132d2972ab5
Thorsten Philipp <thorsten.philipp@msg.group>
2024-11-14 08:27:10 +0100

    Extend mongodb component with mTLS

--- src/backend/base/langflow/components/vectorstores/mongodb_atlas.py
+++ src/backend/base/langflow/components/vectorstores/mongodb_atlas.py
@@ -1,9 +1,13 @@
+from typing import List
+
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 
 from langflow.base.vectorstores.model import LCVectorStoreComponent, check_cached_vector_store
 from langflow.helpers.data import docs_to_data
-from langflow.io import DataInput, HandleInput, IntInput, MultilineInput, SecretStrInput, StrInput
+from langflow.io import DataInput, HandleInput, IntInput, MultilineInput, SecretStrInput, StrInput, BoolInput
 from langflow.schema import Data
+import tempfile
+import certifi
 
 
 class MongoVectorStoreComponent(LCVectorStoreComponent):
@@ -15,6 +19,13 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
 
     inputs = [
         SecretStrInput(name="mongodb_atlas_cluster_uri", display_name="MongoDB Atlas Cluster URI", required=True),
+        BoolInput(name="enable_mtls", display_name="Enable mTLS", value=False, required=True),
+        SecretStrInput(
+            name="mongodb_atlas_client_cert",
+            display_name="MongoDB Atlas Combined Client Certificate",
+            required=False,
+            info='Client Certificate combined with the private key in the following format:\n -----BEGIN PRIVATE KEY-----\n...\n -----END PRIVATE KEY-----\n-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----\n'
+        ),
         StrInput(name="db_name", display_name="Database Name", required=True),
         StrInput(name="collection_name", display_name="Collection Name", required=True),
         StrInput(name="index_name", display_name="Index Name", required=True),
@@ -38,16 +49,38 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
     def build_vector_store(self) -> MongoDBAtlasVectorSearch:
         try:
             from pymongo import MongoClient
-        except ImportError as e:
-            msg = "Please install pymongo to use MongoDB Atlas Vector Store"
-            raise ImportError(msg) from e
+        except ImportError:
+            raise ImportError("Please install pymongo to use MongoDB Atlas Vector Store")
+
+        # Create temporary files for the client certificate
+        if(self.enable_mtls):
+            client_cert_path = None
+            try:
+                client_cert = self.mongodb_atlas_client_cert.replace(" ", "\n")
+                client_cert = client_cert.replace("-----BEGIN\nPRIVATE\nKEY-----", "-----BEGIN PRIVATE KEY-----")
+                client_cert = client_cert.replace("-----END\nPRIVATE\nKEY-----\n-----BEGIN\nCERTIFICATE-----", "-----END PRIVATE KEY-----\n-----BEGIN CERTIFICATE-----")
+                client_cert = client_cert.replace("-----END\nCERTIFICATE-----", "-----END CERTIFICATE-----")
+                with tempfile.NamedTemporaryFile(delete=False) as client_cert_file:
+                    client_cert_file.write(client_cert.encode('utf-8'))
+                    client_cert_path = client_cert_file.name
+
+            except Exception as e:
+                raise ValueError(f"Failed to write certificate to temporary file: {e}")
 
         try:
-            mongo_client: MongoClient = MongoClient(self.mongodb_atlas_cluster_uri)
+            if(self.enable_mtls):
+                mongo_client: MongoClient = MongoClient(
+                    self.mongodb_atlas_cluster_uri,
+                    tls=True,
+                    tlsCertificateKeyFile=client_cert_path,
+                    tlsCAFile=certifi.where()
+                    )
+            else:
+                mongo_client: MongoClient = MongoClient(self.mongodb_atlas_cluster_uri)
             collection = mongo_client[self.db_name][self.collection_name]
+            collection.drop() # Drop collection to override the vector store
         except Exception as e:
-            msg = f"Failed to connect to MongoDB Atlas: {e}"
-            raise ValueError(msg) from e
+            raise ValueError(f"Failed to connect to MongoDB Atlas: {e}")
 
         documents = []
         for _input in self.ingest_data or []:
