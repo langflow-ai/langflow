def37f83a3893d33c2dfe7073204cde603e8643c
Ivan <waterstark97@yandex.ru>
2024-11-19 14:27:07 +0400

    Fix update_build_config

--- src/backend/base/langflow/components/models/aiml.py
+++ src/backend/base/langflow/components/models/aiml.py
@@ -60,7 +60,7 @@ class AIMLModelComponent(LCModelComponent):
     ]
 
     @override
-    def update_build_config(self, field_value: str, build_config: dict, field_name: str | None = None):
+    def update_build_config(self, build_config: dict, field_value: str, field_name: str | None = None):
         if field_name in ("api_key", "aiml_api_base", "model_name"):
             aiml = AimlModels()
             aiml.get_aiml_models()
