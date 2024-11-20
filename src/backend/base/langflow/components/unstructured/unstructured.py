b96b9cfdee2377e4e372539b2415ca2332274d71
Phil Miesle <phil.miesle@datastax.com>
2024-11-20 16:59:48 +0000

    refactor Unstructured to BaseFileComponent

--- src/backend/base/langflow/components/unstructured/unstructured.py
+++ src/backend/base/langflow/components/unstructured/unstructured.py
@@ -1,52 +1,73 @@
 from langchain_unstructured import UnstructuredLoader
 
-from langflow.custom import Component
-from langflow.inputs import FileInput, SecretStrInput
+from langflow.base.data import BaseFileComponent
+from langflow.inputs import SecretStrInput, MessageTextInput, NestedDictInput
 from langflow.schema import Data
-from langflow.template import Output
+from pathlib import Path
 
-
-class UnstructuredComponent(Component):
-    display_name = "Unstructured"
-    description = "Uses Unstructured.io to extract clean text from raw source documents. Supports: PDF, DOCX, TXT"
-    documentation = "https://python.langchain.com/v0.2/docs/integrations/providers/unstructured/"
+class UnstructuredComponent(BaseFileComponent):
+    display_name = "Unstructured API"
+    description = "Uses Unstructured.io API to extract clean text from raw source documents. Supports a wide range of file types."
+    documentation = "https://python.langchain.com/api_reference/unstructured/document_loaders/langchain_unstructured.document_loaders.UnstructuredLoader.html"
     trace_type = "tool"
     icon = "Unstructured"
     name = "Unstructured"
 
+    # https://docs.unstructured.io/api-reference/api-services/overview#supported-file-types
+    VALID_EXTENSIONS = [
+        "bmp", "csv", "doc", "docx", "eml", "epub", "heic",
+        "html", "jpeg", "png", "md", "msg", "odt", "org",
+        "p7s", "pdf", "png", "ppt", "pptx", "rst", "rtf",
+        "tiff", "txt", "tsv", "xls", "xlsx", "xml"
+    ]
+
     inputs = [
-        FileInput(
-            name="file",
-            display_name="File",
-            required=True,
-            info="The path to the file with which you want to use Unstructured to parse. Supports: PDF, DOCX, TXT",
-            file_types=["pdf", "docx", "txt"],  # TODO: Support all unstructured file types
-        ),
+        *BaseFileComponent._base_inputs,
         SecretStrInput(
             name="api_key",
             display_name="Unstructured.io Serverless API Key",
             required=True,
             info="Unstructured API Key. Create at: https://app.unstructured.io/",
         ),
+        MessageTextInput(
+            name="api_url",
+            display_name="Unstructured.io API URL",
+            required=False,
+            info="Unstructured API URL.",
+        ),
+        NestedDictInput(
+            name="unstructured_args",
+            display_name="Additional Arguments",
+            required=False,
+            info=(
+                "Optional dictionary of additional arguments to the Loader. "
+                "See https://docs.unstructured.io/api-reference/api-services/api-parameters for more information."
+            ),
+        ),
     ]
 
     outputs = [
-        Output(name="data", display_name="Data", method="load_documents"),
+        *BaseFileComponent._base_outputs,
     ]
 
-    def build_unstructured(self) -> UnstructuredLoader:
-        file_paths = [self.file]
+    def process_files(self, file_list: list[Path]) -> list[Data]:
+        # https://docs.unstructured.io/api-reference/api-services/api-parameters
+        args = self.unstructured_args or {}
+                    
+        args["api_key"] = self.api_key
+        args["partition_via_api"] = True 
+        if self.api_url:
+            args["url"] = self.api_url
 
-        return UnstructuredLoader(
-            file_paths,
-            api_key=self.api_key,
-            partition_via_api=True,
+        loader = UnstructuredLoader(
+            file_list,
+            **args,
         )
 
-    def load_documents(self) -> list[Data]:
-        unstructured = self.build_unstructured()
+        self.log(f"file_list: {file_list}")
 
-        documents = unstructured.load()
+        documents = loader.load()
+
         data = [Data.from_document(doc) for doc in documents]  # Using the from_document method of Data
 
         self.status = data
