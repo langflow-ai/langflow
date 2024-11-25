2345c7c45de243b7ea2b52362b9a8f99d4acb51f
cristhianzl <cristhian.lousa@gmail.com>
2024-11-25 17:40:33 -0300

    📝 (filterSidebar.spec.ts): remove unnecessary test steps related to filter removal and sidebar interactions to simplify and improve test focus on main functionality

--- src/frontend/tests/core/features/filterSidebar.spec.ts
+++ src/frontend/tests/core/features/filterSidebar.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "@playwright/test";
 
 test(
-  "user must see on handle click the possibility connections - LLMChain",
+  "user must see on handle click the possibility connections",
   { tag: ["@release", "@components", "@api"] },
+
   async ({ page }) => {
     await page.goto("/");
     await page.waitForSelector('[data-testid="mainpage_title"]', {
@@ -60,40 +61,6 @@ test(
         timeout: 3000,
       },
     );
-    await page.getByTestId("handle-apirequest-shownode-urls-left").click();
-
-    await page.waitForTimeout(500);
-
-    expect(await page.getByTestId("icon-ListFilter").first()).toBeVisible();
-
-    await page
-      .getByTestId("icon-X")
-      .first()
-      .hover()
-      .then(async () => {
-        await page
-          .getByText("Remove filter", {
-            exact: false,
-          })
-          .first()
-          .isVisible();
-      });
-
-    await page.getByTestId("sidebar-search-input").click();
-    await page.getByTestId("sidebar-search-input").fill("api request");
-
-    await page.waitForTimeout(1000);
-    await page
-      .getByTestId("dataAPI Request")
-      .dragTo(page.locator('//*[@id="react-flow-id"]'));
-    await page.mouse.up();
-    await page.mouse.down();
-    await page.getByTestId("fit_view").click();
-    await page.getByTestId("zoom_out").click();
-    await page.getByTestId("zoom_out").click();
-    await page.getByTestId("zoom_out").click();
-    await page.waitForTimeout(500);
-
     await page.getByTestId("handle-apirequest-shownode-urls-left").click();
 
     await page.waitForTimeout(500);
