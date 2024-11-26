e0a48d3f89c939fd35359fd7a1c89d19d5524f79
Phil Miesle <mieslep@users.noreply.github.com>
2024-11-26 04:40:59 +0000

    feat: adding list of Data output to Parse Data (#4598)
    * adding list of Data output to Parse Data
    
    * [autofix.ci] apply automated fixes
    
    * enabling connection from more List outputs
    
    * [autofix.ci] apply automated fixes
    
    * refactor to have single return type
    
    * [autofix.ci] apply automated fixes
    
    * addressing linting comments
    
    * [autofix.ci] apply automated fixes
    
    * apparently lint was missed...
    
    * sigh
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Eric Hare <ericrhare@gmail.com>

--- src/backend/base/langflow/components/processing/parse_data.py
+++ src/backend/base/langflow/components/processing/parse_data.py
@@ -1,6 +1,7 @@
 from langflow.custom import Component
-from langflow.helpers.data import data_to_text
+from langflow.helpers.data import data_to_text, data_to_text_list
 from langflow.io import DataInput, MultilineInput, Output, StrInput
+from langflow.schema import Data
 from langflow.schema.message import Message
 
 
@@ -11,7 +12,7 @@ class ParseDataComponent(Component):
     name = "ParseData"
 
     inputs = [
-        DataInput(name="data", display_name="Data", info="The data to convert to text."),
+        DataInput(name="data", display_name="Data", info="The data to convert to text.", is_list=True),
         MultilineInput(
             name="template",
             display_name="Template",
@@ -23,13 +24,36 @@ class ParseDataComponent(Component):
     ]
 
     outputs = [
-        Output(display_name="Text", name="text", method="parse_data"),
+        Output(
+            display_name="Text",
+            name="text",
+            info="Data as a single Message, with each input Data separated by Separator",
+            method="parse_data",
+        ),
+        Output(
+            display_name="Data List",
+            name="data_list",
+            info="Data as a list of new Data, each having `text` formatted by Template",
+            method="parse_data_as_list",
+        ),
     ]
 
-    def parse_data(self) -> Message:
+    def _clean_args(self) -> tuple[list[Data], str, str]:
         data = self.data if isinstance(self.data, list) else [self.data]
         template = self.template
+        sep = self.sep
+        return data, template, sep
 
-        result_string = data_to_text(template, data, sep=self.sep)
+    def parse_data(self) -> Message:
+        data, template, sep = self._clean_args()
+        result_string = data_to_text(template, data, sep)
         self.status = result_string
         return Message(text=result_string)
+
+    def parse_data_as_list(self) -> list[Data]:
+        data, template, _ = self._clean_args()
+        text_list, data_list = data_to_text_list(template, data)
+        for item, text in zip(data_list, text_list, strict=True):
+            item.set_text(text)
+        self.status = data_list
+        return data_list
