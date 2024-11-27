02d9fdca6c54c061d5af42d67dd0410d7bb654ba
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2024-11-27 21:17:21 +0000

    feat: format

--- src/backend/base/langflow/base/models/sambanova_constants.py
+++ src/backend/base/langflow/base/models/sambanova_constants.py
@@ -5,7 +5,7 @@ SAMBANOVA_MODEL_NAMES = [
     "Meta-Llama-3.2-1B-Instruct",
     "Meta-Llama-3.2-3B-Instruct",
     "Llama-3.2-11B-Vision-Instruct",
-    "Llama-3.2-90B-Vision-Instruct"
+    "Llama-3.2-90B-Vision-Instruct",
 ]
 
 MODEL_NAMES = SAMBANOVA_MODEL_NAMES
