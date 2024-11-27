57ca2d9a3c66d8cd19c45323aea8d5faa023662c
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-11-27 17:47:18 -0300

    Fixed constants to follow new page size

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -942,13 +942,13 @@ export const maxSizeFilesInBytes = 10 * 1024 * 1024; // 10MB in bytes
 export const MAX_TEXT_LENGTH = 99999;
 
 export const SEARCH_TABS = ["All", "Flows", "Components"];
-export const PAGINATION_SIZE = 10;
+export const PAGINATION_SIZE = 12;
 export const PAGINATION_PAGE = 1;
 
 export const STORE_PAGINATION_SIZE = 12;
 export const STORE_PAGINATION_PAGE = 1;
 
-export const PAGINATION_ROWS_COUNT = [10, 20, 50, 100];
+export const PAGINATION_ROWS_COUNT = [12, 24, 48, 96];
 export const STORE_PAGINATION_ROWS_COUNT = [12, 24, 48, 96];
 
 export const GRADIENT_CLASS =
