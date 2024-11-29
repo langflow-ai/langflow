b48398f89e18fbe4c7808d17211a2f20366148ee
anovazzi1 <otavio2204@gmail.com>
2024-11-29 15:38:25 -0300

    change to utility function

--- src/frontend/tests/core/integrations/SEO Keyword Generator.spec.ts
+++ src/frontend/tests/core/integrations/SEO Keyword Generator.spec.ts
@@ -9,6 +9,7 @@ import { removeOldApiKeys } from "../../utils/remove-old-api-keys";
 import { selectGptModel } from "../../utils/select-gpt-model";
 import { updateOldComponents } from "../../utils/update-old-components";
 import { waitForOpenModalWithoutChatInput } from "../../utils/wait-for-open-modal";
+import { initialGPTsetup } from "../../utils/initialGPTsetup";
 
 test(
   "SEO Keyword Generator",
@@ -33,11 +34,7 @@ test(
       timeout: 100000,
     });
 
-    await adjustScreenView(page);
-    await updateOldComponents(page);
-    await removeOldApiKeys(page);
-    await addNewApiKeys(page);
-    await selectGptModel(page);
+    await initialGPTsetup(page);
 
     await page.getByTestId("button_run_chat output").click();
     await page.waitForSelector("text=built successfully", { timeout: 30000 });
