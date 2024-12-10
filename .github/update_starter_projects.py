11f68aec15e5bc58520cc57a03e9caa2fabe68be
Jordan Frazier <jordan.frazier@datastax.com>
2024-12-10 14:55:56 -0800

    update script

--- .github/update_starter_projects.py
+++ .github/update_starter_projects.py
@@ -1,4 +1,3 @@
-# Updates the starter projects with the latest component versions
 import os
 import sys
 import asyncio
@@ -8,14 +7,18 @@ src_path = os.path.join(project_root, "src")
 sys.path.insert(0, src_path)
 
 import backend.base.langflow.main
+from backend.base.langflow.initial_setup.setup import create_or_update_starter_projects
 from backend.base.langflow.interface.types import get_and_cache_all_types_dict
 from backend.base.langflow.services.deps import get_settings_service
-from backend.base.langflow.initial_setup.setup import create_or_update_starter_projects
 
 async def main():
+    # Updates the starter projects with the latest component versions
     all_types_dict = await get_and_cache_all_types_dict(get_settings_service())
-    await create_or_update_starter_projects(all_types_dict, do_create=False)
-    pass
+    did_update = await create_or_update_starter_projects(all_types_dict, do_create=False)
+    if did_update:
+        print("Starter projects updated")
+    else:
+        print("Starter projects are already up to date")
 
 if __name__ == "__main__":
     asyncio.run(main())
