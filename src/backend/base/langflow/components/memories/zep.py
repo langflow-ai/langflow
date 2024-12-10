d660be8882247b80570922d0452990a80d8067d5
Edwin Jose <edwin.jose@datastax.com>
2024-12-10 16:00:22 -0500

    Zep chat issues
    https://github.com/langchain-ai/langchain/issues/27356

--- src/backend/base/langflow/components/memories/zep.py
+++ src/backend/base/langflow/components/memories/zep.py
@@ -10,15 +10,13 @@ class ZepChatMemory(LCChatMemoryComponent):
     icon = "ZepMemory"
 
     inputs = [
-        MessageTextInput(name="url", display_name="Zep URL", info="URL of the Zep instance."),
-        SecretStrInput(name="api_key", display_name="API Key", info="API Key for the Zep instance."),
-        DropdownInput(
-            name="api_base_path",
-            display_name="API Base Path",
-            options=["api/v1", "api/v2"],
-            value="api/v1",
+        MessageTextInput(
+            name="url",
+            display_name="Zep URL",
+            info="URL of the Zep instance. Keep empty for cloud Zep instance.",
             advanced=True,
         ),
+        SecretStrInput(name="api_key", display_name="API Key", info="API Key for the Zep instance."),
         MessageTextInput(
             name="session_id", display_name="Session ID", info="Session ID for the message.", advanced=True
         ),
@@ -30,14 +28,11 @@ class ZepChatMemory(LCChatMemoryComponent):
             # avoid 404
             # This is a workaround for the local Zep instance
             # cloud Zep works with v2
-            import zep_python.zep_client
-            from zep_python import ZepClient
-            from zep_python.langchain import ZepChatMessageHistory
+            from langchain_community.chat_message_histories.zep_cloud import ZepCloudChatMessageHistory
 
-            zep_python.zep_client.API_BASE_PATH = self.api_base_path
+
         except ImportError as e:
             msg = "Could not import zep-python package. Please install it with `pip install zep-python`."
             raise ImportError(msg) from e
 
-        zep_client = ZepClient(api_url=self.url, api_key=self.api_key)
-        return ZepChatMessageHistory(session_id=self.session_id, zep_client=zep_client)
+        return ZepCloudChatMessageHistory(session_id=self.session_id, api_key=self.api_key)
