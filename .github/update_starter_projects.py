3e8e451ef14558b88fdde48da6e97f7654446385
Jordan Frazier <jordan.frazier@datastax.com>
2024-12-11 10:56:44 -0800

    Fix import paths'

--- .github/update_starter_projects.py
+++ .github/update_starter_projects.py
@@ -1,15 +1,9 @@
-import os
-import sys
 import asyncio
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-src_path = os.path.join(project_root, "src")
-sys.path.insert(0, src_path)
-
-import backend.base.langflow.main
-from backend.base.langflow.initial_setup.setup import create_or_update_starter_projects
-from backend.base.langflow.interface.types import get_and_cache_all_types_dict
-from backend.base.langflow.services.deps import get_settings_service
+import langflow.main
+from langflow.initial_setup.setup import create_or_update_starter_projects
+from langflow.interface.types import get_and_cache_all_types_dict
+from langflow.services.deps import get_settings_service
 
 async def main():
     # Updates the starter projects with the latest component versions
