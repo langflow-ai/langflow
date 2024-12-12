fa0b10df80f959f6fb95b17edb50c6f640526596
Jordan Frazier <jordan.frazier@datastax.com>
2024-12-11 17:46:57 -0800

    fix script location

--- .github/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""GitHub workflow automation scripts.
-
-This package contains utility scripts and functions used in GitHub Actions workflows
-for automation of build, test, and deployment processes.
-"""
