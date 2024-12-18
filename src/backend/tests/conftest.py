d1e219489369a21511708152d445b749a9573bc1
Christophe Bornet <cbornet@hotmail.com>
2024-12-18 15:43:31 +0100

    feat: Bump blockbuster version to 1.5.2 (#5331)
    Bump blockbuster version to 1.5.2

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -56,35 +56,27 @@ def blockbuster(request):
                 "os.stat",
                 "os.path.abspath",
             ]:
-                bb.functions[func].can_block_functions.append(("settings/service.py", {"initialize"}))
+                bb.functions[func].can_block_in("settings/service.py", "initialize")
             for func in [
                 "io.BufferedReader.read",
                 "io.TextIOWrapper.read",
             ]:
-                bb.functions[func].can_block_functions.append(("importlib_metadata/__init__.py", {"metadata"}))
+                bb.functions[func].can_block_in("importlib_metadata/__init__.py", "metadata")
 
             # TODO: make set_class_code async
-            bb.functions["os.stat"].can_block_functions.append(
-                ("langflow/custom/custom_component/component.py", {"set_class_code"})
-            )
+            bb.functions["os.stat"].can_block_in("langflow/custom/custom_component/component.py", "set_class_code")
 
             # TODO: follow discussion in https://github.com/encode/httpx/discussions/3456
-            bb.functions["os.stat"].can_block_functions.append(("httpx/_client.py", {"_init_transport"}))
+            bb.functions["os.stat"].can_block_in("httpx/_client.py", "_init_transport")
 
-            bb.functions["os.stat"].can_block_functions.append(("linecache.py", {"checkcache", "updatecache"}))
-            bb.functions["os.stat"].can_block_functions.append(("rich/traceback.py", {"_render_stack"}))
-            bb.functions["os.stat"].can_block_functions.append(
-                ("langchain_core/_api/internal.py", {"is_caller_internal"})
+            bb.functions["os.stat"].can_block_in("rich/traceback.py", "_render_stack")
+            bb.functions["os.stat"].can_block_in("langchain_core/_api/internal.py", "is_caller_internal")
+
+            (
+                bb.functions["os.path.abspath"]
+                .can_block_in("loguru/_better_exceptions.py", {"_get_lib_dirs", "_format_exception"})
+                .can_block_in("sqlalchemy/dialects/sqlite/pysqlite.py", "create_connect_args")
             )
-            bb.functions["os.path.abspath"].can_block_functions.extend(
-                [
-                    ("loguru/_better_exceptions.py", {"_get_lib_dirs", "_format_exception"}),
-                    ("sqlalchemy/dialects/sqlite/pysqlite.py", {"create_connect_args"}),
-                    ("_pytest/assertion/rewrite.py", {"_should_rewrite"}),
-                ]
-            )
-            bb.functions["os.mkdir"].can_block_functions.append(("_pytest/assertion/rewrite.py", {"try_makedirs"}))
-            bb.functions["os.replace"].can_block_functions.append(("_pytest/assertion/rewrite.py", {"_write_pyc"}))
             yield bb
 
 
