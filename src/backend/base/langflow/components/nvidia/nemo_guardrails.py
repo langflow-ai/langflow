af3bca4cbced88b1c1676f9dd43e49b3acb0ad49
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-12-20 13:43:08 -0500

    Fixing guardrails and ingest validations

--- src/backend/base/langflow/components/nvidia/nemo_guardrails.py
+++ src/backend/base/langflow/components/nvidia/nemo_guardrails.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
+import yaml
+
+from langflow.base.data.utils import read_text_file
 from langflow.custom import Component
 from langflow.io import FileInput, MultilineInput, Output
-from langflow.base.data.utils import read_text_file
 from langflow.schema import Data
-import yaml
-from pathlib import Path
+
 
 class NVIDIANemoGuardrailsComponent(Component):
     display_name = "NVIDIA NeMo Guardrails"
-    description = "Apply guardrails to LLM interactions. Load guardrail definintions from a YAML file, or provide directly as multiline text"
+    description = "Apply guardrails to LLM interactions. Load guardrail definitions from a YAML file," \
+                  " or provide directly as multiline text"
     icon = "NVIDIA"
     name = "NVIDIANemoGuardrails"
     beta = True
@@ -40,21 +44,25 @@ class NVIDIANemoGuardrailsComponent(Component):
                 data_dict = yaml.safe_load(yaml_content)
                 return Data(data={"text": yaml_content, "parsed_data": data_dict})
             except yaml.YAMLError as e:
-                raise ValueError(f"Invalid YAML syntax: {e}")
+                err_msg=f"Invalid YAML syntax"
+                raise ValueError(err_msg) from e
 
         # Fall back to FileInput
         if not self.path:
-            raise ValueError("Please, upload a file or provide YAML content.")
+            err_msg="Please, upload a file or provide YAML content."
+            raise ValueError(err_msg)
 
         resolved_path = self.resolve_path(self.path)
         extension = Path(resolved_path).suffix[1:].lower()
 
         if extension not in self.file_types:
-            raise ValueError(f"Unsupported file type: {extension}")
+            err_msg=f"Unsupported file type: {extension}"
+            raise ValueError(err_msg)
 
         text = read_text_file(resolved_path)
         try:
             data_dict = yaml.safe_load(text)
             return Data(data={"file_path": resolved_path, "text": text, "parsed_data": data_dict})
         except yaml.YAMLError as e:
-            raise ValueError(f"Invalid YAML syntax in file: {e}")
+            error_msg = f"Invalid YAML syntax in file"
+            raise ValueError(error_msg) from e
