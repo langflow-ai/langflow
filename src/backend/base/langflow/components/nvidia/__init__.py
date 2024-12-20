d77cd9f7eb78c6539acd78eb62f28cc1274c41a2
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-12-20 13:53:48 -0500

    Fixing used imports

--- src/backend/base/langflow/components/nvidia/__init__.py
+++ src/backend/base/langflow/components/nvidia/__init__.py
@@ -1,6 +1,3 @@
-from .nvidia_rerank import NvidiaRerankComponent
-from .nvidia_ingest import NVIDIAIngestComponent
-from .nemo_guardrails import NVIDIANemoGuardrailsComponent
 
 __all__ = [
     "NVIDIAIngestComponent"
