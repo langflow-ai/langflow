e774ccf3042d3fa0b6e09fb1c59cf901e6102aca
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2024-12-21 14:50:56 -0700

    rework nvidia init

--- src/backend/base/langflow/components/nvidia/__init__.py
+++ src/backend/base/langflow/components/nvidia/__init__.py
@@ -1,7 +1,13 @@
+from .nemo_customizer import NVIDIANeMoCustomizerComponent
+from .nemo_evaluator import NVIDIANeMoEvaluatorComponent
+from .nemo_guardrails import NVIDIANeMoGuardrailsComponent
+from .nvidia_ingest import NVIDIAIngestComponent
+from .nvidia_rerank import NvidiaRerankComponent
 
 __all__ = [
-    "NVIDIAIngestComponent"
-    "NVIDIANemoGuardrailsComponent",
-    "NVIDIANemoCustomizerComponent",
-    "NVIDIANemoEvaluatorComponent"
+    "NVIDIAIngestComponent",
+    "NVIDIANeMoCustomizerComponent",
+    "NVIDIANeMoGuardrailsComponent",
+    "NVIDIANeMoEvaluatorComponent",
+    "NvidiaRerankComponent",
 ]
