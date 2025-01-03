1f79c468c43ec01d9dff69c4e88a168ba990c5fc
anovazzi1 <otavio2204@gmail.com>
2025-01-03 20:09:17 -0300

    fix: Remove random factor from paste function in PageComponent

--- src/frontend/src/pages/FlowPage/components/PageComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/PageComponent/index.tsx
@@ -263,8 +263,8 @@ export default function Page({ view }: { view?: boolean }): JSX.Element {
       ) {
         takeSnapshot();
         paste(lastCopiedSelection, {
-          x: position.current.x + Math.random() * 100,
-          y: position.current.y + Math.random() * 100,
+          x: position.current.x,
+          y: position.current.y,
         });
       }
     }
