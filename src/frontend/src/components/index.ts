47272cb31b735c421444745f535e884495f28fb6
anovazzi1 <otavio2204@gmail.com>
2025-01-08 14:35:01 -0300

    create first try of a subpackage for the frontend components

---
+++ src/frontend/src/components/index.ts
@@ -0,0 +1,11 @@
+// Common components
+export * from './common/shadTooltipComponent';
+
+// UI components
+export * from './ui/accordion';
+export * from './ui/button';
+export * from './ui/dialog';
+export * from './ui/dropdown-menu';
+export * from './ui/input';
+export * from './ui/popover';
+export * from './ui/tooltip';
