2a64fac3b832c229097fcfd11bb586298e618d53
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-01-13 12:41:30 -0300

    Added custom edge for looping components

--- src/frontend/src/CustomEdges/index.tsx
+++ src/frontend/src/CustomEdges/index.tsx
@@ -1,4 +1,5 @@
 import useFlowStore from "@/stores/flowStore";
+import { scapeJSONParse } from "@/utils/reactflowUtils";
 import { BaseEdge, EdgeProps, getBezierPath, Position } from "@xyflow/react";
 
 export function DefaultEdge({
@@ -17,9 +18,13 @@ export function DefaultEdge({
   const sourceNode = getNode(source);
   const targetNode = getNode(target);
 
+  const targetHandleObject = scapeJSONParse(targetHandleId!);
+
   const sourceXNew =
     (sourceNode?.position.x ?? 0) + (sourceNode?.measured?.width ?? 0);
   const targetXNew = targetNode?.position.x ?? 0;
+
+  const edgePathLoop = `M ${sourceXNew} ${sourceY} C ${sourceXNew + 200} ${sourceY + 200}, ${targetXNew - 200} ${targetY + 200}, ${targetXNew} ${targetY}`;
 
   const [edgePath] = getBezierPath({
     sourceX: sourceXNew,
@@ -30,5 +35,11 @@ export function DefaultEdge({
     targetY,
   });
 
-  return <BaseEdge path={edgePath} {...props} />;
+  return (
+    <BaseEdge
+      path={targetHandleObject.output_types ? edgePathLoop : edgePath}
+      strokeDasharray={targetHandleObject.output_types ? "5 5" : "0"}
+      {...props}
+    />
+  );
 }
