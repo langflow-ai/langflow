3aad357becee109571e4d336a641fbbe6720f74c
anovazzi1 <otavio2204@gmail.com>
2025-01-13 12:45:24 -0300

    revert changes to keep the plus icon bigger than ... but with right positioning

--- src/frontend/src/components/core/parameterRenderComponent/components/inputListComponent/components/button-input-list.tsx
+++ src/frontend/src/components/core/parameterRenderComponent/components/inputListComponent/components/button-input-list.tsx
@@ -24,27 +24,36 @@ export const ButtonInputList = ({
   return (
     <>
       <ShadTooltip content={listAddLabel} side="top" align="center">
-        <Button
+        <div
           onClick={addNewInput}
-          variant="ghost"
-          size="iconMd"
           className={cn(
-            "group absolute -translate-y-7 translate-x-[15.0rem]",
-            getButtonClassName(disabled),
+            "hit-area-icon group absolute flex -translate-y-8 translate-x-[15.36rem] items-center justify-center bg-background text-center hover:bg-muted",
+            disabled
+              ? "pointer-events-none bg-background hover:bg-background"
+              : "",
           )}
-          data-testid={getTestId("plus", index, editNode, componentName)}
-          disabled={disabled}
         >
-          <IconComponent
-            name="Plus"
+          <Button
+            unstyled
+            size="icon"
             className={cn(
-              "icon-size justify-self-center text-muted-foreground",
-              !disabled && "hover:cursor-pointer hover:text-foreground",
-              "group-hover:text-foreground",
+              "hit-area-icon flex items-center justify-center",
+              getButtonClassName(disabled),
             )}
-            strokeWidth={ICON_STROKE_WIDTH}
-          />
-        </Button>
+            data-testid={getTestId("plus", index, editNode, componentName)}
+            disabled={disabled}
+          >
+            <IconComponent
+              name="Plus"
+              className={cn(
+                "icon-size justify-self-center text-muted-foreground",
+                !disabled && "hover:cursor-pointer hover:text-foreground",
+                "group-hover:text-foreground",
+              )}
+              strokeWidth={ICON_STROKE_WIDTH}
+            />
+          </Button>
+        </div>
       </ShadTooltip>
     </>
   );
