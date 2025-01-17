5ac43f8670fa96ab0f71cd77c667bc53a960a2c8
anovazzi1 <otavio2204@gmail.com>
2025-01-17 11:01:17 -0300

    Add constants for allowed image input extensions and chat input placeholders

---
+++ src/playground/src/constants.ts
@@ -0,0 +1,4 @@
+export const ALLOWED_IMAGE_INPUT_EXTENSIONS = new Set(["png", "jpg", "jpeg"]);
+export const CHAT_INPUT_PLACEHOLDER =
+  "No chat input variables found. Click to run your flow.";
+export const CHAT_INPUT_PLACEHOLDER_SEND = "Send a message...";
