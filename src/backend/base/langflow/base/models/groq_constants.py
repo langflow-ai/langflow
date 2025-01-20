a3905fa2101ec729baa086b1cb4120bde1db3b9a
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-01-20 18:31:19 +0530

    feat(groq): added llama 3.3 models (#5497)
    * Update groq_constants.py
    
    added new llama 3.3 models from groq
    
    * Rechecking the codeflash workflow
    
    ---------
    
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>

--- src/backend/base/langflow/base/models/groq_constants.py
+++ src/backend/base/langflow/base/models/groq_constants.py
@@ -10,6 +10,8 @@ GROQ_MODELS = [
     "llama-3.2-3b-preview",  # Meta
     "llama-3.2-11b-vision-preview",  # Meta
     "llama-3.2-90b-vision-preview",  # Meta
+    "llama-3.3-70b-specdec",  # Meta
+    "llama-3.3-70b-versatile",  # Meta
     "llama-guard-3-8b",  # Meta
     "llama3-70b-8192",  # Meta
     "llama3-8b-8192",  # Meta
