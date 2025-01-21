437cb22ddff20ea42b8b210a92b82bc4385131be
anovazzi1 <otavio2204@gmail.com>
2025-01-21 17:04:09 -0300

    Refactor json-output-view component to add read-only prop

---
+++ src/frontend/src/components/core/jsonOutputComponent/json-output-view.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import JsonEditor from "../jsonEditor";
+
+interface JsonOutputViewComponentProps {
+  data: string | object;
+  width?: string;
+  height?: string;
+}
+
+const JsonOutputViewComponent: React.FC<JsonOutputViewComponentProps> = ({
+  data,
+  width = "100%",
+  height = "600px",
+}) => {
+  const jsonData = typeof data === "string" ? JSON.parse(data) : data;
+
+  return (
+    <JsonEditor
+      data={{ json: jsonData }}
+      readOnly={true}
+      width={width}
+      height={height}
+    />
+  );
+};
+
+export default JsonOutputViewComponent;
