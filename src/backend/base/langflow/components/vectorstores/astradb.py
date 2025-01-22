dcc615903df69b983054f411d694828afce241a3
Eric Hare <ericrhare@gmail.com>
2025-01-22 12:58:26 -0800

    Update astradb.py

--- src/backend/base/langflow/components/vectorstores/astradb.py
+++ src/backend/base/langflow/components/vectorstores/astradb.py
@@ -127,7 +127,7 @@ class AstraDBVectorStoreComponent(LCVectorStoreComponent):
             name="api_endpoint",
             display_name="API Endpoint",
             info="The API endpoint for the Astra DB instance.",
-            advanced=os.getenv("LANGFLOW_HOST") is not None,  # TODO: Clean up all examples of these
+            advanced=os.getenv("LANGFLOW_HOST") is None,  # TODO: Clean up all examples of these
         ),
         DropdownInput(
             name="database_name",
