12e1e712fc79ee4f3a9eeba96135a29d6a6833cb
Vigtu <victor.cgomes@uvvnet.com.br>
2025-01-22 19:25:03 -0300

    fix(duckduckgo): adjust = max_results to required = True

--- src/backend/base/langflow/components/tools/duck_duck_go_search_core.py
+++ src/backend/base/langflow/components/tools/duck_duck_go_search_core.py
@@ -22,6 +22,7 @@ class DuckDuckGoSearchCoreComponent(Component):
             name="max_results",
             display_name="Max Results",
             value=4,
+            required=True,
         ),
         IntInput(name="max_snippet_length", display_name="Max Snippet Length", value=500, advanced=True),
     ]
