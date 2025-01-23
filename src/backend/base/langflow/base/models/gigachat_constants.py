0a14deeec13254ecb655a45d85aea6341acf9bd9
Konstantin Krestnikov <rai220@gmail.com>
2025-01-23 15:18:17 +0300

    feat: support gigachat embeddings

--- src/backend/base/langflow/base/models/gigachat_constants.py
+++ src/backend/base/langflow/base/models/gigachat_constants.py
@@ -4,3 +4,8 @@ GIGACHAT_MODEL_NAMES = [
     "GigaChat-Pro",
     "GigaChat-Max",
 ]
+GIGACHAT_EMBEDDING_MODEL_NAMES = [
+    "Embeddings",
+    "EmbeddingsGigaR",
+]
+GIGACHAT_SCOPE_NAMES = ["GIGACHAT_API_PERS", "GIGACHAT_API_CORP", "GIGACHAT_API_B2B"]
--- src/backend/base/langflow/components/models/gigachat.py
+++ src/backend/base/langflow/components/models/gigachat.py
@@ -1,6 +1,6 @@
 from pydantic.v1 import SecretStr
 
-from langflow.base.models.gigachat_constants import GIGACHAT_MODEL_NAMES
+from langflow.base.models.gigachat_constants import GIGACHAT_MODEL_NAMES, GIGACHAT_SCOPE_NAMES
 from langflow.base.models.model import LCModelComponent
 from langflow.field_typing import LanguageModel
 from langflow.field_typing.range_spec import RangeSpec
@@ -32,7 +32,14 @@ class GigaChatComponent(LCModelComponent):
             name="gigachat_api_base",
             display_name="GigaChat API Base",
             advanced=True,
-            info="The base URL of the GigaChat API. ",
+            info="The base URL of the GigaChat API.",
+        ),
+        DropdownInput(
+            name="scope",
+            display_name="Credentials scope",
+            advanced=False,
+            options=GIGACHAT_SCOPE_NAMES,
+            value=GIGACHAT_SCOPE_NAMES[0],
         ),
         SecretStrInput(
             name="gigachat_credentials",
@@ -55,10 +62,16 @@ class GigaChatComponent(LCModelComponent):
             value="GIGACHAT_PASSWORD",
         ),
         SliderInput(
+            name="top_p",
+            display_name="top_p",
+            value=0.1,
+            range_spec=RangeSpec(min=0.01, max=2, step=0.01),
+        ),
+        SliderInput(
             name="temperature",
             display_name="Temperature",
-            value=0.1,
-            range_spec=RangeSpec(min=0, max=2, step=0.01),
+            value=1,
+            range_spec=RangeSpec(min=0, max=1, step=0.01),
         ),
     ]
 
@@ -70,10 +83,12 @@ class GigaChatComponent(LCModelComponent):
             raise ImportError(msg) from None
         gigachat_credentials = self.gigachat_credentials
         temperature = self.temperature
+        top_p = self.top_p
         model_name: str = self.model_name
-        max_tokens = self.max_tokens
-        gigachat_user = self.gigachat_user
-        password = self.password
+        max_tokens: int = self.max_tokens
+        gigachat_user: str = self.gigachat_user
+        password: str = self.password
+        scope: str = self.scope
         gigachat_api_base = self.gigachat_api_base or None
 
         gigachat_credentials = SecretStr(gigachat_credentials).get_secret_value() if gigachat_credentials else None
@@ -81,9 +96,11 @@ class GigaChatComponent(LCModelComponent):
             credentials=gigachat_credentials,
             user=gigachat_user or None,
             password=password or None,
+            scope=scope,
             max_tokens=max_tokens or None,
             model=model_name,
             base_url=gigachat_api_base,
             temperature=temperature if temperature is not None else 1.0,
+            top_p=top_p if top_p is not None else 1.0,
             verify_ssl_certs=False,
         )
