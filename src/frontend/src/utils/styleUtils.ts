3b35c7c055392de64b02127dd89ea3e61ff11e62
Edwin Jose <edwin.jose@datastax.com>
2025-01-27 09:22:21 -0500

    fix: fixes AgentExecutor handle greyed out  (#5900)
    * Update styleUtils.ts
    
    * 📝 (index.css): update color values for datatype-purple and datatype-purple-foreground to improve design consistency
    
    ---------
    
    Co-authored-by: cristhianzl <cristhian.lousa@gmail.com>

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -432,6 +432,7 @@ export const nodeColors: { [char: string]: string } = {
   BaseLanguageModel: "#c026d3",
   LanguageModel: "#c026d3",
   Agent: "#903BBE",
+  AgentExecutor: "#903BBE",
   Tool: "#00fbfc",
 };
 
@@ -480,6 +481,7 @@ export const nodeColorsName: { [char: string]: string } = {
   BaseLanguageModel: "fuchsia",
   LanguageModel: "fuchsia",
   Agent: "purple",
+  AgentExecutor: "purple",
   Tool: "cyan",
   BaseChatMemory: "cyan",
   BaseChatMessageHistory: "orange",
