09b22d604c324ba84efb2f7295e92a37a84d4d69
anovazzi1 <otavio2204@gmail.com>
2025-01-27 17:56:09 -0300

    Refactor IOModal in PlaygroundPage component

--- src/frontend/src/pages/Playground/index.tsx
+++ src/frontend/src/pages/Playground/index.tsx
@@ -66,7 +66,7 @@ export default function PlaygroundPage() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center align-middle">
       {currentSavedFlow && (
-        <IOModal open={true} setOpen={() => {}} isPlayground>
+        <IOModal open={true} setOpen={() => {}} isPlayground playgroundPage>
           <></>
         </IOModal>
       )}
