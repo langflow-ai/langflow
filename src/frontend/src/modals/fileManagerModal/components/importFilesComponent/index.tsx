b64c87fc952bf6f898edc37ecd18b85b37de3954
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-01-30 17:52:31 -0300

    Added dropdown menu for selecting import source

--- src/frontend/src/modals/fileManagerModal/components/importFilesComponent/index.tsx
+++ src/frontend/src/modals/fileManagerModal/components/importFilesComponent/index.tsx
@@ -1,5 +1,11 @@
 import ForwardedIconComponent from "@/components/common/genericIconComponent";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function ImportFilesComponent() {
   return (
@@ -15,10 +21,32 @@ export default function ImportFilesComponent() {
           </span>
         </div>
       </div>
-      <Button size="sm" className="font-semibold">
-        Import from...
-        <ForwardedIconComponent name="ChevronDown" className="h-4 w-4" />
-      </Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="sm" className="font-semibold">
+            Import from...
+            <ForwardedIconComponent
+              name="ChevronDown"
+              className="ml-2 h-4 w-4"
+            />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="-mr-0 p-1">
+          <DropdownMenuItem className="cursor-pointer gap-2">
+            <ForwardedIconComponent name="GoogleDrive" className="h-4 w-4" />
+            Drive
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer gap-2">
+            <ForwardedIconComponent name="OneDrive" className="h-4 w-4" />
+            OneDrive
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer gap-2">
+            <ForwardedIconComponent name="Dropbox" className="h-4 w-4" />
+            Dropbox
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
