726653b9ab3d8ef7f7454a788015f5815238e59f
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-01-31 18:00:54 -0300

    Added files table

--- src/frontend/src/pages/MainPage/pages/filesPage/index.tsx
+++ src/frontend/src/pages/MainPage/pages/filesPage/index.tsx
@@ -1,12 +1,109 @@
 import ForwardedIconComponent from "@/components/common/genericIconComponent";
 import ShadTooltip from "@/components/common/shadTooltipComponent";
 import CardsWrapComponent from "@/components/core/cardsWrapComponent";
+import TableComponent from "@/components/core/parameterRenderComponent/components/tableComponent";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import ImportButtonComponent from "@/modals/fileManagerModal/components/importButtonComponent";
+import { FILE_ICONS } from "@/utils/styleUtils";
+import { ColDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { useRef, useState } from "react";
 
 export const FilesPage = () => {
+  const tableRef = useRef<AgGridReact<any>>(null);
+  const files = [
+    {
+      type: "json",
+      name: "user_profile_data.json",
+      size: "640 KB",
+      added: "02/02/2025",
+    },
+    {
+      type: "csv",
+      name: "Q4_Reports.csv",
+      size: "80 KB",
+      added: "02/02/2025",
+    },
+    {
+      type: "txt",
+      name: "Highschool Speech.txt",
+      size: "10 KB",
+      added: "01/02/2025",
+    },
+    {
+      type: "pdf",
+      name: "logoconcepts.pdf",
+      size: "1.2 MB",
+      added: "31/01/2025",
+    },
+  ];
+
+  const colDefs: ColDef[] = [
+    {
+      headerName: "Name",
+      field: "name",
+      flex: 2,
+      editable: false,
+      filter: "agTextColumnFilter",
+      resizable: false,
+      cellClass: "cursor-text select-text",
+      cellRenderer: (params) => {
+        return (
+          <div className="flex items-center gap-2 font-medium">
+            <ForwardedIconComponent
+              name={FILE_ICONS[params.value.split(".")[1]]?.icon}
+              className={FILE_ICONS[params.value.split(".")[1]]?.color}
+            />
+            {params.value}
+          </div>
+        );
+      }, //This column will be twice as wide as the others
+    }, //This column will be twice as wide as the others
+    {
+      headerName: "Type",
+      field: "type",
+      filter: "agTextColumnFilter",
+      editable: false,
+      resizable: false,
+      valueFormatter: (params) => {
+        return params.value.toUpperCase();
+      },
+      cellClass: "text-muted-foreground cursor-text select-text",
+    },
+    {
+      headerName: "Size",
+      field: "size",
+      editable: false,
+      resizable: false,
+      cellClass: "text-muted-foreground cursor-text select-text",
+    },
+    {
+      headerName: "Added",
+      field: "added",
+      editable: false,
+      resizable: false,
+      cellClass: "text-muted-foreground cursor-text select-text",
+    },
+    {
+      maxWidth: 60,
+      editable: false,
+      resizable: false,
+      cellClass: "cursor-default",
+      cellRenderer: (params) => {
+        return (
+          <div className="flex h-full cursor-default items-center justify-center">
+            <Button variant="ghost" size="iconMd">
+              <ForwardedIconComponent name="EllipsisVertical" />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const [quickFilterText, setQuickFilterText] = useState("");
   return (
     <CardsWrapComponent
       onFileDrop={() => {}}
@@ -47,8 +144,10 @@ export const FilesPage = () => {
                         type="text"
                         placeholder={`Search files...`}
                         className="mr-2 w-full"
-                        value={""}
-                        onChange={() => {}}
+                        value={quickFilterText || ""}
+                        onChange={(event) => {
+                          setQuickFilterText(event.target.value);
+                        }}
                       />
                     </div>
                     <div className="flex items-center gap-2">
@@ -83,6 +182,28 @@ export const FilesPage = () => {
                   </div>
                 </>
               }
+              <div className="flex h-full flex-col py-4">
+                <TableComponent
+                  rowHeight={45}
+                  headerHeight={45}
+                  cellSelection={false}
+                  tableOptions={{
+                    hide_options: true,
+                  }}
+                  enableCellTextSelection={false}
+                  columnDefs={colDefs}
+                  rowData={files}
+                  className="ag-no-border w-full"
+                  pagination
+                  ref={tableRef}
+                  quickFilterText={quickFilterText}
+                  gridOptions={{
+                    suppressCellFocus: true,
+                    enableCellTextSelection: true,
+                    ensureDomOrder: true,
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
