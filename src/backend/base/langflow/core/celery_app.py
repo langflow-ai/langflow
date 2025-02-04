5930ac013139f8891cccf28147cce1bd3c42fe54
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2025-02-04 13:06:35 -0300

    fix: Update Celery task routes and imports for better task management

--- src/backend/base/langflow/core/celery_app.py
+++ src/backend/base/langflow/core/celery_app.py
@@ -4,7 +4,11 @@ from celery import Celery
 def make_celery(app_name: str, config: str) -> Celery:
     celery_app = Celery(app_name)
     celery_app.config_from_object(config)
-    celery_app.conf.task_routes = {"langflow.worker.tasks.*": {"queue": "langflow"}}
+    celery_app.conf.task_routes = {
+        "langflow.services.task.consumer.consume_task_celery": {"queue": "langflow"},
+        "langflow.worker.simple_run_flow_task_celery": {"queue": "langflow"},
+    }
+    celery_app.conf.imports = ["langflow.services.task.consumer", "langflow.worker"]
     return celery_app
 
 
