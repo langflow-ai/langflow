a290f80a7342b1330cbc5eee9b79e54b98d650cc
anovazzi1 <otavio2204@gmail.com>
2025-02-04 17:20:28 -0300

    feat: add JSIcon component for JavaScript icon representation

---
+++ src/frontend/src/icons/JSicon/index.tsx
@@ -0,0 +1,9 @@
+import React, { forwardRef } from "react";
+import SvgJSIcon from "./JSIcon";
+
+export const JSIcon = forwardRef<
+  SVGSVGElement,
+  React.PropsWithChildren<{}>
+>((props, ref) => {
+  return <SvgJSIcon ref={ref} {...props} />;
+});
