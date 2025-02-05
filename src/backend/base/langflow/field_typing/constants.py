7b8b4da50a4e1a8d6198a7060bdbc951fb928cbb
brian-ogrady <brian.ogrady@datastax.com>
2025-02-05 13:29:29 -0500

    feat: Restructure Rerankers so both NVIDIA and Cohere work properly (#5933)
    * Removing reference to nonexistent method
    
    * Restructuring rerankers to inherit from BaseDocumentCompressor. Adding Voyage AI reranker.
    
    * Removing Voyage AI component and dependency.
    
    * [autofix.ci] apply automated fixes
    
    * feat: Add method to compress documents as DataFrame in LCCompressorComponent
    
    * Changing description of abstract build_compressor method
    
    * [autofix.ci] apply automated fixes
    
    * Adding top_n as an argument to the NVIDIA reranker
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>

--- src/backend/base/langflow/field_typing/constants.py
+++ src/backend/base/langflow/field_typing/constants.py
@@ -7,6 +7,7 @@ from langchain.memory.chat_memory import BaseChatMemory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document
+from langchain_core.documents.compressor import BaseDocumentCompressor
 from langchain_core.embeddings import Embeddings
 from langchain_core.language_models import BaseLanguageModel, BaseLLM
 from langchain_core.language_models.chat_models import BaseChatModel
@@ -68,6 +69,7 @@ LANGCHAIN_BASE_TYPES = {
     "BaseChatMemory": BaseChatMemory,
     "BaseChatModel": BaseChatModel,
     "Memory": Memory,
+    "BaseDocumentCompressor": BaseDocumentCompressor,
 }
 # Langchain base types plus Python base types
 CUSTOM_COMPONENT_SUPPORTED_TYPES = {
@@ -96,6 +98,7 @@ from langchain_core.memory import BaseMemory
 from langchain_core.output_parsers import BaseLLMOutputParser, BaseOutputParser
 from langchain_core.prompts import BasePromptTemplate, ChatPromptTemplate, PromptTemplate
 from langchain_core.retrievers import BaseRetriever
+from langchain_core.documents.compressor import BaseDocumentCompressor
 from langchain_core.tools import BaseTool, Tool
 from langchain_core.vectorstores import VectorStore, VectorStoreRetriever
 from langchain_text_splitters import TextSplitter
