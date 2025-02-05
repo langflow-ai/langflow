207910cb5a8323e512373e4360565145f030d98f
Pedro Pacheco <pedro.pacheco@yahoo.com>
2025-02-05 15:43:33 -0700

    S3 Bucket Uploader, unit test and module init

--- src/backend/base/langflow/components/data/__init__.py
+++ src/backend/base/langflow/components/data/__init__.py
@@ -6,7 +6,7 @@ from .json_to_data import JSONToDataComponent
 from .sql_executor import SQLExecutorComponent
 from .url import URLComponent
 from .webhook import WebhookComponent
-
+from .s3_bucket_uploader import S3BucketUploaderComponent
 __all__ = [
     "APIRequestComponent",
     "CSVToDataComponent",
@@ -16,4 +16,5 @@ __all__ = [
     "SQLExecutorComponent",
     "URLComponent",
     "WebhookComponent",
+    "S3BucketUploaderComponent",
 ]
