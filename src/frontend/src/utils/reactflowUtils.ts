ac4c170b13163dc964caf67e89db7b055b4992f8
cristhianzl <cristhian.lousa@gmail.com>
2025-02-05 13:17:10 -0300

    ✨ (reactflowUtils.ts): add new function filterHiddenFieldsEdges to filter out edges based on hidden fields in node templates

--- src/frontend/src/utils/reactflowUtils.ts
+++ src/frontend/src/utils/reactflowUtils.ts
@@ -133,7 +133,35 @@ export function cleanEdges(nodes: AllNodeType[], edges: EdgeType[]) {
         }
       }
     }
+
+    newEdges = filterHiddenFieldsEdges(nodes, edge, newEdges);
   });
+  return newEdges;
+}
+
+export function filterHiddenFieldsEdges(
+  nodes: AllNodeType[],
+  edge: EdgeType,
+  newEdges: EdgeType[],
+) {
+  const node = nodes.find((n) => n.id === edge.target);
+
+  if (node) {
+    const nodeInputType = edge.data?.targetHandle?.inputTypes;
+    const nodeTemplates = node.data.node!.template;
+
+    Object.keys(nodeTemplates).forEach((key) => {
+      if (!nodeTemplates[key]?.input_types) return;
+      if (
+        nodeTemplates[key]?.input_types?.some((type) =>
+          nodeInputType!.includes(type),
+        ) &&
+        !nodeTemplates[key].show
+      ) {
+        newEdges = newEdges.filter((e) => e.id !== edge.id);
+      }
+    });
+  }
   return newEdges;
 }
 
