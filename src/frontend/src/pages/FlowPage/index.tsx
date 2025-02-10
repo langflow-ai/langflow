6421b3071a130fbb47a93472afb305774ec61941
anovazzi1 <otavio2204@gmail.com>
2025-02-10 19:17:47 -0300

    Add debug log for flow retrieval in FlowPage component

--- src/frontend/src/pages/FlowPage/index.tsx
+++ src/frontend/src/pages/FlowPage/index.tsx
@@ -143,6 +143,7 @@ export default function FlowPage({ view }: { view?: boolean }): JSX.Element {
 
   const getFlowToAddToCanvas = async (id: string) => {
     const flow = await getFlow({ id: id });
+    console.log("flow", flow);
     setCurrentFlow(flow);
   };
 
