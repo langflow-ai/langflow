66b4584ce00942e02a1e78da1780a45450f1a6b8
cristhianzl <cristhian.lousa@gmail.com>
2025-02-11 11:12:51 -0300

    ✨ (NodeDescription/index.tsx): refactor renderedDescription useMemo to improve readability and maintainability ♻️ (GenericNode/index.tsx): refactor code to improve readability and maintainability, and optimize rendering logic

--- src/frontend/src/CustomNodes/GenericNode/components/NodeDescription/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/NodeDescription/index.tsx
@@ -69,23 +69,23 @@ export default function NodeDescription({
   }, [description]);
 
   const MemoizedMarkdown = memo(Markdown);
-  const renderedDescription = useMemo(
-    () =>
-      description === "" || !description ? (
-        emptyPlaceholder
-      ) : (
-        <MemoizedMarkdown
-          linkTarget="_blank"
-          className={cn(
-            "markdown prose flex w-full flex-col text-[13px] leading-5 word-break-break-word [&_pre]:whitespace-break-spaces [&_pre]:!bg-code-description-background [&_pre_code]:!bg-code-description-background",
-            mdClassName,
-          )}
-        >
-          {String(description)}
-        </MemoizedMarkdown>
-      ),
-    [description, emptyPlaceholder, mdClassName],
-  );
+
+  const renderedDescription = useMemo(() => {
+    if (description === "" || !description) {
+      return emptyPlaceholder;
+    }
+    return (
+      <MemoizedMarkdown
+        linkTarget="_blank"
+        className={cn(
+          "markdown prose flex w-full flex-col text-[13px] leading-5 word-break-break-word [&_pre]:whitespace-break-spaces [&_pre]:!bg-code-description-background [&_pre_code]:!bg-code-description-background",
+          mdClassName,
+        )}
+      >
+        {String(description)}
+      </MemoizedMarkdown>
+    );
+  }, [description, emptyPlaceholder, mdClassName]);
 
   const handleBlurFn = () => {
     setNodeDescription(nodeDescription);
