b240ea19b7ceb107de469ea591bd9b8956f984b4
Edwin Jose <edwin.jose@datastax.com>
2025-02-11 11:40:12 -0500

    feat: option to hide a column in frontend when defining a Table Input, hides Tool identifier from table display (#6222)
    * added changes to hide fields from table input
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: anovazzi1 <otavio2204@gmail.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/frontend/src/types/utils/functions.ts
+++ src/frontend/src/types/utils/functions.ts
@@ -26,6 +26,7 @@ export interface ColumnField {
   formatter?: FormatterType;
   description?: string;
   disable_edit?: boolean;
-  default?: any; // Add this line
+  default?: any;
   edit_mode?: "modal" | "inline" | "popover";
+  hidden?: boolean;
 }
