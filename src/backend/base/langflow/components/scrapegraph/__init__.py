dd12b602f5f23d50cc7e40164904ca08163785e8
Marco Vinciguerra <mvincig11@gmail.com>
2025-02-12 17:09:46 +0100

    feat: add search

--- src/backend/base/langflow/components/scrapegraph/__init__.py
+++ src/backend/base/langflow/components/scrapegraph/__init__.py
@@ -1,4 +1,5 @@
 from .scrapegraph_markdownify_api import ScrapeGraphMarkdownifyApi
 from .scrapegraph_smart_scraper_api import ScrapeGraphSmartScraperApi
+from .scrapegraph_search_api import ScrapeGraphSearchApi
 
-__all__ = ["ScrapeGraphMarkdownifyApi", "ScrapeGraphSmartScraperApi"]
+__all__ = ["ScrapeGraphMarkdownifyApi", "ScrapeGraphSmartScraperApi", "ScrapeGraphSearchApi"]
