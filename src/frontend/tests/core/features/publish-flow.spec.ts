16bd3945affd1c669fe68ea1dc5ed844854f6532
anovazzi1 <otavio2204@gmail.com>
2025-02-14 13:06:52 -0300

    test: add publish feature test using Playwright

---
+++ src/frontend/tests/core/features/publish-flow.spec.ts
@@ -0,0 +1,80 @@
+import { expect, test } from "@playwright/test";
+import { adjustScreenView } from "../../utils/adjust-screen-view";
+import { awaitBootstrapTest } from "../../utils/await-bootstrap-test";
+
+test(
+  "Publish feature test",
+  { tag: ["@release", "@workspace", "@api"] },
+  async ({ page,context }) => {
+    await awaitBootstrapTest(page);
+
+    await page.waitForSelector('[data-testid="blank-flow"]', {
+      timeout: 3000,
+    });
+    // retrieve the flow id from the url
+    const flowId = page.url().split("/").pop();
+    expect(flowId).toBeDefined();
+    expect(flowId).not.toBeNull();
+    expect(flowId!.length).toBeGreaterThan(0);
+    await page.getByTestId("blank-flow").click();
+    await page.waitForSelector('[data-testid="sidebar-search-input"]', {
+      timeout: 3000,
+    });
+
+    await page.getByTestId("sidebar-search-input").click();
+    await page.getByTestId("sidebar-search-input").fill("chat input");
+
+    await page.waitForSelector('[data-testid="inputsChat Input"]', {
+      timeout: 3000,
+    });
+    await page
+      .getByTestId("inputsChat Input")
+      .hover({ timeout: 3000 });
+    await page.getByTestId("add-component-button-chat-input").click();
+
+    await adjustScreenView(page);
+    // check if clicking the publish without active it before will do nothing
+    await page.getByTestId("publish-button").click();
+    await page.waitForSelector('[data-testid="shareable-playground"]', {
+      timeout: 3000,
+    });
+    // expect the save post request to be sent and resolved
+    await expect(page.waitForResponse(response => response.url().includes(flowId!) && response.status() === 200)).resolves.toBeTruthy();
+
+
+    await page.getByTestId("shareable-playground").click();
+    await expect(page.getByTestId("rf__wrapper")).toBeVisible();
+    // check if we can active the publish without going to the shareable playground
+    await page.getByTestId("publish-button").click();
+    await page.getByTestId("publish-switch").click();
+    await expect(page.getByTestId("rf__wrapper")).toBeVisible();
+    // expect publish switch to be checked
+    await expect(page.getByTestId("publish-switch")).toBeChecked();
+    // await for all post event to be resolved
+    const pagePromise = context.waitForEvent('page');
+    await page.getByTestId("shareable-playground").click();
+    const newPage = await pagePromise;
+    await newPage.waitForTimeout(3000);
+    const newUrl = newPage.url();
+    await newPage.getByPlaceholder("Send a message...").fill("Hello");
+    await newPage.getByTestId('button-send').click();
+    await expect(newPage.getByText("Hello")).toBeVisible();
+    // redirect back to the original page
+    await newPage.close();
+    await page.bringToFront();
+    // check if deactivate the publishworks
+    await page.getByTestId("publish-button").click();
+    await page.getByTestId("publish-switch").click();
+    await expect(page.getByTestId("rf__wrapper")).toBeVisible();
+    // expect publish switch to be checked
+    await expect(page.getByTestId("publish-switch")).toBeChecked({checked: false});
+    await page.getByTestId("shareable-playground").click();
+    await expect(page.getByTestId("rf__wrapper")).toBeVisible();
+    // navigate to the new page
+    await page.goto(newUrl);
+    await expect(page.getByTestId("mainpage_title")).toBeVisible();
+
+
+
+  },
+);
