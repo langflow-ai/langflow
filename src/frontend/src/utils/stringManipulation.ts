128ce5b6e9416831fdc67ce9b727c292a11eb825
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-02-14 13:49:40 -0300

    Changed formatFileSize to show B instead of bytes

--- src/frontend/src/utils/stringManipulation.ts
+++ src/frontend/src/utils/stringManipulation.ts
@@ -118,7 +118,7 @@ export const formatFileSize = (bytes: number): string => {
   if (bytes === 0) return "0 Bytes";
 
   const k = 1024;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  const sizes = ["B", "KB", "MB", "GB", "TB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(2))} ${sizes[i]}`;
