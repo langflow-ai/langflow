c8070257b139d43b73e62f69a9bfca0c0833889a
Will Brennan <wbrennan899@gmail.com>
2025-02-18 12:34:26 -0800

    add twelvelabs text embeddings

--- src/backend/base/langflow/components/twelvelabs/__init__.py
+++ src/backend/base/langflow/components/twelvelabs/__init__.py
@@ -1,7 +1,10 @@
 from .twelvelabs_embed import VideoToEmbeddings
+from .twelvelabs_text_embed import TwelveLabsTextEmbeddings
 from .video_file import VideoFile
+
 
 __all__ = [
     "VideoToEmbeddings",
-    "VideoFile"
+    "VideoFile",
+    "TwelveLabsTextEmbeddings"
 ]
