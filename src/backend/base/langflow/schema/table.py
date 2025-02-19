b89fb95ecf6a7ea44c41a7496811890a0d1f1ef3
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-02-19 18:39:38 -0300

    Added validator to make type as formatter and vice versa

--- src/backend/base/langflow/schema/table.py
+++ src/backend/base/langflow/schema/table.py
@@ -39,7 +39,7 @@ class Column(BaseModel):
     formatter: FormatterType | str | None = Field(default=None)
     type: FormatterType | str | None = Field(default=None)
     description: str | None = None
-    default: str | None = None
+    default: str | bool | int | float | None = None
     disable_edit: bool = Field(default=False)
     edit_mode: EditMode | None = Field(default=EditMode.POPOVER)
     hidden: bool = Field(default=False)
@@ -54,6 +54,19 @@ class Column(BaseModel):
     def set_formatter_from_type(self):
         if self.type and not self.formatter:
             self.formatter = self.validate_formatter(self.type)
+        if self.formatter in {"boolean", "bool"}:
+            valid_trues = ["True", "true", "1", "yes"]
+            valid_falses = ["False", "false", "0", "no"]
+            if self.default in valid_trues:
+                self.default = True
+            if self.default in valid_falses:
+                self.default = False
+        elif self.formatter in {"integer", "int"}:
+            self.default = int(self.default)
+        elif self.formatter in {"float"}:
+            self.default = float(self.default)
+        else:
+            self.default = str(self.default)
         return self
 
     @field_validator("formatter", mode="before")
