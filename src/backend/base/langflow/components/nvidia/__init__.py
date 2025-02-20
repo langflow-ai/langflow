4d6ddcc9bc57f121eaf948cbf2a10f2b79353db3
Jordan Frazier <122494242+jordanrfrazier@users.noreply.github.com>
2025-02-20 11:26:03 -0800

    feat: add nvidia ingest component (#6333)
    * initial
    
    * cleanup
    
    * [autofix.ci] apply automated fixes
    
    * ruff
    
    * add else
    
    * update deps
    
    * uv lock
    
    * Make nv-ingest an optional dep
    
    * revert change to validate
    
    * rebase fixes
    
    * [autofix.ci] apply automated fixes
    
    * Update language
    
    * add extra args to make target
    
    * [autofix.ci] apply automated fixes
    
    * update error language
    
    * lockfile update
    
    * rebase lockfile:
    
    * [autofix.ci] apply automated fixes
    
    * Adds nv-ingest by default to -ep docker image
    
    * caps fixes
    
    * update uv lock
    
    * revert ruff upgrade
    
    * ruff
    
    * Fix lint
    
    * [autofix.ci] apply automated fixes
    
    * No code changes made.
    
    * fix: update ruff configuration to ignore additional linting rule and allow built-in modules
    
    * fix: update ruff command to ignore linting rule A005 during autofix
    
    * fix: update Ruff check command to ignore linting rule A005
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>

--- src/backend/base/langflow/components/nvidia/__init__.py
+++ src/backend/base/langflow/components/nvidia/__init__.py
@@ -1,3 +1,4 @@
+from .nvidia_ingest import NvidiaIngestComponent
 from .nvidia_rerank import NvidiaRerankComponent
 
-__all__ = ["NvidiaRerankComponent"]
+__all__ = ["NvidiaIngestComponent", "NvidiaRerankComponent"]
