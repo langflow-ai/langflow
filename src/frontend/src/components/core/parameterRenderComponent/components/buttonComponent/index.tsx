c2424de9e49e60e43706bfcdd48124f683c38e4f
deon-sanchez <deon.sanchez@datastax.com>
2025-03-11 10:03:12 -0600

    feat: Enhance ButtonComponent with dynamic rendering and improved UX

--- src/frontend/src/components/core/parameterRenderComponent/components/buttonComponent/index.tsx
+++ src/frontend/src/components/core/parameterRenderComponent/components/buttonComponent/index.tsx
@@ -10,11 +10,18 @@ import { ReactSortable } from "react-sortablejs";
 type ButtonComponentProps = {
   tooltip?: string;
   type: "tool_name" | "actions" | undefined;
+  name?: string;
 };
 
-const ButtonComponent = ({ tooltip = "", type }: ButtonComponentProps) => {
+const ButtonComponent = ({
+  tooltip = "",
+  type,
+  name,
+}: ButtonComponentProps) => {
+  const isDropdown = type !== "actions";
   const [open, setOpen] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(isDropdown);
+
   const initialActionData = [
     {
       name: "Accept a repository invitation",
@@ -33,97 +40,90 @@ const ButtonComponent = ({ tooltip = "", type }: ButtonComponentProps) => {
   const [actionData, setActionData] = useState(initialActionData);
 
   return (
-    <>
-      {type === "tool_name" ? (
-        <div className="flex w-full flex-row gap-2">
-          <Button
-            variant="outline"
-            size="xs"
-            onClick={() => setOpen(true)}
-            className="w-full py-2"
-          >
+    <div className="flex w-full flex-col gap-2">
+      <div className="flex w-full flex-row gap-2">
+        <Button
+          variant={isDropdown ? "outline" : "default"}
+          size="xs"
+          onClick={() => setOpen(true)}
+          className="w-full py-2"
+        >
+          {isDropdown ? (
             <div className="flex w-full items-center justify-start text-sm">
-              Select a tool...
+              {name || "Select a tool..."}
               <ForwardedIconComponent
                 name="ChevronsUpDown"
                 className="ml-auto h-5 w-5"
               />
             </div>
-          </Button>
-          {!isAuthenticated && (
-            <Button
-              size="icon"
-              variant="warning"
-              className="h-9 w-10 rounded-md border border-accent-amber-foreground"
-              onClick={() => setOpen(true)}
-            >
-              <ForwardedIconComponent
-                name="unplug"
-                className="h-5 w-5 text-accent-amber-foreground"
-              />
-            </Button>
+          ) : (
+            <div className="flex items-center text-sm font-semibold">
+              {name || "Select action"}
+            </div>
           )}
-        </div>
-      ) : (
-        <div className="flex w-full flex-col gap-2">
-          <div className="flex w-full flex-row gap-2">
-            <Button
-              variant="default"
-              size="xs"
-              onClick={() => setOpen(true)}
-              className="w-full py-2"
-            >
-              <div className="flex items-center text-sm font-semibold">
-                Select action
-              </div>
-            </Button>
-          </div>
-          <div className="flex w-full flex-col">
-            <ReactSortable
-              list={actionData}
-              setList={setActionData}
-              className="flex w-full flex-col"
-            >
-              {actionData.map((data, index) => (
-                <li
-                  key={data?.id}
-                  className="group inline-flex h-12 w-full cursor-grab items-center gap-2 text-sm font-medium text-gray-800"
+        </Button>
+        {isAuthenticated && (
+          <Button
+            size="icon"
+            variant="warning"
+            className="h-9 w-10 rounded-md border border-accent-amber-foreground"
+            onClick={() => setOpen(true)}
+          >
+            <ForwardedIconComponent
+              name="unplug"
+              className="h-5 w-5 text-accent-amber-foreground"
+            />
+          </Button>
+        )}
+      </div>
+
+      {!isDropdown && (
+        <div className="flex w-full flex-col">
+          <ReactSortable
+            list={actionData}
+            setList={setActionData}
+            className="flex w-full flex-col"
+          >
+            {actionData.map((data, index) => (
+              <li
+                key={data?.id}
+                className="group inline-flex h-12 w-full cursor-grab items-center gap-2 text-sm font-medium text-gray-800"
+              >
+                <ForwardedIconComponent
+                  name="grid-horizontal"
+                  className="h-5 w-5 fill-gray-300 text-gray-300"
+                />
+
+                <div className="flex w-full items-center gap-x-2">
+                  <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-center text-white">
+                    {index + 1}
+                  </div>
+
+                  <span className="max-w-48 truncate text-primary">
+                    {data.name}
+                  </span>
+                </div>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="ml-auto h-7 w-7 opacity-0 transition-opacity duration-200 hover:bg-red-100 hover:opacity-100"
+                  onClick={() => {
+                    console.log("clicked");
+                  }}
                 >
                   <ForwardedIconComponent
-                    name="grid-horizontal"
-                    className="h-5 w-5 fill-gray-300 text-gray-300"
+                    name="x"
+                    className="h-6 w-6 text-red-500"
                   />
-
-                  <div className="flex w-full items-center gap-x-2">
-                    <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-center text-white">
-                      {index + 1}
-                    </div>
-
-                    <span className="max-w-48 truncate">{data.name}</span>
-                  </div>
-                  <Button
-                    size="icon"
-                    variant="ghost"
-                    className="ml-auto h-7 w-7 opacity-0 transition-opacity duration-200 hover:bg-red-100 hover:opacity-100"
-                    onClick={() => {
-                      console.log("clicked");
-                    }}
-                  >
-                    <ForwardedIconComponent
-                      name="x"
-                      className="h-6 w-6 text-red-500"
-                    />
-                  </Button>
-                </li>
-              ))}
-            </ReactSortable>
-          </div>
+                </Button>
+              </li>
+            ))}
+          </ReactSortable>
         </div>
       )}
-      <ListSelectionComponent open={open} onClose={() => setOpen(false)} />
-    </>
 
-    // <ListSelectionComponent open={open} onClose={() => setOpen(false)} />
+      <ListSelectionComponent open={open} onClose={() => setOpen(false)} />
+    </div>
   );
 };
 
