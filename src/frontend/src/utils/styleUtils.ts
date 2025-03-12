1b5bf3ec3c4e0f9f9eccf57cfd669abc84f54778
galatro <galatrogiovanni@gmail.com>
2025-03-12 20:13:18 +0100

    feat: add icon to styleUtils

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -312,6 +312,7 @@ import { Streamlit } from "../icons/Streamlit";
 import { UpstashSvgIcon } from "../icons/Upstash";
 import { VectaraIcon } from "../icons/VectaraIcon";
 import { VertexAIIcon } from "../icons/VertexAI";
+import { WatsonxAiIcon } from "@/icons/IBMWatsonx";
 import { WeaviateIcon } from "../icons/Weaviate";
 import SvgWikipedia from "../icons/Wikipedia/Wikipedia";
 import SvgWolfram from "../icons/Wolfram/Wolfram";
@@ -749,6 +750,7 @@ export const nodeIconsLucide: iconsType = {
   Arize: ArizeIcon,
   Apify: ApifyIcon,
   ApifyWhite: ApifyWhiteIcon,
+  WatsonxAI: WatsonxAiIcon,
 
   //Node Icons
   model_specs: FileSliders,
