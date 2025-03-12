0a319579e6320c22ec5f7853ec3f8ca26dbc4355
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2025-03-12 20:20:01 -0300

    fix: Disable retries in usePostValidateComponentCode mutation (#7044)
    ♻️ (use-post-validate-component-code.ts): refactor usePostValidateComponentCode function to include retry and retryDelay options for better control over mutation behavior
    
    Co-authored-by: Gabriel Luiz Freitas Almeida <gabriel@langflow.org>

--- src/frontend/src/controllers/API/queries/nodes/use-post-validate-component-code.ts
+++ src/frontend/src/controllers/API/queries/nodes/use-post-validate-component-code.ts
@@ -40,11 +40,11 @@ export const usePostValidateComponentCode: useMutationFunctionType<
     CustomComponentRequest,
     ResponseErrorTypeAPI,
     IPostValidateComponentCode
-  > = mutate(
-    ["usePostValidateComponentCode"],
-    postValidateComponentCodeFn,
-    options,
-  );
+  > = mutate(["usePostValidateComponentCode"], postValidateComponentCodeFn, {
+    ...options,
+    retry: 0,
+    retryDelay: 0,
+  });
 
   return mutation;
 };
