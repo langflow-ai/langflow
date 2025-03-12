934573dfd9c7dfecbe62999f08fa2aec95a1b87f
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2025-03-12 20:44:13 -0300

    feat: Add updateHiddenOutputs helper function to manage output visibility (#6932)
    ✨ (update-hidden-outputs.ts): add a new helper function to update hidden outputs in the frontend CustomNodes module
    ♻️ (use-update-all-nodes.ts): refactor useUpdateAllNodes hook to update hidden outputs for all nodes in the frontend CustomNodes module
    ♻️ (use-update-node-code.ts): refactor useUpdateNodeCode hook to update hidden outputs for a specific node's code in the frontend CustomNodes module
    ♻️ (index.ts): refactor types in the flow module to include OutputFieldType for better type checking and consistency

--- src/frontend/src/CustomNodes/hooks/use-update-all-nodes.ts
+++ src/frontend/src/CustomNodes/hooks/use-update-all-nodes.ts
@@ -1,7 +1,8 @@
 import { AllNodeType } from "@/types/flow";
 import { cloneDeep } from "lodash";
 import { useCallback } from "react";
-import { APIClassType } from "../../types/api";
+import { APIClassType, OutputFieldType } from "../../types/api";
+import { updateHiddenOutputs } from "../helpers/update-hidden-outputs";
 
 export type UpdateNodesType = {
   nodeId: string;
@@ -27,6 +28,8 @@ const useUpdateAllNodes = (
           if (nodeIndex === -1) return;
 
           const updatedNode = newNodes[nodeIndex];
+          const outputs = updatedNode?.data.node?.outputs;
+
           updatedNode.data = {
             ...updatedNode.data,
             node: {
@@ -44,6 +47,13 @@ const useUpdateAllNodes = (
           }
 
           updatedNode.data.node!.template[name].value = code;
+
+          const updatedOutputs = updatedNode.data.node?.outputs;
+          updatedNode.data.node!.outputs = updateHiddenOutputs(
+            outputs!,
+            updatedOutputs!,
+          );
+
           updateNodeInternals(nodeId);
         });
 
