138ccbe6ed4947a7dd0d1453247bd285d77285a3
Eddie Ho <noodleslove@icloud.com>
2025-03-12 20:52:40 -0700

    fix: pass props to SvgAnthropicBox component in light mode

--- src/frontend/src/icons/Anthropic/Anthropic.jsx
+++ src/frontend/src/icons/Anthropic/Anthropic.jsx
@@ -21,6 +21,7 @@ const SvgAnthropicBox = (props) => {
       viewBox="0 0 280 196"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M201.88 0H159.04L237.16 196H280L201.88 0Z" fill="#1F1F1E" />
       <path
