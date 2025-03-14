78ef46c08d24312e60f6c76c114909f6e23afbb4
italojohnny <italojohnnydosanjos@gmail.com>
2025-03-14 14:30:58 -0300

    fix: alembic multiple heads error

--- src/backend/base/langflow/alembic/versions/f3b2d1f1002d_add_column_access_type_to_flow.py
+++ src/backend/base/langflow/alembic/versions/f3b2d1f1002d_add_column_access_type_to_flow.py
@@ -1,7 +1,7 @@
 """add column 'access_type' to flow
 
 Revision ID: f3b2d1f1002d
-Revises: e3162c1804e6
+Revises: 93e2705fa8d6
 Create Date: 2025-02-05 14:35:29.658101
 
 """
@@ -14,7 +14,7 @@ from langflow.utils import migration
 
 # revision identifiers, used by Alembic.
 revision: str = 'f3b2d1f1002d'
-down_revision: Union[str, None] = 'dd9e0804ebd1'
+down_revision: Union[str, None] = '93e2705fa8d6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
