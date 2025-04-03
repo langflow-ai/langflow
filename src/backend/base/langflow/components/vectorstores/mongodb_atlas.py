c7dfabc7d207fbe2d0a7c3ced4a03de238223d32
Gustavo Costa <gsantosaero@gmail.com>
2025-04-03 13:58:06 -0300

    create verify_search_index

--- src/backend/base/langflow/components/vectorstores/mongodb_atlas.py
+++ src/backend/base/langflow/components/vectorstores/mongodb_atlas.py
@@ -1,8 +1,10 @@
 import tempfile
+import time
 
 import certifi
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from pymongo.collection import Collection
+from pymongo.operations import SearchIndexModel
 
 from langflow.base.vectorstores.model import LCVectorStoreComponent, check_cached_vector_store
 from langflow.helpers.data import docs_to_data
@@ -16,7 +18,8 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
     name = "MongoDBAtlasVector"
     icon = "MongoDB"
     INSERT_MODES = ["append", "overwrite"]
-
+    SIMILARITY_OPTIONS = ["cosine", "euclidean", "dot product"]
+    QUANTIZATION_OPTIONS = ["scalar", "binary"]
     inputs = [
         SecretStrInput(name="mongodb_atlas_cluster_uri", display_name="MongoDB Atlas Cluster URI", required=True),
         BoolInput(name="enable_mtls", display_name="Enable mTLS", value=False, advanced=True, required=True),
@@ -51,6 +54,41 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
             display_name="Number of Results",
             info="Number of results to return.",
             value=4,
+            advanced=True,
+        ),
+        StrInput(
+            name="index_field",
+            display_name="Index Field",
+            advanced=True,
+            required=True,
+            info="The field to index.",
+            value="embedding",
+        ),
+        StrInput(
+            name="filter_field", display_name="Filter Field", advanced=True, info="The field to filter the index."
+        ),
+        IntInput(
+            name="number_dimensions",
+            display_name="Number of Dimensions",
+            info="Embedding Context Length.",
+            value=1536,
+            advanced=True,
+            required=True,
+        ),
+        DropdownInput(
+            name="similarity",
+            display_name="Similarity",
+            options=SIMILARITY_OPTIONS,
+            value=SIMILARITY_OPTIONS[0],
+            info="The method used to measure the similarity between vectors.",
+            advanced=True,
+        ),
+        DropdownInput(
+            name="quantization",
+            display_name="Quantization",
+            options=QUANTIZATION_OPTIONS,
+            value=None,
+            info="Quantization reduces memory costs converting 32-bit floats to smaller data types",
             advanced=True,
         ),
     ]
@@ -123,6 +161,8 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
 
         vector_store = self.build_vector_store()
 
+        self.verify_search_index(vector_store._collection)
+
         if self.search_query and isinstance(self.search_query, str):
             docs = vector_store.similarity_search(
                 query=self.search_query,
@@ -141,3 +181,37 @@ class MongoVectorStoreComponent(LCVectorStoreComponent):
     def __insert_mode(self, collection: Collection) -> None:
         if self.insert_mode == "overwrite":
             collection.delete_many({})  # Delete all documents while preserving collection structure
+
+    def verify_search_index(self, collection: Collection) -> None:
+        """Verify if the search index exists, if not, create it.
+
+        Args:
+            collection (Collection): The collection to verify the search index on.
+        """
+        indexes = collection.list_search_indexes()
+
+        index_names_types = {idx["name"]: idx["type"] for idx in indexes}
+        index_names = list(index_names_types.keys())
+        index_type = index_names_types.get(self.index_name)
+        if self.index_name not in index_names and index_type != "vectorSearch":
+            collection.create_search_index(self.__create_index_definition())
+
+            time.sleep(20)  # Give some time for index to be ready
+
+    def __create_index_definition(self) -> SearchIndexModel:
+        fields = [
+            {
+                "type": "vector",
+                "path": self.index_field,
+                "numDimensions": self.number_dimensions,
+                "similarity": self.similarity,
+                "quantization": self.quantization,
+            }
+        ]
+        if self.filter_field:
+            fields.append({"type": "filter", "path": self.filter_field})
+        return SearchIndexModel(
+            definition={"fields": fields},
+            name=self.index_name,
+            type="vectorSearch",
+        )
