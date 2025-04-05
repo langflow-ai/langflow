337c03bb756085656a3da03d1a4ea05cf12bd948
abhishekpatil4 <abhishekpersonal2@gmail.com>
2025-04-05 22:09:54 +0530

    chore: update googlemeet component filename

--- src/backend/base/langflow/components/composio/__init__.py
+++ src/backend/base/langflow/components/composio/__init__.py
@@ -2,7 +2,7 @@ from .composio_api import ComposioAPIComponent
 from .github_api import GitHubAPIComponent
 from .gmail_composio import ComposioGmailAPIComponent
 from .googlecalendar_api import GooglecalendarAPIComponent
-from .googlemeet_api import ComposioGooglemeetAPIComponent
+from .googlemeet_composio import ComposioGooglemeetAPIComponent
 from .googlesheets_api import GooglesheetsAPIComponent
 from .slack_api import SlackAPIComponent
 
