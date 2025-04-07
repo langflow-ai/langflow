a28a7c21cdba099191ce4ac2382ef9928f3eebde
abhishekpatil4 <abhishekpersonal2@gmail.com>
2025-04-07 15:58:03 +0530

    feat: update google sheets component to use new inputs & Composio base class

--- src/backend/base/langflow/components/composio/__init__.py
+++ src/backend/base/langflow/components/composio/__init__.py
@@ -3,7 +3,7 @@ from .github_composio import ComposioGitHubAPIComponent
 from .gmail_composio import ComposioGmailAPIComponent
 from .googlecalendar_composio import ComposioGoogleCalendarAPIComponent
 from .googlemeet_composio import ComposioGooglemeetAPIComponent
-from .googlesheets_api import GooglesheetsAPIComponent
+from .googlesheets_composio import ComposioGooglesheetsAPIComponent
 from .slack_api import SlackAPIComponent
 
 __all__ = [
@@ -12,6 +12,6 @@ __all__ = [
     "ComposioGmailAPIComponent",
     "ComposioGoogleCalendarAPIComponent",
     "ComposioGooglemeetAPIComponent",
-    "GooglesheetsAPIComponent",
+    "ComposioGooglesheetsAPIComponent",
     "SlackAPIComponent",
 ]
