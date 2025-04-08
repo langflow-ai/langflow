a8e532541e49eb1200f6e731bb6202a31e5d6a03
Gustavo Costa <gsantosaero@gmail.com>
2025-04-08 16:37:01 -0300

    param filter in pgvector

--- src/backend/base/langflow/components/vectorstores/pgvector.py
+++ src/backend/base/langflow/components/vectorstores/pgvector.py
@@ -62,8 +62,7 @@ class PGVectorStoreComponent(LCVectorStoreComponent):
 
         if self.search_query and isinstance(self.search_query, str) and self.search_query.strip():
             docs = vector_store.similarity_search(
-                query=self.search_query,
-                k=self.number_of_results,
+                query=self.search_query, k=self.number_of_results, filter=self.search_filter
             )
 
             data = docs_to_data(docs)
