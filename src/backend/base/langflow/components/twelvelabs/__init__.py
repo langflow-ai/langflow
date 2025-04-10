19106a4e58704361ddbfe4e114de0ca460466bef
Will Brennan <wbrennan899@gmail.com>
2025-04-09 19:02:46 -0700

    remove custom astra components

--- src/backend/base/langflow/components/twelvelabs/__init__.py
+++ src/backend/base/langflow/components/twelvelabs/__init__.py
@@ -1,19 +1,14 @@
 from .twelvelabs_pegasus import TwelveLabsPegasus
 from .video_file import VideoFile
 from .twelve_labs_multi_text import TwelveLabsMultiTextInput
-from .twelve_labs_astra_upload import TwelveLabsAstraUpload
-from .twelve_labs_astra_text_video_search import TwelveLabsAstraTextVideoSearch
 from .video_embeddings import TwelveLabsVideoEmbeddingsComponent
 from .text_embeddings import TwelveLabsTextEmbeddingsComponent
 from .split_video import SplitVideoComponent
 
 __all__ = [
     "VideoFile",
-    "VideoDirectoryComponent",
     "TwelveLabsPegasus",
     "TwelveLabsMultiTextInput",
-    "TwelveLabsAstraUpload",
-    "TwelveLabsAstraTextVideoSearch",
     "TwelveLabsTextEmbeddingsComponent",
     "TwelveLabsVideoEmbeddingsComponent",
     "SplitVideoComponent",
