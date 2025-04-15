cf1d734dfaa9a560c28897661dff29b51b4f3795
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-04-15 15:35:23 -0300

    Added Tools modal with AgGrid

--- src/frontend/src/modals/toolsModal/index.tsx
+++ src/frontend/src/modals/toolsModal/index.tsx
@@ -2,11 +2,20 @@ import ForwardedIconComponent from "@/components/common/genericIconComponent";
 import TableComponent, {
   TableComponentProps,
 } from "@/components/core/parameterRenderComponent/components/tableComponent";
-import ComboBoxItem from "@/CustomNodes/GenericNode/components/ListSelectionComponent/ComboBoxItem";
+import { Input } from "@/components/ui/input";
 import { handleOnNewValueType } from "@/CustomNodes/hooks/use-handle-new-value";
 import { TableOptionsTypeAPI } from "@/types/api";
+import { ColDef } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
-import { ForwardedRef, forwardRef } from "react";
+import { cloneDeep } from "lodash";
+import {
+  ForwardedRef,
+  forwardRef,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import BaseModal from "../baseModal";
 
 interface ToolsModalProps {
@@ -17,6 +26,7 @@ interface ToolsModalProps {
     name: string;
     tags: string[];
     description: string;
+    status: boolean;
   }[];
   handleOnNewValue: handleOnNewValueType;
   title: string;
@@ -37,9 +47,75 @@ const ToolsModal = forwardRef<AgGridReact, ToolsModalProps>(
     }: ToolsModalProps,
     ref: ForwardedRef<AgGridReact>,
   ) => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedRows, setSelectedRows] = useState<any[] | null>(null);
+    const agGrid = useRef<AgGridReact>(null);
+    const [data, setData] = useState<any[]>(cloneDeep(rows));
+
     const handleSetOpen = (newOpen: boolean) => {
       if (setOpen) {
         setOpen(newOpen);
+      }
+    };
+
+    useEffect(() => {
+      const initialData = cloneDeep(rows);
+      setData(initialData);
+      const filter = initialData.filter((row) => row.status === true);
+      setSelectedRows(filter);
+    }, [rows, open]);
+
+    useEffect(() => {
+      const initialData = cloneDeep(rows);
+      const filter = initialData.filter((row) => row.status === true);
+      if (agGrid.current) {
+        agGrid.current?.api.forEachNode((node) => {
+          if (filter.some((row) => row.name === node.data.name)) {
+            node.setSelected(true);
+          } else {
+            node.setSelected(false);
+          }
+        });
+      }
+    }, [agGrid.current]);
+
+    useEffect(() => {
+      if (!open && selectedRows) {
+        handleOnNewValue({
+          value: rows.map((row) =>
+            selectedRows?.some((selected) => selected.name === row.name)
+              ? { ...row, status: true }
+              : { ...row, status: false },
+          ),
+        });
+      }
+    }, [open]);
+
+    const columnDefs: ColDef[] = [
+      {
+        field: "name",
+        headerName: "Name",
+        flex: 1,
+        valueGetter: (params) => params.data.name.replaceAll("-", " "),
+      },
+      {
+        field: "description",
+        headerName: "Description",
+        flex: 2,
+        resizable: false,
+      },
+      {
+        field: "tags",
+        headerName: "Tags",
+        flex: 1,
+        hide: true,
+        valueGetter: (params) => params.data.tags.join(", "),
+      },
+    ];
+    const handleSelectionChanged = (event) => {
+      if (open) {
+        const selectedData = event.api.getSelectedRows();
+        setSelectedRows(selectedData);
       }
     };
 
@@ -54,20 +130,38 @@ const ToolsModal = forwardRef<AgGridReact, ToolsModalProps>(
       >
         <BaseModal.Header>
           <div className="flex w-full flex-row items-center border-b border-border px-4 py-3">
-            <div className="mr-2 rounded-md border border-border bg-primary">
-              <ForwardedIconComponent
-                name={icon ?? "Table"}
-                className="h-6 w-6 p-1.5"
-              />
-            </div>
-            <div className="text-[13px]">{title}</div>
+            <ForwardedIconComponent
+              name={icon ?? "Table"}
+              className="mr-2 h-6 w-6"
+            />
+            <div>{title}</div>
           </div>
         </BaseModal.Header>
-        <BaseModal.Content className="px-4 py-3">
-          <div className="flex flex-col gap-2">
-            {rows.map((item) => (
-              <ComboBoxItem key={item.name} item={item} />
-            ))}
+        <BaseModal.Content className="flex flex-col gap-2 px-0 py-3">
+          <div className="flex-1 px-4">
+            <Input
+              icon="Search"
+              placeholder="Search actions..."
+              inputClassName="h-8"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          <div className="h-[400px]">
+            <TableComponent
+              columnDefs={columnDefs}
+              rowData={data}
+              quickFilterText={searchQuery}
+              ref={agGrid}
+              rowSelection="multiple"
+              className="ag-tool-mode w-full overflow-visible"
+              headerHeight={32}
+              rowHeight={32}
+              onSelectionChanged={handleSelectionChanged}
+              tableOptions={{
+                block_hide: true,
+              }}
+            />
           </div>
         </BaseModal.Content>
       </BaseModal>
