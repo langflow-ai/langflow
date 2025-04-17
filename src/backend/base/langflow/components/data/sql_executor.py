3b60ec1b86928c0b84638daf63136f0819c27256
phact <estevezsebastian@gmail.com>
2025-04-17 00:08:22 -0400

    cache

--- src/backend/base/langflow/components/data/sql_executor.py
+++ src/backend/base/langflow/components/data/sql_executor.py
@@ -3,33 +3,57 @@ import logging
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain_community.utilities import SQLDatabase
 
-from langflow.custom.custom_component.component import Component
+from langflow.custom.custom_component.component_with_cache import ComponentWithCache
 from langflow.io import MessageTextInput, Output
 from langflow.schema.message import Message
+from langflow.services.cache.utils import CacheMiss
 
 logger = logging.getLogger(__name__)
 
 
-class SQLComponent(Component):
+class SQLComponent(ComponentWithCache):
     """A sql component."""
 
-    display_name = "SQL"
-    description = "Load and parse child links from a root URL recursively"
+    display_name = "SQL Query"
+    description = "Execute SQL Query"
     icon = "layout-template"
     name = "SQLComponent"
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.db = None
+        self.maybe_create_db()
+
+    def maybe_create_db(self):
+        if self.database_url != "":
+            cached_db = self._shared_component_cache.get(self.database_url)
+            if not isinstance(cached_db, CacheMiss):
+                self.db = cached_db
+                return
+            logger.info("Connecting to database")
+            try:
+                self.db = SQLDatabase.from_uri(self.database_url)
+            except Exception as e:
+                msg = f"An error occurred while connecting to the database: {e}"
+                raise ValueError(msg) from e
+            self._shared_component_cache.set(self.database_url, self.db)
 
     inputs = [
         MessageTextInput(
             name="database_url",
-            display_name="database url",
+            display_name="Database URL",
             placeholder="Enter a URL...",
-            list_add_label="Add URL",
         ),
         MessageTextInput(
             name="query",
-            display_name="query",
+            display_name="Query",
             placeholder="query...",
-            list_add_label="query",
+            tool_mode=True,
+        ),
+        MessageTextInput(
+            name="include_columns",
+            display_name="Include Columns",
+            placeholder="False",
             tool_mode=True,
         ),
     ]
@@ -42,16 +66,14 @@ class SQLComponent(Component):
         self,
     ) -> Message:
         error = None
+        self.maybe_create_db()
         try:
-            database = SQLDatabase.from_uri(self.database_url)
-        except Exception as e:
-            msg = f"An error occurred while connecting to the database: {e}"
-            raise ValueError(msg) from e
-        try:
-            tool = QuerySQLDataBaseTool(db=database)
-            result = tool.run(self.query, include_columns=True)
+            tool = QuerySQLDataBaseTool(db=self.db)
+            result = tool.run(self.query, include_columns=self.include_columns)
             self.status = result
-        except Exception as e:  # noqa: BLE001
+        except Exception as e:
+            msg = f"An error occurred while running the SQL Query: {e}"
+            logger.exception(msg)
             result = str(e)
             self.status = result
             error = repr(e)
