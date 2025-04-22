ecf897d5e9ebc0f7c3c2918ba86e353d57e13bd6
omahs <73983677+omahs@users.noreply.github.com>
2025-04-22 11:44:53 +0200

    fix: typo

--- src/frontend/src/modals/IOModal/components/chatView/chatMessage/components/content-view.tsx
+++ src/frontend/src/modals/IOModal/components/chatView/chatMessage/components/content-view.tsx
@@ -66,7 +66,7 @@ export const ErrorView = ({
                             {content.component && (
                               <>
                                 <span>
-                                  An error occured in the{" "}
+                                  An error occurred in the{" "}
                                   <span
                                     className={cn(
                                       closeChat ?? "cursor-pointer underline",
