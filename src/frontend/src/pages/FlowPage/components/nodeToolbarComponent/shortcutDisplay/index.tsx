fd41e7b638713b144f6c2c82a947952669e82657
cristhianzl <cristhian.lousa@gmail.com>
2025-04-23 17:42:12 -0300

    ✨ (frontend): Update icon sizes in various components for better UI consistency and aesthetics.

--- src/frontend/src/pages/FlowPage/components/nodeToolbarComponent/shortcutDisplay/index.tsx
+++ src/frontend/src/pages/FlowPage/components/nodeToolbarComponent/shortcutDisplay/index.tsx
@@ -18,7 +18,7 @@ export default function ShortcutDisplay({
           {display_name && <span> {display_name} </span>}
           <span
             className={cn(
-              "flex h-[16px] w-[16px] items-center justify-center rounded-sm bg-muted text-muted-foreground",
+              "flex h-4 w-4 items-center justify-center rounded-sm bg-muted text-muted-foreground",
               display_name && "ml-3",
             )}
           >
