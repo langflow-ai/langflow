f6b7fcc426f249ef6852e517a8ea9cfef2b0a9ad
Kiko Castillo <fjcastillocarrasco@gmail.com>
2025-04-25 17:24:54 -0700

    fix: authorization header for Arize

--- src/backend/base/langflow/services/tracing/arize_phoenix.py
+++ src/backend/base/langflow/services/tracing/arize_phoenix.py
@@ -103,7 +103,7 @@ class ArizePhoenixTracer(BaseTracer):
         enable_arize_tracing = bool(arize_api_key and arize_space_id)
         arize_endpoint = f"{arize_collector_endpoint}/v1"
         if enable_arize_tracing:
-            os.environ["OTEL_EXPORTER_OTLP_TRACES_HEADERS"] = f"api_key={arize_api_key},space_id={arize_space_id}"
+            os.environ["OTEL_EXPORTER_OTLP_TRACES_HEADERS"] = f"authorization=Bearer {arize_api_key},space_id={arize_space_id}"
 
         # Phoenix Config
         phoenix_api_key = os.getenv("PHOENIX_API_KEY", None)
