99f90a31aa3ad9a1270f9652d964f642fcf20726
Jordan Frazier <jordan.frazier@datastax.com>
2025-04-28 12:01:14 -0700

    Fix fastapi type

--- src/backend/base/langflow/api/v1/endpoints.py
+++ src/backend/base/langflow/api/v1/endpoints.py
@@ -266,7 +266,7 @@ async def run_flow_generator(
         await event_manager.queue.put((None, None, time.time))
 
 
-@router.post("/run/{flow_id_or_name}", response_model=RunResponse, response_model_exclude_none=True)
+@router.post("/run/{flow_id_or_name}", response_model=None, response_model_exclude_none=True)
 async def simplified_run_flow(
     *,
     background_tasks: BackgroundTasks,
