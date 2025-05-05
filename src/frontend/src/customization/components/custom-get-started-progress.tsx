822405b8db77e24ff5147e5f44a6a7a4c67ebf5b
cristhianzl <cristhian.lousa@gmail.com>
2025-05-05 11:55:41 -0300

    ✨ (frontend): Add new customization components and controllers for the frontend application.

---
+++ src/frontend/src/customization/components/custom-get-started-progress.tsx
@@ -0,0 +1,5 @@
+export function GetStartedProgress() {
+  return <></>;
+}
+
+export default GetStartedProgress;
---
+++ src/frontend/src/customization/components/custom-linkComponent.tsx
@@ -0,0 +1,5 @@
+export function LinkComponent() {
+  return <></>;
+}
+
+export default LinkComponent;
