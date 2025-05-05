09718b74668b86ad22e515ab8bc9ae18a95043e1
Edwin Jose <edwin.jose@datastax.com>
2025-05-05 15:04:23 -0400

    fix format

--- src/backend/base/langflow/base/composio/composio_base.py
+++ src/backend/base/langflow/base/composio/composio_base.py
@@ -282,8 +282,8 @@ class ComposioBaseComponent(Component):
         configured_tools = []
         for tool in tools:
             # Set the sanitized name
-            display_name = (
-                self._actions_data.get(tool.name, {}).get("display_name", self._sanitized_names.get(tool.name, self._name_sanitizer.sub("-", tool.name)))
+            display_name = self._actions_data.get(tool.name, {}).get(
+                "display_name", self._sanitized_names.get(tool.name, self._name_sanitizer.sub("-", tool.name))
             )
             # Set the tags
             tool.tags = [tool.name]
