1cad972e9ebf4de35cc0cfc9cb86e5d063709152
phact <estevezsebastian@gmail.com>
2025-05-12 09:50:50 -0400

    maybe_register bug fix

--- src/backend/base/langflow/components/nvidia/system_assist.py
+++ src/backend/base/langflow/components/nvidia/system_assist.py
@@ -23,17 +23,16 @@ class NvidiaSystemAssistComponent(ComponentWithCache):
         super().__init__(*args, **kwargs)
 
     def maybe_register_rise_client(self):
-        if self.database_url != "":
-            rise_initialized = self._shared_component_cache.get("rise_initialized")
-            if not isinstance(rise_initialized, CacheMiss) and rise_initialized:
-                return
-            self.log("Initializing Rise Client")
-            try:
-                register_rise_client()
-                self._shared_component_cache.set(key="rise_initialized", value=True)
-            except Exception as e:
-                msg = f"An error occurred initializing rise client: {e}"
-                raise ValueError(msg) from e
+        rise_initialized = self._shared_component_cache.get("rise_initialized")
+        if not isinstance(rise_initialized, CacheMiss) and rise_initialized:
+            return
+        self.log("Initializing Rise Client")
+        try:
+            register_rise_client()
+            self._shared_component_cache.set(key="rise_initialized", value=True)
+        except Exception as e:
+            msg = f"An error occurred initializing rise client: {e}"
+            raise ValueError(msg) from e
 
     inputs = [
         MessageTextInput(
