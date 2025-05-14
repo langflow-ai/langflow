b28232e4a9a8a57e0e5d53c4ce943e3431e734cd
Chris Mauck <chrismauck10@gmail.com>
2025-05-14 16:31:37 -0400

    Fix icon logic and add support for light/dark mode dynamic logo

--- src/frontend/src/icons/Cleanlab/index.tsx
+++ src/frontend/src/icons/Cleanlab/index.tsx
@@ -1,3 +1,4 @@
+import { useDarkStore } from "@/stores/darkStore";
 import React, { forwardRef } from "react";
 import SvgCleanlab from "./Cleanlab";
 
@@ -5,5 +6,6 @@ export const CleanlabIcon = forwardRef<
   SVGSVGElement,
   React.PropsWithChildren<{}>
 >((props, ref) => {
-  return <SvgCleanlab ref={ref} {...props} />;
+  const isdark = useDarkStore((state) => state.dark).toString();
+  return <SvgCleanlab ref={ref} isdark={isdark} {...props} />;
 });
