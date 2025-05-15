794c186685071314562a6a751fa03cba563a53e2
nischayparekh <nischay@linkup.so>
2025-05-15 15:09:36 +0200

    feat: Linkup Search Tool
    - added Linkup Search Tool

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -372,6 +372,7 @@ export const nodeIconToDisplayIconMap: Record<string, string> = {
   HuggingFaceEmbeddings: "HuggingFace",
   HuggingFaceHub: "HuggingFace",
   IFixitLoader: "IFixIt",
+  LinkupIcon: "Linkup",
   ListFlows: "Group",
   MistralAI: "Mistral",
   MongoDBAtlasVectorSearch: "MongoDB",
