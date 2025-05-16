cbf4ef4b78a9c00d9eb56cca1d2e8394520f95b6
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-05-16 13:37:30 -0300

    Added z index to header

--- src/frontend/src/components/core/appHeaderComponent/index.tsx
+++ src/frontend/src/components/core/appHeaderComponent/index.tsx
@@ -51,7 +51,7 @@ export default function AppHeader(): JSX.Element {
 
   return (
     <div
-      className={`flex h-[48px] w-full items-center justify-between border-b px-6 dark:bg-background`}
+      className={`z-10 flex h-[48px] w-full items-center justify-between border-b px-6 dark:bg-background`}
       data-testid="app-header"
     >
       {/* Left Section */}
