089c8da58c42378739643d94e315afda2d8f8537
phact <estevezsebastian@gmail.com>
2025-05-15 22:03:20 -0400

    do check in the __init__

--- src/backend/base/langflow/components/nvidia/__init__.py
+++ src/backend/base/langflow/components/nvidia/__init__.py
@@ -1,5 +1,11 @@
 from .nvidia_ingest import NvidiaIngestComponent
 from .nvidia_rerank import NvidiaRerankComponent
-from .system_assist import NvidiaSystemAssistComponent
 
-__all__ = ["NvidiaIngestComponent", "NvidiaRerankComponent", "NvidiaSystemAssistComponent"]
+try:
+    import gassist.rise  # noqa: F401
+
+    from .system_assist import NvidiaSystemAssistComponent
+
+    __all__ = ["NvidiaIngestComponent", "NvidiaRerankComponent", "NvidiaSystemAssistComponent"]
+except ImportError:  # skip the component if gassist is not installed (sys_platform != win32)
+    __all__ = ["NvidiaIngestComponent", "NvidiaRerankComponent"]
--- src/backend/base/langflow/components/nvidia/system_assist.py
+++ src/backend/base/langflow/components/nvidia/system_assist.py
@@ -1,60 +1,56 @@
-HAS_GASSIST = True
-try:
-    from gassist.rise import register_rise_client, send_rise_command
-except ImportError:  # skip this component if gassist is not installed (sys_platform != win32)
-    HAS_GASSIST = False
+import asyncio
 
-if HAS_GASSIST:
-    import asyncio
+from gassist.rise import register_rise_client, send_rise_command
 
-    from langflow.custom.custom_component.component_with_cache import ComponentWithCache
-    from langflow.io import MessageTextInput, Output
-    from langflow.schema import Message
-    from langflow.services.cache.utils import CacheMiss
+from langflow.custom.custom_component.component_with_cache import ComponentWithCache
+from langflow.io import MessageTextInput, Output
+from langflow.schema import Message
+from langflow.services.cache.utils import CacheMiss
 
-    class NvidiaSystemAssistComponent(ComponentWithCache):
-        display_name = "NVIDIA System-Assist"
-        description = (
-            "Prompts NVIDIA System-Assist to interact with the NVIDIA GPU Driver. "
-            "The user may query GPU specifications, state, and ask the NV-API to perform "
-            "several GPU-editing acations. The prompt must be human-readable language."
-        )
-        documentation = "https://docs.langflow.org/components-custom-components"
-        icon = "NVIDIA"
-        rise_initialized = False
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+class NvidiaSystemAssistComponent(ComponentWithCache):
+    display_name = "NVIDIA System-Assist"
+    description = (
+        "Prompts NVIDIA System-Assist to interact with the NVIDIA GPU Driver. "
+        "The user may query GPU specifications, state, and ask the NV-API to perform "
+        "several GPU-editing acations. The prompt must be human-readable language."
+    )
+    documentation = "https://docs.langflow.org/components-custom-components"
+    icon = "NVIDIA"
+    rise_initialized = False
 
-        def maybe_register_rise_client(self):
-            rise_initialized = self._shared_component_cache.get("rise_initialized")
-            if not isinstance(rise_initialized, CacheMiss) and rise_initialized:
-                return
-            self.log("Initializing Rise Client")
-            try:
-                register_rise_client()
-                self._shared_component_cache.set(key="rise_initialized", value=True)
-            except Exception as e:
-                msg = f"An error occurred initializing rise client: {e}"
-                raise ValueError(msg) from e
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        inputs = [
-            MessageTextInput(
-                name="prompt",
-                display_name="System-Assist Prompt",
-                info="Enter a prompt for NVIDIA System-Assist to process.",
-                value="",
-                tool_mode=True,
-            ),
-        ]
+    def maybe_register_rise_client(self):
+        rise_initialized = self._shared_component_cache.get("rise_initialized")
+        if not isinstance(rise_initialized, CacheMiss) and rise_initialized:
+            return
+        self.log("Initializing Rise Client")
+        try:
+            register_rise_client()
+            self._shared_component_cache.set(key="rise_initialized", value=True)
+        except Exception as e:
+            msg = f"An error occurred initializing rise client: {e}"
+            raise ValueError(msg) from e
 
-        outputs = [
-            Output(display_name="Response", name="response", method="sys_assist_prompt"),
-        ]
+    inputs = [
+        MessageTextInput(
+            name="prompt",
+            display_name="System-Assist Prompt",
+            info="Enter a prompt for NVIDIA System-Assist to process.",
+            value="",
+            tool_mode=True,
+        ),
+    ]
 
-        async def sys_assist_prompt(self) -> Message:
-            self.maybe_register_rise_client()
+    outputs = [
+        Output(display_name="Response", name="response", method="sys_assist_prompt"),
+    ]
 
-            response = await asyncio.to_thread(send_rise_command, self.prompt)
+    async def sys_assist_prompt(self) -> Message:
+        self.maybe_register_rise_client()
 
-            return Message(text=response["completed_response"]) if response is not None else Message(text=None)
+        response = await asyncio.to_thread(send_rise_command, self.prompt)
+
+        return Message(text=response["completed_response"]) if response is not None else Message(text=None)
