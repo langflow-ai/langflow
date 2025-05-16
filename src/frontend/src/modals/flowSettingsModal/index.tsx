e2a4d819428122716bdab99328f9d9fdf6e554df
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-05-16 12:29:44 -0300

    Changed Flow Settings Modal to use new component

--- src/frontend/src/modals/flowSettingsModal/index.tsx
+++ src/frontend/src/modals/flowSettingsModal/index.tsx
@@ -1,3 +1,4 @@
+import FlowSettingsComponent from "@/components/core/flowSettingsComponent";
 import useSaveFlow from "@/hooks/flows/use-save-flow";
 import useAlertStore from "@/stores/alertStore";
 import useFlowStore from "@/stores/flowStore";
@@ -19,109 +20,23 @@ export default function FlowSettingsModal({
   details,
 }: FlowSettingsPropsType): JSX.Element {
   if (!open) return <></>;
-
-  const saveFlow = useSaveFlow();
-  const currentFlow = useFlowStore((state) =>
-    flowData ? undefined : state.currentFlow,
-  );
-  const setCurrentFlow = useFlowStore((state) => state.setCurrentFlow);
-  const setSuccessData = useAlertStore((state) => state.setSuccessData);
-  const flows = useFlowsManagerStore((state) => state.flows);
-  const flow = flowData ?? currentFlow;
-  const [name, setName] = useState(flow?.name ?? "");
-  const [description, setDescription] = useState(flow?.description ?? "");
-  const [endpoint_name, setEndpointName] = useState(flow?.endpoint_name ?? "");
-  const [isSaving, setIsSaving] = useState(false);
-  const [disableSave, setDisableSave] = useState(true);
-  const autoSaving = useFlowsManagerStore((state) => state.autoSaving);
-
-  useEffect(() => {
-    setName(flow?.name ?? "");
-    setDescription(flow?.description ?? "");
-    setEndpointName(flow?.endpoint_name ?? "");
-  }, [flow?.name, flow?.description, flow?.endpoint_name, open]);
-
-  function handleClick(): void {
-    setIsSaving(true);
-    if (!flow) return;
-    const newFlow = cloneDeep(flow);
-    newFlow.name = name;
-    newFlow.description = description;
-    newFlow.endpoint_name =
-      endpoint_name && endpoint_name.length > 0 ? endpoint_name : null;
-
-    if (autoSaving) {
-      saveFlow(newFlow)
-        ?.then(() => {
-          setOpen(false);
-          setIsSaving(false);
-          setSuccessData({ title: "Changes saved successfully" });
-        })
-        .catch(() => {
-          setIsSaving(false);
-        });
-    } else {
-      setCurrentFlow(newFlow);
-      setOpen(false);
-      setIsSaving(false);
-    }
-  }
-
-  const [nameLists, setNameList] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (flows) {
-      const tempNameList: string[] = [];
-      flows.forEach((flow: FlowType) => {
-        tempNameList.push(flow.name);
-      });
-      setNameList(tempNameList.filter((name) => name !== flow!.name));
-    }
-  }, [flows]);
-
-  useEffect(() => {
-    if (
-      (!nameLists.includes(name) && flow?.name !== name) ||
-      flow?.description !== description ||
-      ((flow?.endpoint_name ?? "") !== endpoint_name &&
-        isEndpointNameValid(endpoint_name ?? "", 50))
-    ) {
-      setDisableSave(false);
-    } else {
-      setDisableSave(true);
-    }
-  }, [nameLists, flow, description, endpoint_name, name]);
   return (
     <BaseModal
       open={open}
       setOpen={setOpen}
-      size="smaller-h-full"
-      onSubmit={handleClick}
+      size="small-update"
+      className="p-4"
     >
-      <BaseModal.Header description={SETTINGS_DIALOG_SUBTITLE}>
-        <span className="pr-2">Details</span>
-        <IconComponent name="SquarePen" className="mr-2 h-4 w-4" />
+      <BaseModal.Header>
+        <span className="text-base font-semibold">Flow Details</span>
       </BaseModal.Header>
       <BaseModal.Content>
-        <EditFlowSettings
-          invalidNameList={nameLists}
-          name={name}
-          description={description}
-          endpointName={endpoint_name}
-          setName={setName}
-          setDescription={setDescription}
-          setEndpointName={details ? undefined : setEndpointName}
+        <FlowSettingsComponent
+          flowData={flowData}
+          details={details}
+          close={() => setOpen(false)}
         />
       </BaseModal.Content>
-
-      <BaseModal.Footer
-        submit={{
-          label: "Save",
-          dataTestId: "save-flow-settings",
-          disabled: disableSave,
-          loading: isSaving,
-        }}
-      />
     </BaseModal>
   );
 }
