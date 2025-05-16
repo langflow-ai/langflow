d6903fee8da92958dccb1e11afffe78b3172f894
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-05-16 09:26:21 -0300

    Updated node name to follow design and include Beta

--- src/frontend/src/CustomNodes/GenericNode/components/NodeName/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/NodeName/index.tsx
@@ -1,3 +1,4 @@
+import ForwardedIconComponent from "@/components/common/genericIconComponent";
 import { Input } from "@/components/ui/input";
 import useFlowsManagerStore from "@/stores/flowsManagerStore";
 import useFlowStore from "@/stores/flowStore";
@@ -10,8 +11,6 @@ export default function NodeName({
   selected,
   nodeId,
   showNode,
-  validationStatus,
-  isOutdated,
   beta,
   editNameDescription,
   toggleEditNameDescription,
@@ -21,8 +20,6 @@ export default function NodeName({
   selected?: boolean;
   nodeId: string;
   showNode: boolean;
-  validationStatus: VertexBuildTypeAPI | null;
-  isOutdated: boolean;
   beta: boolean;
   editNameDescription: boolean;
   toggleEditNameDescription: () => void;
@@ -85,32 +82,33 @@ export default function NodeName({
         onChange={onChange}
         data-testid={`input-title-${display_name}`}
         onKeyDown={handleKeyDown}
-        className="py-1"
+        className="px-2 py-0.5"
       />
     </div>
   ) : (
-    <div className="group flex w-full items-center gap-1">
+    <div className="group flex flex-1 items-center gap-2 overflow-hidden">
       <div
         data-testid={"title-" + display_name}
         className={cn(
-          "nodoubleclick w-full truncate font-medium text-primary",
+          "nodoubleclick truncate font-medium text-primary",
           showNode ? "cursor-text" : "cursor-default",
         )}
       >
         <div className="flex cursor-grab items-center gap-2">
-          <span
-            className={cn(
-              "max-w-44 cursor-grab truncate text-sm",
-              validationStatus?.data?.duration && "max-w-36",
-              beta && "max-w-36",
-              validationStatus?.data?.duration && beta && "max-w-20",
-              isOutdated && "max-w-40",
-              !showNode && "max-w-28",
-            )}
-          >
+          <span className={cn("max-w-44 cursor-grab truncate text-sm")}>
             {display_name}
           </span>
         </div>
+      </div>
+      <div className="shrink-0">
+        {beta && (
+          <div className="flex h-4 w-4 items-center justify-center rounded-sm border border-datatype-purple-foreground p-0.5">
+            <ForwardedIconComponent
+              name="FlaskConical"
+              className="text-datatype-purple-foreground"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
