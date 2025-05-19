76842ac564dfb1ebcd86aee8b947e4694a8270af
Sandesh R <2getsandesh@gmail.com>
2025-05-19 10:32:14 +0530

    Only Traceloop Working code

--- src/backend/base/langflow/services/tracing/service.py
+++ src/backend/base/langflow/services/tracing/service.py
@@ -46,6 +46,10 @@ def _get_arize_phoenix_tracer():
 
     return ArizePhoenixTracer
 
+def _get_traceloop_tracer():
+    from langflow.services.tracing.traceloop import TraceloopTracer
+
+    return TraceloopTracer
 
 def _get_opik_tracer():
     from langflow.services.tracing.opik import OpikTracer
@@ -182,6 +186,15 @@ class TracingService(Service):
             trace_id=trace_context.run_id,
         )
 
+    def _initialize_traceloop_tracer(self, trace_context: TraceContext) -> None:
+        traceloop_tracer = _get_traceloop_tracer()
+        trace_context.tracers["traceloop"] = traceloop_tracer(
+            trace_name=trace_context.run_name,
+            trace_type="chain",
+            project_name=trace_context.project_name,
+            trace_id=trace_context.run_id,
+        )
+
     def _initialize_opik_tracer(self, trace_context: TraceContext) -> None:
         opik_tracer = _get_opik_tracer()
         trace_context.tracers["opik"] = opik_tracer(
@@ -218,6 +231,7 @@ class TracingService(Service):
             self._initialize_langwatch_tracer(trace_context)
             self._initialize_langfuse_tracer(trace_context)
             self._initialize_arize_phoenix_tracer(trace_context)
+            self._initialize_traceloop_tracer(trace_context)
             self._initialize_opik_tracer(trace_context)
         except Exception as e:  # noqa: BLE001
             logger.debug(f"Error initializing tracers: {e}")
