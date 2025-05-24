2c7b6d96708b61397fa1a5c3c5c4b482d21369ac
Edwin Jose <edwin.jose@datastax.com>
2025-05-23 23:32:09 -0400

    Update __init__.py

--- src/backend/base/langflow/components/tavily/__init__.py
+++ src/backend/base/langflow/components/tavily/__init__.py
@@ -1,9 +1,7 @@
 from .tavily_extract import TavilyExtractComponent
 from .tavily_search import TavilySearchComponent
-from .tavily_search_tool import TavilySearchToolComponent
 
 __all__ = [
     "TavilyExtractComponent",
-    "TavilySearchComponent",
-    "TavilySearchToolComponent",
+    "TavilySearchComponent"
 ]
