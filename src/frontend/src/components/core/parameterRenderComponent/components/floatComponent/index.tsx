f0183788abe9f0aa0010b076ab02859828708e45
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-05-30 11:38:05 -0300

    Removed float limits when not defined

--- src/frontend/src/components/core/parameterRenderComponent/components/floatComponent/index.tsx
+++ src/frontend/src/components/core/parameterRenderComponent/components/floatComponent/index.tsx
@@ -20,8 +20,8 @@ export default function FloatComponent({
   id = "",
 }: InputProps<number, FloatComponentType>): JSX.Element {
   const step = rangeSpec?.step ?? 0.1;
-  const min = rangeSpec?.min ?? -2;
-  const max = rangeSpec?.max ?? 2;
+  const min = rangeSpec?.min;
+  const max = rangeSpec?.max;
 
   // Local state for input value
   const [localValue, setLocalValue] = useState<string>(value.toString());
