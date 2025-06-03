ebeced60971cdd2b53ae0a47022bb7c6d6988f1c
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2025-06-03 16:45:14 -0400

    add-new-svgs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -292,8 +292,8 @@ const config = {
         hideOnScroll: true,
         logo: {
           alt: "Langflow",
-          src: "img/langflow-logo-black.svg",
-          srcDark: "img/langflow-logo-white.svg",
+          src: "img/lf-docs.svg",
+          srcDark: "img/lf-docs.svg",
         },
         items: [
           // right
@@ -346,8 +346,8 @@ const config = {
       footer: {
         logo: {
           alt: "Langflow",
-          src: "img/langflow-logo-black.svg",
-          srcDark: "img/langflow-logo-white.svg",
+          src: "img/lf-docs.svg",
+          srcDark: "img/lf-docs.svg",
           width: 160,
           height: 40,
         },
