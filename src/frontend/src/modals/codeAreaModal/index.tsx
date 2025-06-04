b9bbd3a6a34ca45b30ab6d2d5c2d7d6a085a9bca
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-06-04 16:30:26 -0300

    fixed bug on code area modal

--- src/frontend/src/modals/codeAreaModal/index.tsx
+++ src/frontend/src/modals/codeAreaModal/index.tsx
@@ -200,12 +200,9 @@ export default function CodeAreaModal({
         />
       </BaseModal.Header>
       <BaseModal.Content>
-        <Input
-          value={code}
-          readOnly
-          className="absolute left-[500%] top-[500%]"
-          id="codeValue"
-        />
+        <div className="absolute left-[500%] top-[500%]">
+          <Input value={code} readOnly id="codeValue" />
+        </div>
         <div className="flex h-full w-full flex-col transition-all">
           <div className="h-full w-full">
             <AceEditor
