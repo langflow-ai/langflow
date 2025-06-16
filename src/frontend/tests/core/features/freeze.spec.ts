867cb8664b9afe807ba9e140f44db0a04e27fc69
cristhianzl <cristhian.lousa@gmail.com>
2025-06-16 11:43:29 -0300

    ✨ (freeze.spec.ts): add support for adding a flow to test on an empty langflow if the first run langflow is greater than 0

--- src/frontend/tests/core/features/freeze.spec.ts
+++ src/frontend/tests/core/features/freeze.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from "@playwright/test";
+import { addFlowToTestOnEmptyLangflow } from "../../utils/add-flow-to-test-on-empty-langflow";
 import { addLegacyComponents } from "../../utils/add-legacy-components";
 import { awaitBootstrapTest } from "../../utils/await-bootstrap-test";
 import { zoomOut } from "../../utils/zoom-out";
@@ -9,6 +10,14 @@ test(
 
   async ({ page }) => {
     await awaitBootstrapTest(page);
+
+    const firstRunLangflow = await page
+      .getByTestId("empty-project-description")
+      .count();
+
+    if (firstRunLangflow > 0) {
+      await addFlowToTestOnEmptyLangflow(page);
+    }
 
     await page.getByTestId("blank-flow").click();
 
