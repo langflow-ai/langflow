02649c2627557eebbc9d071a0ad9eb88f481dbd7
Michele Dolfi <dol@zurich.ibm.com>
2025-06-23 13:28:50 +0200

    remove LoadDoclingDocument
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- src/backend/base/langflow/components/docling/__init__.py
+++ src/backend/base/langflow/components/docling/__init__.py
@@ -2,12 +2,10 @@ from .chunk_docling_document import ChunkDoclingDocumentComponent
 from .docling_inline import DoclingInlineComponent
 from .docling_remote import DoclingRemoteComponent
 from .export_docling_document import ExportDoclingDocumentComponent
-from .load_docling_document import LoadDoclingDocumentComponent
 
 __all__ = [
     "ChunkDoclingDocumentComponent",
     "DoclingInlineComponent",
     "DoclingRemoteComponent",
     "ExportDoclingDocumentComponent",
-    "LoadDoclingDocumentComponent",
 ]
