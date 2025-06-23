3b62945acd43e649663722212ecb62981bfc09e5
Lucas Oliveira <62335616+lucaseduoli@users.noreply.github.com>
2025-06-23 15:07:54 -0300

    fix: added props to all icons and added width and height defs to node icon (#8649)
    * Updated icons to include props
    
    * Updated node icon to include width and height

--- src/frontend/src/icons/ScrapeGraphAI/ScrapeGraphAI.jsx
+++ src/frontend/src/icons/ScrapeGraphAI/ScrapeGraphAI.jsx
@@ -5,8 +5,6 @@ const ScrapeGraphAI = (props) => {
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="400"
-      height="400"
       viewBox="0, 0, 400,400"
       {...props}
     >
