610f12a5cb4c5297760f3f3a38c69d3e6e5dbe2d
Yuqi Tang <yuqi.tang@datastax.com>
2025-06-25 14:59:06 -0700

    fix order

--- src/backend/base/langflow/initial_setup/starter_projects/vector_store_rag.py
+++ src/backend/base/langflow/initial_setup/starter_projects/vector_store_rag.py
@@ -1,12 +1,11 @@
 from textwrap import dedent
 
 from langflow.components.data import FileComponent
-from langflow.components.embeddings import OpenAIEmbeddingsComponent
 from langflow.components.input_output import ChatInput, ChatOutput
+from langflow.components.openai.openai import OpenAIEmbeddingsComponent
 from langflow.components.openai.openai_chat_model import OpenAIModelComponent
-from langflow.components.processing import ParserComponent
+from langflow.components.processing import ParserComponent, PromptComponent
 from langflow.components.processing.split_text import SplitTextComponent
-from langflow.components.prompts import PromptComponent
 from langflow.components.vectorstores import AstraDBVectorStoreComponent
 from langflow.graph import Graph
 
