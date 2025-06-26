e8c40a37a9e4eaa02d579f6b1a2acb1836cc9160
Eric Hare <ericrhare@gmail.com>
2025-06-26 12:21:16 -0700

    Update fileUploadComponent.spec.ts

--- src/frontend/tests/core/unit/fileUploadComponent.spec.ts
+++ src/frontend/tests/core/unit/fileUploadComponent.spec.ts
@@ -251,7 +251,7 @@ test(
     await adjustScreenView(page);
 
     await page
-      .getByTestId("handle-file-shownode-raw content-right")
+      .getByTestId("handle-file-shownode-files-right")
       .first()
       .click();
 
