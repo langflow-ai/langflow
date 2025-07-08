9effe40a8585531e6e8e3d63595550c1e393f76a
Cristhian Zanforlin Lousa <cristhian.lousa@gmail.com>
2025-07-08 19:28:06 -0300

    feat: Add dynamic theming support to WatsonxAI icon (#8935)
    * ✨ (WatsonxAI.jsx): Add support for dynamic fill color based on isdark prop value
    📝 (index.tsx): Import useDarkStore hook and pass isdark prop to SvgWatsonxAI component
    
    * ✨ (WatsonxAI.jsx): Remove unnecessary console.log statement
    ♻️ (index.tsx): Remove unnecessary console.log statement

--- src/frontend/src/icons/IBMWatsonx/index.tsx
+++ src/frontend/src/icons/IBMWatsonx/index.tsx
@@ -1,3 +1,4 @@
+import { useDarkStore } from "@/stores/darkStore";
 import React, { forwardRef } from "react";
 import SvgWatsonxAI from "./WatsonxAI";
 
@@ -5,5 +6,6 @@ export const WatsonxAiIcon = forwardRef<
   SVGSVGElement,
   React.PropsWithChildren<{}>
 >((props, ref) => {
-  return <SvgWatsonxAI ref={ref} {...props} />;
+  const isdark = useDarkStore((state) => state.dark).toString();
+  return <SvgWatsonxAI ref={ref} isdark={isdark} {...props} />;
 });
