9ca315a2b469af734a690f9d609ed2c2aad5d8d2
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-07-09 16:27:55 -0300

    made edges change trigger re-render of handle color

--- src/frontend/src/CustomNodes/GenericNode/components/handleRenderComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/handleRenderComponent/index.tsx
@@ -222,6 +222,8 @@ const HandleRenderComponent = memo(function HandleRenderComponent({
     targetHandle: semiConnection.targetHandle ?? myId,
   });
 
+  const edges = useFlowStore((state) => state.edges);
+
   const {
     sameNode,
     ownHandle,
@@ -266,11 +268,9 @@ const HandleRenderComponent = memo(function HandleRenderComponent({
     const openHandle = filterOpenHandle || draggingOpenHandle;
     const filterPresent = handleDragging || filterType;
 
-    const connectedEdge = useFlowStore
-      .getState()
-      .edges.find(
-        (edge) => edge.target === nodeId && edge.targetHandle === myId,
-      );
+    const connectedEdge = edges.find(
+      (edge) => edge.target === nodeId && edge.targetHandle === myId,
+    );
     const outputType = connectedEdge?.data?.sourceHandle?.output_types?.[0];
     const connectedColor = outputType ? nodeColorsName[outputType] : "gray";
 
@@ -344,6 +344,7 @@ const HandleRenderComponent = memo(function HandleRenderComponent({
     colors,
     colorName,
     tooltipTitle,
+    edges,
   ]);
 
   const handleMouseDown = useCallback(
