0bfe67acc9c2ff503202a4be79d0cb1686699765
Ronnie Miller <ronnie.miller@datastax.com>
2025-07-10 13:55:31 -0700

    docs: Configure Segment

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -130,6 +130,7 @@ const config = {
   plugins: [
     ["docusaurus-node-polyfills", { excludeAliases: ["console"] }],
     "docusaurus-plugin-image-zoom",
+    ["./src/plugins/segment", { segmentPublicWriteKey: process.env.SEGMENT_PUBLIC_WRITE_KEY, allowedInDev: true }],
     [
       "@docusaurus/plugin-client-redirects",
       {
@@ -324,6 +325,8 @@ const config = {
             className: "header-github-link",
             target: "_blank",
             rel: null,
+            'data-event': 'Docs.langflow.org - Social Clicked',
+            'data-platform': 'github'
           },
           {
             position: "right",
@@ -331,6 +334,8 @@ const config = {
             className: "header-twitter-link",
             target: "_blank",
             rel: null,
+            'data-event': 'Docs.langflow.org - Social Clicked',
+            'data-platform': 'x'
           },
           {
             position: "right",
@@ -338,6 +343,8 @@ const config = {
             className: "header-discord-link",
             target: "_blank",
             rel: null,
+            'data-event': 'Docs.langflow.org - Social Clicked',
+            'data-platform': 'discord'
           },
         ],
       },
