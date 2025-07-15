8f71414be4f2e10ccd1f2cd9d6e76d47609fdd78
cristhianzl <cristhian.lousa@gmail.com>
2025-07-15 15:13:00 -0300

    🐛 (api.tsx): fix issue where Authorization header was not being set correctly for internal requests 💡 (api.tsx): improve logic to set Authorization header only for internal requests and add custom headers for all requests

--- src/frontend/src/controllers/API/api.tsx
+++ src/frontend/src/controllers/API/api.tsx
@@ -45,11 +45,11 @@ function ApiInterceptor() {
       request: function (url, config) {
         const accessToken = customGetAccessToken();
 
-        if (accessToken && !isAuthorizedURL(config?.url)) {
-          config.headers["Authorization"] = `Bearer ${accessToken}`;
-        }
-
         if (!isExternalURL(url)) {
+          if (accessToken && !isAuthorizedURL(config?.url)) {
+            config.headers["Authorization"] = `Bearer ${accessToken}`;
+          }
+
           for (const [key, value] of Object.entries(customHeaders)) {
             config.headers[key] = value;
           }
