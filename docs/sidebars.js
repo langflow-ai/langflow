640f018078b63cbaa08a4189d3efdb434f3e6f05
April M <april.murphy@datastax.com>
2025-07-22 15:17:26 -0700

    incorporate PR 9138

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -110,7 +110,6 @@ module.exports = {
       type: "category",
       label: "Develop",
       items: [
-        "Develop/develop-application",
         {
           type: "doc",
           id: "Develop/install-custom-dependencies",
@@ -190,6 +189,7 @@ module.exports = {
           type: "category",
           label: "Containerized deployments",
           items: [
+            "Develop/develop-application",
             {
               type: "doc",
               id: "Deployment/deployment-docker",
