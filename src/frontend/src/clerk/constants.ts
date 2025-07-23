9f839da8495ec3927cd28368065e2c147c3f5f9f
unknown <kabik5095@gmail.com>
2025-07-23 11:50:44 +0530

    removed constant file

--- src/frontend/src/clerk/constants.ts
+++
@@ -1,6 +0,0 @@
-// src/clerk/constants.ts
-export const IS_CLERK_AUTH =
-  String(import.meta.env.VITE_CLERK_AUTH_ENABLED).toLowerCase() === "true";
-
-export const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || "";
-export const CLERK_DUMMY_PASSWORD = "clerk_dummy_password";
