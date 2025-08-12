28a82bf285d6316ce5f6698d9bd89b0ca3493cc5
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-12 12:54:51 -0300

    Use playground component on playground page

--- src/frontend/src/pages/Playground/index.tsx
+++ src/frontend/src/pages/Playground/index.tsx
@@ -11,97 +11,89 @@ import { useUtilityStore } from "@/stores/utilityStore";
 import { type CookieOptions, getCookie, setCookie } from "@/utils/utils";
 import useFlowsManagerStore from "../../stores/flowsManagerStore";
 import { getInputsAndOutputs } from "../../utils/storeUtils";
+import { PlaygroundComponent } from "@/components/core/playgroundComponent/playground-component";
 export default function PlaygroundPage() {
-  useGetConfig();
-  const setCurrentFlow = useFlowsManagerStore((state) => state.setCurrentFlow);
-  const currentSavedFlow = useFlowsManagerStore((state) => state.currentFlow);
-  const setClientId = useUtilityStore((state) => state.setClientId);
+	useGetConfig();
+	const setCurrentFlow = useFlowsManagerStore((state) => state.setCurrentFlow);
+	const currentSavedFlow = useFlowsManagerStore((state) => state.currentFlow);
+	const setClientId = useUtilityStore((state) => state.setClientId);
 
-  const { id } = useParams();
-  const { mutateAsync: getFlow } = useGetFlow();
+	const { id } = useParams();
+	const { mutateAsync: getFlow } = useGetFlow();
 
-  const navigate = useCustomNavigate();
+	const navigate = useCustomNavigate();
 
-  const currentFlowId = useFlowsManagerStore((state) => state.currentFlowId);
-  const setIsLoading = useFlowsManagerStore((state) => state.setIsLoading);
-  const setPlaygroundPage = useFlowStore((state) => state.setPlaygroundPage);
+	const currentFlowId = useFlowsManagerStore((state) => state.currentFlowId);
+	const setIsLoading = useFlowsManagerStore((state) => state.setIsLoading);
+	const setPlaygroundPage = useFlowStore((state) => state.setPlaygroundPage);
 
-  async function getFlowData() {
-    try {
-      const flow = await getFlow({ id: id!, public: true });
-      return flow;
-    } catch (error: any) {
-      console.error(error);
-      navigate("/");
-    }
-  }
+	async function getFlowData() {
+		try {
+			const flow = await getFlow({ id: id!, public: true });
+			return flow;
+		} catch (error: any) {
+			console.error(error);
+			navigate("/");
+		}
+	}
 
-  useEffect(() => {
-    const initializeFlow = async () => {
-      setIsLoading(true);
-      if (currentFlowId === "") {
-        const flow = await getFlowData();
-        if (flow) {
-          setCurrentFlow(flow);
-        } else {
-          navigate("/");
-        }
-      }
-    };
+	useEffect(() => {
+		const initializeFlow = async () => {
+			setIsLoading(true);
+			if (currentFlowId === "") {
+				const flow = await getFlowData();
+				if (flow) {
+					setCurrentFlow(flow);
+				} else {
+					navigate("/");
+				}
+			}
+		};
 
-    initializeFlow();
-    setIsLoading(false);
-  }, [id]);
+		initializeFlow();
+		setIsLoading(false);
+	}, [id]);
 
-  useEffect(() => {
-    if (id) track("Playground Page Loaded", { flowId: id });
-    setPlaygroundPage(true);
-  }, []);
+	useEffect(() => {
+		if (id) track("Playground Page Loaded", { flowId: id });
+		setPlaygroundPage(true);
+	}, []);
 
-  useEffect(() => {
-    document.title = currentSavedFlow?.name || "Langflow";
-    if (currentSavedFlow?.data) {
-      const { inputs, outputs } = getInputsAndOutputs(
-        currentSavedFlow?.data?.nodes || [],
-      );
-      if (
-        (inputs.length === 0 && outputs.length === 0) ||
-        currentSavedFlow?.access_type !== "PUBLIC"
-      ) {
-        // redirect to the home page
-        navigate("/");
-      }
-    }
-  }, [currentSavedFlow]);
+	useEffect(() => {
+		document.title = currentSavedFlow?.name || "Langflow";
+		if (currentSavedFlow?.data) {
+			const { inputs, outputs } = getInputsAndOutputs(
+				currentSavedFlow?.data?.nodes || [],
+			);
+			if (
+				(inputs.length === 0 && outputs.length === 0) ||
+				currentSavedFlow?.access_type !== "PUBLIC"
+			) {
+				// redirect to the home page
+				navigate("/");
+			}
+		}
+	}, [currentSavedFlow]);
 
-  useEffect(() => {
-    // Get client ID from cookie or create new one
-    const clientId = getCookie("client_id");
-    if (!clientId) {
-      const newClientId = uuid();
-      const cookieOptions: CookieOptions = {
-        secure: window.location.protocol === "https:",
-        sameSite: "Strict",
-      };
-      setCookie("client_id", newClientId, cookieOptions);
-      setClientId(newClientId);
-    } else {
-      setClientId(clientId);
-    }
-  }, []);
+	useEffect(() => {
+		// Get client ID from cookie or create new one
+		const clientId = getCookie("client_id");
+		if (!clientId) {
+			const newClientId = uuid();
+			const cookieOptions: CookieOptions = {
+				secure: window.location.protocol === "https:",
+				sameSite: "Strict",
+			};
+			setCookie("client_id", newClientId, cookieOptions);
+			setClientId(newClientId);
+		} else {
+			setClientId(clientId);
+		}
+	}, []);
 
-  return (
-    <div className="flex h-full w-full flex-col items-center justify-center align-middle">
-      {currentSavedFlow && (
-        <CustomIOModal
-          open={true}
-          setOpen={() => {}}
-          isPlayground
-          playgroundPage
-        >
-          <></>
-        </CustomIOModal>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex h-full w-full flex-col items-center justify-center align-middle">
+			{currentSavedFlow && <PlaygroundComponent />}
+		</div>
+	);
 }
