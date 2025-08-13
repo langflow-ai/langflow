60b4c4a14b0849b3928dce19a11344a552157285
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-13 18:19:28 -0300

    pass header to playground component

--- src/frontend/src/pages/FlowPage/components/PlaygroundSidebar/index.tsx
+++ src/frontend/src/pages/FlowPage/components/PlaygroundSidebar/index.tsx
@@ -1,11 +1,9 @@
 import {
   SimpleSidebar,
   SimpleSidebarContent,
-  SimpleSidebarHeader,
   useSimpleSidebar,
 } from "@/components/ui/simple-sidebar";
 import { PlaygroundComponent } from "@/components/core/playgroundComponent/playground-component";
-import { PlaygroundHeader } from "@/components/core/playgroundComponent/components/playgroundHeader/playground-header";
 
 export function PlaygroundSidebar(): JSX.Element {
   const { setOpen } = useSimpleSidebar();
@@ -16,12 +14,8 @@ export function PlaygroundSidebar(): JSX.Element {
 
   return (
     <SimpleSidebar side="right" className="noflow select-none border-l">
-      <SimpleSidebarHeader className="px-4 py-2 overflow-hidden w-full">
-        <PlaygroundHeader onClose={handleClose} />
-      </SimpleSidebarHeader>
-
       <SimpleSidebarContent className="p-0 flex h-full w-full bg-background">
-        <PlaygroundComponent />
+        <PlaygroundComponent onClose={handleClose} />
       </SimpleSidebarContent>
     </SimpleSidebar>
   );
