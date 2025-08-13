3b67e5d7298adc5f65e448d0839e0eb00d52fb48
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-13 17:48:12 -0300

    componentized playground sidebar

--- src/frontend/src/pages/FlowPage/components/PlaygroundSidebar/index.tsx
+++ src/frontend/src/pages/FlowPage/components/PlaygroundSidebar/index.tsx
@@ -1,76 +1,26 @@
 import {
-	SimpleSidebar,
-	SimpleSidebarContent,
-	SimpleSidebarHeader,
-	useSimpleSidebar,
+  SimpleSidebar,
+  SimpleSidebarContent,
+  SimpleSidebarHeader,
+  useSimpleSidebar,
 } from "@/components/ui/simple-sidebar";
-import IconComponent from "../../../../components/common/genericIconComponent";
-import { Button } from "../../../../components/ui/button";
 import { PlaygroundComponent } from "@/components/core/playgroundComponent/playground-component";
+import { PlaygroundHeader } from "@/components/core/playgroundComponent/components/playgroundHeader/playground-header";
 
-export function PlaygroundSidebar() {
-	const { setOpen } = useSimpleSidebar();
+export function PlaygroundSidebar(): JSX.Element {
+  const { setOpen } = useSimpleSidebar();
 
-	return (
-		<SimpleSidebar side="right" className="noflow select-none border-l">
-			<SimpleSidebarHeader className=" p-0 overflow-hidden">
-				<div className="flex items-center justify-between gap-2 px-4 py-2">
-					<div className="flex items-center gap-2">
-						<div className="truncate text-sm font-medium text-secondary-foreground">
-							Flow run{" "}
-							{new Date().toLocaleDateString("en-US", {
-								month: "2-digit",
-								day: "2-digit",
-							})}{" "}
-							{new Date().toLocaleTimeString("en-US", { hour12: false })}
-						</div>
-					</div>
-					<div className="flex items-center gap-1">
-						<Button
-							variant="ghost"
-							size="icon"
-							className="flex h-8 items-center gap-2 text-muted-foreground"
-						>
-							<IconComponent name="Plus" className="h-4 w-4" />
-						</Button>
-						<Button
-							variant="ghost"
-							size="icon"
-							className="flex h-8 items-center gap-2 text-muted-foreground"
-						>
-							<IconComponent name="History" className="h-4 w-4" />
-						</Button>
-						<Button
-							variant="ghost"
-							size="icon"
-							className="flex h-8 items-center gap-2 text-muted-foreground"
-						>
-							<IconComponent name="ExternalLink" className="h-4 w-4" />
-						</Button>
-						<Button
-							variant="ghost"
-							size="iconMd"
-							className="flex h-8 items-center gap-2 text-muted-foreground"
-						>
-							<IconComponent name="MoreHorizontal" className="h-4 w-4" />
-						</Button>
-						<Button
-							variant="ghost"
-							size="iconMd"
-							className="flex h-8 items-center gap-2 text-muted-foreground"
-							onClick={() => setOpen(false)}
-						>
-							<IconComponent name="X" className="h-4 w-4" />
-						</Button>
-					</div>
-				</div>
-			</SimpleSidebarHeader>
+  return (
+    <SimpleSidebar side="right" className="noflow select-none border-l">
+      <SimpleSidebarHeader className="px-4 py-2 overflow-hidden w-full">
+        <PlaygroundHeader onClose={() => setOpen(false)} />
+      </SimpleSidebarHeader>
 
-			<SimpleSidebarContent className="p-0">
-				<div className="flex h-full w-full bg-background">
-					<PlaygroundComponent />
-				</div>
-			</SimpleSidebarContent>
-		</SimpleSidebar>
-	);
+      <SimpleSidebarContent className="p-0">
+        <div className="flex h-full w-full bg-background">
+          <PlaygroundComponent />
+        </div>
+      </SimpleSidebarContent>
+    </SimpleSidebar>
+  );
 }
