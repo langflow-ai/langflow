1693d01451378ba4745c295725aaf1c9da0563bc
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-13 17:48:01 -0300

    added playground header

---
+++ src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/playground-header.tsx
@@ -0,0 +1,25 @@
+import { HeaderButton } from "./components/header-button";
+
+export function PlaygroundHeader({ onClose }: { onClose?: () => void }) {
+  return (
+    <div className="flex items-center justify-between gap-2 flex-1">
+      <div className="flex items-center gap-2">
+        <div className="truncate text-sm font-medium text-secondary-foreground">
+          Flow run{" "}
+          {new Date().toLocaleDateString("en-US", {
+            month: "2-digit",
+            day: "2-digit",
+          })}{" "}
+          {new Date().toLocaleTimeString("en-US", { hour12: false })}
+        </div>
+      </div>
+      <div className="flex items-center gap-1">
+        <HeaderButton icon="Plus" onClick={() => {}} />
+        <HeaderButton icon="History" onClick={() => {}} />
+        <HeaderButton icon="ExternalLink" onClick={() => {}} />
+        <HeaderButton icon="MoreHorizontal" onClick={() => {}} />
+        {onClose && <HeaderButton icon="X" onClick={onClose} />}
+      </div>
+    </div>
+  );
+}
