f853ff7b6582fcc72f83bf83bf109d6160d881e6
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-13 18:17:45 -0300

    delete unused components

--- src/frontend/src/components/core/playgroundComponent/types/chat-view-wrapper.ts
+++
@@ -1,8 +0,0 @@
-export type ChatViewWrapperProps = {
-  selectedViewField: { type: string; id: string } | undefined;
-  visibleSession: string | undefined;
-  messagesFetched: boolean;
-  sessionId: string;
-  sendMessage: (options: { repeat: number; files?: string[] }) => Promise<void>;
-  playgroundPage?: boolean;
-};
