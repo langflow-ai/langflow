507b1d795f4f8e3dc1c7f1108dce6011edfd9a5a
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-14 17:14:20 -0300

    Changed header button to pass ref

--- src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/components/header-button.tsx
+++ src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/components/header-button.tsx
@@ -1,21 +1,21 @@
 import ForwardedIconComponent from "@/components/common/genericIconComponent";
-import { Button } from "@/components/ui/button";
+import { Button, ButtonProps } from "@/components/ui/button";
+import { forwardRef } from "react";
 
-export function HeaderButton({
-  icon,
-  onClick,
-}: {
-  icon: string;
-  onClick: () => void;
-}) {
+export const HeaderButton = forwardRef<
+  HTMLButtonElement,
+  { icon: string; onClick?: () => void }
+>(({ icon, onClick, ...props }, ref) => {
   return (
     <Button
       variant="ghost"
       size="icon"
       className="flex h-8 items-center gap-2 text-muted-foreground"
+      ref={ref}
       onClick={onClick}
+      {...props}
     >
       <ForwardedIconComponent name={icon} className="h-4 w-4" />
     </Button>
   );
-}
+});
