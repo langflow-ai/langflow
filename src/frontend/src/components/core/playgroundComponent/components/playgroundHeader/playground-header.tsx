3d384fa9d9d19084d9d63369185cd128c34f209e
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-18 16:41:50 -0300

    Add "add session" behavior and correct flow name

--- src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/playground-header.tsx
+++ src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/playground-header.tsx
@@ -1,21 +1,29 @@
+import { useShallow } from "zustand/react/shallow";
+import { DEFAULT_SESSION_NAME } from "@/constants/constants";
+import useFlowStore from "@/stores/flowStore";
+import { usePlaygroundStore } from "@/stores/playgroundStore";
 import { HeaderButton } from "./components/header-button";
 import { SessionManagerPopover } from "./components/session-manager-popover";
+import { useAddNewSession } from "./hooks/useAddNewSession";
 
 export function PlaygroundHeader({ onClose }: { onClose?: () => void }) {
+  const selectedSession = usePlaygroundStore((state) => state.selectedSession);
+  const flowId = useFlowStore(useShallow((state) => state.currentFlow?.id));
+
+  const addSession = useAddNewSession();
+
+  const sessionName =
+    selectedSession === flowId ? DEFAULT_SESSION_NAME : selectedSession;
+
   return (
     <div className="flex items-center justify-between gap-2 px-4 py-2">
       <div className="flex items-center gap-2">
         <div className="truncate text-sm font-medium text-secondary-foreground">
-          Flow run{" "}
-          {new Date().toLocaleDateString("en-US", {
-            month: "2-digit",
-            day: "2-digit",
-          })}{" "}
-          {new Date().toLocaleTimeString("en-US", { hour12: false })}
+          {sessionName}
         </div>
       </div>
       <div className="flex items-center gap-1">
-        <HeaderButton icon="Plus" onClick={() => {}} />
+        <HeaderButton icon="Plus" onClick={addSession} />
         <SessionManagerPopover>
           <HeaderButton icon="History" />
         </SessionManagerPopover>
