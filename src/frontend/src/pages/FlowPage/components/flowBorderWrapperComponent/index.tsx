dd79d4b96fc7b8a7dda09ad29ea044046e4fbf9d
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-19 15:47:41 -0300

    Added playground sidebar inside flow border wrapper to apply border to entire canvas

--- src/frontend/src/pages/FlowPage/components/flowBorderWrapperComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/flowBorderWrapperComponent/index.tsx
@@ -1,6 +1,7 @@
 import { useSimpleSidebar } from "@/components/ui/simple-sidebar";
 import { cn } from "@/utils/utils";
 import Page from "../PageComponent";
+import { PlaygroundSidebar } from "../PlaygroundSidebar";
 
 export default function FlowBorderWrapperComponent({
   setIsLoading,
@@ -9,15 +10,23 @@ export default function FlowBorderWrapperComponent({
 }) {
   const { open } = useSimpleSidebar();
   return (
-    <main
+    <div
       className={cn(
-        "flex w-full overflow-hidden transition-all duration-300",
-        open && "m-2 mr-0 rounded-xl"
+        "transition-all duration-300 flex h-full w-full",
+        open && "p-2"
       )}
     >
-      <div className="h-full w-full relative">
-        <Page setIsLoading={setIsLoading} />
-      </div>
-    </main>
+      <main
+        className={cn(
+          "flex w-full overflow-hidden transition-all duration-300",
+          open && "rounded-xl"
+        )}
+      >
+        <div className="h-full w-full relative">
+          <Page setIsLoading={setIsLoading} />
+        </div>
+      </main>
+      <PlaygroundSidebar />
+    </div>
   );
 }
