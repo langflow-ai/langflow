6bd0bca7c598e0d4c1d78d50b3098e0208596a78
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-19 16:55:43 -0300

    Used session menu item on manager item, renamed session item to session manager item

---
+++ src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/components/session-manager-item.tsx
@@ -0,0 +1,40 @@
+import { forwardRef, useState } from "react";
+import { useShallow } from "zustand/react/shallow";
+import { DEFAULT_SESSION_NAME } from "@/constants/constants";
+import useFlowStore from "@/stores/flowStore";
+import { usePlaygroundStore } from "@/stores/playgroundStore";
+import { cn } from "@/utils/utils";
+import { SessionMenuItem } from "./session-menu-item";
+
+interface SessionItemProps {
+  sessionId: string;
+}
+
+export const SessionItem = forwardRef<HTMLDivElement, SessionItemProps>(
+  ({ sessionId }, ref) => {
+    const selectedSession = usePlaygroundStore(
+      (state) => state.selectedSession
+    );
+    const setSelectedSession = usePlaygroundStore(
+      (state) => state.setSelectedSession
+    );
+
+    const flowId = useFlowStore(useShallow((state) => state.currentFlow?.id));
+
+    const handleSessionSelect = () => {
+      setSelectedSession(sessionId);
+    };
+    const isSelected = selectedSession === sessionId;
+    const canEdit = sessionId !== flowId;
+
+    return (
+      <SessionMenuItem
+        className={cn(isSelected && "bg-muted")}
+        onSelect={handleSessionSelect}
+        ref={ref}
+      >
+        {canEdit ? sessionId : DEFAULT_SESSION_NAME}
+      </SessionMenuItem>
+    );
+  }
+);
