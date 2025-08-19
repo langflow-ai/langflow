aa6f1499be83e0ece32aa6ca964cd6b5606747ba
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-19 17:00:50 -0300

    Changed default session name

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -1034,7 +1034,7 @@ export const POLLING_MESSAGES = {
   STREAMING_NOT_SUPPORTED: "Streaming not supported",
 } as const;
 
-export const DEFAULT_SESSION_NAME = "Default Session";
+export const DEFAULT_SESSION_NAME = "Default session";
 export const NEW_SESSION_NAME = "New chat";
 
 export const BUILD_POLLING_INTERVAL = 25;
