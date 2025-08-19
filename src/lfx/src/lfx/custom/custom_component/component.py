ac2f78a19a05b514b6e9e0ac2c1a74fe3bf42b05
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2025-08-19 15:17:22 -0300

    fix: change async methods to synchronous in Component class for toolkit conversion

--- src/lfx/src/lfx/custom/custom_component/component.py
+++ src/lfx/src/lfx/custom/custom_component/component.py
@@ -1311,7 +1311,7 @@ class Component(CustomComponent):
     def _get_fallback_input(self, **kwargs):
         return Input(**kwargs)
 
-    async def to_toolkit(self) -> list[Tool]:
+    def to_toolkit(self) -> list[Tool]:
         """Convert component to a list of tools.
 
         This is a template method that defines the skeleton of the toolkit creation
@@ -1325,7 +1325,7 @@ class Component(CustomComponent):
                 - tags: List of tags associated with the tool
         """
         # Get tools from subclass implementation
-        tools = await self._get_tools()
+        tools = self._get_tools()
 
         if hasattr(self, TOOLS_METADATA_INPUT_NAME):
             tools = self._filter_tools_by_status(tools=tools, metadata=self.tools_metadata)
@@ -1334,7 +1334,7 @@ class Component(CustomComponent):
         # If no metadata exists yet, filter based on enabled_tools
         return self._filter_tools_by_status(tools=tools, metadata=None)
 
-    async def _get_tools(self) -> list[Tool]:
+    def _get_tools(self) -> list[Tool]:
         """Get the list of tools for this component.
 
         This method can be overridden by subclasses to provide custom tool implementations.
