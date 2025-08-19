fa774b92064291c243d00d13c61a7cf13a210447
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-19 15:47:50 -0300

    Removed playground sidebar from flow page

--- src/frontend/src/pages/FlowPage/index.tsx
+++ src/frontend/src/pages/FlowPage/index.tsx
@@ -180,7 +180,6 @@ export default function FlowPage({ view }: { view?: boolean }): JSX.Element {
               shortcut={openPlayground}
             >
               <FlowBorderWrapperComponent setIsLoading={setIsLoading} />
-              <PlaygroundSidebar />
             </SimpleSidebarProvider>
           </>
         )}
