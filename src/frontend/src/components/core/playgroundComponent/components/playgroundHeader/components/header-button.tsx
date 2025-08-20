3eceeea8f3abf7d5a24b2e1ab45c2f644b7531e8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-20 13:50:24 -0300

    Add classname to header button

--- src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/components/header-button.tsx
+++ src/frontend/src/components/core/playgroundComponent/components/playgroundHeader/components/header-button.tsx
@@ -1,16 +1,20 @@
 import { forwardRef } from "react";
 import ForwardedIconComponent from "@/components/common/genericIconComponent";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/utils/utils";
 
 export const HeaderButton = forwardRef<
   HTMLButtonElement,
-  { icon: string; onClick?: () => void }
->(({ icon, onClick, ...props }, ref) => {
+  { icon: string; onClick?: () => void; className?: string }
+>(({ icon, onClick, className, ...props }, ref) => {
   return (
     <Button
       variant="ghost"
       size="icon"
-      className="flex h-8 w-8 items-center gap-2 text-muted-foreground no-focus-visible"
+      className={cn(
+        "flex h-8 w-8 items-center gap-2 text-muted-foreground no-focus-visible",
+        className
+      )}
       ref={ref}
       onClick={onClick}
       {...props}
