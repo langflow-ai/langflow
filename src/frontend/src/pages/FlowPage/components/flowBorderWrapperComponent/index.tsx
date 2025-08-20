8f143fe71a1dc8f186d9818037fd0802109a7586
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-20 12:14:54 -0300

    Changed flow border wrapper to just display margin when not full screen

--- src/frontend/src/pages/FlowPage/components/flowBorderWrapperComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/flowBorderWrapperComponent/index.tsx
@@ -1,4 +1,5 @@
 import { useSimpleSidebar } from "@/components/ui/simple-sidebar";
+import { usePlaygroundStore } from "@/stores/playgroundStore";
 import { cn } from "@/utils/utils";
 import Page from "../PageComponent";
 import { PlaygroundSidebar } from "../PlaygroundSidebar";
@@ -9,12 +10,13 @@ export default function FlowBorderWrapperComponent({
   setIsLoading: (isLoading: boolean) => void;
 }) {
   const { open } = useSimpleSidebar();
+  const isFullscreen = usePlaygroundStore((state) => state.isFullscreen);
   return (
     <>
       <main
         className={cn(
           "flex flex-1 min-w-0 overflow-hidden transition-all duration-300",
-          open && "rounded-xl m-2 mr-0"
+          open && !isFullscreen && "rounded-xl m-2 mr-0"
         )}
       >
         <div className="h-full w-full relative">
