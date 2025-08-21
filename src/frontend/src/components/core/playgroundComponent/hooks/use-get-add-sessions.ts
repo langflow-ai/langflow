ec27a0d4df5f26922c14dda0b3abf385080cc2e8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-21 13:06:01 -0300

    Removed adding new session to the sessions

--- src/frontend/src/components/core/playgroundComponent/hooks/use-get-add-sessions.ts
+++ src/frontend/src/components/core/playgroundComponent/hooks/use-get-add-sessions.ts
@@ -1,32 +1,23 @@
 import { NEW_SESSION_NAME } from "@/constants/constants";
 import { useGetSessionsFromFlowQuery } from "@/controllers/API/queries/messages/use-get-sessions-from-flow";
 import { usePlaygroundStore } from "@/stores/playgroundStore";
-import { useMemo } from "react";
 
 export const useGetAddSessions = ({ flowId }: { flowId?: string }) => {
   const { isPlayground } = usePlaygroundStore();
 
-  const selectedSession = usePlaygroundStore((state) => state.selectedSession);
   const setSelectedSession = usePlaygroundStore(
     (state) => state.setSelectedSession
   );
 
-  const { data: dbSessions } = useGetSessionsFromFlowQuery({
+  const { data: sessions } = useGetSessionsFromFlowQuery({
     flowId,
     useLocalStorage: isPlayground,
   });
 
   const addNewSession = () => {
-    const newSessionId = `${NEW_SESSION_NAME} ${dbSessions?.length ?? 0}`;
+    const newSessionId = `${NEW_SESSION_NAME} ${sessions?.length ?? 0}`;
     setSelectedSession(newSessionId);
   };
-
-  const sessions = useMemo(() => {
-    if (!selectedSession || dbSessions?.includes(selectedSession)) {
-      return dbSessions;
-    }
-    return [...(dbSessions || []), selectedSession];
-  }, [dbSessions, selectedSession]);
 
   return {
     addNewSession,
