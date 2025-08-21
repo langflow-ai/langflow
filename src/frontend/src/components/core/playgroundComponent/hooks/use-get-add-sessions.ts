b6c69f10d50444aadec2bcac0d679ecc1b766eb3
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-21 14:46:21 -0300

    Add ids to sessions

--- src/frontend/src/components/core/playgroundComponent/hooks/use-get-add-sessions.ts
+++ src/frontend/src/components/core/playgroundComponent/hooks/use-get-add-sessions.ts
@@ -1,15 +1,30 @@
+import { useMemo } from "react";
 import { NEW_SESSION_NAME } from "@/constants/constants";
 import { useGetSessionsFromFlowQuery } from "@/controllers/API/queries/messages/use-get-sessions-from-flow";
 import { usePlaygroundStore } from "@/stores/playgroundStore";
 
-export const useGetAddSessions = ({ flowId }: { flowId?: string }) => {
+interface SessionInfo {
+  id: string;
+  sessionId: string;
+}
+
+interface UseGetAddSessionsProps {
+  flowId?: string;
+}
+
+type UseGetAddSessionsReturnType = (props: UseGetAddSessionsProps) => {
+  addNewSession: () => void;
+  sessions: SessionInfo[];
+};
+
+export const useGetAddSessions: UseGetAddSessionsReturnType = ({ flowId }) => {
   const { isPlayground } = usePlaygroundStore();
 
   const setSelectedSession = usePlaygroundStore(
     (state) => state.setSelectedSession
   );
 
-  const { data: sessions } = useGetSessionsFromFlowQuery({
+  const { data: dbSessions } = useGetSessionsFromFlowQuery({
     flowId,
     useLocalStorage: isPlayground,
   });
@@ -18,6 +33,15 @@ export const useGetAddSessions = ({ flowId }: { flowId?: string }) => {
     const newSessionId = `${NEW_SESSION_NAME} ${sessions?.length ?? 0}`;
     setSelectedSession(newSessionId);
   };
+
+  const sessions = useMemo(() => {
+    return (
+      dbSessions?.map((sessionId, index) => ({
+        id: `session-${index}`,
+        sessionId: sessionId,
+      })) ?? []
+    );
+  }, [dbSessions]);
 
   return {
     addNewSession,
