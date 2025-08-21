11599c5e85dbee6bde2a95e75c64ed13cf0fa610
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-21 12:06:54 -0300

    Changed menu ellipsis to menu icon

---
+++ src/frontend/src/components/core/playgroundComponent/components/sessionSidebar/components/menu-icon-button.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import ForwardedIconComponent from "@/components/common/genericIconComponent";
+import { Button, type ButtonProps } from "@/components/ui/button";
+
+interface MenuIconButtonProps extends ButtonProps {
+  icon: string;
+}
+
+export const MenuIconButton = forwardRef<
+  HTMLButtonElement,
+  MenuIconButtonProps
+>(({ icon, ...props }, ref) => {
+  return (
+    <Button
+      variant="menu"
+      size="icon"
+      className="duration-75 text-muted-foreground"
+      ref={ref}
+      {...props}
+    >
+      <ForwardedIconComponent name={icon} />
+    </Button>
+  );
+});
