70769d0836487eb2b878950fe7c81f7d60e11734
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-08-22 09:28:39 -0300

    Removed unused code from playground

--- src/frontend/src/components/core/playgroundComponent/playground-component.tsx
+++ src/frontend/src/components/core/playgroundComponent/playground-component.tsx
@@ -1,32 +1,16 @@
-import { useEffect } from "react";
-import { v5 as uuidv5 } from "uuid";
 import { AnimatedConditional } from "@/components/ui/animated-close";
 import { useIsMobile } from "@/hooks/use-mobile";
-import useFlowsManagerStore from "@/stores/flowsManagerStore";
 import { usePlaygroundStore } from "@/stores/playgroundStore";
-import { useUtilityStore } from "@/stores/utilityStore";
 import ChatView from "./components/chatView/components/chat-view";
 import { PlaygroundHeader } from "./components/playgroundHeader/playground-header";
 import SessionSidebar from "./components/sessionSidebar/session-sidebar";
 
 export function PlaygroundComponent(): JSX.Element {
-  const clientId = useUtilityStore((state) => state.clientId);
-  const realFlowId = useFlowsManagerStore((state) => state.currentFlowId);
   const isPlayground = usePlaygroundStore((state) => state.isPlayground);
-  const currentFlowId = isPlayground
-    ? uuidv5(`${clientId}_${realFlowId}`, uuidv5.DNS)
-    : realFlowId;
 
   const selectedSession = usePlaygroundStore((state) => state.selectedSession);
-  const setSelectedSession = usePlaygroundStore(
-    (state) => state.setSelectedSession,
-  );
 
   const isFullscreen = usePlaygroundStore((state) => state.isFullscreen);
-
-  useEffect(() => {
-    setSelectedSession(currentFlowId);
-  }, [currentFlowId, setSelectedSession]);
 
   const isMobile = useIsMobile();
 
