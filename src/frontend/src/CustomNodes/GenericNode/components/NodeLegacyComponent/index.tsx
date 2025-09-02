b7c0d82239619c351a9e328e0013ba1ec8e33d0d
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-09-02 17:22:22 -0300

    Add components filtering in nodelegacycomponent

--- src/frontend/src/CustomNodes/GenericNode/components/NodeLegacyComponent/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/NodeLegacyComponent/index.tsx
@@ -16,8 +16,8 @@ export default function NodeLegacyComponent({
   const setFilterType = useFlowStore((state) => state.setFilterType);
   const setFilterEdge = useFlowStore((state) => state.setFilterEdge);
 
-  const handleFilterComponent = () => {
-    setFilterComponent(replacement);
+  const handleFilterComponent = (component: string) => {
+    setFilterComponent(component);
     setFilterType(undefined);
     setFilterEdge([]);
   };
@@ -26,25 +26,50 @@ export default function NodeLegacyComponent({
   return (
     <div
       className={cn(
-        "flex w-full items-center gap-3 rounded-t-[0.69rem] border-b bg-muted p-2 px-4 py-2",
+        "flex flex-col w-full items-center gap-3 rounded-t-[0.69rem] border-b bg-muted p-2 px-4 py-2",
       )}
     >
-      <div className="h-2.5 w-2.5 rounded-full bg-warning" />
-      <div className="mb-px flex-1 truncate text-mmd font-medium">Legacy</div>
+      <div className="flex items-center gap-3 w-full">
+        <div className="h-2.5 w-2.5 rounded-full bg-warning" />
+        <div className="mb-px flex-1 truncate text-mmd font-medium">Legacy</div>
 
-      <Button
-        variant="ghost"
-        size="icon"
-        className="shrink-0 !text-mmd"
-        onClick={(e) => {
-          e.stopPropagation();
-          setDismissAll(true);
-        }}
-        aria-label="Dismiss warning bar"
-        data-testid="dismiss-warning-bar"
-      >
-        Dismiss
-      </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="shrink-0 !text-mmd"
+          onClick={(e) => {
+            e.stopPropagation();
+            setDismissAll(true);
+          }}
+          aria-label="Dismiss warning bar"
+          data-testid="dismiss-warning-bar"
+        >
+          Dismiss
+        </Button>
+      </div>
+      <div className="text-mmd text-muted-foreground w-full">
+        {replacement && Array.isArray(replacement) && replacement.length > 0 ? (
+          <span className="block items-center">
+            Use{" "}
+            {foundComponents.map((component, index) => (
+              <>
+                {index > 0 && ", "}
+                <Button
+                  variant="link"
+                  className=" !text-accent-pink-foreground !text-mmd !inline-block"
+                  size={null}
+                  onClick={() => handleFilterComponent(component)}
+                >
+                  {component}
+                </Button>
+              </>
+            ))}
+            .
+          </span>
+        ) : (
+          "No direct replacement."
+        )}
+      </div>
     </div>
   );
 }
