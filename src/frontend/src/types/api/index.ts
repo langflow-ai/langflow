ab73842d0a958c3e7db63bbfa7b43eeddcee5e8f
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-09-02 17:17:38 -0300

    changed replacement type to list

--- src/frontend/src/types/api/index.ts
+++ src/frontend/src/types/api/index.ts
@@ -43,7 +43,7 @@ export type APIClassType = {
   custom_fields?: CustomFieldsType;
   beta?: boolean;
   legacy?: boolean;
-  replacement?: string;
+  replacement?: string[];
   documentation: string;
   error?: string;
   official?: boolean;
