d8512efa9b3eda8d2e0df1db90ea601692492f11
cristhianzl <cristhian.lousa@gmail.com>
2025-09-17 11:51:19 -0300

    ✨ (NodeStatus): Add data-testid attributes for better testing and accessibility ✨ (general-bugs-reset-flow-run.spec.ts): Add test for user to send images in the playground with the agent component ♻️ (zoom-out.ts): Refactor zoomOut function to handle missing zoom_out button and improve usability

--- src/frontend/src/CustomNodes/GenericNode/components/NodeStatus/index.tsx
+++ src/frontend/src/CustomNodes/GenericNode/components/NodeStatus/index.tsx
@@ -409,13 +409,24 @@ export default function NodeStatus({
             >
               <div className="cursor-help">
                 {conditionSuccess && validationStatus?.data?.duration ? (
-                  <div className="flex rounded-sm px-1 font-mono text-xs text-accent-emerald-foreground transition-colors hover:bg-accent-emerald">
+                  <div
+                    className="flex rounded-sm px-1 font-mono text-xs text-accent-emerald-foreground transition-colors hover:bg-accent-emerald"
+                    data-testid={`node_duration_` + display_name.toLowerCase()}
+                  >
                     <span>
                       {normalizeTimeString(validationStatus?.data?.duration)}
                     </span>
                   </div>
                 ) : (
-                  <div className="flex items-center self-center">
+                  <div
+                    data-testid={
+                      `node_status_icon_` +
+                      display_name.toLowerCase() +
+                      `_` +
+                      buildStatus?.toLowerCase()
+                    }
+                    className="flex items-center self-center"
+                  >
                     {iconStatus}
                   </div>
                 )}
--- src/frontend/tests/utils/zoom-out.ts
+++ src/frontend/tests/utils/zoom-out.ts
@@ -1,7 +1,13 @@
 import type { Page } from "@playwright/test";
 
 export async function zoomOut(page: Page, times: number = 2) {
+  const zoomOutButton = await page.getByTestId("zoom_out").count();
+  if (zoomOutButton === 0) {
+    await page.getByTestId("canvas_controls_dropdown").click();
+  }
   for (let i = 0; i < times; i++) {
     await page.getByTestId("zoom_out").click();
   }
+
+  await page.getByTestId("canvas_controls_dropdown").click();
 }
