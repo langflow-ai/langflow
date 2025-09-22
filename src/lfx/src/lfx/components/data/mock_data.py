a5a9a63692efee356930c48c6877c7b6bae3e31c
Lucas Democh <democh@datax.dev>
2025-09-22 18:06:28 -0300

    Refactor string concatenation in mock_data.py

--- src/lfx/src/lfx/components/data/mock_data.py
+++ src/lfx/src/lfx/components/data/mock_data.py
@@ -118,10 +118,9 @@ class MockDataGeneratorComponent(Component):
                 "mollit anim id est laborum."
             ),
             (
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, "(
-                    "totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto "
-                    "beatae vitae dicta sunt explicabo."
-                )
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, "
+                "totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto "
+                "beatae vitae dicta sunt explicabo."
             ),
             (
                 "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, "
