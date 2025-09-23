e47363e3dc029b4b84e49b35c9be1023bc38d02a
Uday-sidagana <uday.sidgana@gmail.com>
2025-09-24 05:26:46 +0530

    Display name consistency

--- src/lfx/src/lfx/components/composio/googlebigquery_composio.py
+++ src/lfx/src/lfx/components/composio/googlebigquery_composio.py
@@ -2,7 +2,7 @@ from lfx.base.composio.composio_base import ComposioBaseComponent
 
 
 class ComposioGoogleBigQueryAPIComponent(ComposioBaseComponent):
-    display_name: str = "Google BigQuery"
+    display_name: str = "GoogleBigQuery"
     icon = "Googlebigquery"
     documentation: str = "https://docs.composio.dev"
     app_name = "googlebigquery"
