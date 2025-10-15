72fb62b8821c8ad0ecde8d5bd33bf53becdc188e
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2025-10-15 10:18:39 -0300

    refactor: enhance lockFlow and unlockFlow functions for improved visibility checks
    - Replaced isVisible calls with waitFor to ensure elements are visible before proceeding, enhancing test reliability.
    - Removed unnecessary waitForTimeout calls to streamline the flow execution process.

--- src/frontend/tests/utils/lock-flow.ts
+++ src/frontend/tests/utils/lock-flow.ts
@@ -3,7 +3,9 @@ import type { Page } from "@playwright/test";
 export async function lockFlow(page: Page) {
   await page.getByTestId("flow_name").click();
   await page.getByTestId("lock-flow-switch").click();
-  await page.getByTestId("icon-Lock").isVisible({ timeout: 5000 });
+  await page
+    .getByTestId("icon-Lock")
+    .waitFor({ state: "visible", timeout: 5000 });
   await page.waitForSelector(
     '[data-testid="lock-flow-switch"][data-state="checked"]',
     {
@@ -11,32 +13,33 @@ export async function lockFlow(page: Page) {
     },
   );
 
-  await page.waitForTimeout(500);
-  await page.getByTestId("save-flow-settings").isEnabled({ timeout: 3000 });
+  await page
+    .getByTestId("save-flow-settings")
+    .waitFor({ state: "visible", timeout: 3000 });
   await page.getByTestId("save-flow-settings").click();
 
   await page.waitForSelector('[data-testid="save-flow-settings"]', {
     state: "hidden",
     timeout: 5000,
   });
-  //ensure the UI is updated
-  await page.getByTestId("icon-Lock").isVisible({ timeout: 5000 });
 }
 
 export async function unlockFlow(page: Page) {
   await page.getByTestId("flow_name").click();
   await page.getByTestId("lock-flow-switch").click();
-  await page.getByTestId("icon-Lock").isVisible({ timeout: 5000 });
+  await page
+    .getByTestId("icon-Lock")
+    .waitFor({ state: "visible", timeout: 5000 });
   await page.waitForSelector(
     '[data-testid="lock-flow-switch"][data-state="unchecked"]',
     {
       timeout: 1000,
     },
   );
-  //ensure the UI is updated
-  await page.waitForTimeout(500);
 
-  await page.getByTestId("save-flow-settings").isEnabled({ timeout: 3000 });
+  await page
+    .getByTestId("save-flow-settings")
+    .waitFor({ state: "visible", timeout: 3000 });
   await page.getByTestId("save-flow-settings").click();
   await page.waitForSelector('[data-testid="save-flow-settings"]', {
     state: "hidden",
