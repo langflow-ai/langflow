444d922cd3e051eded25ccf9e3490705d221da3b
Eric Hare <ericrhare@gmail.com>
2025-10-21 15:49:00 -0700

    feat: Ollama and WatsonX embedding model support

---
+++ src/lfx/src/lfx/base/models/watsonx_constants.py
@@ -0,0 +1,8 @@
+# Granite Embedding models
+WATSONX_EMBEDDING_MODEL_NAMES = [
+    "ibm/granite-embedding-125m-english",
+    "ibm/granite-embedding-278m-multilingual",
+    "ibm/granite-embedding-30m-english",
+    "ibm/granite-embedding-107m-multilingual",
+    "ibm/granite-embedding-30m-sparse",
+]
