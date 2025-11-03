23b3cb46acdccf50263dff9e6a86734a39472820
Olfa Maslah <olfamaslah@macbookpro.war.can.ibm.com>
2025-11-03 12:31:37 -0500

    change pypi_nightly_tag.py version to read it from pyproject.toml directly

--- scripts/ci/pypi_nightly_tag.py
+++ scripts/ci/pypi_nightly_tag.py
@@ -4,6 +4,7 @@
 import sys
 
 import packaging.version
+import requests
 from packaging.version import Version
 
 PYPI_LANGFLOW_URL = "https://pypi.org/pypi/langflow/json"
@@ -16,8 +17,6 @@ ARGUMENT_NUMBER = 2
 
 
 def get_latest_published_version(build_type: str, *, is_nightly: bool) -> Version:
-    import requests
-
     url = ""
     if build_type == "base":
         url = PYPI_LANGFLOW_BASE_NIGHTLY_URL if is_nightly else PYPI_LANGFLOW_BASE_URL
@@ -37,23 +36,60 @@ def get_latest_published_version(build_type: str, *, is_nightly: bool) -> Versio
 
 
 def create_tag(build_type: str):
-    current_version = get_latest_published_version(build_type, is_nightly=False)
-    current_nightly_version = get_latest_published_version(build_type, is_nightly=True)
+    from pathlib import Path
+
+    import tomllib
+
+    # Read version from pyproject.toml
+    main_tag_pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
+    pyproject_data = tomllib.loads(main_tag_pyproject_path.read_text())
+
+    current_version_str = pyproject_data["project"]["version"]
+    current_version = Version(current_version_str)
+
+    latest_base_version = get_latest_published_version(build_type, is_nightly=False)
+    latest_nightly_version = get_latest_published_version(build_type, is_nightly=True)
+    try:
+        current_nightly_version = get_latest_published_version(is_nightly=True)
+        nightly_base_version = current_nightly_version.base_version
+    except (requests.RequestException, KeyError, ValueError):
+        # If main tag nightly doesn't exist on PyPI yet, this is the first nightly
+        current_nightly_version = None
+        nightly_base_version = None
 
     build_number = "0"
     latest_base_version = current_version.base_version
-    nightly_base_version = current_nightly_version.base_version
 
-    if latest_base_version == nightly_base_version:
+    if current_nightly_version and latest_base_version == nightly_base_version:
         # If the latest version is the same as the nightly version, increment the build number
         build_number = str(current_nightly_version.dev + 1)
 
-    new_nightly_version = latest_base_version + ".dev" + build_number
+    new_nightly_version = latest_nightly_version + ".dev" + build_number
 
     # Prepend "v" to the version, if DNE.
     # This is an update to the nightly version format.
     if not new_nightly_version.startswith("v"):
-        new_nightly_version = "v" + new_nightly_version
+        new_nightly_version = current_version_str + ".dev" + new_nightly_version
+
+    # Verify if version is PEP440 compliant.
+    packaging.version.Version(new_nightly_version)
+
+    return new_nightly_version
+
+    # build_number = "0"
+    # latest_base_version = current_version.base_version
+    # nightly_base_version = current_nightly_version.base_version
+
+    # if latest_base_version == nightly_base_version:
+    #     # If the latest version is the same as the nightly version, increment the build number
+    #     build_number = str(current_nightly_version.dev + 1)
+
+    # new_nightly_version = latest_base_version + ".dev" + build_number
+
+    # # Prepend "v" to the version, if DNE.
+    # # This is an update to the nightly version format.
+    # if not new_nightly_version.startswith("v"):
+    #     new_nightly_version = "v" + new_nightly_version
 
     # X.Y.Z.dev.YYYYMMDD
     # This takes the base version of the current version and appends the
@@ -70,9 +106,9 @@ def create_tag(build_type: str):
     # )
 
     # Verify if version is PEP440 compliant.
-    packaging.version.Version(new_nightly_version)
+    # packaging.version.Version(new_nightly_version)
 
-    return new_nightly_version
+    # return new_nightly_version
 
 
 if __name__ == "__main__":
