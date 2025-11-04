09677a4ed6b56d562f14f40359d32ce55f3e45d5
Deon Sanchez <69873175+deon-sanchez@users.noreply.github.com>
2025-11-04 12:37:03 -0700

    fix zoom out click on tests

--- src/frontend/tests/utils/zoom-out.ts
+++ src/frontend/tests/utils/zoom-out.ts
@@ -1,19 +1,19 @@
-import type { Page } from "@playwright/test";
+import type { Page } from '@playwright/test';
 
 export async function zoomOut(page: Page, times: number = 2) {
   await page.waitForSelector('[data-testid="canvas_controls_dropdown"]', {
     timeout: 3000,
   });
 
-  let zoomOutButton = await page.getByTestId("zoom_out").count();
+  let zoomOutButton = await page.getByTestId('zoom_out').count();
   if (zoomOutButton === 0) {
-    await page.getByTestId("canvas_controls_dropdown").click();
-    zoomOutButton = await page.getByTestId("zoom_out").count();
+    await page.getByTestId('canvas_controls_dropdown').click();
+    zoomOutButton = await page.getByTestId('zoom_out').count();
   }
   for (let i = 0; i < times; i++) {
-    await page.getByTestId("zoom_out").click();
+    await page.getByTestId('zoom_out').click();
   }
   if (zoomOutButton > 0) {
-    await page.getByTestId("canvas_controls_dropdown").click();
+    await page.getByTestId('rf__wrapper').click();
   }
 }
