a27c48998e759a74803be19942494afff45416a9
Adam Aghili <Adam.Aghili@ibm.com>
2025-11-05 16:34:12 -0500

    chore: add assistant store

---
+++ src/frontend/src/types/zustand/assistantManager/index.ts
@@ -0,0 +1,43 @@
+import type { FlowType, targetHandleType } from "../../flow";
+
+export type AssistantManagerStoreType = {
+  // Flow Context
+  getFlowById: (id: string) => FlowType | undefined;
+  flows: Array<FlowType> | undefined;
+  setFlows: (flows: FlowType[]) => void;
+  currentFlow: FlowType | undefined;
+  currentFlowId: string;
+  setCurrentFlow: (flow?: FlowType) => void;
+  selectedFlowsComponentsCards: string[];
+  setSelectedFlowsComponentsCards: (selected: string[]) => void;
+
+  // Node Context
+  selectedCompData: targetHandleType | undefined;
+  setSelectedCompData: (compData: targetHandleType | undefined) => void;
+  // Project context
+
+  // UI state
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
+  undo: () => void;
+  redo: () => void;
+  takeSnapshot: () => void;
+  AssistantSidebarOpen: boolean;
+  setAssistantSidebarOpen: (AssistantSideBarOpen: boolean) => void;
+
+  // Chat state
+  examples: Array<FlowType>;
+  setExamples: (examples: FlowType[]) => void;
+  newAssistantChat: boolean;
+  setNewAssistantChat: (newChat: boolean) => void;
+
+  // General use
+  healthCheckMaxRetries: number;
+  setHealthCheckMaxRetries: (healthCheckMaxRetries: number) => void;
+  resetStore: () => void;
+};
+
+export type UseUndoRedoOptions = {
+  maxHistorySize: number;
+  enableShortcuts: boolean;
+};
