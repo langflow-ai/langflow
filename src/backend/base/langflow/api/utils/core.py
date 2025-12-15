364503386b93c49531949bb99423b02b5570589d
Hamza Rashid <hzarashid@gmail.com>
2025-12-15 20:05:40 +0000

    recover line from 1.7.0

--- src/backend/base/langflow/api/utils/core.py
+++ src/backend/base/langflow/api/utils/core.py
@@ -61,7 +61,7 @@ def remove_api_keys(flow: dict):
         node_data = node.get("data").get("node")
         template = node_data.get("template")
         for value in template.values():
-            if isinstance(value, dict) and has_api_terms(value["name"]) and value.get("password"):
+            if isinstance(value, dict) and "name" in value and has_api_terms(value["name"]) and value.get("password"):
                 value["value"] = None
 
     return flow
