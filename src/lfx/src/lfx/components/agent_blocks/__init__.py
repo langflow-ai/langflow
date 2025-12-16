4d62543889da4c94c40e3a718419482049bda07e
ogabrielluiz <gabriel@langflow.org>
2025-12-16 19:54:39 -0300

    feat(agent): add AgentLoopComponent to encapsulate complete agent loop functionality

--- src/lfx/src/lfx/components/agent_blocks/__init__.py
+++ src/lfx/src/lfx/components/agent_blocks/__init__.py
@@ -1,8 +1,10 @@
+from lfx.components.agent_blocks.agent_loop import AgentLoopComponent
 from lfx.components.agent_blocks.agent_step import AgentStepComponent
 from lfx.components.agent_blocks.execute_tool import ExecuteToolComponent
 from lfx.components.agent_blocks.think_tool import ThinkToolComponent
 
 __all__ = [
+    "AgentLoopComponent",
     "AgentStepComponent",
     "ExecuteToolComponent",
     "ThinkToolComponent",
