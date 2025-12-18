df4757764437cc31ea15b1dde3e25c247e5110ce
Olfa Maslah <olfamaslah@Olfas-MacBook-Pro.local>
2025-12-18 15:15:16 -0500

    chore: remove unused sliding container button

--- src/frontend/src/components/core/playgroundComponent/sliding-container/components/index.ts
+++ src/frontend/src/components/core/playgroundComponent/sliding-container/components/index.ts
@@ -1,4 +1,2 @@
 export * from "./flow-page-sliding-container";
 export { FlowPageSlidingContainerContent } from "./flow-page-sliding-container";
-export * from "./playground-button-sliding";
-export { PlaygroundButtonSliding } from "./playground-button-sliding";
