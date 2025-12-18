ef65c6502f7479e38e9df4a22ffbd7c294e74071
ogabrielluiz <gabriel@langflow.org>
2025-12-18 10:53:34 -0300

    feat(agent): add SharedContextComponent for multi-agent collaboration

--- src/lfx/src/lfx/components/agent_blocks/__init__.py
+++ src/lfx/src/lfx/components/agent_blocks/__init__.py
@@ -1,11 +1,13 @@
 from lfx.components.agent_blocks.agent_loop import AgentLoopComponent
 from lfx.components.agent_blocks.agent_step import AgentStepComponent
 from lfx.components.agent_blocks.execute_tool import ExecuteToolComponent
+from lfx.components.agent_blocks.shared_context import SharedContextComponent
 from lfx.components.agent_blocks.think_tool import ThinkToolComponent
 
 __all__ = [
     "AgentLoopComponent",
     "AgentStepComponent",
     "ExecuteToolComponent",
+    "SharedContextComponent",
     "ThinkToolComponent",
 ]
