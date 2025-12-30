a5872539fceab91d6f1f6370d6342442bfd498ee
Hamza Rashid <hzarashid@gmail.com>
2025-12-30 21:31:19 +0000

    misc

--- src/backend/base/langflow/helpers/flow_version.py
+++ src/backend/base/langflow/helpers/flow_version.py
@@ -1,15 +1,15 @@
 
 from __future__ import annotations
 
+from contextlib import asynccontextmanager
 from copy import deepcopy
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
 from uuid import UUID
 
+from lfx.log.logger import logger
 from sqlmodel import select
 
-from langflow.logging.logger import logger
-from langflow.schema.data import Data
 from langflow.services.database.models.flow.model import Flow
 from langflow.services.database.models.flow.utils import get_webhook_component_in_flow
 from langflow.services.database.models.flow_version.model import FlowVersion
@@ -21,26 +21,11 @@ if TYPE_CHECKING:
     from sqlmodel.ext.asyncio.session import AsyncSession
 
 
-FLOW_NOT_FOUND_ERROR_MSG = "Flow not found"
-
-EXCLUDE_NODE_KEYS = {
-    "selected",
-    "dragging",
-    "positionAbsolute",
-    "measured",
-    "resizing",
-    "width",
-    "height",
-    "last_updated",
-}
-
-EXCLUDE_EDGE_KEYS = {
-    "selected",
-    "animated",
-    "className",
-    "style",
-}
-
+########################################################
+# querying
+########################################################
+FLOW_NOT_FOUND_ERROR_MSG = "Flow not found."
+FLOW_VERSION_NOT_FOUND_ERROR_MSG = "Flow version not found."
 
 async def save_flow_checkpoint(
     *,
@@ -58,9 +43,7 @@ async def save_flow_checkpoint(
     If graph data is not present in update_data
     or does not differ from the current flow data,
     then checkpointing is skipped.
-    If graph data is present in update_data
-    and differs from the current flow's data,
-    then the flow's latest version is incremented,
+    Otherwise, the flow's latest version is incremented,
     and a new FlowVersion row is created
     with the latest version number and the new graph data,
 
@@ -72,14 +55,9 @@ async def save_flow_checkpoint(
         update_data: The flow to save in the checkpoint.
 
     Returns:
-        The FlowVersion object if the checkpoint is saved successfully, otherwise None.
+        None
     """
-    if not (user_id and flow_id):
-        msg = "user_id and flow_id are required"
-        raise ValueError(msg)
-    if not update_data:
-        logger.debug("Update data not provided, skipping checkpoint")
-        return None
+    require_user_and_flow_ids(user_id, flow_id)
 
     uuid_user_id = _get_uuid(user_id)
     uuid_flow_id = _get_uuid(flow_id)
@@ -92,9 +70,9 @@ async def save_flow_checkpoint(
             update_data=update_data,
             )
 
-    async with session_scope() as session_:
+    async with session_scope() as _session:
         return await _save_flow_checkpoint(
-            session=session_,
+            session=_session,
             user_id=uuid_user_id,
             flow_id=uuid_flow_id,
             update_data=update_data,
@@ -109,7 +87,7 @@ async def _save_flow_checkpoint(
 ) -> Flow | None:
     """Save a checkpoint of a flow."""
     try:
-        db_flow =  (
+        db_flow = (
             await session.exec(
                 select(Flow)
                 .where(Flow.user_id == user_id)
@@ -125,18 +103,11 @@ async def _save_flow_checkpoint(
         msg = f"Failed to checkpoint flow: {FLOW_NOT_FOUND_ERROR_MSG}"
         raise ValueError(msg)
 
-    update_data.pop("latest_version", None) # prevent bad update to latest_version
-    for key, val in update_data.items():
-        setattr(db_flow, key, val)
-
-    await configure_flow_webhook_and_folder(db_flow, session, user_id)
-    db_flow.updated_at = datetime.now(timezone.utc)
-
-    # create new checkpoint iff graph data changed
+    # save in FlowVersion table iff graph data changed
     if (
         "data" in update_data and
         normalized_flow_data(update_data["data"]) != normalized_flow_data(db_flow.data)
-    ): # note that None and {} (empty dict) are acceptable values for flow data
+        ): # note (that) None and {} (empty dict) are acceptable values for flow data
         db_flow.latest_version += 1
         session.add(
             FlowVersion(
@@ -147,8 +118,21 @@ async def _save_flow_checkpoint(
                 flow_data=update_data["data"],
                 )
             )
+        logger.debug(
+            "Graph data changed for flow %s, created new checkpoint version %s",
+            flow_id,
+            db_flow.latest_version
+            )
 
+    # save in Flow table
+    update_data.pop("latest_version", None) # prevent bad update to latest_version
+    for key, val in update_data.items():
+        setattr(db_flow, key, val)
+
+    await configure_flow_webhook_and_folder(db_flow, session, user_id)
+    db_flow.updated_at = datetime.now(timezone.utc)
     session.add(db_flow)
+
     return db_flow
 
 
@@ -156,14 +140,13 @@ async def list_flow_versions(
     *,
     user_id: str | UUID | None = None,
     flow_id: str | UUID | None = None,
-    ) -> list[Data]:
+    ) -> list[dict] | None:
     """List the versions of the flow."""
-    if not (user_id and flow_id):
-        msg = "user_id and flow_id are required"
-        raise ValueError(msg)
+    require_user_and_flow_ids(user_id, flow_id)
 
     uuid_user_id = _get_uuid(user_id)
     uuid_flow_id = _get_uuid(flow_id)
+
     try:
         async with session_scope() as session:
             flow_versions = (
@@ -174,33 +157,58 @@ async def list_flow_versions(
                     .order_by(FlowVersion.version.desc())
                     )
                 ).all()
-        return [
-            Data(data=dict(version._mapping)) # noqa: SLF001
-            for version in flow_versions
-            ]
+        return [version.model_dump() for version in flow_versions]
     except Exception as e:
         msg = f"Error getting flow history: {e}"
         raise ValueError(msg) from e
-
 
 
 async def restore_flow_checkpoint(
     *,
     user_id: str | None = None,
     flow_id: str | None = None,
-    version_id: int | None = None,
+    version: int | None = None,
     flow_data_current: dict | None = None,
     ):
     """Restore a checkpoint of the flow."""
-    if not (user_id and flow_id):
-        msg = "user_id and flow_id are required"
-        raise ValueError(msg)
-    pass  # noqa: PIE790
+    require_user_and_flow_ids(user_id, flow_id)
+    require_proper_version(version)
+    pass # noqa: PIE790
+
+
+async def get_flow_checkpoint(
+    *,
+    user_id : str | UUID | None,
+    flow_id : str | UUID | None,
+    version : int | None
+    ):
+    require_user_and_flow_ids(user_id, flow_id)
+    require_proper_version(version)
+    pass # noqa: PIE790
 
 
 ########################################################
-# Helper functions for filtering flow data for version comparison
+# flow normalization for version comparison
 ########################################################
+# keys to remove when comparing flows versions
+EXCLUDE_NODE_KEYS = {
+    "selected",
+    "dragging",
+    "positionAbsolute",
+    "measured",
+    "resizing",
+    "width",
+    "height",
+    "last_updated",
+    }
+EXCLUDE_EDGE_KEYS = {
+    "selected",
+    "animated",
+    "className",
+    "style",
+    }
+
+
 def normalized_flow_data(flow_data: dict | None):
     """Filters a deepcopy of flow data to exclude transient state."""
     copy_flow_data = deepcopy(flow_data)
@@ -221,9 +229,10 @@ def remove_keys_from_dicts(dictlist : list[dict], exclude_keys : set):
 
 
 ########################################################
-# Other helper functions
+# flow configuration
 ########################################################
 async def get_default_folder(session: AsyncSession, user_id: UUID):
+    """Get the default folder for the user."""
     return (
         await session.exec(
             select(Folder)
@@ -234,6 +243,7 @@ async def get_default_folder(session: AsyncSession, user_id: UUID):
 
 
 async def configure_flow_webhook_and_folder(db_flow: Flow, session: AsyncSession, user_id: UUID):
+    """Configure the webhook and folder for the flow."""
     webhook_component = get_webhook_component_in_flow(db_flow.data)
     db_flow.webhook = webhook_component is not None
     if db_flow.folder_id is None:
@@ -241,5 +251,29 @@ async def configure_flow_webhook_and_folder(db_flow: Flow, session: AsyncSession
         db_flow.folder_id = default_folder.id if default_folder else None
 
 
+########################################################
+# validation
+########################################################
+MISSING_USER_OR_FLOW_ID_MSG = "user_id and flow_id are required."
+IMPROPER_VERSION_NUMBER_MSG = (
+    "Received invalid value for version number. "
+    "Please provide a version number greater than or equal to 0."
+    )
+
+
+def require_user_and_flow_ids(
+    user_id: str | UUID | None,
+    flow_id: str | UUID | None,
+    ):
+    if not (user_id and flow_id):
+        raise ValueError(MISSING_USER_OR_FLOW_ID_MSG)
+
+
+def require_proper_version(version : int | None):
+    if not (version and version > -1):
+        raise ValueError(IMPROPER_VERSION_NUMBER_MSG)
+
+
 def _get_uuid(value: str | UUID) -> UUID:
+    """Get a UUID from a string or UUID."""
     return UUID(value) if isinstance(value, str) else value
