2d433b37cca7329bb378c364388202e5c4355d38
Eric Hare <ericrhare@gmail.com>
2026-01-02 08:35:28 -0800

    Update index.tsx

--- src/frontend/src/pages/SettingsPage/pages/StorageSettingsPage/index.tsx
+++ src/frontend/src/pages/SettingsPage/pages/StorageSettingsPage/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ForwardedIconComponent } from "@/components/common/genericIconComponent";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
