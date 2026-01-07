f0001886a321346e60821b816a259de5cb4995ea
cristhianzl <cristhian.lousa@gmail.com>
2026-01-07 08:55:14 -0300

    fix erro gen bug 3

--- src/frontend/tests/extended/regression/generalBugs-shard-3.spec.ts
+++ src/frontend/tests/extended/regression/generalBugs-shard-3.spec.ts
@@ -59,6 +59,10 @@ test(
     await initialGPTsetup(page);
     await adjustScreenView(page);
 
+    await page.getByTestId("popover-anchor-input-api_key").fill(
+      process.env.OPENAI_API_KEY || "",
+    );
+
     await page
       .getByTestId("handle-chatinput-noshownode-chat message-source")
       .click();
