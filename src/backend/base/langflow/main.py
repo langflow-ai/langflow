cad2cb4d75ab2a055a3a847f1fcec749a29dd0d1
Adam Aghili <Adam.Aghili@ibm.com>
2026-01-07 13:31:19 -0500

    chore: test if test failures are do to import
    testing to see if test failures are caused by importing router_v1 or our new router is casuing the issue

--- src/backend/base/langflow/main.py
+++ src/backend/base/langflow/main.py
@@ -513,16 +513,16 @@ def create_app():
 
         router.include_router(mcp_router)
 
-    # # Import router_v1 and conditionally add agentic_router before including in main router
-    # from langflow.api import router_v1
+    # Import router_v1 and conditionally add agentic_router before including in main router
+    from langflow.api import router_v1
 
     # if settings.agentic_experience:
     #     from langflow.agentic.api.router import router as agentic_router
 
     #     router_v1.include_router(agentic_router)
 
-    # # Include router_v1 in router after all conditional routes are added
-    # router.include_router(router_v1)
+    # Include router_v1 in router after all conditional routes are added
+    router.include_router(router_v1)
 
     app.include_router(router)
     app.include_router(health_check_router)
