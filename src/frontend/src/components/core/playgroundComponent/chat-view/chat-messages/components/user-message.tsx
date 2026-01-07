ab5c071e8a121a0364e1b69be77256d11d66df40
Olfa Maslah <olfamaslah@Olfas-MacBook-Pro.local>
2026-01-06 22:43:43 -0500

    Show chat avatars from sm breakpoint (>=40rem)

--- src/frontend/src/components/core/playgroundComponent/chat-view/chat-messages/components/user-message.tsx
+++ src/frontend/src/components/core/playgroundComponent/chat-view/chat-messages/components/user-message.tsx
@@ -107,7 +107,7 @@ export const UserMessage = memo(
           >
             {/* Avatar */}
             <div
-              className="relative hidden h-8 w-8 items-center justify-center overflow-hidden rounded border border-border text-2xl hover:border-input min-[720px]:flex"
+              className="relative hidden h-8 w-8 items-center justify-center overflow-hidden rounded border border-border text-2xl hover:border-input sm:flex"
               style={
                 chat.properties?.background_color
                   ? { backgroundColor: chat.properties.background_color }
