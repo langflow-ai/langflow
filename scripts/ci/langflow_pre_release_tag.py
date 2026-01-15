4ae79155fae804d3bd1bd0c97dcf70d324187450
Adam Aghili <Adam.Aghili@ibm.com>
2026-01-15 18:14:06 -0500

    chore: change script vars to more meaningful ones

--- scripts/ci/langflow_pre_release_tag.py
+++ scripts/ci/langflow_pre_release_tag.py
@@ -5,20 +5,20 @@ import sys
 ARGUMENT_NUMBER = 3
 
 
-def create_tag(package_version: str, latest_pypi_version: str | None) -> str:
+def create_tag(package_version: str, latest_released_version: str | None) -> str:
     new_pre_release_version = package_version + ".rc1"
-    if latest_pypi_version and package_version in latest_pypi_version and ".rc" in latest_pypi_version:
-        rc_number = int(latest_pypi_version.split(".rc")[-1])
+    if latest_released_version and package_version in latest_released_version and ".rc" in latest_released_version:
+        rc_number = int(latest_released_version.split(".rc")[-1])
         new_pre_release_version = package_version + ".rc" + str(rc_number + 1)
     return new_pre_release_version
 
 
 if __name__ == "__main__":
     if len(sys.argv) != ARGUMENT_NUMBER:
-        msg = "Specify base or main"
+        msg = "Specify package_version and latest_released_version"
         raise ValueError(msg)
 
     package_version: str = sys.argv[1]
-    latest_pypi_version: str = sys.argv[2]
-    tag = create_tag(package_version, latest_pypi_version)
+    latest_released_version: str = sys.argv[2]
+    tag = create_tag(package_version, latest_released_version)
     print(tag)
