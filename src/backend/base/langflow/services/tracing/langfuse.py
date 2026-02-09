55608c690a8fb50d2e1d26f85691a527bb15c4f3
ogabrielluiz <gabriel@langflow.org>
2026-02-09 15:29:55 -0300

    fix: update environment variable for Langfuse host to LANGFUSE_BASE_URL

--- src/backend/base/langflow/services/tracing/langfuse.py
+++ src/backend/base/langflow/services/tracing/langfuse.py
@@ -217,7 +217,9 @@ class LangFuseTracer(BaseTracer):
     def _get_config() -> dict:
         secret_key = os.getenv("LANGFUSE_SECRET_KEY", None)
         public_key = os.getenv("LANGFUSE_PUBLIC_KEY", None)
-        host = os.getenv("LANGFUSE_HOST", None)
+        host = os.getenv(
+            "LANGFUSE_BASE_URL", os.getenv("LANGFUSE_HOST", None)
+        )  # support legacy env var for backward compatibility
         if secret_key and public_key and host:
             return {"secret_key": secret_key, "public_key": public_key, "host": host}
         return {}
