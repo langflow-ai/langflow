7e0c89fce7aedcdd57f5f924943af9469c1aa543
Jordan Frazier <jordan.frazier@datastax.com>
2026-02-22 22:51:55 -0500

    Change confirmation to pop up

--- src/frontend/src/components/ui/dropdown-menu.tsx
+++ src/frontend/src/components/ui/dropdown-menu.tsx
@@ -57,9 +57,11 @@ DropdownMenuSubContent.displayName =
 
 const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-  <DropdownMenuPrimitive.Portal>
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content> & {
+    container?: HTMLElement | null;
+  }
+>(({ className, sideOffset = 4, container, ...props }, ref) => (
+  <DropdownMenuPrimitive.Portal container={container}>
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
