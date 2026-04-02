eb64496e2cd3b468248304f5db632bef9e123eb2
cristhianzl <cristhian.lousa@gmail.com>
2026-04-02 14:50:26 -0300

    fix files integration on windows

--- src/frontend/tests/core/unit/fileUploadComponent.spec.ts
+++ src/frontend/tests/core/unit/fileUploadComponent.spec.ts
@@ -4,7 +4,7 @@ import { expect, test } from "../../fixtures";
 import { addLegacyComponents } from "../../utils/add-legacy-components";
 import { adjustScreenView } from "../../utils/adjust-screen-view";
 import { awaitBootstrapTest } from "../../utils/await-bootstrap-test";
-import { ensureCheckboxChecked } from "../../utils/ensure-checkbox-checked";
+import { ensureFileSelected } from "../../utils/ensure-checkbox-checked";
 import { generateRandomFilename } from "../../utils/generate-filename";
 import {
   disableInspectPanel,
@@ -95,8 +95,7 @@ test(
         timeout: 5000,
       });
 
-      const checkbox = page.getByTestId(`checkbox-${sourceFileName}`).last();
-      await ensureCheckboxChecked(checkbox);
+      await ensureFileSelected(page);
 
       // Create DataTransfer object and file
       const dataTransfer = await page.evaluateHandle((jsonFileName) => {
