5a6e1d4b245289520798fa4ab5136043a39135f5
cristhianzl <cristhian.lousa@gmail.com>
2026-04-24 10:45:47 -0300

    fix UI on shareable playground

--- src/frontend/src/modals/IOModal/components/chatView/chatInput/components/input-wrapper.tsx
+++ src/frontend/src/modals/IOModal/components/chatView/chatInput/components/input-wrapper.tsx
@@ -90,7 +90,7 @@ const InputWrapper: React.FC<InputWrapperProps> = ({
     <div className="flex w-full flex-col-reverse">
       <div
         data-testid="input-wrapper"
-        className="flex w-full flex-col rounded-md border cursor-text border-input p-4 hover:border-muted-foreground focus:border-[1.75px] has-[:focus]:border-primary"
+        className="flex w-full flex-col rounded-md border cursor-text border-input bg-muted p-4 hover:border-muted-foreground focus:border-[1.75px] has-[:focus]:border-primary"
         onClick={onClick}
         onMouseDown={onMouseDown}
         onKeyDown={onKeyDown}
