4644645df6f8ca9aaa653d27e0f81dc94c4ef80c
dejaguarkyng <deinvinciblekyng.1@gmail.com>
2026-06-02 22:41:19 +0000

    fix(frontend): sort Jungle Grid icon imports

--- src/frontend/src/icons/JungleGrid/index.tsx
+++ src/frontend/src/icons/JungleGrid/index.tsx
@@ -1,6 +1,6 @@
-import { useDarkStore } from "@/stores/darkStore";
 import type React from "react";
 import { forwardRef } from "react";
+import { useDarkStore } from "@/stores/darkStore";
 import JungleGridIconSvg from "./JungleGridIcon";
 
 export const JungleGridIcon = forwardRef<
