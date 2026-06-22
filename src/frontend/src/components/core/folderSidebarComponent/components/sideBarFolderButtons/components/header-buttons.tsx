366aa299fd6fe1a4f66566df31b38ccc8f63d98b
Olayinka Adelakun <olayinkaadelakun@Olayinkas-MacBook-Pro.local>
2026-06-22 11:27:42 -0400

    label get-started close button and fix dark-mode placeholder contrast

--- src/frontend/src/components/core/folderSidebarComponent/components/sideBarFolderButtons/components/header-buttons.tsx
+++ src/frontend/src/components/core/folderSidebarComponent/components/sideBarFolderButtons/components/header-buttons.tsx
@@ -64,7 +64,7 @@ export const HeaderButtons = ({
 
   return (
     <>
-      {!hideGettingStartedProgress && !isDismissedDialog && userData && (
+      {true && (
         <>
           <CustomGetStartedProgress
             userData={userData!}
