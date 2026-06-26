61a64eb1d55a2bc819a7e76da032f7471f8786f4
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2026-06-26 14:43:23 -0400

    docs: add keycloak example

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -115,10 +115,16 @@ module.exports = {
       label: "Develop",
       className: "sidebar-category-with-icon sidebar-icon-code",
       items: [
-        "Develop/api-keys-and-authentication",
-        "Develop/jwt-authentication",
-        "Develop/external-authentication",
-        "Develop/authorization",
+        {
+          type: "category",
+          label: "Authentication and authorization",
+          items: [
+            "Develop/authentication-overview",
+            "Develop/api-keys-and-authentication",
+            "Develop/external-authentication",
+            "Develop/authorization",
+          ],
+        },
         "Develop/install-custom-dependencies",
         "Develop/configuration-global-variables",
         "Develop/environment-variables",
