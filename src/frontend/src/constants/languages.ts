5a8a2cba65e5f59fd386a18a018e408adb25a35f
Ben <hj.ben.kim@gmail.com>
2026-07-17 09:43:59 +0900

    fix(typo): typo fix

--- src/frontend/src/constants/languages.ts
+++ src/frontend/src/constants/languages.ts
@@ -5,6 +5,6 @@ export const SUPPORTED_LANGUAGES = [
   { code: "de", label: "Deutsch" },
   { code: "pt", label: "Português" },
   { code: "ja", label: "日本語" },
-  { code: "ko", lable: "한국어" },
+  { code: "ko", label: "한국어" },
   { code: "zh-Hans", label: "中文" },
 ] as const;
