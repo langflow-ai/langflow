0a58b76642bfc2b38ad933c8ba7577ebee624183
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2026-07-20 16:31:24 -0400

    docs: bump version to 1.11.0

---
+++ docs/versioned_docs/version-1.11.0/API-Reference/javascript-examples/api-build/build-flow-and-stream-events.js
@@ -0,0 +1,25 @@
+const url = `${process.env.LANGFLOW_URL ?? ""}/api/v1/build/${process.env.FLOW_ID ?? ""}/flow`;
+
+const options = {
+  method: 'POST',
+  headers: {
+    "accept": `application/json`,
+    "Content-Type": `application/json`,
+    "x-api-key": `${process.env.LANGFLOW_API_KEY ?? ""}`,
+  },
+  body: JSON.stringify({
+  "inputs": {
+    "input_value": "Tell me a story"
+  }
+}),
+};
+
+fetch(url, options)
+  .then(async (response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status}`);
+    }
+    const text = await response.text();
+    console.log(text);
+  })
+  .catch((error) => console.error(error));
