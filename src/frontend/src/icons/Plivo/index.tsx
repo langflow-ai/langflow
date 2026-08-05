5a489514c5a8d5ee3ee070a97e5169b364fdf3ca
Sarvesh Patil <sarvesh.patil@plivo.com>
2026-08-05 22:24:53 +0530

    Bump lfx-bundles version for the Plivo bundle and format the icon
    Signed-off-by: Sarvesh Patil <sarvesh.patil@plivo.com>

--- src/frontend/src/icons/Plivo/index.tsx
+++ src/frontend/src/icons/Plivo/index.tsx
@@ -2,9 +2,8 @@ import type React from "react";
 import { forwardRef } from "react";
 import Plivo from "./Plivo";
 
-export const PlivoIcon = forwardRef<
-  SVGSVGElement,
-  React.PropsWithChildren<{}>
->((props, ref) => {
-  return <Plivo ref={ref} {...props} />;
-});
+export const PlivoIcon = forwardRef<SVGSVGElement, React.PropsWithChildren<{}>>(
+  (props, ref) => {
+    return <Plivo ref={ref} {...props} />;
+  },
+);
