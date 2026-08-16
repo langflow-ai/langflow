1e088da95f212a9a29ef70af316d67660f4ac014
Awais Yousaf <awaisyousaf75@gmail.com>
2026-08-16 18:02:27 +0500

    fix: correct forwardRef generic syntax in Olostep icon

--- src/frontend/src/icons/Olostep/index.tsx
+++ src/frontend/src/icons/Olostep/index.tsx
@@ -1,8 +1,8 @@
-import type React from "react";
+﻿import type React from "react";
 import { forwardRef } from "react";
 import SvgOlostep from "./Olostep";
 
-export const OlostepIcon = forwardRef
+export const OlostepIcon = forwardRef<
   SVGSVGElement,
   React.PropsWithChildren<{}>
 >((props, ref) => {
