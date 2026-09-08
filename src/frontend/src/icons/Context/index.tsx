45bb114f1371923f2890cece136bb5ec48f0b465
aadithyan rajesh <nair.aadithyan999@gmail.com>
2026-09-08 17:24:59 +0530

    fix(frontend): isolate Context icon masks

--- src/frontend/src/icons/Context/index.tsx
+++ src/frontend/src/icons/Context/index.tsx
@@ -1,10 +1,12 @@
 import type React from "react";
-import { forwardRef } from "react";
+import { forwardRef, useId } from "react";
 import Context from "./ContextIcon";
 
 export const ContextIcon = forwardRef<
   SVGSVGElement,
   React.PropsWithChildren<{}>
 >((props, ref) => {
-  return <Context ref={ref} {...props} />;
+  const maskId = `context-cutout-${useId().replaceAll(":", "")}`;
+
+  return <Context ref={ref} maskId={maskId} {...props} />;
 });
